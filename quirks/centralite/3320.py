ddac75b8e407d8e75bf83995e1a0ab1cb63bbcba
David Mulcahey <david.mulcahey@icloud.com>
2018-11-22 19:44:30 -0500

    battery reporting

--- quirks/centralite/3320.py
+++ quirks/centralite/3320.py
@@ -1,6 +1,7 @@
 from zigpy.profiles import PROFILES, zha
 from zigpy.zcl.clusters.general import Basic, OnOff, Identify,\
     PollControl, Ota, LevelControl
+from zigpy.zcl.clusters.security import IasZone
 from quirks.centralite import PowerConfigurationCluster
 from zigpy.zcl.clusters.measurement import TemperatureMeasurement
 from quirks import EventableCluster
@@ -10,43 +11,42 @@ from zigpy.quirks import CustomDevice
 DIAGNOSTICS_CLUSTER_ID = 0x0B05  # decimal = 2821
 
 
-class CentraLite3130(CustomDevice):
-
-    class EventableOnOffCluster(EventableCluster, OnOff):
-        cluster_id = OnOff.cluster_id
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-    class EventableLevelControlCluster(EventableCluster, LevelControl):
-        cluster_id = LevelControl.cluster_id
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+class CentraLite3320(CustomDevice):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1
-        #  device_version=0 input_clusters=[0, 1, 3, 32, 1026, 2821]
-        #  output_clusters=[3, 6, 8, 25]>
+        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026 device_version=0 input_clusters=[0, 1, 3, 32, 1026, 1280, 2821] output_clusters=[25]>
+        #  <SimpleDescriptor endpoint=2 profile=49887 device_type=12 device_version=0 input_clusters=[0, 1, 3, 2821, 64527] output_clusters=[3]>
         1: {
             'profile_id': zha.PROFILE_ID,
-            'device_type': zha.DeviceType.LEVEL_CONTROL_SWITCH,
+            'device_type': zha.DeviceType.IAS_ZONE,
             'input_clusters': [
                 Basic.cluster_id,
                 PowerConfigurationCluster.cluster_id,
                 Identify.cluster_id,
                 PollControl.cluster_id,
                 TemperatureMeasurement.cluster_id,
+                IasZone.cluster_id,
                 DIAGNOSTICS_CLUSTER_ID
             ],
             'output_clusters': [
-                Identify.cluster_id,
-                EventableOnOffCluster.cluster_id,
-                EventableLevelControlCluster.cluster_id,
                 Ota.cluster_id
+            ],
+        },
+        2: {
+            'profile_id': 49887,
+            'device_type': 12,
+            'input_clusters': [
+                Basic.cluster_id,
+                PowerConfigurationCluster.cluster_id,
+                Identify.cluster_id,
+                DIAGNOSTICS_CLUSTER_ID,
+                64527
+            ],
+            'output_clusters': [
+                Identify.cluster_id
             ],
         },
     }
@@ -60,13 +60,19 @@ class CentraLite3130(CustomDevice):
                     Identify.cluster_id,
                     PollControl.cluster_id,
                     TemperatureMeasurement.cluster_id,
+                    IasZone.cluster_id,
                     DIAGNOSTICS_CLUSTER_ID
                 ],
                 'output_clusters': [
-                    Identify.cluster_id,
-                    EventableOnOffCluster,
-                    EventableLevelControlCluster,
                     Ota.cluster_id
+                ],
+            },
+            2: {
+                'input_clusters': [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    DIAGNOSTICS_CLUSTER_ID,
+                    64527
                 ],
             }
         },
---
+++ quirks/centralite/__init__.py
@@ -0,0 +1,53 @@
+
+import logging
+
+from zigpy.zcl.clusters.general import PowerConfiguration
+from zigpy.quirks import CustomCluster
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class PowerConfigurationCluster(CustomCluster, PowerConfiguration):
+    cluster_id = PowerConfiguration.cluster_id
+    BATTERY_VOLTAGE_ATTR = 0x0020
+    BATTERY_PERCENTAGE_REMAINING = 0x0021
+    MIN_VOLTS = 15
+    MAX_VOLTS = 28
+    VOLTS_TO_PERCENT = {
+        28: 100,
+        27: 100,
+        26: 100,
+        25: 90,
+        24: 90,
+        23: 70,
+        22: 70,
+        21: 50,
+        20: 50,
+        19: 30,
+        18: 30,
+        17: 15,
+        16: 1,
+        15: 0
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _update_attribute(self, attrid, value):
+        if attrid == self.BATTERY_VOLTAGE_ATTR:
+            super()._update_attribute(
+                self.BATTERY_PERCENTAGE_REMAINING,
+                self._calculate_battery_percentage(value)
+            )
+        else:
+            super()._update_attribute(attrid, value)
+
+    def _calculate_battery_percentage(self, rawValue):
+        volts = rawValue
+        if rawValue < self.MIN_VOLTS:
+            volts = self.MIN_VOLTS
+        elif rawValue > self.MAX_VOLTS:
+            volts = self.MAX_VOLTS
+
+        return self.VOLTS_TO_PERCENT.get(volts, 'unknown')
