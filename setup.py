630844a3238e2027b301c261ab2c082b5551d46b
David Mulcahey <david.mulcahey@icloud.com>
2018-12-01 16:43:40 -0500

    pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,26 @@
 
 from setuptools import find_packages, setup
 
+VERSION = "0.0.2"
+
+
+def readme():
+    """print long description"""
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name="zha-quirks",
-    version="0.0.1",
-    description="Library implementing Zigpy quirks for the ZHA component in Home Assistant",
-    url="http://github.com/dmulcahey/zha-device-handlers",
+    version=VERSION,
+    description="Library implementing Zigpy quirks for ZHA in Home Assistant",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/dmulcahey/zha-device-handlers",
     author="David F. Mulcahey",
     author_email="david.mulcahey@icloud.com",
     license="Apache License Version 2.0",
-    packages=find_packages(exclude=['*.tests'])
+    keywords='zha quirks homeassistant hass',
+    packages=find_packages(exclude=['*.tests']),
+    python_requires='>=3'
 )
