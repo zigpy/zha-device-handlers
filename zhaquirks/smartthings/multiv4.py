d1fdf19c42f16af4a82f2158775831839ccb09b9
roblandry <rob.a.landry@gmail.com>
2019-03-09 09:03:14 -0500

    Quirks for Smartthings MultiV4 (#21)
    * Quirks for SmartThings MultiV4
    
    * Correct device_type signature for SmartThings MultiV4
    
    * SmartThings MultiV4 accelerometer testing
    
    * SmartThings MultiV4 correct cluster_id and add manufacturer/model per endpoint
    
    * SmartThings MultiV4 correct cluster_id and begin using zhaquirks.
    
    * Update Readme.md for CentraLite 3300S, 3305S, 3325S, and SmartThings MotionV4.
    
    * Format bulleted list and add battery level vebiage to include all SmartThings devices.
    
    * Removed comment
    
    * Added SmartthingsAccelCluster
    
    * Address comments from @dmulcahey
    
    * Did not mean to touch readme
    
    * Try to revert readme changes again
    
    * Fix readme again
    
    * Fix readme again

---
+++ zhaquirks/smartthings/multiv4.py
@@ -0,0 +1,59 @@
+from zigpy.profiles import PROFILES, zha
+from zigpy.zcl.clusters.general import Basic, Identify,\
+    PollControl, Ota, BinaryInput
+from zigpy.zcl.clusters.security import IasZone
+from zhaquirks.centralite import PowerConfigurationCluster,\
+    CentraLiteAccelCluster
+from zigpy.zcl.clusters.measurement import TemperatureMeasurement
+from zigpy.quirks import CustomDevice
+
+
+class SmartThingsMultiV4(CustomDevice):
+
+    signature = {
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=1026
+        # device_version=0
+        # input_clusters=[0, 1, 3, 15, 32, 1026, 1280, 64514]
+        # output_clusters=[25]>
+        1: {
+            'manufacturer': 'SmartThings',
+            'model': 'multiv4',
+            'profile_id': zha.PROFILE_ID,
+            'device_type': zha.DeviceType.IAS_ZONE,
+            'input_clusters': [
+                Basic.cluster_id,
+                PowerConfigurationCluster.cluster_id,
+                Identify.cluster_id,
+                BinaryInput.cluster_id,
+                PollControl.cluster_id,
+                TemperatureMeasurement.cluster_id,
+                IasZone.cluster_id,
+                CentraLiteAccelCluster.cluster_id
+            ],
+            'output_clusters': [
+                Ota.cluster_id
+            ],
+        }
+    }
+
+    replacement = {
+        'endpoints': {
+            1: {
+                'manufacturer': 'SmartThings',
+                'model': 'multiv4',
+                'input_clusters': [
+                    Basic.cluster_id,
+                    PowerConfigurationCluster,
+                    Identify.cluster_id,
+                    BinaryInput.cluster_id,
+                    PollControl.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    IasZone.cluster_id,
+                    CentraLiteAccelCluster
+                ],
+                'output_clusters': [
+                    Ota.cluster_id
+                ],
+            }
+        },
+    }
