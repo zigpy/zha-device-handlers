c5df93440702fa094fb2b4bf7b6495a58f972ca1
roblandry <rob.a.landry@gmail.com>
2019-03-09 09:02:38 -0500

    Quirks for CentraLite 3310S (#38)
    * Initial implimentation of CentraLite 3310S, with battery reports
    
    * Doh

---
+++ zhaquirks/centralite/3310S.py
@@ -0,0 +1,69 @@
+from zigpy.profiles import PROFILES, zha
+from zigpy.zcl.clusters.general import Basic, Identify,\
+    PollControl, Ota
+from zhaquirks.centralite import PowerConfigurationCluster
+from zigpy.zcl.clusters.measurement import TemperatureMeasurement
+from zigpy.quirks import CustomDevice
+from zigpy.quirks.smartthings import SmartthingsRelativeHumidityCluster,\
+    SmartthingsTemperatureHumiditySensor
+from zigpy import quirks
+
+
+DIAGNOSTICS_CLUSTER_ID = 0x0B05  # decimal = 2821
+
+
+#  remove the zigpy version of this device handler
+if SmartthingsTemperatureHumiditySensor in quirks._DEVICE_REGISTRY:
+    quirks._DEVICE_REGISTRY.remove(SmartthingsTemperatureHumiditySensor)
+
+
+class CentraLite3310S(CustomDevice):
+
+    signature = {
+        #  <SimpleDescriptor endpoint=1 profile=260 device_type=770
+        #  device_version=0
+        #  input_clusters=[0, 1, 3, 32, 1026, 2821, 64581]
+        #  output_clusters=[3, 25]>
+
+        1: {
+            'manufacturer': 'CentraLite',
+            'model': '3310-S',
+            'profile_id': zha.PROFILE_ID,
+            'device_type': zha.DeviceType.TEMPERATURE_SENSOR,
+            'input_clusters': [
+                Basic.cluster_id,
+                PowerConfigurationCluster.cluster_id,
+                Identify.cluster_id,
+                PollControl.cluster_id,
+                TemperatureMeasurement.cluster_id,
+                DIAGNOSTICS_CLUSTER_ID,
+                SmartthingsRelativeHumidityCluster.cluster_id
+            ],
+            'output_clusters': [
+                Identify.cluster_id,
+                Ota.cluster_id
+            ],
+        },
+    }
+
+    replacement = {
+        'endpoints': {
+            1: {
+                'manufacturer': 'CentraLite',
+                'model': '3310-S',
+                'input_clusters': [
+                    Basic.cluster_id,
+                    PowerConfigurationCluster,
+                    Identify.cluster_id,
+                    PollControl.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    DIAGNOSTICS_CLUSTER_ID,
+                    SmartthingsRelativeHumidityCluster
+                ],
+                'output_clusters': [
+                    Identify.cluster_id,
+                    Ota.cluster_id
+                ],
+            }
+        },
+    }
