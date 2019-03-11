5a17ce9516d4e75a6042467afdcecbba9b80ea21
Socalix <48040807+Socalix@users.noreply.github.com>
2019-03-10 22:29:58 -0500

    Add manufacturer/model to centralite signatures

--- zhaquirks/centralite/3320.py
+++ zhaquirks/centralite/3320.py
@@ -20,6 +20,8 @@ class CentraLite3320(CustomDevice):
         #  input_clusters=[0, 1, 3, 32, 1026, 1280, 2821]
         #  output_clusters=[25]>
         1: {
+            'manufacturer': 'CentraLite',
+            'model': '3320-L',
             'profile_id': zha.PROFILE_ID,
             'device_type': zha.DeviceType.IAS_ZONE,
             'input_clusters': [
@@ -40,6 +42,8 @@ class CentraLite3320(CustomDevice):
         #  input_clusters=[0, 1, 3, 2821, 64527]
         #  output_clusters=[3]>
         2: {
+            'manufacturer': 'CentraLite',
+            'model': '3320-L',
             'profile_id': 49887,
             'device_type': 12,
             'input_clusters': [
