9c002d960b1f7056691ef3296e2f937688b4f1e3
David Mulcahey <david.mulcahey@me.com>
2019-04-13 18:49:32 -0400

    add hive home motion sensors

---
+++ zhaquirks/hivehome/__init__.py
@@ -0,0 +1,34 @@
+"""Hive Home."""
+import asyncio
+from zigpy.zcl.clusters.security import IasZone
+from zigpy.quirks import CustomCluster
+
+ON = 1
+OFF = 0
+ZONE_STATE = 0
+
+
+class MotionCluster(CustomCluster, IasZone):
+    """Motion cluster."""
+
+    cluster_id = IasZone.cluster_id
+
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+        self._timer_handle = None
+
+    def handle_cluster_request(self, tsn, command_id, args):
+        """Handle the cluster command."""
+        if command_id == 0:
+            if self._timer_handle:
+                self._timer_handle.cancel()
+            loop = asyncio.get_event_loop()
+            self._timer_handle = loop.call_later(30, self._turn_off)
+
+    def _turn_off(self):
+        self._timer_handle = None
+        self.listener_event('cluster_command', 999, 0, [
+            0, 0, 0, 0
+        ])
+        self._update_attribute(ZONE_STATE, OFF)
---
+++ zhaquirks/hivehome/mot003V6.py
@@ -0,0 +1,64 @@
+"""Device handler for hivehome.com MOT003 sensors."""
+import logging
+from zigpy.profiles import zha
+from zigpy.zcl.clusters.general import (
+    Basic, Identify, PollControl, Ota, PowerConfiguration
+)
+from zigpy.zcl.clusters.security import IasZone
+from zigpy.zcl.clusters.measurement import (
+    TemperatureMeasurement, IlluminanceMeasurement
+)
+from zigpy.quirks import CustomDevice
+from . import MotionCluster
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class MOT003(CustomDevice):
+    """hivehome.com MOT003."""
+
+    signature = {
+        #  <SimpleDescriptor endpoint=6 profile=260 device_type=1026
+        # device_version=6
+        # input_clusters=[0, 1, 3, 32, 1024, 1026, 1280]
+        # output_clusters=[25]>
+        6: {
+            'manufacturer': 'HiveHome.com',
+            'model': 'MOT003',
+            'profile_id': zha.PROFILE_ID,
+            'device_type': zha.DeviceType.IAS_ZONE,
+            'input_clusters': [
+                Basic.cluster_id,
+                PowerConfiguration.cluster_id,
+                Identify.cluster_id,
+                PollControl.cluster_id,
+                TemperatureMeasurement.cluster_id,
+                IlluminanceMeasurement.cluster_id,
+                IasZone.cluster_id,
+            ],
+            'output_clusters': [
+                Ota.cluster_id
+            ],
+        }
+    }
+
+    replacement = {
+        'endpoints': {
+            6: {
+                'manufacturer': 'SmartThings',
+                'model': 'motionv4',
+                'input_clusters': [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    PollControl.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    IlluminanceMeasurement.cluster_id,
+                    MotionCluster,
+                ],
+                'output_clusters': [
+                    Ota.cluster_id
+                ],
+            }
+        },
+    }
