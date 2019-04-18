053341a7723a3bd9adbaf6d9574b767d6d531d6e
David Mulcahey <david.mulcahey@me.com>
2019-04-18 09:09:38 -0400

    clean up imports

--- zhaquirks/xiaomi/mija/sensor_ht.py
+++ zhaquirks/xiaomi/mija/sensor_ht.py
@@ -5,8 +5,9 @@ from zigpy.profiles import zha
 from zigpy.zcl.clusters.general import (
     Groups, Identify, Ota, MultistateInput, Scenes, AnalogInput
 )
-from zigpy.zcl.clusters.measurement import TemperatureMeasurement,\
-    PressureMeasurement, RelativeHumidity
+from zigpy.zcl.clusters.measurement import (
+    TemperatureMeasurement, RelativeHumidity
+)
 from zigpy import quirks
 from zigpy.quirks.xiaomi import TemperatureHumiditySensor
 from zhaquirks.xiaomi import BasicCluster, PowerConfigurationCluster,\
