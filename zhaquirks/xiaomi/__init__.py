23317c6ca5c7f827a942fd526e1bdd2bd58d6cf7
David F. Mulcahey <david.mulcahey@me.com>
2019-04-30 08:03:41 -0400

    fix mija lifeline report (#61)

--- zhaquirks/xiaomi/__init__.py
+++ zhaquirks/xiaomi/__init__.py
@@ -6,16 +6,19 @@ from zigpy.zcl.clusters.measurement import TemperatureMeasurement
 from zigpy.quirks import CustomCluster, CustomDevice
 from zhaquirks import LocalDataCluster, Bus
 
-XIAOMI_ATTRIBUTE = 0xFF01
+XIAOMI_AQARA_ATTRIBUTE = 0xFF01
+XIAOMI_MIJA_ATTRIBUTE = 0xFF02
 BATTERY_REPORTED = 'battery_reported'
 TEMPERATURE_REPORTED = 'temperature_reported'
 TEMPERATURE_MEASURED_VALUE = 0x0000
 BATTERY_LEVEL = 'battery_level'
 TEMPERATURE = 'temperature'
 BATTERY_VOLTAGE_MV = 'battery_voltage_mV'
+XIAOMI_ATTR_3 = 'X-attrib-3'
 XIAOMI_ATTR_4 = 'X-attrib-4'
 XIAOMI_ATTR_5 = 'X-attrib-5'
 XIAOMI_ATTR_6 = 'X-attrib-6'
+STATE = 'state'
 PATH = 'path'
 BATTERY_PERCENTAGE_REMAINING = 0x0021
 
@@ -40,28 +43,34 @@ class BasicCluster(CustomCluster, Basic):
     cluster_id = Basic.cluster_id
 
     def _update_attribute(self, attrid, value):
-        super()._update_attribute(attrid, value)
-        if attrid == XIAOMI_ATTRIBUTE:
-            attributes = self._parse_attributes(value)
-            _LOGGER.debug(
-                "%s - Attribute report. attribute_id: [%s] value: [%s]",
-                self.endpoint.device._ieee,
-                attrid,
-                attributes
-            )
-            if BATTERY_LEVEL in attributes:
-                self.endpoint.device.batteryBus.listener_event(
-                    BATTERY_REPORTED,
-                    attributes[BATTERY_LEVEL],
-                    attributes[BATTERY_VOLTAGE_MV]
-                )
-            if TEMPERATURE in attributes:
-                self.endpoint.device.temperatureBus.listener_event(
-                    TEMPERATURE_REPORTED,
-                    attributes[TEMPERATURE]
-                )
+        if attrid != XIAOMI_MIJA_ATTRIBUTE:
+            super()._update_attribute(attrid, value)
 
-    def _parse_attributes(self, value):
+        attributes = {}
+        if attrid == XIAOMI_AQARA_ATTRIBUTE:
+            attributes = self._parse_aqara_attributes(value)
+        elif attrid == XIAOMI_MIJA_ATTRIBUTE:
+            attributes = self._parse_mija_attributes(value)
+
+        _LOGGER.debug(
+            "%s - Attribute report. attribute_id: [%s] value: [%s]",
+            self.endpoint.device._ieee,
+            attrid,
+            attributes
+        )
+        if BATTERY_LEVEL in attributes:
+            self.endpoint.device.batteryBus.listener_event(
+                BATTERY_REPORTED,
+                attributes[BATTERY_LEVEL],
+                attributes[BATTERY_VOLTAGE_MV]
+            )
+        if TEMPERATURE in attributes:
+            self.endpoint.device.temperatureBus.listener_event(
+                TEMPERATURE_REPORTED,
+                attributes[TEMPERATURE]
+            )
+
+    def _parse_aqara_attributes(self, value):
         """Parse non standard atrributes."""
         from zigpy.zcl import foundation as f
         attributes = {}
@@ -88,6 +97,32 @@ class BasicCluster(CustomCluster, Basic):
                     attributes[BATTERY_VOLTAGE_MV]
                 )
             )
+        return attributes
+
+    def _parse_mija_attributes(self, value):
+        """Parse non standard atrributes."""
+        attributes = {}
+        attribute_names = (
+            STATE,
+            BATTERY_VOLTAGE_MV,
+            XIAOMI_ATTR_3,
+            XIAOMI_ATTR_4,
+            XIAOMI_ATTR_5,
+            XIAOMI_ATTR_6,
+        )
+        result = []
+
+        for attr_value in value:
+            result.append(attr_value.value)
+        attributes = dict(zip(attribute_names, result))
+
+        if BATTERY_VOLTAGE_MV in attributes:
+            attributes[BATTERY_LEVEL] = int(
+                self._calculate_remaining_battery_percentage(
+                    attributes[BATTERY_VOLTAGE_MV]
+                )
+            )
+
         return attributes
 
     def _calculate_remaining_battery_percentage(self, voltage):
