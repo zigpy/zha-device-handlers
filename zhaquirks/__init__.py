54894850ef2c40ffe404a01515a9bc83d07ef492
David Mulcahey <david.mulcahey@me.com>
2019-05-03 08:16:11 -0400

    lint

--- zhaquirks/__init__.py
+++ zhaquirks/__init__.py
@@ -1,3 +1,4 @@
+"""Quirks implementations for the ZHA component of Homeassistant."""
 import importlib
 import pkgutil
 from zigpy.quirks import CustomCluster
@@ -8,18 +9,23 @@ UNKNOWN = 'Unknown'
 
 
 class Bus(ListenableMixin):
+    """Event bus implementation."""
 
     def __init__(self, *args, **kwargs):
+        """Init event bus."""
         super().__init__(*args, **kwargs)
         self._listeners = {}
 
 
 class LocalDataCluster(CustomCluster):
+    """Cluster meant to prevent remote calls."""
 
     def __init__(self, *args, **kwargs):
+        """Init method."""
         super().__init__(*args, **kwargs)
 
     async def read_attributes_raw(self, attributes, manufacturer=None):
+        """Prevent remote reads."""
         attributes = [types.uint16_t(a) for a in attributes]
         v = [self._attr_cache.get(attr) for attr in attributes]
         return v
@@ -29,11 +35,14 @@ class LocalDataCluster(CustomCluster):
 
 
 class EventableCluster(CustomCluster):
+    """Cluster that generates events."""
 
     def __init__(self, *args, **kwargs):
+        """Init method."""
         super().__init__(*args, **kwargs)
 
     def handle_cluster_request(self, tsn, command_id, args):
+        """Send cluster requests as events."""
         super().handle_cluster_request(tsn, command_id, args)
         if self.server_commands is not None and\
                 self.server_commands.get(command_id) is not None:
@@ -56,6 +65,7 @@ class EventableCluster(CustomCluster):
                 'value': value
             }
         )
+
 
 name = __name__
 path = __path__
