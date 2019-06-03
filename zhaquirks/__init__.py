12dceb19aefa85ba763d5fda4548145effc66109
Philip Wasserman <bballwiz5@users.noreply.github.com>
2019-06-03 19:47:28 -0400

    Add support for Lutron Connected Bulb Remote
    Remote only supports binding to groups, not devices.
    Add a global group id which is subscribed to by
    the coordinator and used for receiving broadcasts
    from the device.

--- zhaquirks/__init__.py
+++ zhaquirks/__init__.py
@@ -5,6 +5,7 @@ import pkgutil
 from zigpy.quirks import CustomCluster
 import zigpy.types as types
 from zigpy.util import ListenableMixin
+from zigpy.zdo import types as zdotypes
 
 UNKNOWN = 'Unknown'
 
@@ -55,6 +56,35 @@ class EventableCluster(CustomCluster):
                 'value': value
             }
         )
+
+
+class GroupBoundCluster(CustomCluster):
+    """
+    Cluster that can only bind to a group instead of direct to hub.
+
+    Binding this cluster results in binding to a group that the coordinator
+    is a member of.
+    """
+
+    COORDINATOR_GROUP_ID = 0x30  # Group id with only coordinator as a member
+
+    async def bind(self):
+        """Bind cluster to a group."""
+        # Ensure coordinator is a member of the group
+        application = self._endpoint.device.application
+        coordinator = application.get_device(application.ieee)
+        await coordinator.add_to_group(self.COORDINATOR_GROUP_ID)
+
+        # Bind cluster to group
+        dstaddr = zdotypes.MultiAddress()
+        dstaddr.addrmode = 1
+        dstaddr.nwk = self.COORDINATOR_GROUP_ID
+        dstaddr.endpoint = self._endpoint.endpoint_id
+        return await self._endpoint.device.zdo.Bind_req(
+            self._endpoint.device.ieee,
+            self._endpoint.endpoint_id,
+            self.cluster_id,
+            dstaddr)
 
 
 NAME = __name__
