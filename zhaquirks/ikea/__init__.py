506da146cda114b2e9e790f16505c1548e9e9ee4
David Mulcahey <david.mulcahey@me.com>
2019-06-17 09:44:23 -0400

    guard against lack of coordinator device

--- zhaquirks/ikea/__init__.py
+++ zhaquirks/ikea/__init__.py
@@ -1,6 +1,9 @@
 """Ikea module."""
+import logging
 from zigpy.zcl.clusters.lightlink import LightLink
 from zigpy.quirks import CustomCluster
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class LightLinkCluster(CustomCluster, LightLink):
@@ -9,7 +12,12 @@ class LightLinkCluster(CustomCluster, LightLink):
     async def bind(self):
         """Bind LightLink cluster to coordinator."""
         application = self._endpoint.device.application
-        coordinator = application.get_device(application.ieee)
+        try:
+            coordinator = application.get_device(application.ieee)
+        except KeyError:
+            _LOGGER.warning(
+                "Aborting - unable to locate required coordinator device."
+            )
         group_list = await self.get_group_identifiers(0)
         group_record = group_list[2]
         group_id = group_record[0].group_id
