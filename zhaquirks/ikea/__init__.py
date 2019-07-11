858dadf462297926ac44a7e973125dd981ef5125
David Mulcahey <david.mulcahey@me.com>
2019-07-11 07:42:33 -0400

    return status from bind method

--- zhaquirks/ikea/__init__.py
+++ zhaquirks/ikea/__init__.py
@@ -22,4 +22,5 @@ class LightLinkCluster(CustomCluster, LightLink):
         group_list = await self.get_group_identifiers(0)
         group_record = group_list[2]
         group_id = group_record[0].group_id
-        await coordinator.add_to_group(group_id)
+        status = await coordinator.add_to_group(group_id)
+        return [status]
