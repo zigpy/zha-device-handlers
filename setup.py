9109d90f2f5f45a990563af71b3f6b054dd6e649
Alexei Chetroi <alexei.chetroi@outlook.com>
2019-09-07 21:17:35 -0400

    Tell pylint we need a newer zigpy version.

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
     keywords="zha quirks homeassistant hass",
     packages=find_packages(exclude=["*.tests"]),
     python_requires=">=3",
-    install_requires=["zigpy-homeassistant>=0.6.1"],
+    install_requires=["zigpy-homeassistant>=0.8.0"],
     tests_require=["pytest"],
 )
