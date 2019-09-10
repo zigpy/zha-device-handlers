6f783a4066654a2e657fb8775d51187ec48d1aea
David F. Mulcahey <dmulcahey@amica.com>
2019-09-10 10:09:07 -0400

    add custom osram cluster to the 4 button remote (#149)

--- zhaquirks/osram/lightifyx4.py
+++ zhaquirks/osram/lightifyx4.py
@@ -2,15 +2,17 @@
 import logging
 
 from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
+from zigpy.quirks import CustomDevice, CustomCluster
 from zigpy.zcl.clusters.general import (
     Basic, Groups, Identify, LevelControl, OnOff, Ota, PollControl,
     PowerConfiguration, Scenes)
 from zigpy.zcl.clusters.lighting import Color
 from zigpy.zcl.clusters.lightlink import LightLink
+import zigpy.types as t
 
 OSRAM_DEVICE = 0x0810  # 2064 base 10
 OSRAM_CLUSTER = 0xFD00  # 64768 base 10
+OSRAM_MFG_CODE = 0x110c
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -18,6 +20,56 @@ _LOGGER = logging.getLogger(__name__)
 
 class LightifyX4(CustomDevice):
     """Osram Lightify X4 device."""
+
+    class OsramButtonCluster(CustomCluster):
+        """OsramButtonCluster."""
+
+        cluster_id = OSRAM_CLUSTER
+        name = 'OsramCluster'
+        ep_attribute = 'osram_cluster'
+        attributes = {
+            0x000A: ('osram_1', t.uint8_t),
+            0x000B: ('osram_2', t.uint8_t),
+            0x000C: ('osram_3', t.uint16_t),
+            0x000D: ('osram_4', t.uint16_t),
+            0x0019: ('osram_5', t.uint8_t),
+            0x001A: ('osram_6', t.uint16_t),
+            0x001B: ('osram_7', t.uint16_t),
+            0x001C: ('osram_8', t.uint8_t),
+            0x001D: ('osram_9', t.uint16_t),
+            0x001E: ('osram_10', t.uint16_t),
+            0x002C: ('osram_11', t.uint16_t),
+            0x002D: ('osram_12', t.uint16_t),
+            0x002E: ('osram_13', t.uint16_t),
+            0x002F: ('osram_14', t.uint16_t),
+        }
+        server_commands = {}
+        client_commands = {}
+        attr_config = {
+            0x000A: 0x01,
+            0x000B: 0x00,
+            0x000C: 0xFFFF,
+            0x000D: 0xFFFF,
+            0x0019: 0x06,
+            0x001A: 0x0001,
+            0x001B: 0x0026,
+            0x001C: 0x07,
+            0x001D: 0xFFFF,
+            0x001E: 0xFFFF,
+            0x002C: 0xFFFF,
+            0x002D: 0xFFFF,
+            0x002E: 0xFFFF,
+            0x002F: 0xFFFF
+        }
+
+        async def bind(self):
+            """Bind cluster."""
+            result = await super().bind()
+            await self.write_attributes(
+                self.attr_config,
+                manufacturer=OSRAM_MFG_CODE
+            )
+            return result
 
     signature = {
         #  <SimpleDescriptor endpoint=1 profile=260 device_type=2064
@@ -172,7 +224,7 @@ class LightifyX4(CustomDevice):
                     PowerConfiguration.cluster_id,
                     PollControl.cluster_id,
                     LightLink.cluster_id,
-                    OSRAM_CLUSTER,
+                    OsramButtonCluster,
                 ],
                 'output_clusters': [
                     Groups.cluster_id,
@@ -191,7 +243,7 @@ class LightifyX4(CustomDevice):
                 'input_clusters': [
                     Basic.cluster_id,
                     LightLink.cluster_id,
-                    OSRAM_CLUSTER
+                    OsramButtonCluster
                 ],
                 'output_clusters': [
                     Groups.cluster_id,
@@ -209,7 +261,7 @@ class LightifyX4(CustomDevice):
                 'input_clusters': [
                     Basic.cluster_id,
                     LightLink.cluster_id,
-                    OSRAM_CLUSTER
+                    OsramButtonCluster
                 ],
                 'output_clusters': [
                     Groups.cluster_id,
@@ -227,7 +279,7 @@ class LightifyX4(CustomDevice):
                 'input_clusters': [
                     Basic.cluster_id,
                     LightLink.cluster_id,
-                    OSRAM_CLUSTER
+                    OsramButtonCluster
                 ],
                 'output_clusters': [
                     Groups.cluster_id,
