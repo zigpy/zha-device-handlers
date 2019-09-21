877dff30a03116dbaf9ea2cedc3e876b7062b89d
David Mulcahey <david.mulcahey@me.com>
2019-09-21 09:42:21 -0400

    black test

--- tests/test_xiaomi.py
+++ tests/test_xiaomi.py
@@ -8,22 +8,22 @@ def test_basic_cluster_deserialize_wrong_len():
     """Test attr report with model and xiaomi attr."""
     cluster = BasicCluster(mock.MagicMock())
 
-    data = b'\x1c_\x11\x12\n'
+    data = b"\x1c_\x11\x12\n"
     data += b'\x05\x00B\x15lumi.sensor_wleak.aq1\x01\xffB"\x01!\xb3\x0b\x03('
-    data += b'\x17\x04!\xa8C\x05!\xa7\x00\x06$\x00\x00\x00\x00\x00\x08!\x04'
-    data += b'\x02\n!\x00\x00d\x10\x01'
+    data += b"\x17\x04!\xa8C\x05!\xa7\x00\x06$\x00\x00\x00\x00\x00\x08!\x04"
+    data += b"\x02\n!\x00\x00d\x10\x01"
 
-    d = cluster.deserialize(data)
-    assert d[3]
+    deserialized = cluster.deserialize(data)
+    assert deserialized[3]
 
 
 def test_basic_cluster_deserialize_wrong_len_2():
     """Test attr report with xiaomi attr."""
     cluster = BasicCluster(mock.MagicMock())
 
-    data = b'\x1c_\x11\x12\n'
+    data = b"\x1c_\x11\x12\n"
     data += b'\x01\xffB"\x01!\xb3\x0b\x03(\x17\x04!\xa8C\x05!\xa7\x00\x06$\x15'
-    data += b'\x00\x14\x00\x00\x08!\x04\x02\n!\x00\x00d\x10\x01'
+    data += b"\x00\x14\x00\x00\x08!\x04\x02\n!\x00\x00d\x10\x01"
 
-    d = cluster.deserialize(data)
-    assert d[3]
+    deserialized = cluster.deserialize(data)
+    assert deserialized[3]
