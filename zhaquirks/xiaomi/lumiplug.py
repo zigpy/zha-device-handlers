9ca37c7240847cd800a94145dfe3155d8ffba64f
Rob Landry <rob.a.landry@gmail.com>
2019-12-23 08:52:18 -0500

    Black

--- zhaquirks/xiaomi/lumiplug.py
+++ zhaquirks/xiaomi/lumiplug.py
@@ -2,18 +2,25 @@
 from zigpy.profiles import zha
 import homeassistant.components.zha.const as zha_const
 from zigpy.zcl.clusters.general import (
-    Basic, PowerConfiguration, DeviceTemperature, Identify, Groups, Scenes,
-    OnOff, Time, AnalogInput, BinaryOutput, Ota
+    Basic,
+    PowerConfiguration,
+    DeviceTemperature,
+    Identify,
+    Groups,
+    Scenes,
+    OnOff,
+    Time,
+    AnalogInput,
+    BinaryOutput,
+    Ota,
 )
 from zigpy.quirks import CustomDevice
 from zhaquirks import CustomCluster
 
 STATUS_TYPE_ATTR = 0x0055  # decimal = 85
-CONST_POWER = 'power'
+CONST_POWER = "power"
 
-zha_const.SINGLE_INPUT_CLUSTER_DEVICE_CLASS.update({
-    AnalogInput: 'sensor'
-})
+zha_const.SINGLE_INPUT_CLUSTER_DEVICE_CLASS.update({AnalogInput: "sensor"})
 
 
 class LUMIlumiplug(CustomDevice):
@@ -39,18 +46,16 @@ class LUMIlumiplug(CustomDevice):
                 if value:
                     self._currentState[STATUS_TYPE_ATTR] = CONST_POWER
                 # show something in the sensor in HA
-                super()._update_attribute(
-                    0,
-                    '{}'.format(round(float(value), 2))
-                )
-                if STATUS_TYPE_ATTR in self._currentState and self._currentState[STATUS_TYPE_ATTR] is not None:
+                super()._update_attribute(0, "{}".format(round(float(value), 2)))
+                if (
+                    STATUS_TYPE_ATTR in self._currentState
+                    and self._currentState[STATUS_TYPE_ATTR] is not None
+                ):
                     self.listener_event(
-                        'zha_send_event',
+                        "zha_send_event",
                         self,
                         self._currentState[STATUS_TYPE_ATTR],
-                        {
-                            'power': value
-                        }
+                        {"power": value},
                     )
 
     signature = {
@@ -59,9 +64,9 @@ class LUMIlumiplug(CustomDevice):
         #  input_clusters=[0, 1, 2, 3, 4, 5, 6, 10, 16]
         #  output_clusters=[10, 25]>
         1: {
-            'profile_id': zha.PROFILE_ID,
-            'device_type': zha.DeviceType.SMART_PLUG,
-            'input_clusters': [
+            "profile_id": zha.PROFILE_ID,
+            "device_type": zha.DeviceType.SMART_PLUG,
+            "input_clusters": [
                 Basic.cluster_id,
                 PowerConfiguration.cluster_id,
                 DeviceTemperature.cluster_id,
@@ -72,50 +77,38 @@ class LUMIlumiplug(CustomDevice):
                 Time.cluster_id,
                 BinaryOutput.cluster_id,
             ],
-            'output_clusters': [
-                Time.cluster_id,
-                Ota.cluster_id,
-            ],
+            "output_clusters": [Time.cluster_id, Ota.cluster_id,],
         },
         #  <SimpleDescriptor endpoint=2 profile=260 device_type=9
         #  device_version=1
         #  input_clusters=[12]
         #  output_clusters=[4, 12]>
         2: {
-            'profile_id': zha.PROFILE_ID,
-            'device_type': zha.DeviceType.MAIN_POWER_OUTLET,
-            'input_clusters': [
-                AnalogInput.cluster_id,
-            ],
-            'output_clusters': [
-                Groups.cluster_id,
-                AnalogInput.cluster_id,
-            ],
+            "profile_id": zha.PROFILE_ID,
+            "device_type": zha.DeviceType.MAIN_POWER_OUTLET,
+            "input_clusters": [AnalogInput.cluster_id,],
+            "output_clusters": [Groups.cluster_id, AnalogInput.cluster_id,],
         },
         #  <SimpleDescriptor endpoint=3 profile=260 device_type=83
         #  device_version=1
         #  input_clusters=[12]
         #  output_clusters=[12]>
         3: {
-            'profile_id': zha.PROFILE_ID,
-            'device_type': zha.DeviceType.METER_INTERFACE,
-            'input_clusters': [
-                AnalogInput.cluster_id,
-            ],
-            'output_clusters': [
-                AnalogInput.cluster_id,
-            ],
+            "profile_id": zha.PROFILE_ID,
+            "device_type": zha.DeviceType.METER_INTERFACE,
+            "input_clusters": [AnalogInput.cluster_id,],
+            "output_clusters": [AnalogInput.cluster_id,],
         },
     }
 
     replacement = {
-        'endpoints': {
+        "endpoints": {
             1: {
-                'manufacturer': 'LUMI',
-                'model': 'lumi.plug',
-                'profile_id': zha.PROFILE_ID,
-                'device_type': zha.DeviceType.SMART_PLUG,
-                'input_clusters': [
+                "manufacturer": "LUMI",
+                "model": "lumi.plug",
+                "profile_id": zha.PROFILE_ID,
+                "device_type": zha.DeviceType.SMART_PLUG,
+                "input_clusters": [
                     Basic.cluster_id,
                     PowerConfiguration.cluster_id,
                     DeviceTemperature.cluster_id,
@@ -126,35 +119,23 @@ class LUMIlumiplug(CustomDevice):
                     Time.cluster_id,
                     BinaryOutput.cluster_id,
                 ],
-                'output_clusters': [
-                    Time.cluster_id,
-                    Ota.cluster_id,
-                ],
+                "output_clusters": [Time.cluster_id, Ota.cluster_id,],
             },
             2: {
-                'manufacturer': 'LUMI',
-                'model': 'lumi.plug',
-                'profile_id': zha.PROFILE_ID,
-                'device_type': zha.DeviceType.MAIN_POWER_OUTLET,
-                'input_clusters': [
-                    AnalogInputCluster,
-                ],
-                'output_clusters': [
-                    Groups.cluster_id,
-                    AnalogInput.cluster_id,
-                ],
+                "manufacturer": "LUMI",
+                "model": "lumi.plug",
+                "profile_id": zha.PROFILE_ID,
+                "device_type": zha.DeviceType.MAIN_POWER_OUTLET,
+                "input_clusters": [AnalogInputCluster,],
+                "output_clusters": [Groups.cluster_id, AnalogInput.cluster_id,],
             },
             3: {
-                'manufacturer': 'LUMI',
-                'model': 'lumi.plug',
-                'profile_id': zha.PROFILE_ID,
-                'device_type': zha.DeviceType.METER_INTERFACE,
-                'input_clusters': [
-                    AnalogInputCluster,
-                ],
-                'output_clusters': [
-                    AnalogInput.cluster_id,
-                ],
+                "manufacturer": "LUMI",
+                "model": "lumi.plug",
+                "profile_id": zha.PROFILE_ID,
+                "device_type": zha.DeviceType.METER_INTERFACE,
+                "input_clusters": [AnalogInputCluster,],
+                "output_clusters": [AnalogInput.cluster_id,],
             },
         }
     }
