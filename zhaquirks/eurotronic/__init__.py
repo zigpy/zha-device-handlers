269295623f1f5d2487cd1bbac73388cd2591a6c6
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-02-01 19:08:05 +0100

    Add SPZB0001 quirk (initial)

---
+++ zhaquirks/eurotronic/__init__.py
@@ -0,0 +1,3 @@
+"""Eurotronic devices."""
+
+EUROTRONIC = "Eurotronic"
---
+++ zhaquirks/eurotronic/spzb0001.py
@@ -0,0 +1,99 @@
+
+from zigpy.profiles import PROFILES, zha
+from zigpy.quirks import CustomCluster, CustomDevice
+import zigpy.types as types
+from zigpy.zcl import foundation
+from zigpy.zcl.clusters.general import (
+    Basic,
+    Groups,
+    Identify,
+    Ota,
+    PowerConfiguration,
+    Time,
+)
+from zigpy.zcl.clusters.hvac import Thermostat
+
+from zhaquirks.const import (
+    ARGS,
+    CLUSTER_ID,
+    COMMAND,
+    COMMAND_OFF,
+    COMMAND_ON,
+    COMMAND_STEP,
+    DEVICE_TYPE,
+    DIM_DOWN,
+    DIM_UP,
+    ENDPOINT_ID,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+    SHORT_PRESS,
+    TURN_OFF,
+    TURN_ON,
+)
+
+from . import EUROTRONIC
+
+THERMOSTAT_CHANNEL = "thermostat"
+
+
+class SPZB0001(CustomDevice):
+
+    signature = {
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=769
+        # device_version=1
+        # input_clusters=[0, 1, 3, 513, 25, 10]
+        # output_clusters=[0, 1, 3, 4, 513, 25, 10]>
+        MODELS_INFO: [(EUROTRONIC, "SPZB0001")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.THERMOSTAT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Thermostat.cluster_id,
+                    Ota.cluster_id,
+                    Time.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Thermostat.cluster_id,
+                    Ota.cluster_id,
+                    Time.cluster_id,
+                ],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.THERMOSTAT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Thermostat.cluster_id,
+                    Ota.cluster_id,
+                    Time.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Thermostat.cluster_id,
+                    Ota.cluster_id,
+                    Time.cluster_id,
+                ],
+            }
+        }
+    }
