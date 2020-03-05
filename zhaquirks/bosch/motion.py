5bf38f6084137f2c4667ed31210262bf5f8dd841
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-03-04 21:05:30 -0500

    Black and Isort.

--- zhaquirks/bosch/motion.py
+++ zhaquirks/bosch/motion.py
@@ -2,11 +2,13 @@
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
 from zigpy.zcl.clusters.general import Basic, Identify, Ota, PollControl
+from zigpy.zcl.clusters.homeautomation import Diagnostic
 from zigpy.zcl.clusters.measurement import TemperatureMeasurement
 from zigpy.zcl.clusters.security import IasZone
-from zigpy.zcl.clusters.homeautomation import Diagnostic
 
 from zhaquirks import PowerConfigurationCluster
+
+from . import BOSCH
 from ..const import (
     DEVICE_TYPE,
     ENDPOINTS,
@@ -16,11 +18,10 @@ from ..const import (
     PROFILE_ID,
 )
 
-from . import BOSCH
-
 
 class BoschPowerConfiguration(PowerConfigurationCluster):
     """Bosch power configuration cluster for motion sensor."""
+
     MAX_VOLTS = 3.0
     MIN_VOLTS = 1.9
 
@@ -33,9 +34,7 @@ class ISWZPR1WP13(CustomDevice):
         #  device_version=0
         #  input_clusters=[0, 1, 3, 1026, 1280, 32, 2821]
         #  output_clusters=[25]>
-        MODELS_INFO: [
-            (BOSCH, "ISW-ZPR1-WP13"),
-        ],
+        MODELS_INFO: [(BOSCH, "ISW-ZPR1-WP13")],
         ENDPOINTS: {
             5: {
                 PROFILE_ID: zha.PROFILE_ID,
@@ -50,7 +49,7 @@ class ISWZPR1WP13(CustomDevice):
                     Diagnostic.cluster_id,
                 ],
                 OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
+            }
         },
     }
 
@@ -67,6 +66,6 @@ class ISWZPR1WP13(CustomDevice):
                     Diagnostic.cluster_id,
                 ],
                 OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
+            }
         }
     }
