086f69927b1685a2e56cbb833299b5f9d2bae962
bs42 <bs_42@outlook.com>
2020-03-16 15:57:56 -0700

    Add support for te SAGE Doorbell (AKA Echostar Bell)

---
+++ zhaquirks/echostar/bell.py
@@ -0,0 +1,87 @@
+"""Echostar Bell Device"""
+"""Sage Doorbell Sensor Device"""
+
+from zigpy.profiles import zha
+from zigpy.quirks import CustomDevice
+from zigpy.zcl.clusters.general import (
+    Basic,
+    Identify,
+    Alarms,
+    OnOff,
+    LevelControl,
+    Ota,
+    PowerConfiguration,
+)
+
+from ..const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+    SHORT_PRESS,
+    TURN_ON,
+    CLUSTER_ID,
+    ENDPOINT_ID,
+    COMMAND,
+    COMMAND_ON,
+    COMMAND_OFF,
+)
+
+MANUFACTURER = " Echostar"
+MODEL = "  Bell"
+
+
+class Bell(CustomDevice):
+    """Echostar Bell device"""
+
+    signature = {
+        # <SimpleDescriptor endpoint=18 profile=260 device_type=260
+        # device_version=0
+        # input_clusters=[0, 3, 9, 1]
+        # output_clusters=[3, 6, 8, 25]>
+        # MODELS_INFO: [(MANUFACTURER, MODEL)],
+        ENDPOINTS: {
+            18: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.DIMMER_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Alarms.cluster_id,
+                    PowerConfiguration.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,
+                    OnOff.cluster_id,
+                    LevelControl.cluster_id,
+                    Ota.cluster_id,
+                ],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            18: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Alarms.cluster_id,
+                    PowerConfiguration.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,
+                    OnOff.cluster_id,
+                    LevelControl.cluster_id,
+                    Ota.cluster_id,
+                ],
+            }
+        },
+    }
+    device_automation_triggers = {
+        (SHORT_PRESS, TURN_ON): {COMMAND: COMMAND_ON, CLUSTER_ID: 6, ENDPOINT_ID: 18,},
+    }
