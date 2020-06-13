f84a2e23cdbc9ba58a3f989fe513e7e6b9b6224c
Alexei Chetroi <lexoid@gmail.com>
2020-06-13 15:47:36 -0400

    Fix PowerConfig cluster reporting 0V (#376)

--- zhaquirks/__init__.py
+++ zhaquirks/__init__.py
@@ -154,15 +154,13 @@ class PowerConfigurationCluster(CustomCluster, PowerConfiguration):
 
     def _update_attribute(self, attrid, value):
         super()._update_attribute(attrid, value)
-        if attrid == self.BATTERY_VOLTAGE_ATTR:
+        if attrid == self.BATTERY_VOLTAGE_ATTR and value not in (0, 255):
             super()._update_attribute(
                 self.BATTERY_PERCENTAGE_REMAINING,
                 self._calculate_battery_percentage(value),
             )
 
     def _calculate_battery_percentage(self, raw_value):
-        if raw_value in (0, 255):
-            return -1
         volts = raw_value / 10
         volts = max(volts, self.MIN_VOLTS)
         volts = min(volts, self.MAX_VOLTS)
