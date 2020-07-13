89a8cc14e63f028cd6d1f100008368b1e6467cf8
abmantis <amfcalt@gmail.com>
2020-07-14 00:51:40 +0100

    Add EDP WithUs SmartPlug quirk.
    This device wasn't providing any values for divisor and multiplier
    attributes, leading to incorrect values.

---
+++ zhaquirks/edpwithus/__init__.py
@@ -0,0 +1,16 @@
+"""EDP WithUs module."""
+import logging
+
+import zigpy.types as t
+from zigpy.quirks import CustomCluster
+from zigpy.zcl.clusters.smartenergy import Metering
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class MeteringCluster(CustomCluster, Metering):
+    """EDP WithUs Metering cluster."""
+
+    MULTIPLIER = 0x0301
+    DIVISOR = 0x0302
+    _CONSTANT_ATTRIBUTES = {MULTIPLIER: 1, DIVISOR: 1000}
