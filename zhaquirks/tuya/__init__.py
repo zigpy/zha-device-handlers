d284616810cb8f0cf86590380e48db0e8f2a8f26
Piotr Mis <misieq@compower.pl>
2020-09-06 11:10:41 +0000

    feat(tuya): First sketch of quirk for single channel Tuya-based switch

---
+++ zhaquirks/tuya/__init__.py
@@ -0,0 +1,86 @@
+"""Module for Tuya based devices."""
+
+import logging
+
+import zigpy.types as t
+from zigpy.quirks import CustomCluster
+from zigpy.zcl import foundation
+from zigpy.zcl.clusters.general import OnOff
+
+TUYA = "Tuya"
+MANUFACTURER_SPECIFIC_CLUSTER_ID = 0xEF00  # Decimal: 61184
+_LOGGER = logging.getLogger(__name__)
+
+class TuyaSwitchCluster(CustomCluster,OnOff):
+    """Tuya Switch Cluster."""
+
+    cluster_id = MANUFACTURER_SPECIFIC_CLUSTER_ID
+    ep_attribute = "on_off"
+    name = "Tuya Switch"
+
+    manufacturer_server_commands = {
+        0x0000: (
+            "write_state",
+            (
+                foundation.Status,
+                t.uint8_t,   # transactionId
+                t.uint16_t,  # attribute
+                t.uint8_t,   # always 0
+                t.uint8_t,   # length of data
+                t.Bool       # data
+            ),
+            False,
+        ),
+        0x0001: (
+            "read_state",
+            (
+                foundation.Status,
+                t.uint8_t,   # transactionId
+                t.uint16_t,  # attribute
+                t.uint8_t,   # always 0
+                t.uint8_t,   # length of data
+                t.Bool,      # data
+
+            ),
+            False,
+        )
+    }
+
+    manufacturer_client_commands = {
+        0x0001: (
+            "read_state_rsp",
+            (
+                foundation.Status,
+                t.uint8_t,   # transactionId
+                t.uint16_t,  # attribute
+                t.uint8_t,   # always 0
+                t.uint8_t,   # length of data
+                t.Bool,      # data
+            ),
+            False,
+        ),
+        0x0002: (
+            "state_change_ntfy",
+            (
+                foundation.Status,
+                t.uint8_t,   # transactionId
+                t.uint16_t,  # attribute
+                t.uint8_t,   # always 0
+                t.uint8_t,   # length of data
+                t.Bool,      # data
+            ),
+            False,
+        ),
+    }
+
+    def handle_cluster_request(self, tsn, command_id, args):
+        """Handle the cluster command."""
+        _LOGGER.debug(
+            "TuyaSwitchCluster - handle_cluster_request tsn: [%s] command id: %s - args: [%s]",
+            tsn,
+            command_id,
+            args,
+        )
+
+
+
---
+++ zhaquirks/tuya/singleswitch.py
@@ -0,0 +1,78 @@
+"""Tuya based button sensor."""
+import logging
+
+from zigpy.profiles import zha
+from zigpy.zcl.clusters.general import Basic, Groups, Scenes, Time, Ota
+from ..tuya import TuyaSwitchCluster
+
+from zigpy.quirks import CustomDevice
+from ..const import (
+    ARGS,
+    ATTRIBUTE_ID,
+    ATTRIBUTE_NAME,
+    CLUSTER_ID,
+    COMMAND,
+    COMMAND_ATTRIBUTE_UPDATED,
+    COMMAND_TRIPLE,
+    DEVICE_TYPE,
+    DOUBLE_PRESS,
+    ENDPOINT_ID,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+    SHORT_PRESS,
+    SKIP_CONFIGURATION,
+    TRIPLE_PRESS,
+    UNKNOWN,
+    VALUE,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SingleSwitch(CustomDevice):
+    """Tuya single channel switch device."""
+
+    signature = {
+        # "node_descriptor": "<NodeDescriptor byte1=1 byte2=64 mac_capability_flags=142 manufacturer_code=4098
+        #                       maximum_buffer_size=82 maximum_incoming_transfer_size=82 server_mask=11264
+        #                       maximum_outgoing_transfer_size=82 descriptor_capability_field=0>",
+        # device_version=1
+        # input_clusters=[0x0000,0x0004, 0x0005,0x000a, 0xef00]
+        # output_clusters=[0x0019]
+        MODELS_INFO: [("_TZE200_7tdtqgwv", "TS0601")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Time.cluster_id,
+                    TuyaSwitchCluster.cluster_id],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id,
+                ],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Time.cluster_id,
+                    TuyaSwitchCluster,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id,
+                ],
+            }
+        },
+    }
+
