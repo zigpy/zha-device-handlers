29145cd6fede01cb0d2690ec5ff335e70640abc5
Duncan Jowsey <choif@users.noreply.github.com>
2020-10-14 18:07:55 +0100

    Ikea blind signature fix (#306)

--- zhaquirks/ikea/blinds.py
+++ zhaquirks/ikea/blinds.py
@@ -31,9 +31,9 @@ class IkeaTradfriRollerBlinds(CustomDevice):
     """Custom device representing IKEA of Sweden TRADFRI Fyrtur blinds."""
 
     signature = {
-        # <SimpleDescriptor endpoint=1 profile=260 device_type=2080
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=514
         # device_version=1
-        # input_clusters=[0, 1, 3, 4, 5, 32, 258, 4096]
+        # input_clusters=[0, 1, 3, 4, 5, 32, 258, 4096, 64636]
         # output_clusters=[25, 4096]>
         MODELS_INFO: [
             (IKEA, "FYRTUR block-out roller blind"),
@@ -52,6 +52,7 @@ class IkeaTradfriRollerBlinds(CustomDevice):
                     PollControl.cluster_id,
                     WindowCovering.cluster_id,
                     LightLink.cluster_id,
+                    IKEA_CLUSTER_ID,
                 ],
                 OUTPUT_CLUSTERS: [Ota.cluster_id, LightLink.cluster_id],
             }
@@ -59,11 +60,11 @@ class IkeaTradfriRollerBlinds(CustomDevice):
     }
 
     replacement = {
-        "endpoints": {
+        ENDPOINTS: {
             1: {
-                "profile_id": zha.PROFILE_ID,
-                "device_type": zha.DeviceType.WINDOW_COVERING_DEVICE,
-                "input_clusters": [
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
                     Basic.cluster_id,
                     DoublingPowerConfigurationCluster,
                     Identify.cluster_id,
@@ -72,8 +73,9 @@ class IkeaTradfriRollerBlinds(CustomDevice):
                     PollControl.cluster_id,
                     WindowCovering.cluster_id,
                     LightLink.cluster_id,
+                    IKEA_CLUSTER_ID,
                 ],
-                "output_clusters": [Ota.cluster_id, LightLink.cluster_id],
+                OUTPUT_CLUSTERS: [Ota.cluster_id, LightLink.cluster_id],
             }
         }
     }
