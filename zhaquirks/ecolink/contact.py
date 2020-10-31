0617ca6c6289f7f4c2bfccfebc498a1a30928273
tube0013 <wpoland@gmail.com>
2020-10-31 14:13:51 -0400

    try again to make checks happy

--- zhaquirks/ecolink/contact.py
+++ zhaquirks/ecolink/contact.py
@@ -1,12 +1,9 @@
 """Ecolink 4655BC0-R device."""
+
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import (
-    Basic,
-    Identify,
-    Ota,
-    PollControl,
-)
+from zigpy.zcl.clusters.general import Basic, Identify, Ota, PollControl
+
 from zigpy.zcl.clusters.measurement import TemperatureMeasurement
 
 from zigpy.zcl.clusters.security import IasZone
