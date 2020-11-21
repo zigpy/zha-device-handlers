a4d68dd66dd561704a3558c5913482d031e743cc
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-11-21 15:02:51 -0500

    Bump up zigpy dependency

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
     keywords="zha quirks homeassistant hass",
     packages=find_packages(exclude=["*.tests"]),
     python_requires=">=3",
-    install_requires=["zigpy>=0.22.1"],
+    install_requires=["zigpy>=0.28.0"],
     tests_require=["pytest"],
 )
