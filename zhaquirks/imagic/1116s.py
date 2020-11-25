67d71d33ed687a5c8f7a4084a154d49f4facc4ca
Brian Rogers <brg468@hotmail.com>
2020-11-24 19:12:35 -0500

    Add iMagic by GreatStar 1116-S quirk (#588)
    * Add iMagic by GreatStar 11116-S quirk
    
    * Add to correct location
    
    Wasn't on the latest version of zha-device-handlers, didn't see there was already an imagic folder.

---
+++ zhaquirks/imagic/1116s.py
@@ -0,0 +1,71 @@
+"""Device handler for GreatStar iMagic 1116-S sensor."""
+from zigpy.profiles import zha
+from zigpy.quirks import CustomDevice
+from zigpy.zcl.clusters.general import Basic, Identify, Ota, PollControl
+from zigpy.zcl.clusters.measurement import TemperatureMeasurement
+from zigpy.zcl.clusters.security import IasZone
+
+from zhaquirks import PowerConfigurationCluster
+
+from . import IMAGIC
+from ..const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+
+DIAGNOSTICS_CLUSTER_ID = 0x0B05  # decimal = 2821
+MANUFACTURER_SPECIFIC_CLUSTER_ID = 0xFC01  # decimal = 64513
+MANUFACTURER_SPECIFIC_CLUSTER_ID_2 = 0xFC02  # decimal = 64514
+
+
+class iMagic1116(CustomDevice):
+    """Custom device representing iMagic 1116-S sensor."""
+
+    signature = {
+        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
+        #  device_version=0
+        #  input_clusters=[0, 1, 3, 32, 1026, 1280, 2821, 64513, 64514]
+        #  output_clusters=[3, 25]>
+        MODELS_INFO: [(IMAGIC, "1116-S")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfigurationCluster.cluster_id,
+                    Identify.cluster_id,
+                    MANUFACTURER_SPECIFIC_CLUSTER_ID,
+                    MANUFACTURER_SPECIFIC_CLUSTER_ID_2,
+                    PollControl.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    IasZone.cluster_id,
+                    DIAGNOSTICS_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id, Identify.cluster_id],
+            },
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfigurationCluster,
+                    Identify.cluster_id,
+                    MANUFACTURER_SPECIFIC_CLUSTER_ID,
+                    MANUFACTURER_SPECIFIC_CLUSTER_ID_2,
+                    PollControl.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    IasZone.cluster_id,
+                    DIAGNOSTICS_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id, Identify.cluster_id],
+            },
+        }
+    }
