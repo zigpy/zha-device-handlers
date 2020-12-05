ec9508dd28c640f112e13f6e7343f7eaa113d275
tube0013 <wpoland@gmail.com>
2020-12-04 23:55:28 -0500

    Add 2nd Variant

--- zhaquirks/konke/button.py
+++ zhaquirks/konke/button.py
@@ -2,7 +2,14 @@
 import logging
 
 from zigpy.profiles import zha
-from zigpy.zcl.clusters.general import Basic, Identify, OnOff, PowerConfiguration
+from zigpy.zcl.clusters.general import (
+    Basic,
+    Groups,
+    Identify,
+    OnOff,
+    PowerConfiguration,
+    Scenes,
+)
 
 from .. import CustomCluster, CustomDevice, PowerConfigurationCluster
 from ..const import (
@@ -108,6 +115,70 @@ class KonkeButtonRemote(CustomDevice):
                 OUTPUT_CLUSTERS: [
                     Identify.cluster_id,
                     KONKE_CLUSTER_ID,
+                ],
+            },
+        },
+    }
+
+
+class KonkeButtonRemote2(CustomDevice):
+    """Konke 1-button remote device 2nd variant."""
+
+    def handle_message(self, profile, cluster, src_ep, dst_ep, message):
+        """Handle a device message."""
+        if (
+            profile == 260
+            and cluster == 6
+            and len(message) == 7
+            and message[0] == 0x08
+            and message[2] == 0x0A
+        ):
+            # use the 7th byte as command_id
+            new_message = bytearray(4)
+            new_message[0] = message[0]
+            new_message[1] = message[1]
+            new_message[2] = message[6]
+            new_message[3] = 0
+            message = type(message)(new_message)
+            super().handle_message(profile, cluster, src_ep, dst_ep, message)
+
+    signature = {
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=2
+        # device_version=0
+        # input_clusters=[0, 1, 3, 4, 5, 6]
+        # output_clusters=[3]>
+        MODELS_INFO: [("KONKE", "3AFE170100510001")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.REMOTE_CONTROL,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Identify.cluster_id],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.REMOTE_CONTROL,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfigurationCluster,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    KonkeOnOffCluster,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,
                 ],
             },
         },
