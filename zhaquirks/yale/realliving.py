c834dbdc962f330d14a49b9f5813c769979862d3
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-01-25 20:02:01 +1100

    Cleanup Yale

--- zhaquirks/yale/realliving.py
+++ zhaquirks/yale/realliving.py
@@ -15,7 +15,7 @@ from ..const import (
 )
 
 
-class YaleRealLiving(CustomDevice):
+class YRD210PBDB220TSLL(CustomDevice):
     """Custom device representing Yale Real Living devices."""
 
     signature = {
@@ -55,6 +55,50 @@ class YaleRealLiving(CustomDevice):
                     Time.cluster_id,
                     DoorLock.cluster_id,
                     PollControl.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [DoorLock.cluster_id, Ota.cluster_id],
+            }
+        }
+    }
+
+class YRD220240TSDB(CustomDevice):
+    """Custom device representing Yale Real Living devices."""
+
+    signature = {
+        #  <SimpleDescriptor endpoint=1 profile=260 device_type=10
+        #  device_version=0
+        #  input_clusters=[0, 1, 9, 10, 32, 257]
+        #  output_clusters=[10, 25]>
+        MODELS_INFO: [("Yale", "YRD220/240 TSDB")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.DOOR_LOCK,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    DoublingPowerConfigurationCluster.cluster_id,
+                    Alarms.cluster_id,
+                    Time.cluster_id,
+                    PollControl.cluster_id,
+                    DoorLock.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.DOOR_LOCK,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    DoublingPowerConfigurationCluster,
+                    Alarms.cluster_id,
+                    Time.cluster_id,
+                    PollControl.cluster_id,
+                    DoorLock.cluster_id,
                 ],
                 OUTPUT_CLUSTERS: [DoorLock.cluster_id, Ota.cluster_id],
             }
