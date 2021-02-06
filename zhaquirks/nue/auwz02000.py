93e565209cf52d4fb1cf0be37ede222bb18df0ac
bikkuri10 <bikkuri10@users.noreply.github.com>
2021-02-06 20:54:27 +1100

    Get black to fix formatting

--- zhaquirks/nue/auwz02000.py
+++ zhaquirks/nue/auwz02000.py
@@ -19,80 +19,70 @@ class auwz02000(CustomDevice):
     """Model NUE-AUWZ02-000 Double GPO - Australia plug."""
 
     signature = {
-        MODELS_INFO: [
-                ("3A Smart Home DE", "LXN56-TS27LX1.2")
-        ],
+        MODELS_INFO: [("3A Smart Home DE", "LXN56-TS27LX1.2")],
         ENDPOINTS: {
             #  <SimpleDescriptor endpoint=1 profile=260 device_type=256
             #  device_version=1
             #  input_clusters=[0, 3, 4, 5, 6, 4096]
             #  output_clusters=[25]>
             1: {
-              PROFILE_ID: zha.PROFILE_ID,
-              DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
-              INPUT_CLUSTERS: [
-                  Basic.cluster_id,
-                  Identify.cluster_id,
-                  Groups.cluster_id,
-                  Scenes.cluster_id,
-                  OnOff.cluster_id,
-                  LightLink.cluster_id
-              ],
-              OUTPUT_CLUSTERS: [
-                  Ota.cluster_id
-              ]
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    LightLink.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
             },
             #  <SimpleDescriptor endpoint=2 profile=260 device_type=256
             #  device_version=1
             #  input_clusters=[0, 3, 4, 5, 6]
             #  output_clusters=[25]>
             2: {
-              PROFILE_ID: zha.PROFILE_ID,
-              DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
-              INPUT_CLUSTERS: [
-                  Basic.cluster_id,
-                  Identify.cluster_id,
-                  Groups.cluster_id,
-                  Scenes.cluster_id,
-                  OnOff.cluster_id
-              ],
-              OUTPUT_CLUSTERS: [
-                  Ota.cluster_id
-              ]
-            }
-        }
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
     }
 
     replacement = {
         ENDPOINTS: {
             1: {
-              PROFILE_ID: zha.PROFILE_ID,
-              DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET, #0x0009
-              INPUT_CLUSTERS: [
-                  Basic.cluster_id,
-                  Identify.cluster_id,
-                  Groups.cluster_id,
-                  Scenes.cluster_id,
-                  OnOff.cluster_id,
-                  LightLink.cluster_id
-              ],
-              OUTPUT_CLUSTERS: [
-                  Ota.cluster_id
-              ]
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET,  # 0x0009
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    LightLink.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
             },
             2: {
-              PROFILE_ID: zha.PROFILE_ID,
-              DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET, #0x0009
-              INPUT_CLUSTERS: [
-                  Basic.cluster_id,
-                  Identify.cluster_id,
-                  Groups.cluster_id,
-                  Scenes.cluster_id,
-                  OnOff.cluster_id
-              ],
-              OUTPUT_CLUSTERS: [
-                  Ota.cluster_id
-              ]
-            }
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET,  # 0x0009
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
         }
     }
