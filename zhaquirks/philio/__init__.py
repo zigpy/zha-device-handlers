1e8da63a69b41d76956b684b50fabe121ab36ebf
Eric Chauvet <eric.chauvet@test-tree.com>
2021-02-10 22:36:00 +0100

    Philio (known as Evology for Leroy Merlin) PST03A quirk

---
+++ zhaquirks/philio/__init__.py
@@ -0,0 +1,10 @@
+"""Module for Philio quirks implementations."""
+
+from .. import MotionWithReset
+
+PHILIO = "Philio"
+
+class MotionCluster(MotionWithReset):
+    """Motion cluster."""
+
+    reset_s: int = 20
---
+++ zhaquirks/philio/pst03a.py
@@ -0,0 +1,93 @@
+"""Device handler for Philio PST03A-v2.2.5."""
+from zigpy.profiles import zha
+from zigpy.quirks import CustomDevice
+from zigpy.zcl.clusters.general import (
+    Basic,
+    PowerConfiguration,
+    Alarms,
+    OnOff,
+    BinaryInput,
+    Ota,
+    PollControl,
+)
+from zigpy.zcl.clusters.measurement import (
+    TemperatureMeasurement, 
+    IlluminanceMeasurement, 
+    OccupancySensing,
+)
+from zigpy.zcl.clusters.security import IasZone
+
+from zhaquirks import PowerConfigurationCluster
+
+from . import PHILIO, MotionCluster
+from ..const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS, 
+    MANUFACTURER,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+
+
+class PST03A(CustomDevice):
+    """Custom device representing PST03A 4in1 motion/opening/temperature/illuminance sensors."""
+
+    signature = {
+        MODELS_INFO: [("", "PST03A-v2.2.5")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Alarms.cluster_id,
+                    OccupancySensing.cluster_id,
+                    IasZone.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [OnOff.cluster_id, Ota.cluster_id],
+            },
+            2: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Alarms.cluster_id,
+                    BinaryInput.cluster_id,
+                    IasZone.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Alarms.cluster_id,
+                    MotionCluster,
+                    TemperatureMeasurement.cluster_id,
+                    IlluminanceMeasurement.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+            2: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Alarms.cluster_id,
+                    IasZone.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            }
+        },
+        MANUFACTURER: PHILIO,
+    }
