b0216b3da2070abf3c2fdfee250bd0dca5223dc1
Eric van Blokland <ericvanblokland@gmail.com>
2021-04-16 18:08:21 +0200

    Added support for icasa ICZB-KPD18S

---
+++ zhaquirks/icasa/iczb-kpd18s.py
@@ -0,0 +1,213 @@
+"""icasa KPD18S device."""
+from zigpy.profiles import zha
+from zigpy.quirks import CustomDevice
+from zigpy.zcl.clusters.general import (
+    Basic,
+    Groups,
+    Identify,
+    LevelControl,
+    OnOff,
+    Ota,
+    PowerConfiguration,
+    Scenes,
+)
+from zigpy.zcl.clusters.lightlink import LightLink
+
+from zigpy.zcl.clusters.homeautomation import Diagnostic
+
+from zigpy.zcl.clusters.lighting import Color
+
+from zhaquirks.const import (
+    COMMAND,
+    DEVICE_TYPE,
+    ENDPOINTS,
+    CLUSTER_ID,
+    ENDPOINT_ID,
+    ARGS,
+    INPUT_CLUSTERS,
+    LONG_PRESS,
+    LONG_RELEASE,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+    SHORT_PRESS,
+    TURN_ON,
+    TURN_OFF,
+    DIM_UP,
+    DIM_DOWN,
+    COMMAND_ON,
+    COMMAND_OFF,
+    COMMAND_MOVE_ON_OFF,
+    COMMAND_STOP,
+    BUTTON_1,
+    BUTTON_2,
+    BUTTON_3,
+    BUTTON_4,
+    BUTTON_5,
+    BUTTON_6,
+)
+
+COMMAND_STORE = 'store'
+COMMAND_RECALL = 'recall'
+
+
+class IcasaKPD18S(CustomDevice):
+    """icasa KPD18S device (this is a variant of ."""
+
+    signature = {
+        MODELS_INFO: [("icasa", "ICZB-KPD18S")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.LEVEL_CONTROL_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Diagnostic.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    LevelControl.cluster_id,
+                    Ota.cluster_id,
+                    Color.cluster_id,
+                    LightLink.cluster_id,
+                ],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.LEVEL_CONTROL_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Diagnostic.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    LevelControl.cluster_id,
+                    Ota.cluster_id,
+                    Color.cluster_id,
+                    LightLink.cluster_id,
+                ],
+            }
+        }
+    }
+
+    device_automation_triggers = {
+        (SHORT_PRESS, TURN_ON): {
+            COMMAND: COMMAND_ON,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 6,
+        },
+        (LONG_PRESS, DIM_UP): {
+            COMMAND: COMMAND_MOVE_ON_OFF,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 8,
+            ARGS: [0, 50],
+        },
+        (LONG_RELEASE, DIM_UP): {
+            COMMAND: COMMAND_STOP,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 8,
+        },
+        (SHORT_PRESS, TURN_OFF): {
+            COMMAND: COMMAND_OFF,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 6,
+        },
+        (LONG_PRESS, DIM_DOWN): {
+            COMMAND: COMMAND_MOVE_ON_OFF,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 8,
+            ARGS: [1, 50],
+        },
+        (LONG_RELEASE, DIM_DOWN): {
+            COMMAND: COMMAND_STOP,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 8,
+        },
+        (SHORT_PRESS, BUTTON_1): {
+            COMMAND: COMMAND_RECALL,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 1],
+        },
+        (SHORT_PRESS, BUTTON_2): {
+            COMMAND: COMMAND_RECALL,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 2],
+        },
+        (SHORT_PRESS, BUTTON_3): {
+            COMMAND: COMMAND_RECALL,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 3],
+        },
+        (SHORT_PRESS, BUTTON_4): {
+            COMMAND: COMMAND_RECALL,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 4],
+        },
+        (SHORT_PRESS, BUTTON_5): {
+            COMMAND: COMMAND_RECALL,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 5],
+        },
+        (SHORT_PRESS, BUTTON_6): {
+            COMMAND: COMMAND_STORE,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 6],
+        },
+        (LONG_PRESS, BUTTON_1): {
+            COMMAND: COMMAND_STORE,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 1],
+        },
+        (LONG_PRESS, BUTTON_2): {
+            COMMAND: COMMAND_STORE,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 2],
+        },
+        (LONG_PRESS, BUTTON_3): {
+            COMMAND: COMMAND_STORE,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 3],
+        },
+        (LONG_PRESS, BUTTON_4): {
+            COMMAND: COMMAND_STORE,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 4],
+        },
+        (LONG_PRESS, BUTTON_5): {
+            COMMAND: COMMAND_STORE,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 5],
+        },
+        (LONG_PRESS, BUTTON_6): {
+            COMMAND: COMMAND_STORE,
+            ENDPOINT_ID: 1,
+            CLUSTER_ID: 5,
+            ARGS: [0, 6],
+        }
+    }
