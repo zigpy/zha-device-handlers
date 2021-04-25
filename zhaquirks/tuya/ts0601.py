247a7529683c1eb7d1cb902da21d5410f37dfd35
holdestmade <holdestmade@users.noreply.github.com>
2021-04-25 20:15:29 +0100

    Add support for Zemismart Curtain Motor TZE200_3i3exuay (#859)
    * Update ts0601.py
    
    * Update ts0601.py

--- zhaquirks/tuya/ts0601.py
+++ zhaquirks/tuya/ts0601.py
@@ -112,6 +112,53 @@ class TuyaZemismartSmartCover0601(TuyaWindowCover):
     }
 
 
+class TuyaZemismartSmartCover0601_2(TuyaWindowCover):
+    """Tuya Zemismart curtain cover motor."""
+
+    signature = {
+        # "node_descriptor": "<NodeDescriptor byte1=1 byte2=64 mac_capability_flags=142 manufacturer_code=4098
+        #                       maximum_buffer_size=82 maximum_incoming_transfer_size=82 server_mask=11264
+        #                       maximum_outgoing_transfer_size=82 descriptor_capability_field=0>",
+        # input_clusters=[0x0000, 0x000a, 0x0004, 0x0005, 0xef00]
+        # output_clusters=[0x0019]
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=81 input_clusters=[0, 10, 4, 5, 61184] output_clusters=[25]>
+        MODELS_INFO: [
+            ("_TZE200_3i3exuay", "TS0601"),
+        ],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Time.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    TuyaManufCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Time.cluster_id,
+                    TuyaManufacturerWindowCover,
+                    TuyaWindowCoverControl,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
+
+
 class TuyaMoesCover0601(TuyaWindowCover):
     """Tuya blind controller device."""
 
