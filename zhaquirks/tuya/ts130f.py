e2f397233d5a27d06d714b6fed290fc2bf58f2b5
Michael Bouchaud <michael.bouchaud@gmail.com>
2021-04-26 00:44:17 +0200

    add ts130f tuya cover curtain (#836)
    * add ts130f tuya cover curtain
    
    * Fix flake8 errors and warnings
    
    * fix black check
    
    * fix flake8 check
    
    * Fix isort check
    
    * more fix on flake8 and black
    
    * Switch relative import to absolute import
    
    Co-authored-by: Michaël Bouchaud (yoz) <yoz@efl.so>

---
+++ zhaquirks/tuya/ts130f.py
@@ -0,0 +1,104 @@
+"""Device handler for loratap TS130F smart curtain switch."""
+from zigpy.profiles import zha
+from zigpy.quirks import CustomCluster, CustomDevice
+import zigpy.types as t
+from zigpy.zcl.clusters.closures import WindowCovering
+from zigpy.zcl.clusters.general import Basic, Groups, OnOff, Ota, Scenes, Time
+
+from zhaquirks.const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+
+ATTR_CURRENT_POSITION_LIFT_PERCENTAGE = 0x0008
+CMD_GO_TO_LIFT_PERCENTAGE = 0x0005
+
+
+class TuyaWithBacklightOnOffCluster(CustomCluster):
+    """TuyaSmartCurtainOnOffCluster: fire events corresponding to press type."""
+
+    cluster_id = OnOff.cluster_id
+
+    LIGHT_MODE_1 = {0x8001: 0}
+    LIGHT_MODE_2 = {0x8001: 1}
+    LIGHT_MODE_3 = {0x8001: 2}
+
+    attributes = {0x8001: ("backlight_mode", t.enum8)}
+
+
+class TuyaCoveringCluster(CustomCluster, WindowCovering):
+    """TuyaSmartCurtainWindowCoveringCluster: Allow to setup Window covering tuya devices."""
+
+    attributes = WindowCovering.attributes.copy()
+    attributes.update({0xF000: ("tuya_moving_state", t.enum8)})
+    attributes.update({0xF001: ("calibration", t.enum8)})
+    attributes.update({0xF002: ("motor_reversal", t.enum8)})
+
+    def _update_attribute(self, attrid, value):
+        if attrid == ATTR_CURRENT_POSITION_LIFT_PERCENTAGE:
+            # Invert the percentage value (cf https://github.com/dresden-elektronik/deconz-rest-plugin/issues/3757)
+            value = 100 - value
+        super()._update_attribute(attrid, value)
+
+    async def command(
+        self, command_id, *args, manufacturer=None, expect_reply=True, tsn=None
+    ):
+        """Override default command to invert percent lift value."""
+        if command_id == CMD_GO_TO_LIFT_PERCENTAGE:
+            percent = args[0]
+            # Invert the percentage value (cf https://github.com/dresden-elektronik/deconz-rest-plugin/issues/3757)
+            percent = 100 - percent
+            v = (percent,)
+            return await super().command(command_id, *v)
+        return await super().command(
+            command_id,
+            *args,
+            manufacturer=manufacturer,
+            expect_reply=expect_reply,
+            tsn=tsn
+        )
+
+
+class TuyaTS130F(CustomDevice):
+    """Tuya smart curtain roller shutter."""
+
+    signature = {
+        # SizePrefixedSimpleDescriptor(endpoint=1, profile=260, device_type=0x0202, device_version=1, input_clusters=[0, 4, 5, 6, 10, 0x0102], output_clusters=[25]))
+        MODELS_INFO: [("_TZ3000_8kzqqzu4", "TS130F")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Time.cluster_id,
+                    OnOff.cluster_id,
+                    WindowCovering.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Time.cluster_id,
+                    TuyaWithBacklightOnOffCluster,
+                    TuyaCoveringCluster,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+        },
+    }
