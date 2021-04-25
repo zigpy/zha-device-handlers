7cd92f2b52dd1dd3a53ac2d0102fbf83aa6342a8
rednus <avvari.shyam@gmail.com>
2021-04-25 20:14:57 +0100

    Issue #744 Tuya Curtain Motor Updates (#856)
    * Issue #744 Tuya Curtain Motor Updates
    
    * Corrections to failed checks
    
    * Fix Merge Request Errors
    
    * Fix Merge Request Errors
    
    * Fix flake8 check errors
    
    * Fix flake8 errors
    
    * Hopefully last fix for Flake8
    
    * Wow Flake8 is picky.
    
    * Updated self._attr_cache[xx] to self._attr_cache.get(xx) as per Adminiuga's suggestion
    
    * Re-commit after pre-commit run
    
    * Updated cover inversion handling
    
    * Updated TUYA_COVER_COMMAND mapping as per @jasmin-mistry

--- zhaquirks/tuya/ts0601.py
+++ zhaquirks/tuya/ts0601.py
@@ -129,6 +129,7 @@ class TuyaMoesCover0601(TuyaWindowCover):
         MODELS_INFO: [
             ("_TZE200_zah67ekd", "TS0601"),
             ("_TZE200_xuzcvlku", "TS0601"),
+            ("_TZE200_rddyvrci", "TS0601"),
         ],  # Not tested
         ENDPOINTS: {
             1: {
