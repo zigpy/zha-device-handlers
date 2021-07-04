04dbc5b4ec0c29276b0d34bfa7858869b628bcf4
Geoffrey Lagaisse <geoffreylagaisse@gmail.com>
2021-07-04 21:44:02 +0000

    Working Termostatfunction

--- zhaquirks/tuya/valve.py
+++ zhaquirks/tuya/valve.py
@@ -749,7 +749,9 @@ ZONNSMART_WINDOW_DETECT_ATTR = 0x0108  # [0] inactive [1] active
 ZONNSMART_TARGET_TEMP_ATTR = 0x0210  # [0,0,0,210] target room temp (decidegree)
 ZONNSMART_TEMPERATURE_ATTR = 0x0218  # [0,0,0,200] current room temp (decidegree)
 ZONNSMART_BATTERY_ATTR = 0x0223  # [0,0,0,98] battery charge
-ZONNSMART_MODE_ATTR = 0x0402  # [0] off [1] scheduled [2] manual
+ZONNSMART_MODE_ATTR = (
+    0x0402  # [0] Scheduled/auto [1] manual [2] Holiday [3] HolidayReady
+)
 ZONNSMART_HEATING_STOPPING = 0x016B  # [0] inactive [1] active
 ZONNSMART_BOOST_TIME_ATTR = 0x0265  # BOOST mode operating time in (sec)
 ZONNSMART_UPTIME_TIME_ATTR = (
@@ -772,23 +774,25 @@ class ZONNSMARTManufCluster(TuyaManufClusterAttributes):
         ZONNSMART_HEATING_STOPPING: ("heating_stop", t.uint8_t),
     }
 
-    TEMPERATURE_ATTRS = {
+    DIRECT_MAPPED_ATTRS = {
         ZONNSMART_TEMPERATURE_ATTR: ("local_temp", lambda value: value * 10),
         ZONNSMART_TARGET_TEMP_ATTR: (
             "occupied_heating_setpoint",
             lambda value: value * 10,
         ),
+        ZONNSMART_BOOST_TIME_ATTR: ("boost_duration_seconds", None),
+        ZONNSMART_UPTIME_TIME_ATTR: ("uptime_duration_hours", None),
     }
 
     def _update_attribute(self, attrid, value):
         super()._update_attribute(attrid, value)
-        if attrid in self.TEMPERATURE_ATTRS:
+        if attrid in self.DIRECT_MAPPED_ATTRS:
             self.endpoint.device.thermostat_bus.listener_event(
                 "temperature_change",
-                self.TEMPERATURE_ATTRS[attrid][0],
+                self.DIRECT_MAPPED_ATTRS[attrid][0],
                 value
-                if self.TEMPERATURE_ATTRS[attrid][1] is None
-                else self.TEMPERATURE_ATTRS[attrid][1](
+                if self.DIRECT_MAPPED_ATTRS[attrid][1] is None
+                else self.DIRECT_MAPPED_ATTRS[attrid][1](
                     value
                 ),  # decidegree to centidegree
             )
@@ -796,81 +800,70 @@ class ZONNSMARTManufCluster(TuyaManufClusterAttributes):
             self.endpoint.device.thermostat_bus.listener_event("mode_change", value)
         elif attrid == ZONNSMART_HEATING_STOPPING:
             self.endpoint.device.thermostat_bus.listener_event(
-                "state_change", value < 1
+                "state_change", value == 0
             )
         elif attrid == ZONNSMART_CHILD_LOCK_ATTR:
             mode = 1 if value else 0
             self.endpoint.device.ui_bus.listener_event("child_lock_change", mode)
         elif attrid == ZONNSMART_BATTERY_ATTR:
             self.endpoint.device.battery_bus.listener_event("battery_change", value)
-        elif attrid == ZONNSMART_BOOST_TIME_ATTR:
-            self.endpoint.device.battery_bus.listener_event(
-                "boost_duration_seconds", value
-            )
-        elif attrid == ZONNSMART_UPTIME_TIME_ATTR:
-            self.endpoint.device.battery_bus.listener_event(
-                "uptime_duration_hours", value
-            )
 
 
 class ZONNSMARTThermostat(TuyaThermostatCluster):
     """Thermostat cluster for some thermostatic valves."""
 
+    DIRECT_MAPPING_ATTRS = {
+        "occupied_heating_setpoint": (
+            ZONNSMART_TARGET_TEMP_ATTR,
+            lambda value: round(value / 10),
+        ),
+        "operation_preset": (ZONNSMART_MODE_ATTR, None),
+        "boost_duration_seconds": (ZONNSMART_BOOST_TIME_ATTR, None),
+    }
+
     def map_attribute(self, attribute, value):
         """Map standardized attribute value to dict of manufacturer values."""
 
-        if attribute == "occupied_heating_setpoint":
-            # centidegree to decidegree
-            return {ZONNSMART_TARGET_TEMP_ATTR: round(value / 10)}
-        if attribute in ("programing_oper_mode", "occupancy"):
-            if attribute == "occupancy":
-                occupancy = value
+        if attribute in self.DIRECT_MAPPING_ATTRS:
+            return {
+                self.DIRECT_MAPPING_ATTRS[attribute][0]: value
+                if self.DIRECT_MAPPING_ATTRS[attribute][1] is None
+                else self.DIRECT_MAPPING_ATTRS[attribute][1](value)
+            }
+        if attribute in ("system_mode", "programing_oper_mode"):
+            if attribute == "system_mode":
+                system_mode = value
                 oper_mode = self._attr_cache.get(
                     self.attridx["programing_oper_mode"],
                     self.ProgrammingOperationMode.Simple,
                 )
             else:
-                occupancy = self._attr_cache.get(
-                    self.attridx["occupancy"], self.Occupancy.Occupied
+                system_mode = self._attr_cache.get(
+                    self.attridx["system_mode"], self.SystemMode.Heat
                 )
                 oper_mode = value
-            if occupancy == self.Occupancy.Occupied:
+            if system_mode == self.SystemMode.Off:
+                return {ZONNSMART_HEATING_STOPPING: 1}
+            if system_mode == self.SystemMode.Heat:
                 if oper_mode == self.ProgrammingOperationMode.Schedule_programming_mode:
                     return {ZONNSMART_MODE_ATTR: 0}
                 if oper_mode == self.ProgrammingOperationMode.Simple:
                     return {ZONNSMART_MODE_ATTR: 1}
-                if oper_mode == self.ProgrammingOperationMode.Economy_mode:
-                    return {ZONNSMART_MODE_ATTR: 3}
                 self.error("Unsupported value for ProgrammingOperationMode")
             else:
-                self.error("Unsupported value for Occupancy")
-        if attribute == "system_mode":
-            return {
-                ZONNSMART_MODE_ATTR: self._attr_cache.get(
-                    self.attridx["operation_preset"], 0
-                )
-            }
+                self.error("Unsupported value for SystemMode")
 
     def mode_change(self, value):
         """System Mode change."""
         if value == 0:
             prog_mode = self.ProgrammingOperationMode.Schedule_programming_mode
-            occupancy = self.Occupancy.Occupied
         elif value == 1:
             prog_mode = self.ProgrammingOperationMode.Simple
-            occupancy = self.Occupancy.Occupied
-        elif value == 2:
-            prog_mode = self.ProgrammingOperationMode.Simple
-            occupancy = self.Occupancy.Occupied
-        elif value == 3:
-            prog_mode = self.ProgrammingOperationMode.Simple
-            occupancy = self.Occupancy.Occupied
         else:
             prog_mode = self.ProgrammingOperationMode.Simple
-            occupancy = self.Occupancy.Occupied
 
+        self._update_attribute(self.attridx["system_mode"], self.SystemMode.Heat)
         self._update_attribute(self.attridx["programing_oper_mode"], prog_mode)
-        self._update_attribute(self.attridx["occupancy"], occupancy)
 
 
 class ZONNSMARTUserInterface(TuyaUserInterfaceCluster):
@@ -1059,13 +1052,8 @@ class MoesHY368_Type2(TuyaThermostat):
     }
 
 
-class ZonnsmartTV01_ZG_Type1(TuyaThermostat):
+class ZonnsmartTV01_ZG(TuyaThermostat):
     """ZONNSMART TV01-ZG Thermostatic radiator valve."""
-
-    def __init__(self, *args, **kwargs):
-        """Init device."""
-        self.window_detection_bus = Bus()
-        super().__init__(*args, **kwargs)
 
     signature = {
         #  endpoint=1 profile=260 device_type=81 device_version=0 input_clusters=[0, 4, 5, 61184]
