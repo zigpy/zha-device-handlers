400547a3794d7f5a85d5a26c5ecb47be11978106
MidnessX <72510075+MidnessX@users.noreply.github.com>
2021-07-09 14:59:09 +0200

    Add support for BTicino L441C/N4411C/NT4411C.

--- zhaquirks/legrand/dimmer.py
+++ zhaquirks/legrand/dimmer.py
@@ -130,3 +130,42 @@ class DimmerWithoutNeutral2(DimmerWithoutNeutral):
             },
         },
     }
+
+
+class DimmerWithNeutral(DimmerWithoutNeutral):
+    """Dimmer switch with neutral."""
+
+    signature = {
+        #  <SimpleDescriptor endpoint=1 profile=260 device_type=256
+        # device_version=1
+        # input_clusters=[0, 3, 4, 8, 6, 5, 15, 64513]
+        # output_clusters=[0, 25, 64513]>
+        MODELS_INFO: [(f" {LEGRAND}", " Dimmer switch with neutral")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    OnOff.cluster_id,
+                    LevelControl.cluster_id,
+                    Scenes.cluster_id,
+                    BinaryInput.cluster_id,
+                    MANUFACTURER_SPECIFIC_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    MANUFACTURER_SPECIFIC_CLUSTER_ID,
+                    Ota.cluster_id,
+                ],
+            },
+            242: {
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 0x0066,
+                INPUT_CLUSTERS: [0x0021],
+                OUTPUT_CLUSTERS: [0x0021],
+            },
+        },
+    }
