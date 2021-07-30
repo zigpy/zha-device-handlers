faa9003043fe7c4e896be58047b1ab691c894487
MattWestb <49618193+MattWestb@users.noreply.github.com>
2021-07-30 11:54:50 +0200

    Create ts004f.py
    OPS !!

--- zhaquirks/tuya/ts004f.py
+++ zhaquirks/tuya/ts004f.py
@@ -71,11 +71,11 @@ class Tuya4NewButtonTriggers:
             ENDPOINT_ID: 1,
             ARGS: [1, 51],
         },
-+        (LONG_RELEASE, DIM_DOWN): {
-+            COMMAND: COMMAND_STOP,
-+            CLUSTER_ID: 8,
-+            ENDPOINT_ID: 1,
-+        },
+        (LONG_RELEASE, DIM_DOWN): {
+            COMMAND: COMMAND_STOP,
+            CLUSTER_ID: 8,
+            ENDPOINT_ID: 1,
+        },
      }
 
 
