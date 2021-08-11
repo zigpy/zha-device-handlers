361e0acd7eecf2fc61716560508f8c9721ffa623
Daniel <danieldelnaaij@gmail.com>
2021-08-11 14:05:26 +0200

    refactor: Combine models info for same device in 1 file
    3gang_a and 3gang_b are the same device but with a different model
    info.
    These can be combined in the same file.

--- zhaquirks/tuya/ts0601_touch_3gang.py
+++ zhaquirks/tuya/ts0601_touch_3gang.py
@@ -36,7 +36,10 @@ class TuyaTouch3GangA(TuyaSwitch):
         # input_clusters=[0x0000,0x0004, 0x0005, 0xef00]
         # output_clusters=[0x000a, 0x0019]
         # <SimpleDescriptor endpoint=1 profile=260 device_type=0x0051 device_version=1 input_clusters=[0, 4, 5, 61184] output_clusters=[10, 25]>
-        MODELS_INFO: [("_TZE200_kyfqmmyl", "TS0601")],
+        MODELS_INFO: [
+            ("_TZE200_kyfqmmyl", "TS0601"),
+            ("_TZE200_vhy3iakz", "TS0601")
+        ],
         ENDPOINTS: {
             1: {
                 PROFILE_ID: zha.PROFILE_ID,
