f3471c606a0e5f0eac7a988b7efef1becbf31260
MattWestb <49618193+MattWestb@users.noreply.github.com>
2021-08-25 15:01:15 +0200

    Adding _TZE200_gubdgai2 TS0601 (#1009)
    * Adding _TZE200_gubdgai2 TS0601 one more MOES cover / blind controller.
    
    Closing: #934
    
    * Update __init__.py
    
    Adding the init

--- zhaquirks/tuya/ts0601.py
+++ zhaquirks/tuya/ts0601.py
@@ -229,6 +229,7 @@ class TuyaMoesCover0601(TuyaWindowCover):
             ("_TZE200_xuzcvlku", "TS0601"),
             ("_TZE200_rddyvrci", "TS0601"),
             ("_TZE200_nueqqe6k", "TS0601"),
+            ("_TZE200_gubdgai2", "TS0601"),
         ],  # Not tested
         ENDPOINTS: {
             1: {
