ad8f44cb4becd6dc2b89c03069ecf05e7725ce90
EverythingSmartHome <info@everythingsmarthome.co.uk>
2021-09-10 21:04:07 +0100

    Removing unneeded classes and attributes.

--- zhaquirks/develco/__init__.py
+++ zhaquirks/develco/__init__.py
@@ -19,28 +19,6 @@ class DevelcoPowerConfiguration(PowerConfigurationCluster):
 class DevelcoIasZone(CustomCluster, IasZone):
     """Custom IasZone for Develco."""
 
-    class ZoneStatus(t.uint16_t):
-        """ZoneStatus attribute."""
-
-        Alarm_1 = 0x0000
-        Battery = 0x0003
-        Supervision_reports = 0x0004
-        Restore_reports = 0x0005
-        Test = 0x0008
-
-    cluster_id = 0x0500
-    name = "IAS Zone"
-    ep_attribute = "ias_zone"
-    attributes = {
-        # Zone Information
-        0x0000: ("zone_state", IasZone.ZoneState),
-        0x0001: ("zone_type", IasZone.ZoneType),
-        0x0002: ("zone_status", ZoneStatus),
-        # Zone Settings
-        0x0010: ("cie_addr", t.EUI64),
-        0x0011: ("zone_id", t.uint8_t),
-    }
-
     server_commands = {
         0x0000: ("status_change_notification", (t.uint16_t,), False),
         0x0001: (
