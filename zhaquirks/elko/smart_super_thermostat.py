f47cf3dc47e6b0bbc77fa0f985a0675ac77e5760
Trond Nordheim <trond@nordheim.io>
2021-09-22 14:23:49 +0200

    Power reading support for Elko Super RF thermostat

--- zhaquirks/elko/smart_super_thermostat.py
+++ zhaquirks/elko/smart_super_thermostat.py
@@ -17,6 +17,7 @@ from zhaquirks.elko import (
     ElkoThermostat,
     ElkoThermostatCluster,
     ElkoUserInterfaceCluster,
+    ElkoElectricalMeasurementCluster,
 )
 
 LOCAL_TEMP = 0x0000
@@ -129,6 +130,9 @@ class ElkoSuperTRThermostatCluster(ElkoThermostatCluster):
                 and self.active_sensor == self.Sensor.FLOOR
             ):
                 attrid = LOCAL_TEMP
+        elif attrid == POWER_CONSUMPTION:
+            if value is not None and value >= 0:
+                self.endpoint.device.power_bus.listener_event("power_reported", value);
 
         super()._update_attribute(attrid, value)
 
@@ -169,6 +173,7 @@ class ElkoSuperTRThermostat(ElkoThermostat):
                     Scenes.cluster_id,
                     ElkoSuperTRThermostatCluster,
                     ElkoUserInterfaceCluster,
+                    ElkoElectricalMeasurementCluster,
                 ],
                 OUTPUT_CLUSTERS: [
                     Identify.cluster_id,
