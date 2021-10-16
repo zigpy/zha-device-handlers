0054ff84f099d378bfa03c97d73886d309693c75
miit86 <47698637+miit86@users.noreply.github.com>
2021-10-16 14:07:24 +0200

    Create Quirk for TS011F (#1057)
    * Create TS011F
    
    Quirk for the TS011F 
    _TZ3000_cphmq0q7'},
    _TZ3000_ew3ldmgx'},
    _TZ3000_ps3dmato'},
    _TZ3000_mraovvmm'},
    _TZ3000_jvzvulen'},
    _TZ3000_dpo1ysak'}],
    
    * Update TS011F
    
    * Rename TS011F to TS011F_plug.py
    
    * Update TS011F_plug.py
    
    Made changes but stil using ```    attributes = OnOff.attributes.copy()
        attributes.update({0x8002: ("power_on_state", PowerOnState)})```
    
    * Update TS011F_plug.py
    
    Used TuyaZBOnOffRestorePowerCluster from INIT. PR1056
    
    * Update TS011F_plug.py
    
    used flake 8 in VSCode
    
    * Update TS011F_plug.py
    
    Added the missing dots
    
    * Update TS011F_plug.py
    
    I Sort and Flake 8 fixed
    
    * Update TS011F_plug.py
    
    ISORT says files is OK. Using ISORT in VSCode
    
    * Update TS011F_plug.py
    
    moved line 10 to 24
    
    * Update TS011F_plug.py
    
    Changed Imports Manually
    
    * Update TS011F_plug.py
    
    * Update TS011F_plug.py
    
    Replaced E000 with new class
    Replaced E001 with new class
    
    * Update TS011F_plug.py
    
    * Update TS011F_plug.py
    
    * Update TS011F_plug.py
    
    Final Version
    
    * Update TS011F_plug.py
    
    * Update TS011F_plug.py
    
    * Update TS011F_plug.py

---
+++ zhaquirks/tuya/TS011F_plug.py
@@ -0,0 +1,118 @@
+"""TS011F plug."""
+
+from zigpy.profiles import zha
+from zigpy.quirks import CustomCluster, CustomDevice
+from zigpy.zcl.clusters.general import (
+    Basic,
+    GreenPowerProxy,
+    Groups,
+    Identify,
+    OnOff,
+    Ota,
+    Scenes,
+    Time,
+)
+from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
+from zigpy.zcl.clusters.smartenergy import Metering
+
+from zhaquirks.const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODEL,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+from zhaquirks.tuya import TuyaZBOnOffRestorePowerCluster
+
+
+class TuyaClusterE000(CustomCluster):
+    """Tuya manufacturer specific cluster 57344."""
+
+    name = "Tuya Manufacturer Specific"
+    cluster_id = 0xE000
+    ep_attribute = "tuya_is_pita_0"
+
+
+class TuyaClusterE001(CustomCluster):
+    """Tuya manufacturer specific cluster 57345."""
+
+    name = "Tuya Manufacturer Specific"
+    cluster_id = 0xE001
+    ep_attribute = "tuya_is_pita_1"
+
+
+class TuyaZBMetering(CustomCluster, Metering):
+    """Divides the KwH for tuya."""
+
+    MULTIPLIER = 0x0301
+    DIVISOR = 0x0302
+    _CONSTANT_ATTRIBUTES = {MULTIPLIER: 1, DIVISOR: 100}
+
+
+class TuyaZBElectricalMeasurement(CustomCluster, ElectricalMeasurement):
+    """Divides the Current for tuya."""
+
+    AC_VOLTAGE_MULTIPLIER = 0x0600
+    AC_VOLTAGE_DIVISOR = 0x0601
+    _CONSTANT_ATTRIBUTES = {AC_VOLTAGE_MULTIPLIER: 1, AC_VOLTAGE_DIVISOR: 1000}
+
+
+class Plug(CustomDevice):
+    """Tuya plug with restore power state support."""
+
+    signature = {
+        MODEL: "TS011F",
+        ENDPOINTS: {
+            # <SimpleDescriptor endpoint=1 profile=266 device_type=81
+            # device_version=1
+            # input_clusters=[0, 3, 4, 5, 6, 9, 1794, 2820, 57344, 57345]
+            # output_clusters=[10, 25]>
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    Metering.cluster_id,
+                    ElectricalMeasurement.cluster_id,
+                    TuyaClusterE000.cluster_id,
+                    TuyaClusterE001.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            # <SimpleDescriptor endpoint=242 profile=41440 device_type=97
+            # device_version=1
+            # input_clusters=[]
+            # output_clusters=[33]>
+            242: {
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 97,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    TuyaZBOnOffRestorePowerCluster,
+                    TuyaZBMetering,
+                    TuyaZBElectricalMeasurement,
+                    TuyaClusterE000,
+                    TuyaClusterE001,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+        },
+    }
