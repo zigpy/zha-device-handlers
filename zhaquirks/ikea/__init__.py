1ea9918ace0fc4b4b4a41fdef195990db763679c
MattWestb <49618193+MattWestb@users.noreply.github.com>
2021-10-23 15:53:20 +0200

    Re coded

--- zhaquirks/ikea/__init__.py
+++ zhaquirks/ikea/__init__.py
@@ -3,8 +3,9 @@ import logging
 
 from zigpy.quirks import CustomCluster
 import zigpy.types as t
-from zigpy.zcl.clusters.general import PowerConfiguration, Scenes
+from zigpy.zcl.clusters.general import Scenes
 from zigpy.zcl.clusters.lightlink import LightLink
+from zhaquirks import DoublingPowerConfigurationCluster
 
 _LOGGER = logging.getLogger(__name__)
 IKEA = "IKEA of Sweden"
@@ -48,103 +49,59 @@ class ScenesCluster(CustomCluster, Scenes):
     }
 
 
-class PowerConfiguration2AAACluster(CustomCluster, PowerConfiguration):
-    """PowerConfiguration cluster implementation.
+class PowerConfiguration2AAACluster(DoublingPowerConfigurationCluster):
+    """Updating Power attributes 2 AAA."""
 
-    This implementation doubles battery pct remaining for non standard devices
-    that don't follow the reporting spec and fixing 2 AAA.
-    """
-
-    cluster_id = PowerConfiguration.cluster_id
-    BATTERY_PERCENTAGE_REMAINING = 0x0021
     BATTERY_SIZES = 0x0031
     BATTERY_QUANTITY = 0x0033
     BATTERY_RATED_VOLTAGE = 0x0034
 
     _CONSTANT_ATTRIBUTES = {
         BATTERY_SIZES: 4,
+        BATTERY_QUANTITY: 2,
         BATTERY_RATED_VOLTAGE: 15,
-        BATTERY_QUANTITY: 2,
     }
 
-    def _update_attribute(self, attrid, value):
-        if attrid == self.BATTERY_PERCENTAGE_REMAINING:
-            value = value * 2
-        super()._update_attribute(attrid, value)
 
+class PowerConfiguration2CRCluster(DoublingPowerConfigurationCluster):
+    """Updating Power attributes 2 CR2032."""
 
-class PowerConfiguration2CRCluster(CustomCluster, PowerConfiguration):
-    """PowerConfiguration cluster implementation.
-
-    This implementation doubles battery pct remaining for non standard devices
-    that don't follow the reporting spec and fixing 2 CR.
-    """
-
-    cluster_id = PowerConfiguration.cluster_id
-    BATTERY_PERCENTAGE_REMAINING = 0x0021
     BATTERY_SIZES = 0x0031
     BATTERY_QUANTITY = 0x0033
     BATTERY_RATED_VOLTAGE = 0x0034
 
     _CONSTANT_ATTRIBUTES = {
         BATTERY_SIZES: 10,
-        BATTERY_RATED_VOLTAGE: 30,
         BATTERY_QUANTITY: 2,
+        BATTERY_RATED_VOLTAGE: 30,
     }
 
-    def _update_attribute(self, attrid, value):
-        if attrid == self.BATTERY_PERCENTAGE_REMAINING:
-            value = value * 2
-        super()._update_attribute(attrid, value)
 
+class PowerConfiguration1CRCluster(DoublingPowerConfigurationCluster):
+    """Updating Power attributes 1 CR2032."""
 
-class PowerConfiguration1CRCluster(CustomCluster, PowerConfiguration):
-    """PowerConfiguration cluster implementation.
-
-    This implementation doubles battery pct remaining for non standard devices
-    that don't follow the reporting spec and fixing 1 CR.
-    """
-
-    cluster_id = PowerConfiguration.cluster_id
-    BATTERY_PERCENTAGE_REMAINING = 0x0021
     BATTERY_SIZES = 0x0031
     BATTERY_QUANTITY = 0x0033
     BATTERY_RATED_VOLTAGE = 0x0034
 
     _CONSTANT_ATTRIBUTES = {
         BATTERY_SIZES: 10,
-        BATTERY_RATED_VOLTAGE: 30,
         BATTERY_QUANTITY: 1,
+        BATTERY_RATED_VOLTAGE: 30,
     }
 
-    def _update_attribute(self, attrid, value):
-        if attrid == self.BATTERY_PERCENTAGE_REMAINING:
-            value = value * 2
-        super()._update_attribute(attrid, value)
 
+class PowerConfiguration1CRXCluster(DoublingPowerConfigurationCluster):
+    """Updating Power attributes 1 CR2032 and Zero voltage."""
 
-class PowerConfiguration1CRXCluster(CustomCluster, PowerConfiguration):
-    """PowerConfiguration cluster implementation.
-
-    This implementation doubles battery pct remaining for non standard devices
-    that don't follow the reporting spec and fixing 1 CR and BV.
-    """
-
-    cluster_id = PowerConfiguration.cluster_id
     BATTERY_VOLTAGE = 0x0020
-    BATTERY_PERCENTAGE_REMAINING = 0x0021
     BATTERY_SIZES = 0x0031
     BATTERY_QUANTITY = 0x0033
     BATTERY_RATED_VOLTAGE = 0x0034
 
     _CONSTANT_ATTRIBUTES = {
-        BATTERY_SIZES: 10,
         BATTERY_VOLTAGE: 0,
-        BATTERY_RATED_VOLTAGE: 30,
+        BATTERY_SIZES: 10,
         BATTERY_QUANTITY: 1,
+        BATTERY_RATED_VOLTAGE: 30,
     }
-
-    def _update_attribute(self, attrid, value):
-        if attrid == self.BATTERY_PERCENTAGE_REMAINING:
-            value = value * 2
-        super()._update_attribute(attrid, value)
