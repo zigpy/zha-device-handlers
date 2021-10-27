3af7d9776b90f275b068bb91e00e8e0633bef1ef
MattWestb <49618193+MattWestb@users.noreply.github.com>
2021-10-27 11:41:39 +0200

    Adding back light and switch mode attribute

--- zhaquirks/tuya/__init__.py
+++ zhaquirks/tuya/__init__.py
@@ -734,7 +734,23 @@ class TuyaSmartRemoteOnOffCluster(OnOff, EventableCluster):
             )
 
 
-class TZBPowerOnState(t.enum8):
+# Tuya Zigbee OnOff Cluster Attribute Implementation
+class SwitchBackLight(t.enum8):
+    """Tuya switch back light mode enum."""
+
+    Mode_0 = 0x00
+    Mode_1 = 0x01
+    Mode_2 = 0x02
+
+
+class SwitchMode(t.enum8):
+    """Tuya switch mode enum."""
+
+    Command = 0x00
+    Event = 0x01
+
+
+class PowerOnState(t.enum8):
     """Tuya power on state enum."""
 
     Off = 0x00
@@ -742,13 +758,16 @@ class TZBPowerOnState(t.enum8):
     LastState = 0x02
 
 
-class TuyaZBOnOffRestorePowerCluster(CustomCluster, OnOff):
-    """Tuya on off Zigbee cluster with restore state."""
+class TuyaZBOnOffAttributeCluster(CustomCluster, OnOff):
+    """Tuya Zigbee On Off cluster with extra attributes."""
 
     attributes = OnOff.attributes.copy()
-    attributes.update({0x8002: ("power_on_state", TZBPowerOnState)})
+    attributes.update({0x8001: ("backlight_mode", SwitchBackLight)})
+    attributes.update({0x8002: ("power_on_state", PowerOnState)})
+    attributes.update({0x8004: ("switch_mode", SwitchMode)})
 
 
+# Tuya Zigbee Metering Cluster Correction Implementation
 class TuyaZBMeteringCluster(CustomCluster, Metering):
     """Divides the kWh for tuya."""
 
