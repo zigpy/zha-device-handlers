2b1d52a8a3e1468f590eb4a95763b77941575ac4
Joao Silva <3317453+joaoasilva@users.noreply.github.com>
2021-11-08 14:38:20 +0000

    Adding _TZE200_yenbr4om cover to the inverted position list (#1133)
    Adding _TZE200_yenbr4om cover to the inverted position list to accurately report the position of the cover

--- zhaquirks/tuya/__init__.py
+++ zhaquirks/tuya/__init__.py
@@ -111,6 +111,7 @@ TUYA_COVER_INVERTED_BY_DEFAULT = [
     "_TZE200_nogaemzt",
     "_TZE200_xuzcvlku",
     "_TZE200_xaabybja",
+    "_TZE200_yenbr4om",
 ]
 
 # ---------------------------------------------------------
