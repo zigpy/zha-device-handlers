4a7fc5b18b4174eb3affe119def94e85cd556a12
MattWestb <49618193+MattWestb@users.noreply.github.com>
2021-11-11 13:28:31 +0100

    Update ts0121_plug.py

--- zhaquirks/tuya/ts0121_plug.py
+++ zhaquirks/tuya/ts0121_plug.py
@@ -16,7 +16,7 @@ from zhaquirks.const import (
 from zhaquirks.tuya import (
     TuyaZBElectricalMeasurement,
     TuyaZBMeteringCluster,
-    TuyaZBOnOffRestorePowerCluster,
+    TuyaZBOnOffAttributeCluster,
 )
 
 
@@ -54,7 +54,7 @@ class Plug(CustomDevice):
                     Basic.cluster_id,
                     Groups.cluster_id,
                     Scenes.cluster_id,
-                    TuyaZBOnOffRestorePowerCluster,
+                    TuyaZBOnOffAttributeCluster,
                     TuyaZBMeteringCluster,
                     TuyaZBElectricalMeasurement,
                 ],
