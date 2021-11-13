593dc28bc01102d2d3675f64857ee9b0c89db181
MattWestb <49618193+MattWestb@users.noreply.github.com>
2021-11-13 14:44:30 +0100

    Update ts011f_plug.py

--- zhaquirks/tuya/ts011f_plug.py
+++ zhaquirks/tuya/ts011f_plug.py
@@ -1,7 +1,7 @@
 """TS011F plug."""
 
 from zigpy.profiles import zha
-from zigpy.quirks import CustomCluster, CustomDevice
+from zigpy.quirks import CustomDevice
 from zigpy.zcl.clusters.general import (
     Basic,
     GreenPowerProxy,
