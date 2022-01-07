0d5085b129782a2e99e6e42b5a790d79b4433833
gromgsxr <super_gromit@hotmail.com>
2022-01-07 23:17:58 +0000

    Update TS0501A.py

--- zhaquirks/lidl/TS0501A.py
+++ zhaquirks/lidl/TS0501A.py
@@ -1,6 +1,6 @@
 """Lidl dimmable bulb."""
 from zigpy.profiles import zha
-from zigpy.quirks import CustomCluster, CustomDevice
+from zigpy.quirks import CustomDevice
 from zigpy.zcl.clusters.general import (
     Basic,
     GreenPowerProxy,
