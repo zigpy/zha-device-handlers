92cfa26f11dc26cb0dbc5b325fccf7074b26543f
tube0013 <wpoland@gmail.com>
2022-01-18 21:44:55 -0500

    add attribute names

--- zhaquirks/linkind/__init__.py
+++ zhaquirks/linkind/__init__.py
@@ -4,7 +4,17 @@ import zigpy.types as t
 from zigpy.zcl.clusters.general import Basic
 
 
+class LinkindLeakMode(t.enum8):
+    """Linkind Leak Sensor mode enum."""
+
+    Siren_Off_LED_Off = 0x00
+    Siren_Off_LED_On = 0x01
+    Siren_On_LED_Off = 0x02
+    Siren_On_LED_On = 0x03
+
+
 class LinkindBasicCluster(CustomCluster, Basic):
     """Linkind Basic cluster."""
 
-    manufacturer_attributes = {0x0400A: ("linkind", t.uint8_t)}
+    attributes = Basic.attributes.copy()
+    attributes.update({0x0400A: ("siren_LED_mode", LinkindLeakMode)})
