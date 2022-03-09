cff1380fcb942afa698b5999d96d540fa4c9c77f
Jonny <jonny.langefeld@gmail.com>
2022-03-08 18:33:03 -0800

    Add device support for TS110E
    This device uses a non-default set level command.
    Tested in local docker environment.
    Thanks to @javicalle this was possible.
    Fix #1415

---
+++ zhaquirks/tuya/ts110e.py
@@ -0,0 +1,142 @@
+"""Tuya Dimmer TS110E."""
+from zigpy.profiles import zha
+import zigpy.types as t
+from zigpy.zcl.clusters.general import (
+    Basic,
+    GreenPowerProxy,
+    Groups,
+    LevelControl,
+    OnOff,
+    Ota,
+    Scenes,
+    Time,
+)
+
+from zigpy.zcl import foundation
+from typing import Optional, Union
+
+from zhaquirks.const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+
+from zhaquirks.tuya import (
+    TuyaDimmerSwitch,
+    TuyaZBExternalSwitchTypeCluster,
+)
+
+class TuyaLevelPayload(t.Struct):
+    """Tuya Level payload."""
+
+    level: t.uint16_t
+    transtime: t.uint16_t
+
+TUYA_LEVEL_ATTRIBUTE = 0xF000
+TUYA_CUSTOM_LEVEL_COMMAND = 0x00F0
+
+class F000LevelControlCluster(LevelControl):
+    """LevelControlCluster that reports to attrid 0xF000."""
+
+    manufacturer_attributes = {TUYA_LEVEL_ATTRIBUTE: ("manufacturer_current_level", t.uint16_t),} 
+
+    manufacturer_server_commands = {
+        TUYA_CUSTOM_LEVEL_COMMAND: ("moveToLevelTuya", (TuyaLevelPayload,), False),
+    }
+
+    # 0xF000 reported values are 0-1000, convert to 0-255
+    def _update_attribute(self, attrid, value):
+        if attrid == TUYA_LEVEL_ATTRIBUTE:
+            value = (value * 255) // 1000
+            attrid = 0x0000
+
+        super()._update_attribute(attrid, value)
+
+    async def command(
+        self,
+        command_id: Union[foundation.Command, int, t.uint8_t],
+        *args,
+        manufacturer: Optional[Union[int, t.uint16_t]] = None,
+        expect_reply: bool = True,
+        tsn: Optional[Union[int, t.uint8_t]] = None,
+    ):
+        """Override the default Cluster command."""
+        self.debug(
+            "Sending Cluster Command. Cluster Command is %x, Arguments are %s",
+            command_id,
+            args,
+        )
+        # move_to_level, move, move_to_level_with_on_off
+        if command_id in (0x0000, 0x0001, 0x0004):
+            # convert dim values to 0-1000
+            brightness = (args[0] * 1000) // 255
+            return await super().command(
+                TUYA_CUSTOM_LEVEL_COMMAND, 
+                TuyaLevelPayload(level=brightness, transtime=0), 
+                manufacturer=manufacturer, 
+                expect_reply=expect_reply,
+                tsn=tsn
+            )
+
+        return super().command(command_id, *args, manufacturer, expect_reply, tsn)
+
+
+class DimmerSwitchWithNeutral1Gang(TuyaDimmerSwitch):
+    """Tuya Dimmer Switch Module With Neutral 1 Gang"""
+
+    signature = {
+        MODELS_INFO: [("_TZ3210_ngqk6jia", "TS110E")],
+        ENDPOINTS: {
+            # <SimpleDescriptor endpoint=1 profile=260 device_type=257
+            # input_clusters=[0, 4, 5, 6, 8, 57345]
+            # output_clusters=[10, 25]>
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.DIMMABLE_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    LevelControl.cluster_id,
+                    TuyaZBExternalSwitchTypeCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            242: {
+                # <SimpleDescriptor endpoint=242 profile=41440 device_type=97
+                # input_clusters=[]
+                # output_clusters=[33]
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 97,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.DIMMABLE_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    F000LevelControlCluster,
+                    TuyaZBExternalSwitchTypeCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            242: {
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 97,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
