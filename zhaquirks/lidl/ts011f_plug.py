bf58f6d3b76b959d8774a7425a3e0442be0b6a07
mdeweerd <mdeweerd@users.noreply.github.com>
2022-03-12 12:30:52 +0100

    Add rejoin & refactor tuya magic spell implementation

--- zhaquirks/lidl/ts011f_plug.py
+++ zhaquirks/lidl/ts011f_plug.py
@@ -25,6 +25,39 @@ from zhaquirks.const import (
 from zhaquirks.tuya.ts011f_plug import Plug_3AC_4USB
 
 
+async def tuya_magic_spell_wand(dev, tries=100) -> None:
+    """Initialize device so that all endpoints become available."""
+    import inspect
+
+    basic_cluster = dev.endpoints[1].in_clusters[0]
+
+    # The magic spell is needed only once.
+    # TODO: Improve by doing this only once (successfully).
+
+    # Magic spell - part 1
+    attr_to_read = [4, 0, 1, 5, 7, 0xFFFE]
+    if "tries" in inspect.getfullargspec(basic_cluster.read_attributes)[0]:
+        await basic_cluster.read_attributes(attr_to_read, tries=tries)
+    else:
+        await basic_cluster.read_attributes(attr_to_read)
+
+    # Magic spell - part 2 (skipped - does not seem to be needed)
+    # attr_to_write={0xffde:13}
+    # basic_cluster.write_attributes(attr_to_write)
+
+    # Leave with rejoin - may need to be adjuste to work everywhere
+    # or require a minimum zigpy version
+    # This should have the device leave and rejoin immediately triggering
+    # the discovery of the endpoints that appear after the magic trick
+    await dev.zdo.request(0x0034, dev.ieee, 0x01, tries)
+
+
+def tuya_magic_spell(dev, tries=100) -> None:
+    """Set up the magic spell asynchrously."""
+
+    dev._magic_spell_task = asyncio.create_task(tuya_magic_spell_wand(dev, tries=tries))
+
+
 class Lidl_Plug_3AC_4USB(Plug_3AC_4USB):
     """LIDL 3 outlet + 4 USB with restore power state support."""
 
@@ -32,7 +65,12 @@ class Lidl_Plug_3AC_4USB(Plug_3AC_4USB):
         """Initialize with task."""
         super().__init__(*args, **kwargs)
 
-        self._init_plug_task = asyncio.create_task(self.spell())
+        # Use internal version, "validated", commented to try next method
+        # self._init_plug_task = asyncio.create_task(self.spell())
+
+        # Use 'external' version that could be called from cluster
+        # customiation
+        tuya_magic_spell(self, tries=3)
 
     async def spell(self) -> None:
         """Initialize device so that all endpoints become available."""
