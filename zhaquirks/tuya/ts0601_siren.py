3fbdcdeb2776e435e929eb2d51f14468b280658b
javicalle <javicalle@gmail.com>
2022-03-13 19:19:37 +0100

    + NeoAlarmVolume

--- zhaquirks/tuya/ts0601_siren.py
+++ zhaquirks/tuya/ts0601_siren.py
@@ -41,7 +41,7 @@ TUYA_BATTERY_ATTR = 0x020F  # [0, 0, 0, 100] battery percentage
 TUYA_ALARM_ATTR = 0x0168  # [0]/[1] Alarm!
 TUYA_TEMP_ALARM_ATTR = 0x0171  # [0]/[1] Disable/Enable alarm by temperature
 TUYA_HUMID_ALARM_ATTR = 0x0172  # [0]/[1] Disable/Enable alarm by humidity
-TUYA_ALARM_DURATION_ATTR = 0x0267  # [0,0,0,10] duration alarm in second
+TUYA_ALARM_DURATION_ATTR = 0x0267  # [0,0,0,10] duration alarm in second (seconds 0-1800)
 TUYA_TEMPERATURE_ATTR = 0x0269  # [0,0,0,240] temperature in decidegree
 TUYA_HUMIDITY_ATTR = 0x026A  # [0,0,0,36] humidity
 TUYA_ALARM_MIN_TEMP_ATTR = 0x026B  # [0,0,0,18] min alarm temperature threshold
@@ -49,9 +49,17 @@ TUYA_ALARM_MAX_TEMP_ATTR = 0x026C  # [0,0,0,18] max alarm temperature threshold
 TUYA_ALARM_MIN_HUMID_ATTR = 0x026D  # [0,0,0,18] min alarm humidity threshold
 TUYA_ALARM_MAX_HUMID_ATTR = 0x026E  # [0,0,0,18] max alarm humidity threshold
 TUYA_MELODY_ATTR = 0x0466  # [5] Melody
-TUYA_VOLUME_ATTR = 0x0474  # [0]/[1]/[2] Volume 0-max, 2-low
+TUYA_VOLUME_ATTR = 0x0474  # [0]/[1]/[2] Volume 0-low, 2-high
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class NeoAlarmVolume(t.enum8):
+    """Neo alarm volume enum."""
+
+    low = 0x00
+    medium = 0x01
+    high = 0x02
 
 
 class TuyaManufClusterSiren(TuyaManufClusterAttributes):
@@ -231,7 +239,7 @@ class TuyaMCUSiren(OnOff, TuyaAttributesCluster):
         TUYA_ALARM_ATTR: ("alarm", t.uint8_t),
         TUYA_ALARM_DURATION_ATTR: ("alarm_duration", t.uint32_t),
         TUYA_MELODY_ATTR: ("melody", t.uint8_t),
-        TUYA_VOLUME_ATTR: ("volume", t.uint8_t),
+        TUYA_VOLUME_ATTR: ("volume", NeoAlarmVolume),
     }
 
     async def write_attributes(self, attributes, manufacturer=None):
@@ -248,11 +256,6 @@ class TuyaMCUSiren(OnOff, TuyaAttributesCluster):
         tsn: Optional[Union[int, t.uint8_t]] = None,
     ):
         """Override the default Cluster command."""
-        self.debug(
-            "Sending Tuya Cluster Command. Cluster Command is %x, Arguments are %s",
-            command_id,
-            args,
-        )
 
         # (off, on)
         if command_id in (0x0000, 0x0001):
@@ -281,6 +284,7 @@ class NeoSirenManufCluster(TuyaMCUCluster):
             TuyaMCUSiren.ep_attribute,
             "volume",
             dp_type=TuyaDPType.ENUM,
+            converter=lambda x: NeoAlarmVolume(x),
         ),
         7: DPToAttributeMapping(
             TuyaMCUSiren.ep_attribute,
