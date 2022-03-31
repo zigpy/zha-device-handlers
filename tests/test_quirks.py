88a45e9ea895d4b921a096651480b322271683c7
David Mulcahey <david.mulcahey@me.com>
2022-03-31 10:00:20 -0400

    add test

--- tests/test_quirks.py
+++ tests/test_quirks.py
@@ -1,5 +1,7 @@
 """General quirk tests."""
+from __future__ import annotations
 
+from pathlib import Path
 from unittest import mock
 
 import pytest
@@ -7,11 +9,13 @@ import zigpy.device
 import zigpy.endpoint
 import zigpy.profiles
 import zigpy.quirks as zq
+from zigpy.quirks import CustomDevice
 import zigpy.types
 import zigpy.zcl as zcl
 import zigpy.zdo.types
 
 import zhaquirks
+import zhaquirks.bosch.motion
 from zhaquirks.const import (
     DEVICE_TYPE,
     ENDPOINTS,
@@ -62,7 +66,7 @@ SIGNATURE_REPLACEMENT_ALLOWED = {
 
 
 @pytest.mark.parametrize("quirk", ALL_QUIRK_CLASSES)
-def test_quirk_replacements(quirk):
+def test_quirk_replacements(quirk: CustomDevice) -> None:
     """Test all quirks have a replacement."""
 
     assert quirk.signature
@@ -72,7 +76,7 @@ def test_quirk_replacements(quirk):
 
 
 @pytest.fixture
-def raw_device():
+def raw_device() -> zigpy.device.Device:
     """Raw device."""
     app = mock.MagicMock()
     ieee = zigpy.types.EUI64.convert("11:22:33:44:55:66:77:88")
@@ -80,7 +84,7 @@ def raw_device():
     return zigpy.device.Device(app, ieee, nwk)
 
 
-def test_dev_from_signature_incomplete_sig(raw_device):
+def test_dev_from_signature_incomplete_sig(raw_device: zigpy.device.Device) -> None:
     """Test device initialization from quirk's based on incomplete signature."""
 
     class BadSigNoSignature(zhaquirks.QuickInitDevice):
@@ -224,7 +228,9 @@ def test_dev_from_signature_incomplete_sig(raw_device):
         },
     ),
 )
-def test_dev_from_signature(raw_device, quirk_signature):
+def test_dev_from_signature(
+    raw_device: zigpy.device.Device, quirk_signature: dict
+) -> None:
     """Test device initialization from quirk's based on signature."""
 
     class QuirkDevice(zhaquirks.QuickInitDevice):
@@ -255,7 +261,7 @@ def test_dev_from_signature(raw_device, quirk_signature):
 @pytest.mark.parametrize(
     "quirk", (q for q in ALL_QUIRK_CLASSES if issubclass(q, zhaquirks.QuickInitDevice))
 )
-def test_quirk_quickinit(quirk):
+def test_quirk_quickinit(quirk: zigpy.quirks.CustomDevice) -> None:
     """Make sure signature in QuickInit Devices have all required attributes."""
 
     if not issubclass(quirk, zhaquirks.QuickInitDevice):
@@ -273,10 +279,10 @@ def test_quirk_quickinit(quirk):
 
 
 @pytest.mark.parametrize("quirk", ALL_QUIRK_CLASSES)
-def test_signature(quirk):
+def test_signature(quirk: CustomDevice) -> None:
     """Make sure signature look sane for all custom devices."""
 
-    def _check_range(cluster):
+    def _check_range(cluster: zcl.Cluster) -> bool:
         for range in zcl.Cluster._registry_range.keys():
             if range[0] <= cluster <= range[1]:
                 return True
@@ -360,7 +366,7 @@ def test_signature(quirk):
 
 
 @pytest.mark.parametrize("quirk", ALL_QUIRK_CLASSES)
-def test_quirk_importable(quirk):
+def test_quirk_importable(quirk: CustomDevice) -> None:
     """Ensure all quirks can be imported with a normal Python `import` statement."""
 
     path = f"{quirk.__module__}.{quirk.__name__}"
@@ -369,7 +375,7 @@ def test_quirk_importable(quirk):
     ), f"{path} is not importable"
 
 
-def test_quirk_loading_error(tmp_path):
+def test_quirk_loading_error(tmp_path: Path) -> None:
     """Ensure quirks do not silently fail to load."""
 
     custom_quirks = tmp_path / "custom_zha_quirks"
@@ -394,7 +400,9 @@ def test_quirk_loading_error(tmp_path):
         zhaquirks.setup({zhaquirks.CUSTOM_QUIRKS_PATH: str(custom_quirks)})
 
 
-def test_custom_quirk_loading(zigpy_device_from_quirk, tmp_path):
+def test_custom_quirk_loading(
+    zigpy_device_from_quirk: CustomDevice, tmp_path: Path
+) -> None:
     """Make sure custom quirks take priority over regular quirks."""
 
     device = zigpy_device_from_quirk(
@@ -486,3 +494,17 @@ class TestReplacementISWZPR1WP13(CustomDevice):
 
     assert not isinstance(zq.get_device(device), zhaquirks.bosch.motion.ISWZPR1WP13)
     assert type(zq.get_device(device)).__name__ == "TestReplacementISWZPR1WP13"
+
+
+def test_zigpy_custom_cluster_pollution() -> None:
+    """Ensure all quirks subclass `CustomCluster`."""
+    non_zigpy_clusters = {
+        cluster
+        for cluster in zcl.Cluster._registry.values()
+        if not cluster.__module__.startswith("zigpy.")
+    }
+
+    if non_zigpy_clusters:
+        raise RuntimeError(
+            f"Custom clusters must subclass `CustomCluster`: {non_zigpy_clusters}"
+        )
