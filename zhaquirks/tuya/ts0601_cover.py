c15ed8ac58d353afe5743d13db63923b373ac5a9
Miguel Diaz Gonçalves <39262857+mdiazgoncalves@users.noreply.github.com>
2022-04-26 14:40:13 +0100

    Add support for Moes Cover Switch _TZE200_nhyj64w2 (#1518)
    * Add support for Moes Cover Switch _TZE200_nhyj64w2
    
    Add support for Tuya Moes Cover Switch TS0601 MODELS_INFO  _TZE200_nhyj64w2
    
    * Update __init__.py

--- zhaquirks/tuya/ts0601_cover.py
+++ zhaquirks/tuya/ts0601_cover.py
@@ -188,6 +188,7 @@ class TuyaMoesCover0601(TuyaWindowCover):
             ("_TZE200_68nvbio9", "TS0601"),
             ("_TZE200_zuz7f94z", "TS0601"),
             ("_TZE200_ergbiejo", "TS0601"),
+            ("_TZE200_nhyj64w2", "TS0601"),
         ],
         ENDPOINTS: {
             1: {
