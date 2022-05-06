0840a29d4b5b700501c788c477d719ffb2385563
David F. Mulcahey <david.mulcahey@me.com>
2022-05-06 15:56:01 -0400

    initial work for configurable detection interval

--- zhaquirks/xiaomi/aqara/motion_ac02.py
+++ zhaquirks/xiaomi/aqara/motion_ac02.py
@@ -1,4 +1,6 @@
 """Quirk for LUMI lumi.motion.ac02."""
+import logging
+from typing import Any
 
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
@@ -22,6 +24,8 @@ from zhaquirks.xiaomi import (
     XiaomiPowerConfiguration,
 )
 
+_LOGGER = logging.getLogger(__name__)
+
 
 class OppleCluster(XiaomiAqaraE1Cluster):
     """Opple cluster."""
@@ -32,12 +36,23 @@ class OppleCluster(XiaomiAqaraE1Cluster):
         0x010C: ("motion_sensitivity", types.uint8_t, True),
     }
 
-    def _update_attribute(self, attrid, value):
+    def _update_attribute(self, attrid: int, value: Any) -> None:
         super()._update_attribute(attrid, value)
         if attrid == 274:
             value = value - 65536
             self.endpoint.illuminance.illuminance_reported(value)
             self.endpoint.occupancy.update_attribute(0, 1)
+
+    async def write_attributes(
+        self, attributes: dict[str | int, Any], manufacturer: int | None = None
+    ) -> list:
+        """Write attributes to device with internal 'attributes' validation."""
+        result = await super().write_attributes(attributes, manufacturer)
+        interval = attributes.get("detection_interval", attributes.get(0x0102))
+        _LOGGER.warning("interval: %s", interval)
+        if interval is not None:
+            self.endpoint.ias_zone.reset_s = int(interval)
+        return result
 
 
 class LumiMotionAC02(CustomDevice):
