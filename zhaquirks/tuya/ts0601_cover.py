18dce91e53f736ac3b521422ec5013dcb3ca8277
datebo <104939422+datebo@users.noreply.github.com>
2022-05-07 09:22:40 +0200

    Revert "Fixing issues with _TZE200_3i3exuay"
    This reverts commit 2fa04816dab25af89ce2483c20b5b4571cefe6a0.

--- zhaquirks/tuya/ts0601_cover.py
+++ zhaquirks/tuya/ts0601_cover.py
@@ -121,9 +121,9 @@ class TuyaZemismartSmartCover0601_2(TuyaWindowCover):
         # "node_descriptor": "<NodeDescriptor byte1=1 byte2=64 mac_capability_flags=142 manufacturer_code=4098
         #                       maximum_buffer_size=82 maximum_incoming_transfer_size=82 server_mask=11264
         #                       maximum_outgoing_transfer_size=82 descriptor_capability_field=0>",
-        # input_clusters=[0x0000, 0x0004, 0x0005, 0xef00]
-        # output_clusters=[0x000a, 0x0019]
-        # <SimpleDescriptor endpoint=1 profile=260 device_type=81 input_clusters=[0, 4, 5, 61184] output_clusters=[10, 25]>
+        # input_clusters=[0x0000, 0x000a, 0x0004, 0x0005, 0xef00]
+        # output_clusters=[0x0019]
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=81 input_clusters=[0, 10, 4, 5, 61184] output_clusters=[25]>
         MODELS_INFO: [
             ("_TZE200_3i3exuay", "TS0601"),
         ],
@@ -133,11 +133,12 @@ class TuyaZemismartSmartCover0601_2(TuyaWindowCover):
                 DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
                 INPUT_CLUSTERS: [
                     Basic.cluster_id,
+                    Time.cluster_id,
                     Groups.cluster_id,
                     Scenes.cluster_id,
                     TuyaManufCluster.cluster_id,
                 ],
-                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
             },
         },
     }
@@ -150,10 +151,11 @@ class TuyaZemismartSmartCover0601_2(TuyaWindowCover):
                     Basic.cluster_id,
                     Groups.cluster_id,
                     Scenes.cluster_id,
+                    Time.cluster_id,
                     TuyaManufacturerWindowCover,
                     TuyaWindowCoverControl,
                 ],
-                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
             },
         },
     }
