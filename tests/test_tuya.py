f929f514b08099ff7d9f4334f0747709c74d6c2f
Simon Baudart <simon@contact-me.be>
2022-05-12 11:44:48 +0200

    Add unit tests

--- tests/test_tuya.py
+++ tests/test_tuya.py
@@ -26,6 +26,7 @@ from zhaquirks.tuya import Data, TuyaManufClusterAttributes
 import zhaquirks.tuya.ts0042
 import zhaquirks.tuya.ts0043
 import zhaquirks.tuya.ts0601_electric_heating
+import zhaquirks.tuya.ts0601_garden
 import zhaquirks.tuya.ts0601_motion
 import zhaquirks.tuya.ts0601_siren
 import zhaquirks.tuya.ts0601_trv
@@ -1385,3 +1386,30 @@ async def test_tuya_wildcard_manufacturer(zigpy_device_from_quirk, quirk, manufa
 
     quirked_dev = get_device(zigpy_dev)
     assert isinstance(quirked_dev, quirk)
+
+
+def test_ts0601_garden_signature(assert_signature_matches_quirk):
+    """Test TS0601 garden watering remote signature is matched to its quirk."""
+    signature = {
+        "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.EndDevice: 2>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.AllocateAddress: 128>, manufacturer_code=4098, maximum_buffer_size=82, maximum_incoming_transfer_size=82, server_mask=11264, maximum_outgoing_transfer_size=82, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=True, *is_full_function_device=False, *is_mains_powered=False, *is_receiver_on_when_idle=False, *is_router=False, *is_security_capable=False)",
+        "endpoints": {
+            "1": {
+                "profile_id": 260,
+                "device_type": "0x0051",
+                "in_clusters": [
+                    "0x0000",
+                    "0x0004",
+                    "0x0005",
+                    "0xef00"
+                ],
+                "out_clusters": [
+                    "0x000a",
+                    "0x0019"
+                ]
+            }
+        },
+        "manufacturer": "_TZE200_81isopgh",
+        "model": "TS0601",
+        "class": "ts0601_garden.TuyaGardenWatering"
+    }
+    assert_signature_matches_quirk(zhaquirks.tuya.ts0601_garden.TuyaGardenWatering, signature)
