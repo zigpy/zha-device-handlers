b8fb247319898b3a7ff4ec63d2226d0f975a1870
rforro <kiko39@protonmail.com>
2022-05-21 01:34:17 -0500

    code cleanup

--- zhaquirks/lidl/ts0601_water_valve.py
+++ zhaquirks/lidl/ts0601_water_valve.py
@@ -39,69 +39,6 @@ from zhaquirks.tuya.mcu import (
 _LOGGER = logging.getLogger(__name__)
 
 
-async def cast_tuya_magic_spell_task(
-        dev: zigpy.device.Device, tries: int = 100, rejoin: bool = False
-) -> None:
-    """Initialize device so that all endpoints become available."""
-    import inspect
-
-    basic_cluster = dev.endpoints[1].in_clusters[0]
-
-    # The magic spell is needed only once.
-    # TODO: Improve by doing this only once (successfully).
-
-    # Magic spell - part 1
-    # Note: attribute order is important
-    attr_to_read = [
-        "manufacturer",
-        "zcl_version",
-        "app_version",
-        "model",
-        "power_source",
-        0xFFFE,
-    ]
-    if "tries" in inspect.getfullargspec(basic_cluster.read_attributes)[0]:
-        _LOGGER.debug(f"Cast Tuya Magic Spell on {dev.ieee!r} with {tries} tries")
-        res = await basic_cluster.read_attributes(attr_to_read, tries=tries)
-    else:
-        _LOGGER.debug(f"Cast Tuya Magic Spell on {dev.ieee!r}")
-        res = await basic_cluster.read_attributes(attr_to_read)
-
-    _LOGGER.debug(f"Tuya Magic Spell result {res!r} for {dev.ieee!r}")
-
-    # # Magic spell - part 2 (skipped - does not seem to be needed)
-    # attr_to_write={0xffde:13}
-    # basic_cluster.write_attributes(attr_to_write)
-
-    if rejoin:
-        # Leave with rejoin - may need to be adjuste to work everywhere
-        # or require a minimum zigpy version
-        # This should have the device leave and rejoin immediately triggering
-        # the discovery of the endpoints that appear after the magic trick
-
-        # Note: this is not validated yet and disabled by default
-        _LOGGER.debug(f"Send leave with rejoin request to {dev.ieee!r}")
-        res = await dev.zdo.request(0x0034, dev.ieee, 0x01, tries)
-        _LOGGER.debug(f"Leave with rejoin result {res!r} for {dev.ieee!r}")
-
-        app = dev.application
-        # Delete the device from the database
-        app.listener_event("device_removed", dev)
-
-        # Delete the device from zigpy
-        app.devices.pop(dev.ieee, None)
-
-
-def cast_tuya_magic_spell(dev: zigpy.device.Device, tries: int = 3) -> None:
-    """Set up the magic spell asynchronously."""
-
-    # Note for sleepy devices the number of tries may need to be increased to 100.
-
-    dev._magic_spell_task = asyncio.create_task(
-        cast_tuya_magic_spell_task(dev, tries=tries)
-    )
-
-
 class TuyaWaterValveManufCluster(TuyaMCUCluster):
     attributes = TuyaMCUCluster.attributes.copy()
     attributes.update(
@@ -166,6 +103,7 @@ class TuyaWaterValveManufCluster(TuyaMCUCluster):
         await self.write_attributes({self.attributes_by_name["frost_lock_reset"].id: 0})
         return result
 
+
 class ParksidePSBZS(CustomDevice):
     """LIDL Parkside water without implemented scheduler."""
 
@@ -180,7 +118,7 @@ class ParksidePSBZS(CustomDevice):
         """Initialize device so that all endpoints become available."""
         basic_cluster = self.endpoints[1].in_clusters[0]
 
-        _LOGGER.warn("Device class will cast Tuya Magic Spell")
+        _LOGGER.debug("Device class will cast Tuya Magic Spell")
         attr_to_read = [4, 0, 1, 5, 7, 0xFFFE]
         await basic_cluster.read_attributes(attr_to_read)
 
