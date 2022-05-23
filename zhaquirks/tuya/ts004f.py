zigpy/zha-device-handlers

commit bdb3ef274ef3537ec6ff41140050b4011160877d

Remove all unnecessary parameters from `MOVE_` and `STEP_` triggers (#1573)

zhaquirks/tuya/ts004f.py
@@ -139,31 +139,31 @@ class TuyaSmartRemote004FROK(CustomDevice):
             COMMAND: COMMAND_MOVE_SATURATION,
             ENDPOINT_ID: 1,
             CLUSTER_ID: 768,
-            PARAMS: {"move_mode": 1, "rate": 200},
+            PARAMS: {"move_mode": 1},
         },
         (ROTATED_SLOW, RIGHT): {
             COMMAND: COMMAND_STEP,
             ENDPOINT_ID: 1,
             CLUSTER_ID: 8,
-            PARAMS: {"step_mode": 0, "step_size": 13, "transition_time": 1},
+            PARAMS: {"step_mode": 0},
         },
         (ROTATED_SLOW, LEFT): {
             COMMAND: COMMAND_STEP,
             ENDPOINT_ID: 1,
             CLUSTER_ID: 8,
-            PARAMS: {"step_mode": 1, "step_size": 13, "transition_time": 1},
+            PARAMS: {"step_mode": 1},
         },
         (ROTATED_FAST, RIGHT): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 37, "transition_time": 2},
+            PARAMS: {"step_mode": 0},
         },
         (ROTATED_FAST, LEFT): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 37, "transition_time": 2},
+            PARAMS: {"step_mode": 1},
         },
         (SHORT_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: SHORT_PRESS},
         (LONG_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: LONG_PRESS},
@@ -286,25 +286,25 @@ class TuyaSmartRemote004FDMS(CustomDevice):
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 51, "transition_time": 10},
+            PARAMS: {"step_mode": 0},
         },
         (LONG_PRESS, DIM_UP): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 0, "rate": 51},
+            PARAMS: {"move_mode": 0},
         },
         (SHORT_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 51, "transition_time": 10},
+            PARAMS: {"step_mode": 1},
         },
         (LONG_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 1, "rate": 51},
+            PARAMS: {"move_mode": 1},
         },
         (LONG_RELEASE, DIM_DOWN): {
             COMMAND: COMMAND_STOP,
@@ -392,25 +392,25 @@ class TuyaSmartRemote004F(CustomDevice):
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 51, "transition_time": 10},
+            PARAMS: {"step_mode": 0},
         },
         (LONG_PRESS, DIM_UP): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 0, "rate": 51},
+            PARAMS: {"move_mode": 0},
         },
         (SHORT_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 51, "transition_time": 10},
+            PARAMS: {"step_mode": 1},
         },
         (LONG_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 1, "rate": 51},
+            PARAMS: {"move_mode": 1},
         },
         (LONG_RELEASE, DIM_DOWN): {
             COMMAND: COMMAND_STOP,

zhaquirks/adeo/color_controller.py
@@ -189,13 +189,13 @@ class AdeoColorController(CustomDevice):
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,  # LevelControl.cluster_id
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 26, "transition_time": 5},
+            PARAMS: {"step_mode": 0},
         },
         (SHORT_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,  # LevelControl.cluster_id
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 26, "transition_time": 5},
+            PARAMS: {"step_mode": 1},
         },
         (SHORT_PRESS, COLOR_UP): {
             COMMAND: COMMAND_STEP_COLOR_TEMP,
@@ -225,25 +225,25 @@ class AdeoColorController(CustomDevice):
             COMMAND: COMMAND_STEP_SATURATION,
             CLUSTER_ID: 768,  # Color.cluster_id
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 26, "transition_time": 5},
+            PARAMS: {"step_mode": 1},
         },
         (SHORT_PRESS, SATURATION_DOWN): {
             COMMAND: COMMAND_STEP_SATURATION,
             CLUSTER_ID: 768,  # Color.cluster_id
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 3, "step_size": 26, "transition_time": 5},
+            PARAMS: {"step_mode": 3},
         },
         (SHORT_PRESS, HUE_LEFT): {
             COMMAND: COMMAND_STEP_HUE,
             CLUSTER_ID: 768,  # Color.cluster_id
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 3, "step_size": 22, "transition_time": 5},
+            PARAMS: {"step_mode": 3},
         },
         (SHORT_PRESS, HUE_RIGHT): {
             COMMAND: COMMAND_STEP_HUE,
             CLUSTER_ID: 768,  # Color.cluster_id
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 22, "transition_time": 5},
+            PARAMS: {"step_mode": 1},
         },
         (SHORT_PRESS, BUTTON_1): {
             COMMAND: "view",

zhaquirks/aduro/adurolightncc.py
@@ -91,12 +91,12 @@ class AdurolightNCC(CustomDevice):
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 16, "transition_time": 9},
+            PARAMS: {"step_mode": 0},
         },
         (SHORT_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 16, "transition_time": 9},
+            PARAMS: {"step_mode": 1},
         },
     }

zhaquirks/aurora/aurora_dimmer.py
@@ -178,73 +178,49 @@ class AuroraDimmerBatteryPowered(CustomDevice):
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 26, "transition_time": 3},
+            PARAMS: {"step_mode": 0},
         },
         (DIM_DOWN, RIGHT): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 26, "transition_time": 3},
+            PARAMS: {"step_mode": 1},
         },
         (COLOR_UP, RIGHT): {
             COMMAND: COMMAND_STEP_COLOR_TEMP,
             CLUSTER_ID: 768,
             ENDPOINT_ID: 1,
-            PARAMS: {
-                "step_mode": 3,
-                "step_size": 47,
-                "transition_time": 3,
-                "color_temp_min_mireds": 153,
-                "color_temp_max_mireds": 454,
-            },
+            PARAMS: {"step_mode": 3},
         },
         (COLOR_DOWN, RIGHT): {
             COMMAND: COMMAND_STEP_COLOR_TEMP,
             CLUSTER_ID: 768,
             ENDPOINT_ID: 1,
-            PARAMS: {
-                "step_mode": 1,
-                "step_size": 47,
-                "transition_time": 3,
-                "color_temp_min_mireds": 153,
-                "color_temp_max_mireds": 454,
-            },
+            PARAMS: {"step_mode": 1},
         },
         (DIM_UP, LEFT): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 2,
-            PARAMS: {"step_mode": 0, "step_size": 26, "transition_time": 3},
+            PARAMS: {"step_mode": 0},
         },
         (DIM_DOWN, LEFT): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 2,
-            PARAMS: {"step_mode": 1, "step_size": 26, "transition_time": 3},
+            PARAMS: {"step_mode": 1},
         },
         (COLOR_UP, LEFT): {
             COMMAND: COMMAND_STEP_COLOR_TEMP,
             CLUSTER_ID: 768,
             ENDPOINT_ID: 1,
-            PARAMS: {
-                "step_mode": 3,
-                "step_size": 47,
-                "transition_time": 3,
-                "color_temp_min_mireds": 153,
-                "color_temp_max_mireds": 454,
-            },
+            PARAMS: {"step_mode": 3},
         },
         (COLOR_DOWN, LEFT): {
             COMMAND: COMMAND_STEP_COLOR_TEMP,
             CLUSTER_ID: 768,
             ENDPOINT_ID: 1,
-            PARAMS: {
-                "step_mode": 1,
-                "step_size": 47,
-                "transition_time": 3,
-                "color_temp_min_mireds": 153,
-                "color_temp_max_mireds": 454,
-            },
+            PARAMS: {"step_mode": 1},
         },
         (SHORT_PRESS, RIGHT): {
             CLUSTER_ID: 6,

zhaquirks/ikea/dimmer.py
@@ -94,12 +94,12 @@ class IkeaDimmer(CustomDevice):
             COMMAND: COMMAND_MOVE_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 0, "rate": 195},
+            PARAMS: {"move_mode": 0},
         },
         (ROTATED, LEFT): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 1, "rate": 195},
+            PARAMS: {"move_mode": 1},
         },
     }

zhaquirks/ikea/fivebtnremote.py
@@ -123,25 +123,25 @@ class IkeaTradfriRemote(CustomDevice):
             COMMAND: COMMAND_STEP_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 43, "transition_time": 5},
+            PARAMS: {"step_mode": 0},
         },
         (LONG_PRESS, DIM_UP): {
             COMMAND: COMMAND_MOVE_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 0, "rate": 83},
+            PARAMS: {"move_mode": 0},
         },
         (SHORT_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 43, "transition_time": 5},
+            PARAMS: {"step_mode": 1},
         },
         (LONG_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 1, "rate": 83},
+            PARAMS: {"move_mode": 1},
         },
         (SHORT_PRESS, LEFT): {
             COMMAND: COMMAND_PRESS,

zhaquirks/ikea/fivebtnremotezha.py
@@ -127,25 +127,25 @@ class IkeaTradfriRemote1(CustomDevice):
             COMMAND: COMMAND_STEP_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 43, "transition_time": 5},
+            PARAMS: {"step_mode": 0},
         },
         (LONG_PRESS, DIM_UP): {
             COMMAND: COMMAND_MOVE_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 0, "rate": 83},
+            PARAMS: {"move_mode": 0},
         },
         (SHORT_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1, "step_size": 43, "transition_time": 5},
+            PARAMS: {"step_mode": 1},
         },
         (LONG_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 1, "rate": 83},
+            PARAMS: {"move_mode": 1},
         },
         (SHORT_PRESS, LEFT): {
             COMMAND: COMMAND_PRESS,
@@ -325,32 +325,25 @@ class IkeaTradfriRemote3(IkeaTradfriRemote1):
             COMMAND: COMMAND_STEP_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0, "step_size": 43, "transition_time": 5},
+            PARAMS: {"step_mode": 0},
         },
         (LONG_PRESS, DIM_UP): {
             COMMAND: COMMAND_MOVE_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 0, "rate": 84},
+            PARAMS: {"move_mode": 0},
         },
         (SHORT_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {
-                "step_mode": 1,
-                "step_size": 43,
-                "transition_time": 5,
-            },
+            PARAMS: {"step_mode": 1},
         },
         (LONG_PRESS, DIM_DOWN): {
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {
-                "move_mode": 1,
-                "rate": 84,
-            },
+            PARAMS: {"move_mode": 1},
         },
         (SHORT_PRESS, LEFT): {
             COMMAND: COMMAND_PRESS,

zhaquirks/ikea/fourbtnremote.py
@@ -108,7 +108,7 @@ class IkeaTradfriRemote(CustomDevice):
             COMMAND: COMMAND_MOVE_ON_OFF,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 0, "rate": 83},
+            PARAMS: {"move_mode": 0},
         },
         (LONG_RELEASE, DIM_UP): {
             COMMAND: COMMAND_STOP_ON_OFF,
@@ -120,7 +120,7 @@ class IkeaTradfriRemote(CustomDevice):
             COMMAND: COMMAND_MOVE,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"move_mode": 1, "rate": 83},
+            PARAMS: {"move_mode": 1},
         },
         (LONG_RELEASE, DIM_DOWN): {
             COMMAND: COMMAND_STOP_ON_OFF,

commit b95837eb20fc99c8d765a8b075fa196b2c3a5902

Fix TS004F quirk broken by #1573

zhaquirks/tuya/ts004f.py
@@ -145,25 +145,25 @@ class TuyaSmartRemote004FROK(CustomDevice):
             COMMAND: COMMAND_STEP,
             ENDPOINT_ID: 1,
             CLUSTER_ID: 8,
-            PARAMS: {"step_mode": 0},
+            PARAMS: {"step_mode": 0, "step_size": 13},
         },
         (ROTATED_SLOW, LEFT): {
             COMMAND: COMMAND_STEP,
             ENDPOINT_ID: 1,
             CLUSTER_ID: 8,
-            PARAMS: {"step_mode": 1},
+            PARAMS: {"step_mode": 1, "step_size": 13},
         },
         (ROTATED_FAST, RIGHT): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 0},
+            PARAMS: {"step_mode": 0, "step_size": 37},
         },
         (ROTATED_FAST, LEFT): {
             COMMAND: COMMAND_STEP,
             CLUSTER_ID: 8,
             ENDPOINT_ID: 1,
-            PARAMS: {"step_mode": 1},
+            PARAMS: {"step_mode": 1, "step_size": 37},
         },
         (SHORT_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: SHORT_PRESS},
         (LONG_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: LONG_PRESS},