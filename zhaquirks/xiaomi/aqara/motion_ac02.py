b92c9fbc1b56746ba6fd4b5f2d0f6bd385c2d7e8
David F. Mulcahey <david.mulcahey@me.com>
2022-05-23 10:37:35 -0400

    remove magic numbers

--- zhaquirks/xiaomi/aqara/motion_ac02.py
+++ zhaquirks/xiaomi/aqara/motion_ac02.py
@@ -1,4 +1,6 @@
 """Quirk for LUMI lumi.motion.ac02."""
+from __future__ import annotations
+
 import logging
 from typing import Any
 
@@ -24,6 +26,11 @@ from zhaquirks.xiaomi import (
     XiaomiPowerConfiguration,
 )
 
+OCCUPANCY = 0
+ON = 1
+MOTION_ATTRIBUTE = 274
+DETECTION_INTERVAL = 0x0102
+MOTION_SENSITIVITY = 0x010C
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -32,24 +39,26 @@ class OppleCluster(XiaomiAqaraE1Cluster):
 
     ep_attribute = "opple_cluster"
     attributes = {
-        0x0102: ("detection_interval", types.uint8_t, True),
-        0x010C: ("motion_sensitivity", types.uint8_t, True),
+        DETECTION_INTERVAL: ("detection_interval", types.uint8_t, True),
+        MOTION_SENSITIVITY: ("motion_sensitivity", types.uint8_t, True),
     }
 
     def _update_attribute(self, attrid: int, value: Any) -> None:
         super()._update_attribute(attrid, value)
-        if attrid == 274:
+        if attrid == MOTION_ATTRIBUTE:
             value = value - 65536
             self.endpoint.illuminance.illuminance_reported(value)
-            self.endpoint.occupancy.update_attribute(0, 1)
+            self.endpoint.occupancy.update_attribute(OCCUPANCY, ON)
 
     async def write_attributes(
         self, attributes: dict[str | int, Any], manufacturer: int | None = None
     ) -> list:
         """Write attributes to device with internal 'attributes' validation."""
         result = await super().write_attributes(attributes, manufacturer)
-        interval = attributes.get("detection_interval", attributes.get(0x0102))
-        _LOGGER.warning("interval: %s", interval)
+        interval = attributes.get(
+            "detection_interval", attributes.get(DETECTION_INTERVAL)
+        )
+        _LOGGER.debug("interval: %s", interval)
         if interval is not None:
             self.endpoint.ias_zone.reset_s = int(interval)
         return result
@@ -61,6 +70,7 @@ class LumiMotionAC02(CustomDevice):
     def __init__(self, *args, **kwargs):
         """Init."""
         self.battery_size = 11
+        self.battery_quantity = 2
         self.battery_bus = Bus()
         self.illuminance_bus = Bus()
         self.motion_bus = Bus()
