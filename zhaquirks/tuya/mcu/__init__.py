ea906611b47b7a6a286987b1c92d4d95e3f9daa0
rforro <kiko39@protonmail.com>
2022-05-25 13:27:58 +0200

    Add Device Support: Parkside water valve _TZE200_htnnfasr (#1574)
    * init parkside valve
    
    * moved power cluster to tuya mcu class
    
    * typing fix
    
    * added automatical frost reset after joining
    
    * modified tests
    
    * code cleanup
    
    * fixes after runnig tests
    
    * fixes after running tests
    
    * test was failing
    
    * typo
    
    * moved to tuya folder, created enchanted dev class
    
    * typo
    
    * isort

--- zhaquirks/tuya/mcu/__init__.py
+++ zhaquirks/tuya/mcu/__init__.py
@@ -1,12 +1,14 @@
 """Tuya MCU comunications."""
+import asyncio
 import dataclasses
 from typing import Any, Callable, Dict, Optional, Tuple, Union
 
+from zigpy.quirks import CustomDevice
 import zigpy.types as t
 from zigpy.zcl import foundation
 from zigpy.zcl.clusters.general import LevelControl, OnOff
 
-from zhaquirks import Bus
+from zhaquirks import Bus, DoublingPowerConfigurationCluster
 from zhaquirks.tuya import (
     ATTR_ON_OFF,
     TUYA_MCU_COMMAND,
@@ -86,6 +88,12 @@ class MoesBacklight(t.enum8):
     light_when_on = 0x01
     light_when_off = 0x02
     freeze = 0x03
+
+
+class TuyaPowerConfigurationCluster(
+    TuyaLocalCluster, DoublingPowerConfigurationCluster
+):
+    """PowerConfiguration cluster for battery-operated tuya devices reporting percentage."""
 
 
 class TuyaAttributesCluster(TuyaLocalCluster):
@@ -568,3 +576,18 @@ class TuyaLevelControlManufCluster(TuyaMCUCluster):
         17: "_dp_2_attr_update",
         18: "_dp_2_attr_update",
     }
+
+
+class EnchantedDevice(CustomDevice):
+    """Class for enchanted Tuya devices which needs to be unlocked by casting a 'spell'."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize with task."""
+        super().__init__(*args, **kwargs)
+        self._init_device_task = asyncio.create_task(self.spell())
+
+    async def spell(self) -> None:
+        """Initialize device so that all endpoints become available."""
+        attr_to_read = [4, 0, 1, 5, 7, 0xFFFE]
+        basic_cluster = self.endpoints[1].in_clusters[0]
+        await basic_cluster.read_attributes(attr_to_read)
--- zhaquirks/tuya/ts0601_valve.py
+++ zhaquirks/tuya/ts0601_valve.py
@@ -1,11 +1,10 @@
-"""Tuya Valve."""
-
+"""Collection of Tuya Valve devices e.g. water valves, gas valve etc."""
 from typing import Dict
 
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
 import zigpy.types as t
-from zigpy.zcl.clusters.general import Basic, Groups, Ota, Scenes, Time
+from zigpy.zcl.clusters.general import Basic, Groups, Identify, OnOff, Ota, Scenes, Time
 from zigpy.zcl.clusters.smartenergy import Metering
 
 from zhaquirks import DoublingPowerConfigurationCluster
@@ -20,9 +19,11 @@ from zhaquirks.const import (
 from zhaquirks.tuya import TuyaLocalCluster
 from zhaquirks.tuya.mcu import (
     DPToAttributeMapping,
+    EnchantedDevice,
     TuyaDPType,
     TuyaMCUCluster,
     TuyaOnOff,
+    TuyaPowerConfigurationCluster,
 )
 
 
@@ -135,4 +136,118 @@ class TuyaValve(CustomDevice):
                 OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
             }
         }
+    }
+
+
+class ParksideTuyaValveManufCluster(TuyaMCUCluster):
+    """Manufacturer Specific Cluster for the _TZE200_htnnfasr water valve sold as PARKSIDE."""
+
+    attributes = TuyaMCUCluster.attributes.copy()
+    attributes.update(
+        {
+            0xEF11: ("timer_duration", t.uint32_t, True),
+            0xEF12: ("timer_time_left", t.uint32_t, True),
+            0xEF13: ("frost_lock", t.Bool, True),
+            0xEF14: ("frost_lock_reset", t.Bool, True),  # 0 resets frost lock
+        }
+    )
+
+    dp_to_attribute: Dict[int, DPToAttributeMapping] = {
+        1: DPToAttributeMapping(
+            TuyaOnOff.ep_attribute,
+            "on_off",
+            TuyaDPType.BOOL,
+        ),
+        5: DPToAttributeMapping(
+            TuyaMCUCluster.ep_attribute,
+            "timer_duration",
+            TuyaDPType.VALUE,
+        ),
+        6: DPToAttributeMapping(
+            TuyaMCUCluster.ep_attribute,
+            "timer_time_left",
+            TuyaDPType.VALUE,
+        ),
+        11: DPToAttributeMapping(
+            TuyaPowerConfigurationCluster.ep_attribute,
+            "battery_percentage_remaining",
+            TuyaDPType.VALUE,
+        ),
+        108: DPToAttributeMapping(
+            TuyaMCUCluster.ep_attribute,
+            "frost_lock",
+            TuyaDPType.BOOL,
+            lambda x: not x,  # invert for lock entity
+        ),
+        109: DPToAttributeMapping(
+            TuyaMCUCluster.ep_attribute,
+            "frost_lock_reset",
+            TuyaDPType.BOOL,
+        ),
+    }
+
+    data_point_handlers = {
+        1: "_dp_2_attr_update",
+        5: "_dp_2_attr_update",
+        6: "_dp_2_attr_update",
+        11: "_dp_2_attr_update",
+        108: "_dp_2_attr_update",
+        109: "_dp_2_attr_update",
+    }
+
+    async def bind(self):
+        """
+        Bind cluster.
+
+        When adding this device tuya gateway issues factory reset,
+        we just need to reset the frost lock, because its state is unknown to us.
+        """
+        result = await super().bind()
+        await self.write_attributes({self.attributes_by_name["frost_lock_reset"].id: 0})
+        return result
+
+
+class ParksidePSBZS(EnchantedDevice):
+    """LIDL Parkside water without implemented scheduler."""
+
+    signature = {
+        MODELS_INFO: [("_TZE200_htnnfasr", "TS0601")],  # HG06875
+        ENDPOINTS: {
+            # <SimpleDescriptor endpoint=1 profile=260 device_type=0
+            # device_version=1
+            # input_clusters=[0, 1, 3, 4, 5, 6, 61184]
+            # output_clusters=[10, 25]>
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    ParksideTuyaValveManufCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    TuyaOnOff,
+                    TuyaPowerConfigurationCluster,
+                    ParksideTuyaValveManufCluster,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+        },
     }
