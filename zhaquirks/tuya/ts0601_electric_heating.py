7d1fc4962c095f9e5c77b4e91106007934a22e75
SentrySOB <91045882+SentrySOB@users.noreply.github.com>
2022-06-14 23:34:12 +1000

    Update ts0601_electric_heating.py
    Add ("_TZE200_2ekuz3dz", "TS0601"), to MOESBHT

--- zhaquirks/tuya/ts0601_electric_heating.py
+++ zhaquirks/tuya/ts0601_electric_heating.py
@@ -282,6 +282,7 @@ class MoesBHT(TuyaThermostat):
         #  output_clusters=[10, 25]
         MODELS_INFO: [
             ("_TZE200_aoclfnxz", "TS0601"),
+            ("_TZE200_2ekuz3dz", "TS0601"),
         ],
         ENDPOINTS: {
             1: {
