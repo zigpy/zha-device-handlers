6d61bbb607e7111b2c7ad2988465f1b340b8084b
TheJulianJES <TheJulianJES@users.noreply.github.com>
2022-07-11 15:46:21 +0200

    Add alternative Xiaomi MMEU01 plug signatures

--- zhaquirks/xiaomi/aqara/plug_mmeu01.py
+++ zhaquirks/xiaomi/aqara/plug_mmeu01.py
@@ -194,3 +194,99 @@ class PlugMAEU01(PlugMMEU01):
             },
         },
     }
+
+
+class PlugMAEU01Alt1(PlugMAEU01):
+    """lumi.plug.maeu01 plug with alternative signature."""
+
+    signature = {
+        MODELS_INFO: PlugMAEU01.signature[MODELS_INFO],
+        ENDPOINTS: {
+            # <SimpleDescriptor endpoint=1 profile=260 device_type=81
+            # device_version=1
+            # input_clusters=[0, 2, 3, 4, 5, 6, 64704]
+            # output_clusters=[10, 25]>
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    DeviceTemperature.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    OppleCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            # <SimpleDescriptor endpoint=21 profile=260 device_type=81
+            # device_version=1
+            # input_clusters=[12]
+            # output_clusters=[]>
+            21: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [AnalogInput.cluster_id],
+            },
+            # <SimpleDescriptor endpoint=31 profile=260 device_type=81
+            # device_version=1
+            # input_clusters=[12]
+            # output_clusters=[]>
+            31: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [AnalogInput.cluster_id],
+            },
+            # <SimpleDescriptor endpoint=242 profile=41440 device_type=97
+            # device_version=0
+            # input_clusters=[]
+            # output_clusters=[33]>
+            242: {
+                PROFILE_ID: XIAOMI_PROFILE_ID,
+                DEVICE_TYPE: XIAOMI_DEVICE_TYPE,
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
+
+    replacement = PlugMAEU01.replacement
+
+
+class PlugMAEU01Alt2(PlugMAEU01):
+    """lumi.plug.maeu01 plug with alternative signature."""
+
+    signature = {
+        MODELS_INFO: PlugMAEU01.signature[MODELS_INFO],
+        ENDPOINTS: {
+            # <SimpleDescriptor endpoint=1 profile=260 device_type=81
+            # device_version=1
+            # input_clusters=[0, 2, 3, 4, 5, 6, 64704]
+            # output_clusters=[10, 25]>
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    DeviceTemperature.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    OppleCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            # <SimpleDescriptor endpoint=242 profile=41440 device_type=97
+            # device_version=0
+            # input_clusters=[]
+            # output_clusters=[33]>
+            242: {
+                PROFILE_ID: XIAOMI_PROFILE_ID,
+                DEVICE_TYPE: XIAOMI_DEVICE_TYPE,
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
+
+    replacement = PlugMAEU01.replacement
