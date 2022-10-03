534c4ecc570726b4d780b0a068ad67341ff7c5ff
puddly <32534428+puddly@users.noreply.github.com>
2022-10-03 12:58:11 -0400

    Fix async unit tests (#1802)
    * Fix existing unit tests
    
    * Add `asyncio_mode = auto` to pytest config
    
    * Fix deprecated use of `load_module`
    
    * Revert "Fix deprecated use of `load_module`"
    
    This reverts commit 8aaa2a53ce97709bfa36fb8e6076afebab1c3830.
    
    * Bump minimum required zigpy version

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     keywords="zha quirks homeassistant hass",
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3",
-    install_requires=["zigpy>=0.45.1"],
+    install_requires=["zigpy>=0.51.1"],
     tests_require=["pytest"],
 )
