01fe943bc4f4939cbbe05bef13cadc85f4b631ff
Adam Honoré <adam@honore.io>
2022-11-08 21:00:15 +0100

    add node and endpoint descriptor comment

--- zhaquirks/tuya/ts0601_keypad.py
+++ zhaquirks/tuya/ts0601_keypad.py
@@ -160,6 +160,23 @@ class WirelessZigbeeKeypad(CustomDevice):
     """MS-K1AZ Wireless Zigbee Keypad."""
 
     signature = {
+        # "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.EndDevice: 2>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.AllocateAddress: 128>, manufacturer_code=4098, maximum_buffer_size=82, maximum_incoming_transfer_size=82, server_mask=11264, maximum_outgoing_transfer_size=82, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=True, *is_full_function_device=False, *is_mains_powered=False, *is_receiver_on_when_idle=False, *is_router=False, *is_security_capable=False)",
+        # "endpoints": {
+        #   "1": {
+        #     "profile_id": 260,
+        #     "device_type": "0x0051",
+        #     "in_clusters": [
+        #       "0x0000",
+        #       "0x0004",
+        #       "0x0005",
+        #       "0xef00"
+        #     ],
+        #     "out_clusters": [
+        #       "0x000a",
+        #       "0x0019"
+        #     ]
+        #   }
+        # }
         MODELS_INFO: [("_TZE200_n9clpsht", "TS0601")],
         ENDPOINTS: {
             1: {
