a5ecf43ea7fb470c9aa6e7ad92a522027370ec1d
Denis Shulyaka <Shulyaka@gmail.com>
2022-11-11 19:06:10 +0300

    make types classes outer

--- zhaquirks/xbee/__init__.py
+++ zhaquirks/xbee/__init__.py
@@ -124,6 +124,133 @@ class Bytes(bytes):
         return cls(data), b""
 
 
+class EUI64(t.EUI64):
+    """EUI64 serializable class."""
+
+    @classmethod
+    def deserialize(cls, data):
+        """Deserialize EUI64."""
+        r, data = super().deserialize(data)
+        return cls(r[::-1]), data
+
+    def serialize(self):
+        """Serialize EUI64."""
+        assert self._length == len(self)
+        return super().serialize()[::-1]
+
+
+class NWK(int):
+    """Network address serializable class."""
+
+    _signed = False
+    _size = 2
+
+    def serialize(self):
+        """Serialize NWK."""
+        return self.to_bytes(self._size, "big", signed=self._signed)
+
+    @classmethod
+    def deserialize(cls, data):
+        """Deserialize NWK."""
+        r = cls(int.from_bytes(data[: cls._size], "big", signed=cls._signed))
+        data = data[cls._size :]
+        return r, data
+
+
+class ATCommand(Bytes):
+    """AT command serializable class."""
+
+    @classmethod
+    def deserialize(cls, data):
+        """Deserialize ATCommand."""
+        return cls(data[:2]), data[2:]
+
+
+class BinaryString(str):
+    """Class to parse and serialize binary data as string."""
+
+    def serialize(self):
+        """Serialize string into bytes."""
+        return bytes(self, encoding="latin1")
+
+    @classmethod
+    def deserialize(cls, data):
+        """Interpret data as string."""
+        data = str(data, encoding="latin1")
+        return (cls(data), b"")
+
+
+class IOSample(bytes):
+    """Parse an XBee IO sample report."""
+
+    # pylint: disable=R0201
+    def serialize(self):
+        """Serialize an IO Sample Report, Not implemented."""
+        _LOGGER.debug("Serialize not implemented.")
+
+    @classmethod
+    def deserialize(cls, data):
+        """Deserialize an xbee IO sample report.
+
+        xbee digital sample format
+        Sample set count byte 0
+        Digital mask byte 1, 2
+        Analog mask byte 3
+        Digital samples byte 4, 5 (if any sample exists)
+        Analog Sample, 2 bytes per
+        """
+        sample_sets = int.from_bytes(data[0:1], byteorder="big")
+        if sample_sets != 1:
+            _LOGGER.warning("Number of sets is not 1")
+        digital_mask = data[1:3]
+        analog_mask = data[3:4]
+        digital_sample = data[4:6]
+        num_bits = 15
+        digital_pins = [
+            (int.from_bytes(digital_mask, byteorder="big") >> bit) & 1
+            for bit in range(num_bits - 1, -1, -1)
+        ]
+        digital_pins = list(reversed(digital_pins))
+        analog_pins = [
+            (int.from_bytes(analog_mask, byteorder="big") >> bit) & 1
+            for bit in range(8 - 1, -1, -1)
+        ]
+        analog_pins = list(reversed(analog_pins))
+        if 1 in digital_pins:
+            digital_samples = [
+                (int.from_bytes(digital_sample, byteorder="big") >> bit) & 1
+                for bit in range(num_bits - 1, -1, -1)
+            ]
+            digital_samples = list(reversed(digital_samples))
+            sample_index = 6
+        else:
+            # skip digital samples block
+            digital_samples = digital_pins
+            sample_index = 4
+        analog_samples = []
+        for apin in analog_pins:
+            if apin == 1:
+                analog_samples.append(
+                    int.from_bytes(
+                        data[sample_index : sample_index + 2], byteorder="big"
+                    )
+                )
+                sample_index += 2
+            else:
+                analog_samples.append(None)
+        for dpin in range(len(digital_pins)):
+            if digital_pins[dpin] == 0:
+                digital_samples[dpin] = None
+
+        return (
+            {
+                "digital_samples": digital_samples,
+                "analog_samples": analog_samples,
+            },
+            data[sample_index:],
+        )
+
+
 # https://github.com/zigpy/zigpy-xbee/blob/dev/zigpy_xbee/api.py
 AT_COMMANDS = {
     # Addressing commands
@@ -266,6 +393,16 @@ ENDPOINT_TO_AT = {
 }
 
 
+class ATCommandResult(enum.IntEnum):
+    """AT command results."""
+
+    OK = 0
+    ERROR = 1
+    INVALID_COMMAND = 2
+    INVALID_PARAMETER = 3
+    TX_FAILURE = 4
+
+
 class XBeeBasic(LocalDataCluster, Basic):
     """XBee Basic Cluster."""
 
@@ -357,37 +494,6 @@ class XBeeRemoteATRequest(LocalDataCluster):
 
     _seq: int = 1
 
-    class EUI64(t.EUI64):
-        """EUI64 serializable class."""
-
-        @classmethod
-        def deserialize(cls, data):
-            """Deserialize EUI64."""
-            r, data = super().deserialize(data)
-            return cls(r[::-1]), data
-
-        def serialize(self):
-            """Serialize EUI64."""
-            assert self._length == len(self)
-            return super().serialize()[::-1]
-
-    class NWK(int):
-        """Network address serializable class."""
-
-        _signed = False
-        _size = 2
-
-        def serialize(self):
-            """Serialize NWK."""
-            return self.to_bytes(self._size, "big", signed=self._signed)
-
-        @classmethod
-        def deserialize(cls, data):
-            """Deserialize NWK."""
-            r = cls(int.from_bytes(data[: cls._size], "big", signed=cls._signed))
-            data = data[cls._size :]
-            return r, data
-
     def _save_at_request(self, frame_id, future):
         self._endpoint.in_clusters[XBEE_AT_RESPONSE_CLUSTER].save_at_request(
             frame_id, future
@@ -431,8 +537,8 @@ class XBeeRemoteATRequest(LocalDataCluster):
             uint8_t,
             uint8_t,
             uint8_t,
-            self.EUI64,
-            self.NWK,
+            EUI64,
+            NWK,
             Bytes,
             Bytes,
         )
@@ -508,23 +614,6 @@ class XBeeRemoteATResponse(LocalDataCluster):
 
     _awaiting = {}
 
-    class ATCommandResult(enum.IntEnum):
-        """AT command results."""
-
-        OK = 0
-        ERROR = 1
-        INVALID_COMMAND = 2
-        INVALID_PARAMETER = 3
-        TX_FAILURE = 4
-
-    class ATCommand(Bytes):
-        """AT command serializable class."""
-
-        @classmethod
-        def deserialize(cls, data):
-            """Deserialize ATCommand."""
-            return cls(data[:2]), data[2:]
-
     def save_at_request(self, frame_id, future):
         """Save pending request."""
         self._awaiting[frame_id] = (future,)
@@ -546,9 +635,9 @@ class XBeeRemoteATResponse(LocalDataCluster):
             )
             (fut,) = self._awaiting.pop(args.frame_id)
             try:
-                status = self.ATCommandResult(args.status)
+                status = ATCommandResult(args.status)
             except ValueError:
-                status = self.ATCommandResult.ERROR
+                status = ATCommandResult.ERROR
 
             if status:
                 fut.set_exception(
@@ -586,76 +675,6 @@ class DigitalIOCluster(LocalDataCluster, BinaryInput):
     """Digital IO Cluster for the XBee."""
 
     cluster_id = XBEE_IO_CLUSTER
-
-    class IOSample(bytes):
-        """Parse an XBee IO sample report."""
-
-        # pylint: disable=R0201
-        def serialize(self):
-            """Serialize an IO Sample Report, Not implemented."""
-            _LOGGER.debug("Serialize not implemented.")
-
-        @classmethod
-        def deserialize(cls, data):
-            """Deserialize an xbee IO sample report.
-
-            xbee digital sample format
-            Sample set count byte 0
-            Digital mask byte 1, 2
-            Analog mask byte 3
-            Digital samples byte 4, 5 (if any sample exists)
-            Analog Sample, 2 bytes per
-            """
-            sample_sets = int.from_bytes(data[0:1], byteorder="big")
-            if sample_sets != 1:
-                _LOGGER.warning("Number of sets is not 1")
-            digital_mask = data[1:3]
-            analog_mask = data[3:4]
-            digital_sample = data[4:6]
-            num_bits = 15
-            digital_pins = [
-                (int.from_bytes(digital_mask, byteorder="big") >> bit) & 1
-                for bit in range(num_bits - 1, -1, -1)
-            ]
-            digital_pins = list(reversed(digital_pins))
-            analog_pins = [
-                (int.from_bytes(analog_mask, byteorder="big") >> bit) & 1
-                for bit in range(8 - 1, -1, -1)
-            ]
-            analog_pins = list(reversed(analog_pins))
-            if 1 in digital_pins:
-                digital_samples = [
-                    (int.from_bytes(digital_sample, byteorder="big") >> bit) & 1
-                    for bit in range(num_bits - 1, -1, -1)
-                ]
-                digital_samples = list(reversed(digital_samples))
-                sample_index = 6
-            else:
-                # skip digital samples block
-                digital_samples = digital_pins
-                sample_index = 4
-            analog_samples = []
-            for apin in analog_pins:
-                if apin == 1:
-                    analog_samples.append(
-                        int.from_bytes(
-                            data[sample_index : sample_index + 2], byteorder="big"
-                        )
-                    )
-                    sample_index += 2
-                else:
-                    analog_samples.append(None)
-            for dpin in range(len(digital_pins)):
-                if digital_pins[dpin] == 0:
-                    digital_samples[dpin] = None
-
-            return (
-                {
-                    "digital_samples": digital_samples,
-                    "analog_samples": analog_samples,
-                },
-                data[sample_index:],
-            )
 
     def handle_cluster_request(
         self,
@@ -734,19 +753,6 @@ class SerialDataCluster(LocalDataCluster):
     cluster_id = XBEE_DATA_CLUSTER
     ep_attribute = "xbee_serial_data"
 
-    class BinaryString(str):
-        """Class to parse and serialize binary data as string."""
-
-        def serialize(self):
-            """Serialize string into bytes."""
-            return bytes(self, encoding="latin1")
-
-        @classmethod
-        def deserialize(cls, data):
-            """Interpret data as string."""
-            data = str(data, encoding="latin1")
-            return (cls(data), b"")
-
     async def command(
         self,
         command_id,
@@ -757,7 +763,7 @@ class SerialDataCluster(LocalDataCluster):
         tsn=None,
     ):
         """Handle outgoing data."""
-        data = self.BinaryString(data).serialize()
+        data = BinaryString(data).serialize()
         return foundation.GENERAL_COMMANDS[
             foundation.GeneralCommand.Default_Response
         ].schema(
