7e529347b356dc37461cc4106141acc0bf8139f3
javicalle <javicalle@gmail.com>
2022-12-04 19:11:37 +0100

    Fix TuyaDPType.VALUE payload

--- tests/test_tuya_mcu.py
+++ tests/test_tuya_mcu.py
@@ -135,7 +135,7 @@ async def test_tuya_methods(zigpy_device_from_quirk, quirk):
     assert len(result_1.datapoints) == 1
     assert result_1.datapoints[0].dp == 9
     assert result_1.datapoints[0].data.dp_type == TuyaDPType.VALUE
-    assert result_1.datapoints[0].data.raw == b"\x00\x00\x00b"
+    assert result_1.datapoints[0].data.raw == b"b\x00\x00\x00"
 
     tcd_2 = TuyaClusterData(
         endpoint_id=7, cluster_attr="not_exists_attribute", attr_value=25
