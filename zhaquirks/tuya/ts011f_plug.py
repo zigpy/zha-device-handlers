900512d8f0e208f5cc98bd5b5109791fdd7b2d7d
Simon Aldrich <simon@aldrich.cc>
2023-01-16 13:16:39 +0000

    Add quirk for extended on/off cluster of TS011F _TYZB01_hlla45kx (#2085)
    * Add quirk for extended on/off cluster of TS011F _TYZB01_hlla45kx
    Sold in UK as Scolmore ClickSmart+ 30036 Double Socket
    
    * Remove TuyaZBOnOffAttributeCluster from Endpoint 2 to prevent duplicate child-lock entities
    also update formatting for Black
    
    * Device only supports the extended Tuya child-lock on/off cluster attribute
    Backlight mode, power-on state etc are not supported.
      * Create a custom on/off cluster with only this attribute (based on TuyaZBOnOffAttributeCluster) and use for Endpoint 1
      * Add standard on/off cluster back to Endpoint 2
      * Run Black formatting
    
    * Restricting Plug_2AC_hlla45kx quirk specifically to the ClickSmart+ 30036 device using MODELS_INFO
    This is to prevent the limited Tuya on/off cluster (with only child-lock attribute) from being applied to other devices
    
    * Support power-restore state attribute and change custome device class name
      * Rename custom cluster to indicate that it is a variant of the Tuya On/Off cluster
      * Change custom device class name to remove reference to specific device
    
    * Change ts011f_plug.Plug_2AC_var03 back to using TuyaZBOnOffAttributeCluster
    
    * Remove unused import

--- zhaquirks/tuya/ts011f_plug.py
+++ zhaquirks/tuya/ts011f_plug.py
@@ -1071,3 +1071,73 @@ class Plug_v2(EnchantedDevice):
             },
         },
     }
+
+
+class Plug_2AC_var03(CustomDevice):
+    """Tuya 2 socket wall outlet with child lock and power-restore state support."""
+
+    signature = {
+        MODEL: "TS011F",
+        ENDPOINTS: {
+            # <SimpleDescriptor endpoint=1 profile=260 device_type=9
+            # device_version=0
+            # input_clusters=[0, 10, 4, 5, 6]
+            # output_clusters=[25]>
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Time.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+            # <SimpleDescriptor endpoint=2 profile=260 device_type=9
+            # device_version=0
+            # input_clusters=[4, 5, 6]
+            # output_clusters=[]>
+            2: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET,
+                INPUT_CLUSTERS: [
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Time.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    # N.B. The ClickSmart CMA30036 (_TYZB01_hlla45kx), supports
+                    #      but does not implement the backlight_mode extended
+                    #      attribute. For this device, changing this attribute
+                    #      has no effect on the switch lights.
+                    TuyaZBOnOffAttributeCluster,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            },
+            2: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET,
+                INPUT_CLUSTERS: [
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [],
+            },
+        },
+    }
