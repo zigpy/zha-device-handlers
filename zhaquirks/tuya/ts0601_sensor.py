c4079bd223b12f8e80bfaa7a21b502266367fb47
dgaust <dgaust@outlook.com>
2023-01-17 07:03:26 +1100

    Merge separate soil file into the general sensor quirks
    Based on comments
    
    https://github.com/zigpy/zha-device-handlers/pull/2102#issuecomment-1384470379

--- zhaquirks/tuya/ts0601_sensor.py
+++ zhaquirks/tuya/ts0601_sensor.py
@@ -207,3 +207,76 @@ class TuyaTempHumiditySensorVar03(CustomDevice):
             }
         },
     }
+  
+class SoilManufCluster(TuyaMCUCluster):
+    """Tuya Manufacturer Cluster with Temperature and Humidity data points."""
+
+    dp_to_attribute: Dict[int, DPToAttributeMapping] = {
+        5: DPToAttributeMapping(
+            TuyaTemperatureMeasurement.ep_attribute,
+            "measured_value",
+            dp_type=TuyaDPType.VALUE,
+            converter=lambda x: x * 100,
+        ),
+        3: DPToAttributeMapping(
+            TuyaRelativeHumidity.ep_attribute,
+            "measured_value",
+            dp_type=TuyaDPType.VALUE,
+        ),
+        15: DPToAttributeMapping(
+            TuyaPowerConfigurationCluster2AAA.ep_attribute,
+            "battery_percentage_remaining",
+            dp_type=TuyaDPType.VALUE,
+            converter=lambda x: x * 2,  # double reported percentage
+        ),
+    }
+
+    data_point_handlers = {
+        3: "_dp_2_attr_update",
+        5: "_dp_2_attr_update",
+        15: "_dp_2_attr_update",
+    }
+
+
+class TuyaSoilSensor(CustomDevice):
+    """Tuya temp and humidity sensor (variation 03)."""
+
+    signature = {
+        # "profile_id": 260,
+        # "device_type": "0x0051",
+        # "in_clusters": ["0x0000","0x0004","0x0005","0xef00"],
+        # "out_clusters": ["0x000a","0x0019"]
+        MODELS_INFO: [("_TZE200_myd45weu", "TS0601")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    TemperatureHumidityManufCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id, Time.cluster_id],
+            }
+        },
+    }
+
+    replacement = {
+        SKIP_CONFIGURATION: True,
+        ENDPOINTS: {
+            1: {
+                DEVICE_TYPE: zha.DeviceType.TEMPERATURE_SENSOR,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    SoilManufCluster,
+                    TuyaTemperatureMeasurement,
+                    TuyaRelativeHumidity,
+                    TuyaPowerConfigurationCluster2AAA,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id, Time.cluster_id],
+            }
+        },
+    }
