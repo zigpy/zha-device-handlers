c08de28054ef07e3483f6b2081469bb0a244fdf9
TheJulianJES <TheJulianJES@users.noreply.github.com>
2023-01-25 13:35:48 +0100

    Remove additional space from comment

--- zhaquirks/universalelectronics/contact_sensor.py
+++ zhaquirks/universalelectronics/contact_sensor.py
@@ -29,7 +29,7 @@ class ContactSensor(CustomDevice):
     """XHS2-UE Door/Window Sensor."""
 
     signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=1026
         # device_version=0
         # input_clusters=[0, 1, 3, 1026, 1280, 32, 2821]
         # output_clusters=[25]>
