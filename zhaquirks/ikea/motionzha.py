444d38febbbbaa83cd17bafa7fc341d58323c58f
MattWestb <49618193+MattWestb@users.noreply.github.com>
2023-01-26 19:50:22 +0100

    E1525 Zigbee 3 update
    ### Now IKEA is all Zigbee 3.
    As expected it have loosing group binding and reporting wrong battery %.
    Delete the device in ZHA and waiting one minute and adding it new and reconfigure it then its OK pared and its start reporting OK and also is doing check ins every 55 minute.

--- zhaquirks/ikea/motionzha.py
+++ zhaquirks/ikea/motionzha.py
@@ -26,6 +26,7 @@ from zhaquirks.const import (
 from zhaquirks.ikea import (
     IKEA,
     IKEA_CLUSTER_ID,
+    WWAH_CLUSTER_ID,
     LightLinkCluster,
     PowerConfiguration2CRCluster,
 )
@@ -141,6 +142,65 @@ class IkeaTradfriMotionE1745(CustomDevice):
                     Groups.cluster_id,
                     OnOff.cluster_id,
                     LevelControl.cluster_id,
+                    Ota.cluster_id,
+                    LightLink.cluster_id,
+                ],
+            }
+        }
+    }
+
+
+class IkeaTradfriMotionE1525(CustomDevice):
+    """Custom device representing IKEA of Sweden TRADFRI motion sensor E1525 Zigbee 3."""
+
+    signature = {
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=2128
+        # device_version=1
+        # input_clusters=[0, 1, 3, 32, 4096, 64636]
+        # output_clusters=[3, 4, 6, 25, 4096]>
+        MODELS_INFO: [(IKEA, "TRADFRI motion sensor")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SENSOR,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    PollControl.cluster_id,
+                    LightLink.cluster_id,
+                    IKEA_CLUSTER_ID,
+                    WWAH_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    OnOff.cluster_id,
+                    Ota.cluster_id,
+                    LightLink.cluster_id,
+                ],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SENSOR,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration2CRCluster,
+                    Identify.cluster_id,
+                    PollControl.cluster_id,
+                    LightLinkCluster,
+                    IKEA_CLUSTER_ID,
+                    WWAH_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    OnOff.cluster_id,
                     Ota.cluster_id,
                     LightLink.cluster_id,
                 ],
