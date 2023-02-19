797c2c14a97ff0bfe9dd928c72bfdb81f13b9cf8
MattWestb <49618193+MattWestb@users.noreply.github.com>
2023-02-19 16:36:00 +0100

    Adding Appartme APRM-04-001 TRV
    https://github.com/Koenkk/zigbee-herdsman-converters/pull/5497/files

--- zhaquirks/tuya/ts0601_trv.py
+++ zhaquirks/tuya/ts0601_trv.py
@@ -1475,6 +1475,7 @@ class SiterwellGS361_Type2(TuyaThermostat):
             ("_TZE200_8daqwrsj", "TS0601"),
             ("_TZE200_czk78ptr", "TS0601"),
             ("_TZE200_2cs6g9i7", "TS0601"),  # Brennenstuhl Zigbee Connect 01
+            ("_TZE200_2cs6g9i7", "TS0601"),  # Appartme APRM-04-001
         ],
         ENDPOINTS: {
             1: {
