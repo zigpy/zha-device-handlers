8e91d98783bd9bfd1c6906eba9f3853a15ac8c89
Emil Haugbergsmyr <emil@raeven.net>
2023-03-24 14:53:01 +0100

    Add Tuya LH-961ZB motion sensor support (#2289)

--- tests/test_tuya.py
+++ tests/test_tuya.py
@@ -23,6 +23,7 @@ from zhaquirks.const import (
     ZONE_STATE,
 )
 from zhaquirks.tuya import Data, TuyaManufClusterAttributes, TuyaNewManufCluster
+import zhaquirks.tuya.sm0202_motion
 import zhaquirks.tuya.ts0042
 import zhaquirks.tuya.ts0043
 import zhaquirks.tuya.ts0501_fan_switch
@@ -1527,3 +1528,22 @@ async def test_fan_switch_writes_attributes(zigpy_device_from_quirk, quirk):
             1,
             b"\x00\x01\x02\x01\x000\x00",
         )
+
+
+async def test_sm0202_motion_sensor_signature(assert_signature_matches_quirk):
+    """Test LH992ZB motion sensor remote signature is matched to its quirk."""
+    signature = {
+        "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.EndDevice: 2>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.AllocateAddress: 128>, manufacturer_code=4098, maximum_buffer_size=82, maximum_incoming_transfer_size=82, server_mask=11264, maximum_outgoing_transfer_size=82, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=True, *is_full_function_device=False, *is_mains_powered=False, *is_receiver_on_when_idle=False, *is_router=False, *is_security_capable=False)",
+        "endpoints": {
+            "1": {
+                "profile_id": 260,
+                "device_type": "0x0402",
+                "in_clusters": ["0x0000", "0x0001", "0x0003", "0x0500", "0xeeff"],
+                "out_clusters": ["0x0019"],
+            }
+        },
+        "manufacturer": "_TYZB01_z2umiwvq",
+        "model": "SM0202",
+        "class": "zhaquirks.tuya.lh992zb.TuyaMotionSM0202",
+    }
+    assert_signature_matches_quirk(zhaquirks.tuya.sm0202_motion.SM0202Motion, signature)
