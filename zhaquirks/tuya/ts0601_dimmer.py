30da2d4aa6a2e017bc48061668c54f4bd8c78bc6
cwhitmorebeirne <108819863+cwhitmorebeirne@users.noreply.github.com>
2023-03-29 22:41:19 +1100

    Update ts0601_dimmer.py
    Add support for Mercator IKUU SSWRM-ZB Rotary Dimmer Mechanism as per Issue #2307

--- zhaquirks/tuya/ts0601_dimmer.py
+++ zhaquirks/tuya/ts0601_dimmer.py
@@ -48,6 +48,7 @@ class TuyaSingleSwitchDimmer(TuyaDimmerSwitch):
             ("_TZE200_la2c2uo9", "TS0601"),
             ("_TZE200_1agwnems", "TS0601"),  # TODO: validation pending?
             ("_TZE200_9cxuhakf", "TS0601"),  # Added for Mercator IKUU SSWM-DIMZ Device
+            ("_TZE200_a0syesf5", "TS0601"),  # Added for Mercator IKUU SSWRM-ZB Rotary Dimmer Mechanism
             ("_TZE200_p0gzbqct", "TS0601"),
             ("_TZE200_w4cryh2i", "TS0601"),
         ],
