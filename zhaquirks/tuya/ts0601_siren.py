369c684220ded08669211abfb18113a1a1d6eea6
José Manuel <zemanel@conceal.network>
2023-03-29 17:31:43 +0200

    Fix _TZE200_t1blo2bj siren toggle switch

--- zhaquirks/tuya/ts0601_siren.py
+++ zhaquirks/tuya/ts0601_siren.py
@@ -147,11 +147,11 @@ class TuyaSirenOnOff(LocalDataCluster, OnOff):
 
         if command_id in (0x0000, 0x0001):
             (res,) = await self.endpoint.tuya_manufacturer.write_attributes(
-                {TUYA_ALARM_ATTR: command_id}, manufacturer=manufacturer
+                {TUYA_ALARM_ATTR: bool(command_id)}, manufacturer=manufacturer
             )
             return foundation.GENERAL_COMMANDS[
                 foundation.GeneralCommand.Default_Response
-            ].schema(command_id=command_id, status=res[0].status)
+            ].schema(command_id=bool(command_id), status=res[0].status)
 
         return foundation.GENERAL_COMMANDS[
             foundation.GeneralCommand.Default_Response
@@ -293,7 +293,7 @@ class TuyaMCUSiren(OnOff, TuyaAttributesCluster):
                 endpoint_id=self.endpoint.endpoint_id,
                 cluster_name=self.ep_attribute,
                 cluster_attr="on_off",
-                attr_value=command_id,
+                attr_value=bool(command_id),
                 expect_reply=expect_reply,
                 manufacturer=foundation.ZCLHeader.NO_MANUFACTURER_ID,
             )
