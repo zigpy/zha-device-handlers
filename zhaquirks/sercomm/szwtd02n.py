f44081a1c7520f12685289436c50000686794ce3
Steven Barkey <sjbarkey@ucdavis.edu>
2023-04-21 09:46:58 -0700

    Added XHS2-SE

--- zhaquirks/sercomm/szwtd02n.py
+++ zhaquirks/sercomm/szwtd02n.py
@@ -1,10 +1,11 @@
-"""Device handler for Sercomm SZ-WTD02N flood sensor."""
+"""Device handler for Sercomm SZ-WTD02N flood sensor and XHS2-SE contact sensor."""
 
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
 from zigpy.zcl.clusters.general import Basic, Identify, Ota, PollControl
 from zigpy.zcl.clusters.homeautomation import Diagnostic
 from zigpy.zcl.clusters.security import IasZone
+from zigpy.zcl.clusters.measurement import TemperatureMeasurement
 
 from zhaquirks import PowerConfigurationCluster
 from zhaquirks.const import (
@@ -66,3 +67,46 @@ class SZWTD02N(CustomDevice):
             }
         }
     }
+
+class XHS2SE(CustomDevice):
+   """Custom device representing XHS2-SE by Sercomm Corp."""
+
+   signature = {
+       #  <SimpleDescriptor endpoint=1 profile=260 device_type=0x0402
+       #  input_clusters=["0x0000", "0x0001", "0x0003","0x0020","0x0402", "0x0500", "0x0b05","0xfc01","0xfc02"]
+       #  output_clusters=["0x0003","0x0019"]>
+       MODELS_INFO: [(SERCOMM, "XHS2-SE")],
+       ENDPOINTS: {
+           1: {
+               PROFILE_ID: zha.PROFILE_ID,
+               DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+               INPUT_CLUSTERS: [
+                   Basic.cluster_id,
+                   SercommPowerConfiguration.cluster_id,
+                   Identify.cluster_id,
+                   PollControl.cluster_id,
+                   TemperatureMeasurement.cluster_id,
+                   IasZone.cluster_id,
+                   Diagnostic.cluster_id,
+               ],
+               OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
+           }
+       },
+   }
+
+   replacement = {
+       ENDPOINTS: {
+           1: {
+               INPUT_CLUSTERS: [
+                   Basic.cluster_id,
+                   SercommPowerConfiguration,
+                   Identify.cluster_id,
+                   PollControl.cluster_id,
+                   TemperatureMeasurement.cluster_id,
+                   IasZone.cluster_id,
+                   Diagnostic.cluster_id,
+               ],
+               OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
+           }
+       }
+   }
