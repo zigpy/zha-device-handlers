55540010734c84360e85eb73ed06798650b7dd0c
Fabien Brachere <fabien.brachere@ariane.network>
2023-04-24 10:30:58 +0200

    Add signature test for TS1201

--- tests/test_tuya.py
+++ tests/test_tuya.py
@@ -52,6 +52,7 @@ import zhaquirks.tuya.ts0601_motion
 import zhaquirks.tuya.ts0601_siren
 import zhaquirks.tuya.ts0601_trv
 import zhaquirks.tuya.ts0601_valve
+import zhaquirks.tuya.ts1201
 
 from tests.common import ClusterListener, MockDatetime, wait_for_zigpy_tasks
 
@@ -1709,3 +1710,31 @@ def test_tuya_spell_devices_valid():
             pytest.fail(
                 f"{quirk} has more than one cluster subclassing `TuyaEnchantableCluster` on endpoint 1"
             )
+
+
+def test_ts1201_signature(assert_signature_matches_quirk):
+    """Test TS1201 remote signature is matched to its quirk."""
+    signature = {
+        "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.EndDevice: 2>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.AllocateAddress: 128>, manufacturer_code=4098, maximum_buffer_size=82, maximum_incoming_transfer_size=82, server_mask=11264, maximum_outgoing_transfer_size=82, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=True, *is_full_function_device=False, *is_mains_powered=False, *is_receiver_on_when_idle=False, *is_router=False, *is_security_capable=False)",
+        "endpoints": {
+            "1": {
+                "profile_id": 260,
+                "device_type": "0xf000",
+                "in_clusters": [
+                    "0x0000",
+                    "0x0001",
+                    "0x0003",
+                    "0x0004",
+                    "0x0005",
+                    "0x0006",
+                    "0xe004",
+                    "0xed00",
+                ],
+                "out_clusters": ["0x000a", "0x0019"],
+            }
+        },
+        "manufacturer": "_TZ3290_ot6ewjvmejq5ekhl",
+        "model": "TS1201",
+        "class": "zhaquirks.tuya.ts1201.ZosungIRBlaster",
+    }
+    assert_signature_matches_quirk(zhaquirks.tuya.ts1201.ZosungIRBlaster, signature)
