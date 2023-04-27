7099a9b9a899f273bf7e63b151eb15f5c5feccfd
TheJulianJES <TheJulianJES@users.noreply.github.com>
2023-04-27 22:34:34 +0200

    Refactor quirks
    This moves the `SercommPowerConfiguration` class to the Sercomm module `__init__.py`.
    It also splits out the contact and flood sensor quirks into separate files with proper names.

--- zhaquirks/sercomm/contact_sensor.py
+++ zhaquirks/sercomm/contact_sensor.py
@@ -1,4 +1,4 @@
-"""Device handler for Sercomm SZ-WTD02N flood sensor and XHS2-SE contact sensor."""
+"""Device handler for Sercomm XHS2-SE contact sensor."""
 
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
@@ -7,7 +7,6 @@ from zigpy.zcl.clusters.homeautomation import Diagnostic
 from zigpy.zcl.clusters.measurement import TemperatureMeasurement
 from zigpy.zcl.clusters.security import IasZone
 
-from zhaquirks import PowerConfigurationCluster
 from zhaquirks.const import (
     DEVICE_TYPE,
     ENDPOINTS,
@@ -16,57 +15,7 @@ from zhaquirks.const import (
     OUTPUT_CLUSTERS,
     PROFILE_ID,
 )
-from zhaquirks.sercomm import SERCOMM
-
-
-class SercommPowerConfiguration(PowerConfigurationCluster):
-    """Sercomm power configuration cluster for flood sensor."""
-
-    MAX_VOLTS = 3.2
-    MIN_VOLTS = 2.1
-
-
-class SZWTD02N(CustomDevice):
-    """Custom device representing Sercomm SZ-WTD02N flood sensor."""
-
-    signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
-        #  device_version=0
-        #  input_clusters=[0, 1, 3, 1280, 32, 2821]
-        #  output_clusters=[3, 25]>
-        MODELS_INFO: [(SERCOMM, "SZ-WTD02N_SF")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    SercommPowerConfiguration.cluster_id,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    IasZone.cluster_id,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    SercommPowerConfiguration,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    IasZone.cluster_id,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
-            }
-        }
-    }
+from zhaquirks.sercomm import SERCOMM, SercommPowerConfiguration
 
 
 class XHS2SE(CustomDevice):
