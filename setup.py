dfc688f048499cebd41fe0daa6403cdd8833c60d
puddly <32534428+puddly@users.noreply.github.com>
2023-07-26 16:40:16 -0400

    Create a `pyproject.toml` file

--- setup.py
+++ setup.py
@@ -1,25 +1,4 @@
-"""Setup module for ZHAQuirks."""
+import setuptools
 
-import pathlib
-
-from setuptools import find_packages, setup
-
-VERSION = "0.0.101"
-
-
-setup(
-    name="zha-quirks",
-    version=VERSION,
-    description="Library implementing Zigpy quirks for ZHA in Home Assistant",
-    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/dmulcahey/zha-device-handlers",
-    author="David F. Mulcahey",
-    author_email="david.mulcahey@icloud.com",
-    license="Apache License Version 2.0",
-    keywords="zha quirks homeassistant hass",
-    packages=find_packages(exclude=["tests"]),
-    python_requires=">=3.8",
-    install_requires=["zigpy>=0.56.0"],
-    tests_require=["pytest"],
-)
+if __name__ == "__main__":
+    setuptools.setup()
