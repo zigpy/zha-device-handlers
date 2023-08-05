5c87bd92f6d48f8b256bec4651dcab5d45fed871
Jan Vermaete <jan.vermaete@gmail.com>
2023-08-05 18:52:08 +0200

    switch_n0agl1.py
    Found this quirck here: https://community.home-assistant.io/t/lumi-switch-n0agl1-not-working-in-zha/439772
    So all credits to those people
    
    And it does work for my lumi.switch.n0acn2 relay from aquara.

---
+++ zhaquirks/xiaomi/aqara/switch_n0agl1.py
@@ -0,0 +1,220 @@
+import copy
+
+from enum import Enum
+
+from zigpy import types as t
+from zigpy.profiles import zha
+from zigpy.quirks import CustomDevice
+from zigpy.zcl.clusters.general import (
+    Basic,
+    DeviceTemperature,
+    Groups,
+    Identify,
+    OnOff,
+    Ota,
+    AnalogInput,
+    Scenes,
+    Alarms,
+    Time,
+    GreenPowerProxy,
+    MultistateInput,
+)
+from zigpy.zcl.clusters.smartenergy import (
+    Metering,
+)
+from zigpy.zcl.clusters.homeautomation import (
+    ElectricalMeasurement,
+)
+
+from zhaquirks.const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+from zhaquirks.xiaomi import (
+    LUMI,
+    XiaomiAqaraE1Cluster,
+    XiaomiCustomDevice,
+    BasicCluster,
+    DeviceTemperatureCluster,
+    XiaomiMeteringCluster,
+    OnOffCluster
+)
+
+class XiaomiAqaraT1Cluster(XiaomiAqaraE1Cluster):
+
+    ep_attribute = "aqara_cluster"
+    cluster_id = XiaomiAqaraE1Cluster.cluster_id
+    attributes = {
+        0x000A: ("switch_type", t.uint8_t, True)
+    }
+
+class SwitchN0AGL1(XiaomiCustomDevice):
+    """lumi.switch.n0agl1 switch"""
+
+    signature = {
+        MODELS_INFO: [(LUMI, "lumi.switch.n0agl1"), (LUMI, "lumi.switch.n0acn2")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    DeviceTemperature.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Alarms.cluster_id,
+                    Time.cluster_id,
+                    0xfcc0
+                ],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id
+                ]
+            },
+            21: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    AnalogInput.cluster_id
+                ],
+                OUTPUT_CLUSTERS: []
+            },
+            31: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    AnalogInput.cluster_id
+                ],
+                OUTPUT_CLUSTERS: []
+            },
+            41: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    MultistateInput.cluster_id
+                ],
+                OUTPUT_CLUSTERS: []
+            },
+            242: {
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 0x0061,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [
+                    GreenPowerProxy.cluster_id
+                ]
+            }
+        }
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+                INPUT_CLUSTERS: [
+                    BasicCluster,
+                    DeviceTemperatureCluster,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOffCluster,
+                    Alarms.cluster_id,
+                    Time.cluster_id,
+                    XiaomiMeteringCluster,
+                    ElectricalMeasurement.cluster_id,
+                    XiaomiAqaraT1Cluster,
+                ],
+                OUTPUT_CLUSTERS: [
+                    OnOffCluster,
+                    Time.cluster_id,
+                    Ota.cluster_id,
+                ]
+            },
+            21: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+            },
+            31: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+            },
+            41: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+            },
+            242: {
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 0x0061,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [
+                    GreenPowerProxy.cluster_id
+                ]
+            }
+        }
+    }
+
+class SwitchN0AGL1Alt1(SwitchN0AGL1):
+    """lumi.switch.n0agl1 switch with alternative signature"""
+
+    signature = {
+        MODELS_INFO: [(LUMI, "lumi.switch.n0agl1"), (LUMI, "lumi.switch.n0acn2")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    DeviceTemperature.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Alarms.cluster_id,
+                    Time.cluster_id,
+                    0xfcc0
+                ],
+                OUTPUT_CLUSTERS: [
+                    Time.cluster_id,
+                    Ota.cluster_id,
+                    0xffff
+                ]
+            },
+            21: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    AnalogInput.cluster_id
+                ],
+                OUTPUT_CLUSTERS: []
+            },
+            31: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    AnalogInput.cluster_id
+                ],
+                OUTPUT_CLUSTERS: []
+            },
+            41: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    MultistateInput.cluster_id
+                ],
+                OUTPUT_CLUSTERS: []
+            },
+            242: {
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 0x0061,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [
+                    GreenPowerProxy.cluster_id
+                ]
+            }
+        }
+    }
+
+    replacement = SwitchN0AGL1.replacement
