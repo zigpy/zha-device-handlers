a0f298cfe57dc81ce57ccd1f21ebb50d04e18e81
Sebastian Muszynski <basti@linkt.de>
2023-09-03 15:58:11 +0200

    Apply black formatting

--- zhaquirks/heiman/smoke.py
+++ zhaquirks/heiman/smoke.py
@@ -2,7 +2,14 @@
 
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Alarms, Basic, Identify, Ota, PollControl, PowerConfiguration
+from zigpy.zcl.clusters.general import (
+    Alarms,
+    Basic,
+    Identify,
+    Ota,
+    PollControl,
+    PowerConfiguration,
+)
 from zigpy.zcl.clusters.homeautomation import Diagnostic
 from zigpy.zcl.clusters.security import IasWd, IasZone
 import zigpy.zdo.types
