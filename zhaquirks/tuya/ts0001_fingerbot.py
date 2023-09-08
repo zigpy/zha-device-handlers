6f4c2b849e3433facd5b04d28fe0eb7db6c16b6b
Abílio Costa <abmantis@users.noreply.github.com>
2023-09-08 23:23:28 +0100

    Add new model for TS0001 Fingerbot: `_TZ3210_j4pdtz9v` (#2567)
    Add new model for TS0001 Fingerbot: `_TZ3210_j4pdtz9v`

--- zhaquirks/tuya/ts0001_fingerbot.py
+++ zhaquirks/tuya/ts0001_fingerbot.py
@@ -118,7 +118,7 @@ class OnOffEnchantable(TuyaEnchantableCluster, OnOff):
 
 class TuyaFingerbot(EnchantedDevice):
     signature = {
-        MODELS_INFO: [("_TZ3210_dse8ogfy", "TS0001")],
+        MODELS_INFO: [("_TZ3210_dse8ogfy", "TS0001"), ("_TZ3210_j4pdtz9v", "TS0001")],
         ENDPOINTS: {
             1: {
                 PROFILE_ID: zha.PROFILE_ID,
