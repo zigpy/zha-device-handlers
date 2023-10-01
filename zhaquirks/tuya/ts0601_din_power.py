0cdfc1fdd60e2a8860bb8bf944f4e09a1c0daf52
Agócs Dániel <agocsdaniel@gmail.com>
2023-10-01 12:07:55 +0200

    Refactored Zemismart sensor and added SPM02

--- zhaquirks/tuya/ts0601_din_power.py
+++ zhaquirks/tuya/ts0601_din_power.py
@@ -40,6 +40,8 @@ HIKING_REACTIVE_POWER_ATTR = 0x026E
 ZEMISMART_TOTAL_ENERGY_ATTR = 0x0201
 ZEMISMART_TOTAL_REVERSE_ENERGY_ATTR = 0x0202
 ZEMISMART_VCP_ATTR = 0x0006
+ZEMISMART_VCP_P2_ATTR = ZEMISMART_VCP_ATTR + 1
+ZEMISMART_VCP_P3_ATTR = ZEMISMART_VCP_ATTR + 2
 
 
 class TuyaManufClusterDinPower(TuyaManufClusterAttributes):
@@ -184,12 +186,14 @@ class HikingManufClusterDinPower(TuyaManufClusterAttributes):
 
 
 class ZemismartManufCluster(TuyaManufClusterAttributes):
-    """Manufacturer Specific Cluster of the Zemismart SPM01 Power Meter device."""
+    """Manufacturer Specific Cluster of the Zemismart SPM series Power Meter devices."""
 
     attributes = {
         ZEMISMART_TOTAL_ENERGY_ATTR: ("energy", t.uint32_t, True),
         ZEMISMART_TOTAL_REVERSE_ENERGY_ATTR: ("reverse_energy", t.uint32_t, True),
         ZEMISMART_VCP_ATTR: ("vcp_raw", t.data64, True),
+        ZEMISMART_VCP_P2_ATTR: ("vcp_p2_raw", t.data64, True),
+        ZEMISMART_VCP_P3_ATTR: ("vcp_p3_raw", t.data64, True),
     }
 
     def _update_attribute(self, attrid, value):
@@ -199,15 +203,11 @@ class ZemismartManufCluster(TuyaManufClusterAttributes):
         elif attrid == ZEMISMART_TOTAL_REVERSE_ENERGY_ATTR:
             self.endpoint.smartenergy_metering.energy_receive_reported(value)
         elif attrid == ZEMISMART_VCP_ATTR:
-            self.endpoint.electrical_measurement.voltage_reported(
-                (value[7] * 256) + value[6]
-            )
-            self.endpoint.electrical_measurement.current_reported(
-                (value[5] * 256 * 256) + (value[4] * 256) + value[3]
-            )
-            self.endpoint.electrical_measurement.power_reported(
-                (value[2] * 256 * 256) + (value[1] * 256) + value[0]
-            )
+            self.endpoint.electrical_measurement.vcp_reported(value, 0)
+        elif attrid == ZEMISMART_VCP_P2_ATTR:
+            self.endpoint.electrical_measurement.vcp_reported(value, 1)
+        elif attrid == ZEMISMART_VCP_P3_ATTR:
+            self.endpoint.electrical_measurement.vcp_reported(value, 2)
 
 
 class ZemismartPowerMeasurement(LocalDataCluster, ElectricalMeasurement):
@@ -215,44 +215,59 @@ class ZemismartPowerMeasurement(LocalDataCluster, ElectricalMeasurement):
 
     cluster_id = ElectricalMeasurement.cluster_id
 
-    POWER_ID = 0x050B
-    VOLTAGE_ID = 0x0505
-    CURRENT_ID = 0x0508
-
-    AC_VOLTAGE_MULTIPLIER = 0x0600
-    AC_VOLTAGE_DIVISOR = 0x0601
-    AC_CURRENT_MULTIPLIER = 0x0602
-    AC_CURRENT_DIVISOR = 0x0603
-
+    """Setting unit of measurement."""
     _CONSTANT_ATTRIBUTES = {
-        AC_VOLTAGE_MULTIPLIER: 1,
-        AC_VOLTAGE_DIVISOR: 10,
-        AC_CURRENT_MULTIPLIER: 1,
-        AC_CURRENT_DIVISOR: 1000,
+        ElectricalMeasurement.AttributeDefs.ac_voltage_multiplier.id: 1,
+        ElectricalMeasurement.AttributeDefs.ac_voltage_divisor.id: 10,
+        ElectricalMeasurement.AttributeDefs.ac_current_multiplier.id: 1,
+        ElectricalMeasurement.AttributeDefs.ac_current_divisor.id: 1000,
     }
 
-    def voltage_reported(self, value):
-        """Voltage reported."""
-        self._update_attribute(self.VOLTAGE_ID, value)
+    phase_attributes = [
+        {  # Phase 1 (X)
+            "voltage": ElectricalMeasurement.AttributeDefs.rms_voltage.id,
+            "current": ElectricalMeasurement.AttributeDefs.rms_current.id,
+            "power": ElectricalMeasurement.AttributeDefs.active_power.id,
+        },
+        {  # Phase 2 (Y)
+            "voltage": ElectricalMeasurement.AttributeDefs.rms_voltage_ph_b.id,
+            "current": ElectricalMeasurement.AttributeDefs.rms_current_ph_b.id,
+            "power": ElectricalMeasurement.AttributeDefs.active_power_ph_b.id,
+        },
+        {  # Phase 3 (Z)
+            "voltage": ElectricalMeasurement.AttributeDefs.rms_voltage_ph_c.id,
+            "current": ElectricalMeasurement.AttributeDefs.rms_current_ph_c.id,
+            "power": ElectricalMeasurement.AttributeDefs.active_power_ph_c.id,
+        },
+    ]
 
-    def power_reported(self, value):
-        """Power reported."""
-        self._update_attribute(self.POWER_ID, value)
+    # Voltage, current, power is delivered in one value
+    def vcp_reported(self, value, phase=0):
+        if phase < 0 or phase > 2:
+            phase = 0
 
-    def current_reported(self, value):
-        """Ampers reported."""
-        self._update_attribute(self.CURRENT_ID, value)
+        self._update_attribute(
+            self.phase_attributes[phase]["voltage"],
+            int.from_bytes(value[6:8], byteorder="little"),
+        )
+        self._update_attribute(
+            self.phase_attributes[phase]["current"],
+            int.from_bytes(value[3:6], byteorder="little"),
+        )
+        self._update_attribute(
+            self.phase_attributes[phase]["power"],
+            int.from_bytes(value[0:3], byteorder="little"),
+        )
 
 
 class ZemismartElectricalMeasurement(TuyaElectricalMeasurement):
     """Custom class for total energy measurement."""
 
-    POWER_UNIT = 0x300
-    POWER_DIVISOR = 0x302
-    POWER_WATT = 0x0000  # Actually this does not work. Data is interpreted as kWh.
-
     """Setting unit of measurement."""
-    _CONSTANT_ATTRIBUTES = {0x0300: POWER_WATT, POWER_DIVISOR: 100}
+    _CONSTANT_ATTRIBUTES = {
+        Metering.AttributeDefs.unit_of_measure.id: 0,  # kWh
+        Metering.AttributeDefs.divisor.id: 100,
+    }
 
 
 class TuyaPowerMeter(TuyaSwitch):
@@ -371,7 +386,7 @@ class HikingPowerMeter(TuyaSwitch):
 
 
 class TuyaZemismartPowerMeter(CustomDevice):
-    """Tuya power meter device."""
+    """Zemismart power meter device."""
 
     signature = {
         # "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.Router: 1>, complex_descriptor_available=0,
@@ -386,7 +401,9 @@ class TuyaZemismartPowerMeter(CustomDevice):
         # input_clusters=[0x0000, 0x0004, 0x0005, 0xef00]
         # output_clusters=[0x000a, 0x0019]
         MODELS_INFO: [
-            ("_TZE200_bcusnqt8", "TS0601"),
+            ("_TZE200_bcusnqt8", "TS0601"),  # SPM01
+            ("_TZE204_ves1ycwx", "TS0601"),  # SPM02
+            ("_TZE200_ves1ycwx", "TS0601"),  # SPM02
         ],
         ENDPOINTS: {
             # <SimpleDescriptor endpoint=1 profile=260 device_type=51
