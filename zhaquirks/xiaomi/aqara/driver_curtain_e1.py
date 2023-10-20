ad7958dcdcd4e8fd28d201a6406d4f75074b9f52
TheJulianJES <TheJulianJES@users.noreply.github.com>
2023-10-21 01:51:06 +0200

    Remove commented code about inverting current lift percent attribute

--- zhaquirks/xiaomi/aqara/driver_curtain_e1.py
+++ zhaquirks/xiaomi/aqara/driver_curtain_e1.py
@@ -81,19 +81,6 @@ class LocalIlluminanceMeasurementCluster(
 class WindowCoveringE1(CustomCluster, WindowCovering):
     """Xiaomi Window Covering cluster that inverts the motor direction if needed."""
 
-    # TODO: commented out for now
-    # def _update_attribute(self, attrid, value):
-    #     if attrid == WindowCovering.AttributeDefs.current_position_lift_percentage.id:
-    #         # TODO: improve this check? remove default? initialize? does the motor save this attr?
-    #         # TODO: should we do this in HA? (does it have a way now to invert motor?)
-    #         # TODO: should this always be reversed?
-    #         if (
-    #             self.get(WindowCovering.AttributeDefs.window_covering_mode.id, 0)
-    #             & WindowCovering.WindowCoveringMode.Motor_direction_reversed
-    #         ):
-    #             value = 100 - value
-    #     super()._update_attribute(attrid, value)
-
     async def command(
         self,
         command_id: foundation.GeneralCommand | int | t.uint8_t,
