b8f44e985dd9d56906ac99667211fde8f4258c40
Dmitry Berezovsky <corvis@users.noreply.github.com>
2023-10-25 01:11:11 +0300

    Add "power outage memory" support for Aqara LED Bulb T1 (ZNLDP13LM) (#2678)
    * feat: add support for aqara LED Bulb T1 aka Xiaomi ZNLDP13LM
    
    This quirk adds support for power on behavior configuration
    
    * Remove `BinaryInput`, `Metering`, `ElectricalMeasurement` clusters
    
    These do not seem to work and create useless entities in ZHA.
    
    * Re-add Zigbee GreenPower proxy endpoint to `replacement`
    
    ---------
    
    Co-authored-by: TheJulianJES <TheJulianJES@users.noreply.github.com>

--- tests/test_xiaomi.py
+++ tests/test_xiaomi.py
@@ -1539,3 +1539,58 @@ def test_aqara_acn003_signature_match(assert_signature_matches_quirk):
     assert_signature_matches_quirk(
         zhaquirks.xiaomi.aqara.light_acn.LumiLightAcn003, signature
     )
+
+
+def test_aqara_acn014_signature_match(assert_signature_matches_quirk):
+    signature = {
+        "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.Router: 1>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.FullFunctionDevice|MainsPowered|RxOnWhenIdle|AllocateAddress: 142>, manufacturer_code=4447, maximum_buffer_size=82, maximum_incoming_transfer_size=82, server_mask=11264, maximum_outgoing_transfer_size=82, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=False, *is_full_function_device=True, *is_mains_powered=True, *is_receiver_on_when_idle=True, *is_router=True, *is_security_capable=False)",
+        "endpoints": {
+            "1": {
+                "profile_id": 0x0104,
+                "device_type": "0x010c",
+                "in_clusters": [
+                    "0x0000",
+                    "0x0002",
+                    "0x0003",
+                    "0x0004",
+                    "0x0005",
+                    "0x0006",
+                    "0x0008",
+                    "0x0009",
+                    "0x000c",
+                    "0x000f",
+                    "0x0012",
+                    "0x0300",
+                    "0x0702",
+                    "0x0b04",
+                    "0xfcc0",
+                ],
+                "out_clusters": ["0x000a", "0x0019"],
+            },
+            "21": {
+                "profile_id": 0x0104,
+                "device_type": "0x010c",
+                "in_clusters": ["0x000c"],
+                "out_clusters": [],
+            },
+            "31": {
+                "profile_id": 0x0104,
+                "device_type": "0x010c",
+                "in_clusters": ["0x000c"],
+                "out_clusters": [],
+            },
+            "242": {
+                "profile_id": 0xA1E0,
+                "device_type": "0x0061",
+                "in_clusters": [],
+                "out_clusters": ["0x0021"],
+            },
+        },
+        "manufacturer": "LUMI",
+        "model": "lumi.light.acn014",
+        "class": "zigpy.device.Device",
+    }
+
+    assert_signature_matches_quirk(
+        zhaquirks.xiaomi.aqara.light_acn.LumiLightAcn014, signature
+    )
