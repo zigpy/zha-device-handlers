7c1e1d93f4446d1a573b10f66ea684e672ac5bf5
Caius-Bonus <123886836+Caius-Bonus@users.noreply.github.com>
2023-10-27 00:55:42 +0200

    Update zhaquirks/quirk_ids.py
    Co-authored-by: TheJulianJES <TheJulianJES@users.noreply.github.com>

--- zhaquirks/quirk_ids.py
+++ zhaquirks/quirk_ids.py
@@ -4,4 +4,5 @@
 TUYA_PLUG_ONOFF = "tuya.plug_on_off_attributes"  # plugs with configurable attributes on the OnOff cluster
 TUYA_PLUG_MANUFACTURER = "tuya.plug_manufacturer_attributes"  # plugs with configurable attributes on a custom cluster
 
-XIAOMI_AQARA_VIBRATION_AQ1 = "xiaomi.aqara_vibration_aq1"
+# Xiaomi
+XIAOMI_AQARA_VIBRATION_AQ1 = "xiaomi.aqara_vibration_aq1" # vibration sensor with custom cluster handler
