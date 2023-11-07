e71d83dbe620b61c826eeb06a7c62a94665b9375
TheJulianJES <TheJulianJES@users.noreply.github.com>
2023-11-07 02:49:09 +0100

    Merge Samjin button quirks

--- zhaquirks/samjin/button.py
+++ zhaquirks/samjin/button.py
@@ -82,3 +82,47 @@ class SamjinButton(CustomDevice):
         (SHORT_PRESS, SHORT_PRESS): {COMMAND: COMMAND_BUTTON_SINGLE},
         (LONG_PRESS, LONG_PRESS): {COMMAND: COMMAND_BUTTON_HOLD},
     }
+
+
+class SamjinButton2(SamjinButton):
+    """Samjin button device variation without Diagnostic cluster."""
+
+    signature = {
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=1026
+        # device_version=0
+        # input_clusters=[0, 1, 3, 32, 1026, 1280]
+        # output_clusters=[3, 25]>
+        MODELS_INFO: [(SAMJIN, BUTTON)],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    PollControl.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    IasZone.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    PollControl.cluster_id,
+                    TemperatureMeasurement.cluster_id,
+                    SamjinIASCluster,
+                ],
+                OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id],
+            }
+        }
+    }
