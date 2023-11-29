474d015aef53335aaeab43576f28ed1ce42b309f
TheJulianJES <TheJulianJES@users.noreply.github.com>
2023-11-29 03:47:58 +0100

    Improve docstring for opple_switch.py

--- zhaquirks/xiaomi/aqara/opple_switch.py
+++ zhaquirks/xiaomi/aqara/opple_switch.py
@@ -1,4 +1,4 @@
-"""Xiaomi Aqara wall switch devices. Also see switch_h1_single.py for similar H1 single rocker switches."""
+"""Xiaomi Aqara wall switch devices. Also see switch_h1 files similar H1 rocker switches."""
 import copy
 from enum import Enum
 
