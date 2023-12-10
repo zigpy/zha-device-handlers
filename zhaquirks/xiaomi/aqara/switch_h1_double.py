69625e5811f7dd60ab0b5f308289eee3f8cdb57c
TheJulianJES <TheJulianJES@users.noreply.github.com>
2023-12-10 01:39:05 +0100

    Deduplicate `lumi.switch.n2aeu1` signatures
    This uses `XiaomiOpple2ButtonSwitch1` and `XiaomiOpple2ButtonSwitch4` from `opple_switch.py` for the double H1 rocker switches (with neutral), but still keeps them in the separate `switch_h1_double.py` file.

--- zhaquirks/xiaomi/aqara/switch_h1_double.py
+++ zhaquirks/xiaomi/aqara/switch_h1_double.py
@@ -13,8 +13,6 @@ from zigpy.zcl.clusters.general import (
     Scenes,
     Time,
 )
-from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
-from zigpy.zcl.clusters.smartenergy import Metering
 
 from zhaquirks.const import (
     DEVICE_TYPE,
@@ -28,6 +26,7 @@ from zhaquirks.xiaomi import LUMI
 from zhaquirks.xiaomi.aqara.opple_switch import (
     OppleSwitchCluster,
     XiaomiOpple2ButtonSwitch1,
+    XiaomiOpple2ButtonSwitch4,
     XiaomiOpple2ButtonSwitchBase,
 )
 
@@ -37,44 +36,7 @@ class AqaraH1DoubleRockerSwitchWithNeutral(XiaomiOpple2ButtonSwitchBase):
 
     signature = {
         MODELS_INFO: [(LUMI, "lumi.switch.n2aeu1")],
-        ENDPOINTS: {
-            # input_clusters=[0, 2, 3, 4, 5, 6, 9, 1794, 2820], output_clusters=[10, 25]
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    DeviceTemperature.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Alarms.cluster_id,
-                    Metering.cluster_id,
-                    ElectricalMeasurement.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
-            },
-            # input_clusters=[0, 3, 4, 5, 6], output_clusters=[]
-            2: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
-            },
-        },
+        ENDPOINTS: XiaomiOpple2ButtonSwitch1.signature[ENDPOINTS],
     }
 
 
@@ -83,7 +45,7 @@ class AqaraH1DoubleRockerSwitchWithNeutralAlt(XiaomiOpple2ButtonSwitchBase):
 
     signature = {
         MODELS_INFO: [(LUMI, "lumi.switch.n2aeu1")],
-        ENDPOINTS: XiaomiOpple2ButtonSwitch1.signature[ENDPOINTS],
+        ENDPOINTS: XiaomiOpple2ButtonSwitch4.signature[ENDPOINTS],
     }
 
 
