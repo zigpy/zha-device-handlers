d61ea0390e37f8cac861739dcfa3c51b237d9300
theorlangur <theorlangur@gmail.com>
2023-12-11 23:31:04 +0100

    Support for Sonoff SNZB-01P smart button quirk

--- zhaquirks/sonoff/__init__.py
+++ zhaquirks/sonoff/__init__.py
@@ -1,1 +1,9 @@
 """Quirks for Sonoff devices."""
+from zigpy.quirks import CustomCluster
+
+SONOFF_CLUSTER_ID = 0xFC57
+
+
+class SonoffManufactureCluster(CustomCluster):
+    name = "Sonoff Manufacturer Specicific"
+    cluster_id = SONOFF_CLUSTER_ID
