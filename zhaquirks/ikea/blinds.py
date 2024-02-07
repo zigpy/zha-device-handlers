eb08460b33e93361fcf157160a743ae5231da527
alvinchen1 <26409535+alvinchen1@users.noreply.github.com>
2024-02-07 16:03:32 -0500

    Update blinds.py

--- zhaquirks/ikea/blinds.py
+++ zhaquirks/ikea/blinds.py
@@ -112,3 +112,59 @@ class IkeaTradfriRollerBlinds2(CustomDevice):
     }
 
     replacement = IkeaTradfriRollerBlinds.replacement
+
+class IkeaTradfriRollerBlinds3(CustomDevice):
+    """Custom device representing IKEA of Sweden TRADFRI Fyrtur blinds."""
+
+    signature = {
+        # <SimpleDescriptor endpoint=1 profile=260 device_type=514
+        # device_version=1
+        # input_clusters=[0, 1, 3, 4, 5, 32, 258, 4096, 64636]
+        # output_clusters=[3, 25, 4096]>
+        MODELS_INFO: [
+            (IKEA, "FYRTUR block-out roller blind"),
+            (IKEA, "KADRILJ roller blind"),
+            (IKEA, "TREDANSEN block-out cellul blind"),
+            (IKEA, "PRAKTLYSING cellular blind"),
+        ],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    PollControl.cluster_id,
+                    WindowCovering.cluster_id,
+                    LightLink.cluster_id,
+                    IKEA_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [Identify.cluster_id, Ota.cluster_id, LightLink.cluster_id],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    DoublingPowerConfigClusterIKEA,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    PollControl.cluster_id,
+                    WindowCovering.cluster_id,
+                    LightLink.cluster_id,
+                    IKEA_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [Identify.cluster_id,Ota.cluster_id, LightLink.cluster_id],
+            }
+        }
+    }
+
