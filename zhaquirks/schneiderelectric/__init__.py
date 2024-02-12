a7ddd34a46bd07f569cd0a9ad34ab14db6678f88
Christian Spurk <mail@code.chriki.de>
2024-02-12 21:16:33 +0100

    Define cluster attributes in a more modern way
    This was requested here:
    <https://github.com/zigpy/zha-device-handlers/pull/2928#discussion_r1483789337>

--- zhaquirks/schneiderelectric/__init__.py
+++ zhaquirks/schneiderelectric/__init__.py
@@ -1,12 +1,12 @@
 """Quirks implementations for Schneider Electric devices."""
-from typing import Any, Coroutine, Dict, Union
+from typing import Any, Coroutine, Final, Union
 
 from zigpy import types as t
 from zigpy.quirks import CustomCluster
 from zigpy.zcl import foundation
 from zigpy.zcl.clusters.closures import WindowCovering
 from zigpy.zcl.clusters.general import Basic
-from zigpy.zcl.foundation import ZCLAttributeDef
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
 
 SE_MANUF_NAME = "Schneider Electric"
 SE_MANUF_ID = 4190
@@ -15,72 +15,98 @@ SE_MANUF_ID = 4190
 class SEBasic(CustomCluster, Basic):
     """Schneider Electric manufacturer specific Basic cluster."""
 
-    attributes: Dict[int, ZCLAttributeDef] = Basic.attributes.copy()
-
-    attributes.update(
-        {
-            0xE001: (
-                "se_sw_build_id",
-                t.CharacterString,
-                True,
-            ),  # value: "002.004.016 R
-            0xE002: (
-                "unknown_attribute_57346",
-                t.CharacterString,
-                True,
-            ),  # value: "001.000.000"
-            0xE004: (
-                "unknown_attribute_57348",
-                t.CharacterString,
-                True,
-            ),  # value: "213249FEFF5ECFD"
-            0xE007: (
-                "unknown_attribute_57351",
-                t.enum16,
-                True,
-            ),
-            0xE008: (
-                "se_device_type",
-                t.CharacterString,
-                True,
-            ),  # value: "Wiser Light"
-            0xE009: (
-                "se_model",
-                t.CharacterString,
-                True,
-            ),  # value: "NHPB/SHUTTER/1"
-            0xE00A: (
-                "se_realm",
-                t.CharacterString,
-                True,
-            ),  # value: "Wiser Home"
-            0xE00B: (
-                "unknown_attribute_57355",
-                t.CharacterString,
-                True,
-            ),  # value: "http://www.schneider-electric.com"
-        }
-    )
+    class AttributeDefs(Basic.AttributeDefs):
+        se_sw_build_id: Final = ZCLAttributeDef(
+            id=0xE001,
+            type=t.CharacterString,
+            is_manufacturer_specific=True,
+        )  # value: "002.004.016 R"
+        unknown_attribute_57346: Final = ZCLAttributeDef(
+            id=0xE002,
+            type=t.CharacterString,
+            is_manufacturer_specific=True,
+        )  # value: "001.000.000"
+        unknown_attribute_57348: Final = ZCLAttributeDef(
+            id=0xE004,
+            type=t.CharacterString,
+            is_manufacturer_specific=True,
+        )  # value: "213249FEFF5ECFD"
+        unknown_attribute_57351: Final = ZCLAttributeDef(
+            id=0xE007,
+            type=t.enum16,
+            is_manufacturer_specific=True,
+        )
+        se_device_type: Final = ZCLAttributeDef(
+            id=0xE008,
+            type=t.CharacterString,
+            is_manufacturer_specific=True,
+        )  # value: "Wiser Light"
+        se_model: Final = ZCLAttributeDef(
+            id=0xE009,
+            type=t.CharacterString,
+            is_manufacturer_specific=True,
+        )  # value: "NHPB/SHUTTER/1"
+        se_realm: Final = ZCLAttributeDef(
+            id=0xE00A,
+            type=t.CharacterString,
+            is_manufacturer_specific=True,
+        )  # value: "Wiser Home"
+        unknown_attribute_57355: Final = ZCLAttributeDef(
+            id=0xE00B,
+            type=t.CharacterString,
+            is_manufacturer_specific=True,
+        )  # value: "http://www.schneider-electric.com"
 
 
 class SEWindowCovering(CustomCluster, WindowCovering):
     """Schneider Electric manufacturer specific Window Covering cluster."""
 
-    attributes: Dict[int, ZCLAttributeDef] = WindowCovering.attributes.copy()
-
-    attributes.update(
-        {
-            0xFFFD: ("unknown_attribute_65533", t.uint16_t, True),
-            0xE000: ("lift_duration", t.uint16_t, True),
-            0xE010: ("unknown_attribute_57360", t.bitmap8, True),
-            0xE012: ("unknown_attribute_57362", t.uint16_t, True),
-            0xE013: ("unknown_attribute_57363", t.bitmap8, True),
-            0xE014: ("unknown_attribute_57364", t.uint16_t, True),
-            0xE015: ("unknown_attribute_57365", t.uint16_t, True),
-            0xE016: ("unknown_attribute_57366", t.uint16_t, True),
-            0xE017: ("unknown_attribute_57367", t.uint8_t, True),
-        }
-    )
+    class AttributeDefs(WindowCovering.AttributeDefs):
+        unknown_attribute_65533: Final = ZCLAttributeDef(
+            id=0xFFFD,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        lift_duration: Final = ZCLAttributeDef(
+            id=0xE000,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_57360: Final = ZCLAttributeDef(
+            id=0xE010,
+            type=t.bitmap8,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_57362: Final = ZCLAttributeDef(
+            id=0xE012,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_57363: Final = ZCLAttributeDef(
+            id=0xE013,
+            type=t.bitmap8,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_57364: Final = ZCLAttributeDef(
+            id=0xE014,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_57365: Final = ZCLAttributeDef(
+            id=0xE015,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_57366: Final = ZCLAttributeDef(
+            id=0xE016,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_57367: Final = ZCLAttributeDef(
+            id=0xE017,
+            type=t.uint8_t,
+            is_manufacturer_specific=True,
+        )
 
     def _update_attribute(self, attrid: Union[int, t.uint16_t], value: Any):
         if attrid == WindowCovering.AttributeDefs.current_position_lift_percentage.id:
@@ -105,6 +131,19 @@ class SEWindowCovering(CustomCluster, WindowCovering):
         return await super().command(command_id, *args, **kwargs)
 
 
+class SELedIndicatorSignals(t.enum8):
+    """Available LED indicator signal combinations.
+
+    Shutter movement can be indicated with a red LED signal. A green LED
+    light permanently provides orientation, if desired.
+    """
+
+    MOVEMENT_ONLY = 0x00
+    MOVEMENT_AND_ORIENTATION = 0x01
+    ORIENTATION_ONLY = 0x02
+    NONE = 0x03
+
+
 class SESpecific(CustomCluster):
     """Schneider Electric manufacturer specific cluster."""
 
@@ -112,24 +151,39 @@ class SESpecific(CustomCluster):
     ep_attribute = "schneider_electric_manufacturer"
     cluster_id = 0xFF17
 
-    class LedIndicatorSignals(t.enum8):
-        """Available LED indicator signal combinations.
-
-        Shutter movement can be indicated with a red LED signal. A green LED
-        light permanently provides orientation, if desired.
-        """
-
-        MOVEMENT_ONLY = 0x00
-        MOVEMENT_AND_ORIENTATION = 0x01
-        ORIENTATION_ONLY = 0x02
-        NONE = 0x03
-
-    attributes = {
-        0x0000: ("led_indicator_signals", LedIndicatorSignals, True),
-        0x0001: ("unknown_attribute_1", t.enum8, True),
-        0x0010: ("unknown_attribute_16", t.uint8_t, True),
-        0x0011: ("unknown_attribute_17", t.uint16_t, True),
-        0x0020: ("unknown_attribute_32", t.uint8_t, True),
-        0x0021: ("unknown_attribute_33", t.uint16_t, True),
-        0xFFFD: ("unknown_attribute_65533", t.uint16_t, True),
-    }
+    class AttributeDefs(BaseAttributeDefs):
+        led_indicator_signals: Final = ZCLAttributeDef(
+            id=0x0000,
+            type=SELedIndicatorSignals,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_1: Final = ZCLAttributeDef(
+            id=0x0001,
+            type=t.enum8,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_16: Final = ZCLAttributeDef(
+            id=0x0010,
+            type=t.uint8_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_17: Final = ZCLAttributeDef(
+            id=0x0011,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_32: Final = ZCLAttributeDef(
+            id=0x0020,
+            type=t.uint8_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_33: Final = ZCLAttributeDef(
+            id=0x0021,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        unknown_attribute_65533: Final = ZCLAttributeDef(
+            id=0xFFFD,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
