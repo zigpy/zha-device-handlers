24bbca42b05f760fc7d42d5556e041b8bf74a5ef
Christian Kugler <syphdias+git@gmail.com>
2024-03-06 00:56:21 +0100

    Add class for (another) Tuya CO2 Detector
    Link to Amazon (German):
    https://www.amazon.de/-/en/Detector-Quality-Detects-Temperature-Humidity/dp/B0BN66C263/ref=sr_1_15?crid=3MUBZEIE60NO2&dib=eyJ2IjoiMSJ9.6d4lrKBQtHphFxK7zUt3DdTm_Ubeway0SCqBwe3N9VmOUbwtfoUaBY7EYIElgGet79JXVL4wZKnuB53bVBUxGPxwkw_SHIpzcpDgPUO3sJFhExDSRXtiOsjVjk4eeAYlb7U_HJCzSO7Kl7OL_VvA7oEOXMynBcX72RLMVhJYtWoErYZEe5z_QZAwyscwo_tGkJWOC_X7iFUsyQRgApKy3uk3MruyIwKLgHaCWWEF5Dg.qOA81PqXb7m3qyBNe2kMmhalpioOETFOnR-BQut724g&dib_tag=se&keywords=Tuya+Smart+Zigbee+CO2+Detector+Air+Quality&qid=1709683021&s=diy&sprefix=tuya+smart+zigbee+co2+detector+air+quality%2Cdiy%2C136&sr=1-15
    
    As the existing class TuyaNDIRCO2SensorGPP worked but did not have all
    sensors, I copied the entire class and added Temperature and Humidity
    sensors, and pasted the signature.
    
    Issues I have:
    - I am unsure about the device version and
      `SizePrefixedSimpleDescriptor` as I could not find it in the signature
      or the diagnostic information. I left it as is.
    - I do not know how to name this properly; hence suffix of `2`
    
    This fixes #2303

--- zhaquirks/tuya/air/ts0601_air_quality.py
+++ zhaquirks/tuya/air/ts0601_air_quality.py
@@ -196,3 +196,64 @@ class TuyaNDIRCO2SensorGPP(CustomDevice):
             },
         }
     }
+
+
+class TuyaNDIRCO2SensorGPP2(CustomDevice):
+    """Tuya NIDR CO2 sensor."""
+
+    signature = {
+        # NodeDescriptor(logical_type=<LogicalType.Router: 1>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.FullFunctionDevice|MainsPowered|RxOnWhenIdle|AllocateAddress: 142>, manufacturer_code=4417, maximum_buffer_size=66, maximum_incoming_transfer_size=66, server_mask=10752, maximum_outgoing_transfer_size=66, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=False, *is_full_function_device=True, *is_mains_powered=True, *is_receiver_on_when_idle=True, *is_router=True, *is_security_capable=False)
+        # device_version=1
+        # SizePrefixedSimpleDescriptor(endpoint=1, profile=260, device_type=81, device_version=1,
+        # input_clusters=[0, 4, 5, 61184],
+        # output_clusters=[10, 25])
+        MODELS_INFO: [
+            ("_TZE200_3ejwxpmu", "TS0601"),
+        ],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    TuyaCO2ManufCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            242: {
+                # <SimpleDescriptor endpoint=242 profile=41440 device_type=97
+                # input_clusters=[]
+                # output_clusters=[33]
+                PROFILE_ID: zgp.PROFILE_ID,
+                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    TuyaCO2ManufCluster,
+                    TuyaAirQualityCO2,
+                    TuyaAirQualityHumidity,
+                    TuyaAirQualityTemperature,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            242: {
+                PROFILE_ID: zgp.PROFILE_ID,
+                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        }
+    }
