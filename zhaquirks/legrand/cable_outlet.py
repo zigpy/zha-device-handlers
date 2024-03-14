973a07287e006c31d336faeb005c40505e01928c
Paul Bottein <paul.bottein@gmail.com>
2024-03-14 11:16:24 +0100

    Add climate entity

--- zhaquirks/legrand/cable_outlet.py
+++ zhaquirks/legrand/cable_outlet.py
@@ -1,10 +1,10 @@
 """Module for Legrand Cable Outlet (with pilot wire functionality)."""
 
 from zigpy.quirks import CustomCluster
-from zigpy.quirks.v2 import add_to_registry_v2
-from zigpy.quirks.v2.homeassistant import EntityType
+from zigpy.quirks.v2 import CustomDeviceV2, add_to_registry_v2
 import zigpy.types as t
-from zigpy.zcl import ClusterType
+from zigpy.zcl import ClusterType, foundation
+from zigpy.zcl.clusters.hvac import SystemMode, Thermostat
 from zigpy.zcl.foundation import (
     BaseAttributeDefs,
     BaseCommandDefs,
@@ -13,11 +13,15 @@ from zigpy.zcl.foundation import (
     ZCLCommandDef,
 )
 
+from zhaquirks import Bus
 from zhaquirks.legrand import LEGRAND, MANUFACTURER_SPECIFIC_CLUSTER_ID
 
 MANUFACTURER_SPECIFIC_CLUSTER_ID_2 = 0xFC40  # 64576
 
+ZCL_SYSTEM_MODE = Thermostat.attributes_by_name["system_mode"].id
+
 HEAT_MODE_ATTR = 0x00
+OPERATION_PRESET_ATTR = 0x4002
 
 
 class LegrandCluster(CustomCluster):
@@ -50,12 +54,70 @@ class LegrandCluster(CustomCluster):
 class HeatMode(t.enum8):
     """Heat mode."""
 
-    comfort = 0x00
-    comfort_minus_1 = 0x01
-    comfort_minus_2 = 0x02
-    eco = 0x03
-    frost_protection = 0x04
-    off = 0x05
+    Comfort = 0x00
+    Comfort_minus_1 = 0x01
+    Comfort_minus_2 = 0x02
+    Eco = 0x03
+    Frost_protection = 0x04
+    Off = 0x05
+
+
+class LegrandCableOutletThermostatCluster(CustomCluster, Thermostat):
+    """Thermostat cluster for Legrand Cable Outlet."""
+
+    _CONSTANT_ATTRIBUTES = {
+        0x001B: Thermostat.ControlSequenceOfOperation.Heating_Only,
+    }
+
+    attributes = Thermostat.attributes.copy()
+    attributes.update(
+        {
+            OPERATION_PRESET_ATTR: ("operation_preset", HeatMode),
+        }
+    )
+
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+        self.endpoint.device.thermostat_bus.add_listener(self)
+
+    def heat_mode_change(self, value):
+        """Handle the change in heat mode."""
+
+        system_mode = SystemMode.Off
+        if value in (
+            HeatMode.Comfort,
+            HeatMode.Comfort_minus_1,
+            HeatMode.Comfort_minus_2,
+            HeatMode.Eco,
+            HeatMode.Frost_protection,
+        ):
+            system_mode = SystemMode.Heat
+
+        """Heat mode changed reported."""
+        self._update_attribute(ZCL_SYSTEM_MODE, system_mode)
+        self._update_attribute(OPERATION_PRESET_ATTR, value)
+
+    async def write_attributes(self, attributes, manufacturer=None):
+        """Implement writeable attributes."""
+
+        attrs = {}
+        for attr, value in attributes.items():
+            attr_def = self.find_attribute(attr)
+            attr_id = attr_def.id
+            if attr_id == OPERATION_PRESET_ATTR:
+                attrs[HEAT_MODE_ATTR] = value
+            elif attr_id == ZCL_SYSTEM_MODE:
+                current = self._attr_cache.get(ZCL_SYSTEM_MODE)
+                if current != value:
+                    attrs[HEAT_MODE_ATTR] = (
+                        HeatMode.Comfort if value == SystemMode.Heat else HeatMode.Off
+                    )
+            else:
+                attrs[attr] = value
+        await self.endpoint.cable_outlet_cluster.write_attributes(attrs, manufacturer)
+
+        return [[foundation.WriteAttributesStatusRecord(foundation.Status.SUCCESS)]]
 
 
 class LegrandCableOutletCluster(CustomCluster):
@@ -97,17 +159,28 @@ class LegrandCableOutletCluster(CustomCluster):
                 attrs[attr] = value
         return await super().write_attributes(attrs, manufacturer)
 
+    def _update_attribute(self, attrid, value):
+        super()._update_attribute(attrid, value)
+        if attrid == HEAT_MODE_ATTR:
+            self.endpoint.device.thermostat_bus.listener_event(
+                "heat_mode_change", value
+            )
+
+
+class LegrandCableOutletThermostat(CustomDeviceV2):
+    """Legrand Cable Outlet Thermostat device."""
+
+    def __init__(self, *args, **kwargs):
+        """Init device."""
+        self.thermostat_bus = Bus()
+        super().__init__(*args, **kwargs)
+
 
 (
     add_to_registry_v2(f" {LEGRAND}", " Cable outlet")
+    .device_class(LegrandCableOutletThermostat)
     .replaces(LegrandCluster)
     .replaces(LegrandCableOutletCluster)
     .replaces(LegrandCluster, cluster_type=ClusterType.Client)
-    .enum(
-        attribute_name=LegrandCableOutletCluster.AttributeDefs.heat_mode.name,
-        enum_class=HeatMode,
-        cluster_id=LegrandCableOutletCluster.cluster_id,
-        translation_key="heat_mode",
-        entity_type=EntityType.STANDARD,
-    )
+    .adds(LegrandCableOutletThermostatCluster)
 )
