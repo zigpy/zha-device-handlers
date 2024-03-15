25875ca325f8d7c88f4e77170d57a8b19ba02cde
Paul Bottein <paul.bottein@gmail.com>
2024-03-15 10:05:56 +0100

    Remove thermostat

--- zhaquirks/legrand/cable_outlet.py
+++ zhaquirks/legrand/cable_outlet.py
@@ -3,8 +3,7 @@
 from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import add_to_registry_v2
 import zigpy.types as t
-from zigpy.zcl import ClusterType, foundation
-from zigpy.zcl.clusters.hvac import SystemMode, Thermostat
+from zigpy.zcl import ClusterType
 from zigpy.zcl.foundation import (
     BaseAttributeDefs,
     BaseCommandDefs,
@@ -17,10 +16,7 @@ from zhaquirks.legrand import LEGRAND, MANUFACTURER_SPECIFIC_CLUSTER_ID
 
 MANUFACTURER_SPECIFIC_CLUSTER_ID_2 = 0xFC40  # 64576
 
-ZCL_SYSTEM_MODE = Thermostat.attributes_by_name["system_mode"].id
-
 HEAT_MODE_ATTR = 0x00
-OPERATION_PRESET_ATTR = 0x4002
 
 
 class LegrandCluster(CustomCluster):
@@ -59,61 +55,6 @@ class HeatMode(t.enum8):
     Eco = 0x03
     Frost_protection = 0x04
     Off = 0x05
-
-
-class LegrandCableOutletThermostatCluster(CustomCluster, Thermostat):
-    """Thermostat cluster for Legrand Cable Outlet."""
-
-    _CONSTANT_ATTRIBUTES = {
-        0x001B: Thermostat.ControlSequenceOfOperation.Heating_Only,
-    }
-
-    class AttributeDefs(Thermostat.AttributeDefs):
-        """Attribute definitions."""
-
-        operation_preset = ZCLAttributeDef(
-            id=OPERATION_PRESET_ATTR,
-            type=HeatMode,
-            is_manufacturer_specific=True,
-        )
-
-    def heat_mode_change(self, value):
-        """Handle the change in heat mode."""
-
-        system_mode = SystemMode.Off
-        if value in (
-            HeatMode.Comfort,
-            HeatMode.Comfort_minus_1,
-            HeatMode.Comfort_minus_2,
-            HeatMode.Eco,
-            HeatMode.Frost_protection,
-        ):
-            system_mode = SystemMode.Heat
-
-        """Heat mode changed reported."""
-        self._update_attribute(ZCL_SYSTEM_MODE, system_mode)
-        self._update_attribute(OPERATION_PRESET_ATTR, value)
-
-    async def write_attributes(self, attributes, manufacturer=None):
-        """Implement writeable attributes."""
-
-        attrs = {}
-        for attr, value in attributes.items():
-            attr_def = self.find_attribute(attr)
-            attr_id = attr_def.id
-            if attr_id == OPERATION_PRESET_ATTR:
-                attrs[HEAT_MODE_ATTR] = value
-            elif attr_id == ZCL_SYSTEM_MODE:
-                current = self._attr_cache.get(ZCL_SYSTEM_MODE)
-                if current != value:
-                    attrs[HEAT_MODE_ATTR] = (
-                        HeatMode.Comfort if value == SystemMode.Heat else HeatMode.Off
-                    )
-            else:
-                attrs[attr] = value
-        await self.endpoint.cable_outlet_cluster.write_attributes(attrs, manufacturer)
-
-        return [[foundation.WriteAttributesStatusRecord(foundation.Status.SUCCESS)]]
 
 
 class LegrandCableOutletCluster(CustomCluster):
@@ -155,18 +96,12 @@ class LegrandCableOutletCluster(CustomCluster):
                 attrs[attr] = value
         return await super().write_attributes(attrs, manufacturer)
 
-    def _update_attribute(self, attrid, value):
-        super()._update_attribute(attrid, value)
-        if attrid == HEAT_MODE_ATTR:
-            self.endpoint.thermostat.heat_mode_change(value)
-
 
 (
     add_to_registry_v2(f" {LEGRAND}", " Cable outlet")
     .replaces(LegrandCluster)
     .replaces(LegrandCableOutletCluster)
     .replaces(LegrandCluster, cluster_type=ClusterType.Client)
-    .adds(LegrandCableOutletThermostatCluster)
     .enum(
         attribute_name=LegrandCableOutletCluster.AttributeDefs.heat_mode.name,
         enum_class=HeatMode,
