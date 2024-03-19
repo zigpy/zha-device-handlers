bf9dc25aa6e9d536af56f8809547d3b690c00130
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-03-19 03:06:41 +0100

    Merge Sonoff button quirks into one file

--- zhaquirks/sonoff/button.py
+++ zhaquirks/sonoff/button.py
@@ -1,13 +1,15 @@
-"""Device handler for eWeLink WB01."""
+"""Device handler for Sonoff buttons."""
 from zigpy.quirks.v2 import add_to_registry_v2
 
 from zhaquirks.const import (
     BUTTON,
+    CLUSTER_ID,
     COMMAND,
     COMMAND_OFF,
     COMMAND_ON,
     COMMAND_TOGGLE,
     DOUBLE_PRESS,
+    ENDPOINT_ID,
     LONG_PRESS,
     SHORT_PRESS,
 )
@@ -18,6 +20,28 @@ from zhaquirks.const import (
             (SHORT_PRESS, BUTTON): {COMMAND: COMMAND_TOGGLE},
             (DOUBLE_PRESS, BUTTON): {COMMAND: COMMAND_ON},
             (LONG_PRESS, BUTTON): {COMMAND: COMMAND_OFF},
+        }
+    )
+)
+
+(
+    add_to_registry_v2("eWeLink", "SNZB-01P").device_automation_triggers(
+        {
+            (SHORT_PRESS, BUTTON): {
+                COMMAND: COMMAND_TOGGLE,
+                CLUSTER_ID: 6,
+                ENDPOINT_ID: 1,
+            },
+            (DOUBLE_PRESS, BUTTON): {
+                COMMAND: COMMAND_ON,
+                CLUSTER_ID: 6,
+                ENDPOINT_ID: 1,
+            },
+            (LONG_PRESS, BUTTON): {
+                COMMAND: COMMAND_OFF,
+                CLUSTER_ID: 6,
+                ENDPOINT_ID: 1,
+            },
         }
     )
 )
