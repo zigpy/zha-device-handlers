55aa0f270c3796d38f5992113502e7dbf8b13a80
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-03-19 04:07:05 +0100

    Combine both v2 quirks into one, use less strict device triggers

--- zhaquirks/sonoff/button.py
+++ zhaquirks/sonoff/button.py
@@ -3,45 +3,23 @@ from zigpy.quirks.v2 import add_to_registry_v2
 
 from zhaquirks.const import (
     BUTTON,
-    CLUSTER_ID,
     COMMAND,
     COMMAND_OFF,
     COMMAND_ON,
     COMMAND_TOGGLE,
     DOUBLE_PRESS,
-    ENDPOINT_ID,
     LONG_PRESS,
     SHORT_PRESS,
 )
 
 (
-    add_to_registry_v2("eWeLink", "WB01").device_automation_triggers(
+    add_to_registry_v2("eWeLink", "WB01")
+    .also_applies_to("eWeLink", "SNZB-01P")
+    .device_automation_triggers(
         {
             (SHORT_PRESS, BUTTON): {COMMAND: COMMAND_TOGGLE},
             (DOUBLE_PRESS, BUTTON): {COMMAND: COMMAND_ON},
             (LONG_PRESS, BUTTON): {COMMAND: COMMAND_OFF},
-        }
-    )
-)
-
-(
-    add_to_registry_v2("eWeLink", "SNZB-01P").device_automation_triggers(
-        {
-            (SHORT_PRESS, BUTTON): {
-                COMMAND: COMMAND_TOGGLE,
-                CLUSTER_ID: 6,
-                ENDPOINT_ID: 1,
-            },
-            (DOUBLE_PRESS, BUTTON): {
-                COMMAND: COMMAND_ON,
-                CLUSTER_ID: 6,
-                ENDPOINT_ID: 1,
-            },
-            (LONG_PRESS, BUTTON): {
-                COMMAND: COMMAND_OFF,
-                CLUSTER_ID: 6,
-                ENDPOINT_ID: 1,
-            },
         }
     )
 )
