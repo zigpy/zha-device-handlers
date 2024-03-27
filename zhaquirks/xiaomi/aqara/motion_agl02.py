f51046e9f18f1b492d2e9ea79c1113552b151929
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-03-27 15:32:57 +0100

    Move `LocalOccupancyCluster` to Xiaomi file

--- zhaquirks/xiaomi/aqara/motion_agl02.py
+++ zhaquirks/xiaomi/aqara/motion_agl02.py
@@ -1,13 +1,11 @@
 """Xiaomi aqara T1 motion sensor device."""
 from __future__ import annotations
 
-import zigpy
 from zigpy.profiles import zha
-from zigpy.typing import AddressingMode
 from zigpy.zcl.clusters.general import Identify, Ota
 from zigpy.zcl.clusters.measurement import IlluminanceMeasurement, OccupancySensing
 
-from zhaquirks import Bus, LocalDataCluster
+from zhaquirks import Bus
 from zhaquirks.const import (
     DEVICE_TYPE,
     ENDPOINTS,
@@ -20,8 +18,8 @@ from zhaquirks.xiaomi import (
     LUMI,
     BasicCluster,
     IlluminanceMeasurementCluster,
+    LocalOccupancyCluster,
     MotionCluster,
-    OccupancyCluster,
     XiaomiAqaraE1Cluster,
     XiaomiCustomDevice,
     XiaomiPowerConfiguration,
@@ -44,19 +42,6 @@ class XiaomiManufacturerCluster(XiaomiAqaraE1Cluster):
                 OccupancySensing.AttributeDefs.occupancy.id,
                 OccupancySensing.Occupancy.Occupied,
             )
-
-
-class LocalOccupancyCluster(LocalDataCluster, OccupancyCluster):
-    """Local occupancy cluster."""
-
-    def handle_cluster_general_request(
-        self,
-        hdr: zigpy.zcl.foundation.ZCLHeader,
-        args: list,
-        *,
-        dst_addressing: AddressingMode | None = None,
-    ) -> None:
-        """Ignore occupancy attribute reports on this cluster, as they're invalid and sent by the sensor every hour."""
 
 
 class LocalMotionCluster(MotionCluster):
