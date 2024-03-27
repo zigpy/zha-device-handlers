60412038efea9cdfcc0720b871b8d29478e02fb2
Megamannen <mikael@wolfshark.se>
2024-03-27 20:19:54 +0100

    feat: added support for TS0601_smoke with model _TZE200_rccxox8p

--- zhaquirks/tuya/ts0601_smoke.py
+++ zhaquirks/tuya/ts0601_smoke.py
@@ -82,6 +82,7 @@ class TuyaSmokeDetector0601(CustomDevice):
             ("_TZE200_ntcy3xu1", "TS0601"),
             ("_TZE200_vzekyi4c", "TS0601"),
             ("_TZE204_ntcy3xu1", "TS0601"),
+            ("_TZE200_rccxox8p", "TS0601"),
         ],
         ENDPOINTS: {
             1: {
