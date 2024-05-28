9ed6e518079cc26e2e150092345f824b710d2b38
hwzolin <448222024@qq.com>
2024-05-28 14:52:19 +0800

    Add code for zha quirk v2 that supports plug.

--- zhaquirks/thirdreality/plug.py
+++ zhaquirks/thirdreality/plug.py
@@ -2,122 +2,48 @@
 
 from typing import Final
 
-from zigpy.profiles import zgp, zha
-from zigpy.quirks import CustomDevice
 import zigpy.types as t
-from zigpy.zcl.clusters.general import (
-    Basic,
-    GreenPowerProxy,
-    Groups,
-    Identify,
-    OnOff,
-    Ota,
-    Scenes,
-)
-from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
-from zigpy.zcl.clusters.smartenergy import Metering
 from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
-
+from zigpy.quirks.v2 import add_to_registry_v2
 from zhaquirks import CustomCluster
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
+from zigpy.quirks.v2 import ClusterType
 from zhaquirks.thirdreality import THIRD_REALITY
-
-THIRD_REALITY_PLUG_CLUSTER_ID = 0xFF03
-RESET_SUMMATION_DELIVERED_ATTR_ID = 0x0000
-
-
-class ControlMode(t.uint8_t):
-    """Reset mode for not clear and clear."""
-
-    CLEAR = 1
 
 
 class ThirdRealityPlugCluster(CustomCluster):
-    """ThirdReality Acceleration Cluster."""
-
-    cluster_id = THIRD_REALITY_PLUG_CLUSTER_ID
+    cluster_id = 0xFF03
 
     class AttributeDefs(BaseAttributeDefs):
-        """Attribute definitions."""
-
         reset_summation_delivered: Final = ZCLAttributeDef(
-            id=RESET_SUMMATION_DELIVERED_ATTR_ID,
-            type=ControlMode,
+            id=0x0000,
+            name="reset_summation_delivered",
+            type=t.uint8_t,
             is_manufacturer_specific=True,
         )
 
 
+(
+    add_to_registry_v2(THIRD_REALITY, "3RSP02028BZ")
+    .adds(ThirdRealityPlugCluster)
+    .write_attr_button(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.reset_summation_delivered.name,
+        attribute_value=0x01,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        cluster_type=ClusterType.Server,
+        endpoint_id=1,
+        translation_key="reset_summation_delivered"
+    )
+)
 
-class Plug(CustomDevice):
-    """ThirdReality Plug device."""
-
-    signature = {
-        MODELS_INFO: [
-            (THIRD_REALITY, "3RSP019BZ"),
-            (THIRD_REALITY, "3RSP02028BZ"),
-            (THIRD_REALITY, "3RSPE01044BZ"),
-        ],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Metering.cluster_id,
-                    ElectricalMeasurement.cluster_id,
-                    ThirdRealityPlugCluster.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Ota.cluster_id,
-                ],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [
-                    GreenPowerProxy.cluster_id,
-                ],
-            },
-        },
-    }
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Metering.cluster_id,
-                    ElectricalMeasurement.cluster_id,
-                    ThirdRealityPlugCluster,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Ota.cluster_id,
-                ],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [
-                    GreenPowerProxy.cluster_id,
-                ],
-            },
-        },
-    }
+(
+    add_to_registry_v2(THIRD_REALITY, "3RSPE01044BZ")
+    .adds(ThirdRealityPlugCluster)
+    .write_attr_button(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.reset_summation_delivered.name,
+        attribute_value=0x01,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        cluster_type=ClusterType.Server,
+        endpoint_id=1,
+        translation_key="reset_summation_delivered"
+    )
+)
