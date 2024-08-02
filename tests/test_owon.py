6da769272fc516232d79e6e74d3e6edea9b94575
allx <udivankin@users.noreply.github.com>
2024-08-02 15:05:21 +0300

    some more tests

--- tests/test_owon.py
+++ tests/test_owon.py
@@ -6,7 +6,18 @@ from zigpy.zcl.clusters.smartenergy import Metering
 
 import zhaquirks
 from zhaquirks.const import ENDPOINTS, INPUT_CLUSTERS
-from zhaquirks.owon.pc321 import Owon_PC321
+from zhaquirks.owon.pc321 import (
+    ELECTRICAL_MEASUREMENT_CONSTANT_ATTRIBUTES,
+    METERING_CONSTANT_ATTRIBUTES,
+    Owon_PC321,
+    OwonElectricalMeasurementPhaseA,
+    OwonElectricalMeasurementPhaseB,
+    OwonElectricalMeasurementPhaseC,
+    OwonManufacturerSpecific,
+    OwonMeteringPhaseA,
+    OwonMeteringPhaseB,
+    OwonMeteringPhaseC,
+)
 
 zhaquirks.setup()
 
@@ -65,7 +76,7 @@ BUS_NAMES = [
 
 
 @pytest.mark.parametrize("quirk", (zhaquirks.owon.pc321.Owon_PC321,))
-def test_has_correct_replacement(zigpy_device_from_quirk, quirk):
+def test_pc321_replacement(zigpy_device_from_quirk, quirk):
     """Test that the quirk has appropriate replacement endpoints."""
     device = zigpy_device_from_quirk(quirk)
     assert device.replacement is not None
@@ -85,3 +96,49 @@ def test_has_correct_replacement(zigpy_device_from_quirk, quirk):
             assert issubclass(cluster, Metering) or issubclass(
                 cluster, ElectricalMeasurement
             )
+
+
+def test_ManufacturerSpecific_cluster():
+    """Test Manufacturer specific cluster has necessary functions."""
+
+    for fn in BUS_NAMES:
+        assert callable(
+            getattr(OwonManufacturerSpecific, fn.replace("_bus", "_reported"))
+        )
+
+
+def test_ElectricalMeasurement_clusters():
+    """Test ElectricalMeasurement cluster has necessary functions."""
+
+    assert (
+        OwonElectricalMeasurementPhaseA._CONSTANT_ATTRIBUTES
+        == ELECTRICAL_MEASUREMENT_CONSTANT_ATTRIBUTES
+    )
+    assert (
+        OwonElectricalMeasurementPhaseB._CONSTANT_ATTRIBUTES
+        == ELECTRICAL_MEASUREMENT_CONSTANT_ATTRIBUTES
+    )
+    assert (
+        OwonElectricalMeasurementPhaseC._CONSTANT_ATTRIBUTES
+        == ELECTRICAL_MEASUREMENT_CONSTANT_ATTRIBUTES
+    )
+    assert callable(OwonElectricalMeasurementPhaseA.rms_voltage_reported)
+    assert callable(OwonElectricalMeasurementPhaseA.active_power_reported)
+    assert callable(OwonElectricalMeasurementPhaseB.rms_voltage_ph_b_reported)
+    assert callable(OwonElectricalMeasurementPhaseB.active_power_ph_b_reported)
+    assert callable(OwonElectricalMeasurementPhaseC.rms_voltage_ph_c_reported)
+    assert callable(OwonElectricalMeasurementPhaseC.active_power_ph_c_reported)
+
+
+def test_Metering_clusters():
+    """Test Metering cluster has necessary functions."""
+
+    assert OwonMeteringPhaseA._CONSTANT_ATTRIBUTES == METERING_CONSTANT_ATTRIBUTES
+    assert OwonMeteringPhaseB._CONSTANT_ATTRIBUTES == METERING_CONSTANT_ATTRIBUTES
+    assert OwonMeteringPhaseC._CONSTANT_ATTRIBUTES == METERING_CONSTANT_ATTRIBUTES
+    assert callable(OwonMeteringPhaseA.energy_consumption_ph_a_reported)
+    assert callable(OwonMeteringPhaseA.active_power_reported)
+    assert callable(OwonMeteringPhaseB.energy_consumption_ph_b_reported)
+    assert callable(OwonMeteringPhaseB.active_power_ph_b_reported)
+    assert callable(OwonMeteringPhaseC.energy_consumption_ph_c_reported)
+    assert callable(OwonMeteringPhaseC.active_power_ph_c_reported)
