b5869d728dd535c9401728c8860eb998eb4a39be
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-08-26 22:05:07 +0200

    Remove `node_descriptor` comment

--- zhaquirks/tuya/ts1201.py
+++ zhaquirks/tuya/ts1201.py
@@ -453,7 +453,6 @@ class ZosungIRBlaster_ZS06(ZosungIRBlaster):
     """Zosung IR Blaster ZS06."""
 
     signature = {
-        #   "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.Router: 1>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.AllocateAddress|RxOnWhenIdle|MainsPowered|FullFunctionDevice: 142>, manufacturer_code=4098, maximum_buffer_size=82, maximum_incoming_transfer_size=82, server_mask=11264, maximum_outgoing_transfer_size=82, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=False, *is_full_function_device=True, *is_mains_powered=True, *is_receiver_on_when_idle=True, *is_router=True, *is_security_capable=False)",
         #  <SimpleDescriptor endpoint=1, profile=260, device_type=61440
         #  device_version=1
         #  input_clusters=[0, 3, 4, 5, 6, 57348, 60672]
