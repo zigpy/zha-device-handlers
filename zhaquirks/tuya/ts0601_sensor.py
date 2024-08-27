d0ec72749085427c01f824fdb7bb7b68519d4eec
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-08-27 19:47:54 +0200

    Always double reported percentage

--- zhaquirks/tuya/ts0601_sensor.py
+++ zhaquirks/tuya/ts0601_sensor.py
@@ -84,7 +84,7 @@ class TemperatureHumidityBatteryStatesManufCluster(TuyaMCUCluster):
         3: DPToAttributeMapping(
             TuyaPowerConfigurationCluster2AAA.ep_attribute,
             "battery_percentage_remaining",
-            converter=lambda x: {0: 25, 1: 50, 2: 100}[x],  # double reported percentage
+            converter=lambda x: {0: 50, 1: 100, 2: 200}[x],
         ),
     }
 
