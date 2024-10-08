f7ca32fa18eac952a14e74d95312fb37332cf411
simon3panda <340443303@qq.com>
2024-10-08 15:42:49 +0800

    Update __init__.py

--- zhaquirks/linxura/__init__.py
+++ zhaquirks/linxura/__init__.py
@@ -56,7 +56,7 @@ class LinxuraIASCluster(CustomCluster, zigpy.zcl.clusters.security.IasZone):
         else:
             event_args = {
                 PRESS_TYPE: CLICK_TYPES[state],
-                # COMMAND_ID: hdr.command_id,
+                COMMAND_ID: 0,
                 ARGS: args,
             }
             action = f"button_{CLICK_TYPES[state]}"
