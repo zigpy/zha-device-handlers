e4fd04fd88eb3666e9e72b11a237ab51ce13f95d
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-10-18 01:39:34 +0200

    Remove unused `_current_state`

--- zhaquirks/smarthjemmet/quadzigsw.py
+++ zhaquirks/smarthjemmet/quadzigsw.py
@@ -59,11 +59,6 @@ class CR2032PowerConfigurationCluster(PowerConfigurationCluster):
 class CustomMultistateInputCluster(CustomCluster, MultistateInput):
     """Multistate input cluster."""
 
-    def __init__(self, *args, **kwargs):
-        """Init."""
-        self._current_state = {}
-        super().__init__(*args, **kwargs)
-
     async def bind(self):
         """Prevent bind."""
         return zigpy.zcl.foundation.Status.SUCCESS
@@ -81,7 +76,7 @@ class CustomMultistateInputCluster(CustomCluster, MultistateInput):
     def _update_attribute(self, attrid, value):
         super()._update_attribute(attrid, value)
         if attrid == 0x0055:
-            self._current_state[0x0055] = action = ACTION_TYPE.get(value)
+            action = ACTION_TYPE.get(value)
             if action is not None:
                 event_args = {VALUE: value}
                 self.listener_event(ZHA_SEND_EVENT, action, event_args)
