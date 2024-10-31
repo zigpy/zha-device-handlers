61906d212f85c5e713a440311cf797456cae4de1
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-10-31 02:22:58 +0100

    Add translation keys to quirks v2 entities

--- zhaquirks/sonoff/zbminir2.py
+++ zhaquirks/sonoff/zbminir2.py
@@ -65,6 +65,7 @@ class SonoffExternalSwitchTriggerType(types.enum8):
         SonoffCluster.AttributeDefs.external_trigger_mode.name,
         SonoffExternalSwitchTriggerType,
         SonoffCluster.cluster_id,
+        translation_key="external_trigger_mode"
         fallback_name="External trigger mode",
     )
     .switch(
@@ -72,6 +73,7 @@ class SonoffExternalSwitchTriggerType(types.enum8):
         SonoffCluster.cluster_id,
         off_value=9,
         on_value=20,
+        translation_key="turbo_mode",
         fallback_name="Turbo mode",
     )
     .switch(
@@ -79,6 +81,7 @@ class SonoffExternalSwitchTriggerType(types.enum8):
         SonoffCluster.cluster_id,
         off_value=0,
         on_value=1,
+        translation_key="detach_relay"
         fallback_name="Detach relay",
     )
     .add_to_registry()
