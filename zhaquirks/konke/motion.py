22de1ee2d37bb7577c73bb3e5cb6e8ddc6be3d4d
mozzie1121 <sunjian21@foxmail.com>
2024-11-06 19:53:45 +0800

    Update motion.py

--- zhaquirks/konke/motion.py
+++ zhaquirks/konke/motion.py
@@ -2,8 +2,9 @@
 
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Identify, PowerConfiguration
+from zigpy.zcl.clusters.general import Basic, Identify, PowerConfiguration, Ota
 from zigpy.zcl.clusters.security import IasZone
+from zigpy.zcl.clusters.homeautomation import Diagnostic
 
 from zhaquirks import Bus, PowerConfigurationCluster
 from zhaquirks.const import (
@@ -117,3 +118,55 @@ class KonkeMotionB(CustomDevice):
             }
         }
     }
+
+
+class KonkeMotionC(CustomDevice):
+    """Custom device representing konke motion sensors."""
+
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        self.occupancy_bus = Bus()
+        super().__init__(*args, **kwargs)
+
+    signature = {
+        #  <SimpleDescriptor endpoint=1 profile=104 device_type=402
+        #  device_version=0
+        #  input_clusters=[0, 1, 3, 500, 2821, 64704]
+        #  output_clusters=[19, 64704]>
+        MODELS_INFO: [
+            (KONKE, "3AFE28010402000D"),
+        ],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id, #0
+                    PowerConfiguration.cluster_id, #1
+                    Identify.cluster_id, #3
+                    IasZone.cluster_id, #500
+                    Diagnostic.cluster_id, #2821
+                    KONKE_CLUSTER_ID, #64704
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id, KONKE_CLUSTER_ID],
+            }
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfigurationCluster,
+                    Identify.cluster_id,
+                    OccupancyCluster,
+                    MotionCluster,
+                    Diagnostic.cluster_id, 
+                    KONKE_CLUSTER_ID,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id, KONKE_CLUSTER_ID],
+            }
+        }
+    }
+    
