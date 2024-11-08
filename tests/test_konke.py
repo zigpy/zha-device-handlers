02f56229bbf1b1261308d04119fd96e4bc57019d
mozzie1121 <sunjian21@foxmail.com>
2024-11-08 18:52:59 +0800

    Update test_konke.py

--- tests/test_konke.py
+++ tests/test_konke.py
@@ -23,7 +23,7 @@ zhaquirks.setup()
 
 
 @pytest.mark.parametrize(
-    "quirk", (zhaquirks.konke.motion.KonkeMotion, zhaquirks.konke.motion.KonkeMotionB)
+    "quirk", (zhaquirks.konke.motion.KonkeMotion, zhaquirks.konke.motion.KonkeMotionB, zhaquirks.konke.motion.KonkeMotionC, zhaquirks.konke.motion.KonkeMotionD)
 )
 async def test_konke_motion(zigpy_device_from_quirk, quirk):
     """Test konke motion sensor."""
