50b4cfe198abe6d2ec0a903505bf61012471014b
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-12-23 00:38:30 +0100

    Change scale from `1/10` to `0.1`

--- zhaquirks/tuya/ts0601_valve.py
+++ zhaquirks/tuya/ts0601_valve.py
@@ -594,7 +594,7 @@ class RoyalGardineerTimerState(t.enum8):
     .tuya_onoff(dp_id=1)
     .tuya_battery(dp_id=7, power_cfg=TuyaPowerConfigurationCluster2AA)
     # Water consumed (value comes in deciliters - convert it to liters)
-    .tuya_metering(dp_id=5, scale=1 / 10)
+    .tuya_metering(dp_id=5, scale=0.1)
     # Timer time left/remaining (raw value in seconds)
     .tuya_number(
         dp_id=11,
