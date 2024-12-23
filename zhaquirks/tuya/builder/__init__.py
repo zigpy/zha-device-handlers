5ec810adb511a5d9f98c58b315faa10791207e4e
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-12-23 03:07:09 +0100

    Revert Tuya builder changes
    These aren't needed anymore

--- zhaquirks/tuya/builder/__init__.py
+++ zhaquirks/tuya/builder/__init__.py
@@ -46,14 +46,6 @@ class TuyaIasFire(IasZone, TuyaLocalCluster):
     }
 
 
-class TuyaIasMotion(IasZone, TuyaLocalCluster):
-    """Tuya local IAS motion cluster."""
-
-    _CONSTANT_ATTRIBUTES = {
-        IasZone.AttributeDefs.zone_type.id: IasZone.ZoneType.Motion_Sensor
-    }
-
-
 class TuyaRelativeHumidity(RelativeHumidity, TuyaLocalCluster):
     """Tuya local RelativeHumidity cluster."""
 
@@ -130,15 +122,6 @@ class TuyaQuirkBuilder(QuirkBuilder):
             dp_id=dp_id,
             ias_cfg=TuyaIasFire,
             converter=lambda x: IasZone.ZoneStatus.Alarm_1 if x == 0 else 0,
-        )
-        return self
-
-    def tuya_motion(self, dp_id: int):
-        """Add a Tuya IAS motion sensor."""
-        self.tuya_ias(
-            dp_id=dp_id,
-            ias_cfg=TuyaIasMotion,
-            converter=lambda x: IasZone.ZoneStatus.Alarm_1 if x == 2 else 0,
         )
         return self
 
