4b32dcf65aba421258af50e0927b42dc73b9430b
prairiesnpr <midnryanmiller@gmail.com>
2024-12-30 11:08:33 +0000

    Init rain sensor

---
+++ zhaquirks/tuya/tuya_rain.py
@@ -0,0 +1,80 @@
+"""Quirk for TS0207 rain sensors."""
+
+import math
+
+from zigpy.quirks.v2.homeassistant import LIGHT_LUX, EntityType
+from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass, SensorStateClass
+import zigpy.types as t
+from zigpy.zcl.clusters.measurement import IlluminanceMeasurement
+from zigpy.zcl.clusters.security import IasZone
+
+from zhaquirks.tuya import TuyaLocalCluster
+from zhaquirks.tuya.builder import TuyaQuirkBuilder
+
+
+class TuyaIasZone(IasZone, TuyaLocalCluster):
+    """IAS Zone for rain sensors."""
+
+    _CONSTANT_ATTRIBUTES = {
+        IasZone.AttributeDefs.zone_type.id: IasZone.ZoneType.Water_Sensor
+    }
+
+
+class TuyaIlluminanceCluster(IlluminanceMeasurement, TuyaLocalCluster):
+    """Tuya Illuminance cluster."""
+
+
+(
+    TuyaQuirkBuilder("_TZ3210_tgvtvdoc", "TS0207")
+    .tuya_dp(
+        dp_id=101,
+        ep_attribute=TuyaIlluminanceCluster.ep_attribute,
+        attribute_name=TuyaIlluminanceCluster.AttributeDefs.measured_value.name,
+        converter=lambda x: (10000.0 * math.log10(x) + 1.0 if x != 0 else 0),
+    )
+    .adds(TuyaIlluminanceCluster)
+    .tuya_sensor(
+        dp_id=102,
+        attribute_name="average_light_intensity_20mins",
+        type=t.uint32_t,
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.DURATION,
+        unit=LIGHT_LUX,
+        entity_type=EntityType.DIAGNOSTIC,
+        translation_key="average_light_intensity_20mins",
+        fallback_name="Average light intensity last 20 min",
+    )
+    .tuya_sensor(
+        dp_id=103,
+        attribute_name="todays_max_light_intensity",
+        type=t.uint32_t,
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.DURATION,
+        unit=LIGHT_LUX,
+        entity_type=EntityType.DIAGNOSTIC,
+        translation_key="todays_max_light_intensity",
+        fallback_name="Todays max light intensity",
+    )
+    .tuya_binary_sensor(
+        dp_id=104,
+        attribute_name="cleaning_reminder",
+        translation_key="cleaning_reminder",
+        fallback_name="Cleaning reminder",
+    )
+    .tuya_dp(
+        dp_id=105,
+        ep_attribute=TuyaIasZone.ep_attribute,
+        attribute_name=TuyaIasZone.AttributeDefs.zone_status.name,
+        converter=lambda x: IasZone.ZoneStatus.Alarm_1 if x > 100 else 0,
+    )
+    # .sensor(
+    #     type=t.uint32_t,
+    #     attribute_name=TuyaIasZone.AttributeDefs.zone_status.name,
+    #     device_class=SensorDeviceClass.VOLTAGE,
+    #     unit=UnitOfElectricPotential.MILLIVOLT,
+    #     fallback_name="Rain Intensity",
+    # )
+    .adds(TuyaIasZone)
+    .skip_configuration()
+    .add_to_registry()
+)
