02865e66d600fd08ed58b57b194fad5ed4ab1464
prairiesnpr <midnryanmiller@gmail.com>
2025-01-07 00:57:41 +0000

    init quirk

--- zhaquirks/tuya/ts0601_trv.py
+++ zhaquirks/tuya/ts0601_trv.py
@@ -1862,3 +1862,87 @@ class ZonnsmartTV01_ZG(TuyaThermostat):
     .skip_configuration()
     .add_to_registry()
 )
+
+
+(
+    TuyaQuirkBuilder("_TZE200_bvu2wnxz", "TS0601")
+    .applies_to("_TZE200_6rdj8dzm", "TS0601")
+    .applies_to("_TZE200_9xfjixap", "TS0601")
+    .applies_to("_TZE200_p3dbf6qs", "TS0601")
+    .applies_to("_TZE200_rxntag7i", "TS0601")
+    .applies_to("_TZE200_yqgbrdyo", "TS0601")
+    .applies_to("_TZE284_p3dbf6qs", "TS0601")
+    .applies_to("_TZE200_rxq4iti9", "TS0601")
+    .applies_to("_TZE200_hvaxb2tc", "TS0601")
+    .applies_to("_TZE284_o3x45p96", "TS0601")
+    .applies_to("_TZE284_c6wv4xyo", "TS0601")
+    .applies_to("_TZE204_o3x45p96", "TS0601")
+    .tuya_dp(
+        dp_id=2,
+        ep_attribute=TuyaThermostatV2.ep_attribute,
+        attribute_name=TuyaThermostatV2.AttributeDefs.system_mode.name,
+        converter=lambda x: {
+            TuyaThermostatSystemMode.Auto: Thermostat.SystemMode.Auto,
+            TuyaThermostatSystemMode.Heat: Thermostat.SystemMode.Heat,
+            TuyaThermostatSystemMode.Off: Thermostat.SystemMode.Off,
+        }[x],
+        dp_converter=lambda x: {
+            Thermostat.SystemMode.Auto: TuyaThermostatSystemMode.Auto,
+            Thermostat.SystemMode.Heat: TuyaThermostatSystemMode.Heat,
+            Thermostat.SystemMode.Off: TuyaThermostatSystemMode.Off,
+        }[x],
+    )
+    .tuya_dp(
+        dp_id=3,
+        ep_attribute=TuyaThermostatV2.ep_attribute,
+        attribute_name=TuyaThermostatV2.AttributeDefs.running_state.name,
+        converter=lambda x: 0x01 if not x else 0x00,  # Heat, Idle
+    )
+    .tuya_dp(
+        dp_id=4,
+        ep_attribute=TuyaThermostatV2.ep_attribute,
+        attribute_name=TuyaThermostatV2.AttributeDefs.occupied_heating_setpoint.name,
+        converter=lambda x: x * 10,
+        dp_converter=lambda x: x // 10,
+    )
+    .tuya_dp(
+        dp_id=5,
+        ep_attribute=TuyaThermostatV2.ep_attribute,
+        attribute_name=TuyaThermostatV2.AttributeDefs.local_temperature.name,
+        converter=lambda x: x * 10,
+    )
+    .tuya_dp(
+        dp_id=47,
+        ep_attribute=TuyaThermostatV2.ep_attribute,
+        attribute_name=TuyaThermostatV2.AttributeDefs.local_temperature_calibration.name,
+        converter=lambda x: x,
+        dp_converter=lambda x: x + 0x100000000 if x < 0 else x,
+    )
+    .tuya_switch(
+        dp_id=7,
+        attribute_name="child_lock",
+        translation_key="child_lock",
+        fallback_name="Child lock",
+    )
+    .tuya_switch(
+        dp_id=36,
+        attribute_name="frost_protection",
+        translation_key="frost_protection",
+        fallback_name="Frost protection",
+    )
+    .tuya_switch(
+        dp_id=39,
+        attribute_name="scale_protection",
+        translation_key="scale_protection",
+        fallback_name="Scale protection",
+    )
+    .tuya_binary_sensor(
+        dp_id=35,
+        attribute_name="error",
+        translation_key="error",
+        fallback_name="Error or battery low",
+    )
+    .adds(TuyaThermostatV2)
+    .skip_configuration()
+    .add_to_registry()
+)
