25384746ade13e2b519818eded739ac7c7fa438b
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-01-16 23:08:18 +0100

    refactor

--- zhaquirks/tuya/ts0601_motion.py
+++ zhaquirks/tuya/ts0601_motion.py
@@ -1,7 +1,6 @@
 """BlitzWolf IS-3/Tuya motion rechargeable occupancy sensor."""
 
 import asyncio
-import math
 from typing import Any
 
 from zigpy.quirks.v2 import EntityPlatform, EntityType
@@ -9,15 +8,11 @@ from zigpy.quirks.v2.homeassistant import UnitOfLength, UnitOfTime
 from zigpy.quirks.v2.homeassistant.binary_sensor import BinarySensorDeviceClass
 from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass, SensorStateClass
 import zigpy.types as t
-from zigpy.zcl.clusters.measurement import IlluminanceMeasurement, OccupancySensing
+from zigpy.zcl.clusters.measurement import OccupancySensing
 from zigpy.zcl.clusters.security import IasZone
 
 from zhaquirks.tuya import TuyaLocalCluster
-from zhaquirks.tuya.builder import TuyaQuirkBuilder
-
-
-class TuyaIlluminanceCluster(IlluminanceMeasurement, TuyaLocalCluster):
-    """Tuya Illuminance cluster."""
+from zhaquirks.tuya.builder import TuyaIlluminance, TuyaQuirkBuilder
 
 
 class TuyaOccupancySensing(OccupancySensing, TuyaLocalCluster):
@@ -207,7 +202,7 @@ base_tuya_motion = (
         translation_key="distance",
         fallback_name="Target distance",
     )
-    .adds(TuyaIlluminanceCluster)
+    .adds(TuyaIlluminance)
     .skip_configuration()
 )
 
@@ -241,12 +236,7 @@ base_tuya_motion = (
         translation_key="presence_sensitivity",
         fallback_name="Presence sensitivity",
     )
-    .tuya_dp(
-        dp_id=103,
-        ep_attribute=TuyaIlluminanceCluster.ep_attribute,
-        attribute_name=TuyaIlluminanceCluster.AttributeDefs.measured_value.name,
-        converter=lambda x: 10000 * math.log10(x) + 1 if x != 0 else 0,
-    )
+    .tuya_illuminance(103)
     .tuya_number(
         dp_id=105,
         attribute_name="presence_timeout",
@@ -315,12 +305,7 @@ base_tuya_motion = (
         translation_key="presence_sensitivity",
         fallback_name="Presence sensitivity",
     )
-    .tuya_dp(
-        dp_id=104,
-        ep_attribute=TuyaIlluminanceCluster.ep_attribute,
-        attribute_name=TuyaIlluminanceCluster.AttributeDefs.measured_value.name,
-        converter=lambda x: 10000 * math.log10(x) + 1 if x != 0 else 0,
-    )
+    .tuya_illuminance(104)
     # 103 cli, z2m lists as not working
     .add_to_registry()
 )
@@ -363,12 +348,7 @@ base_tuya_motion = (
         translation_key="fading_time",
         fallback_name="Fading time",
     )
-    .tuya_dp(
-        dp_id=104,
-        ep_attribute=TuyaIlluminanceCluster.ep_attribute,
-        attribute_name=TuyaIlluminanceCluster.AttributeDefs.measured_value.name,
-        converter=lambda x: 10000 * math.log10(x) + 1 if x != 0 else 0,
-    )
+    .tuya_illuminance(104)
     .add_to_registry()
 )
 
@@ -686,12 +666,7 @@ base_tuya_motion = (
         fallback_name="Fading time",
     )
     # 103 cline, z2m doesn't expose
-    .tuya_dp(
-        dp_id=104,
-        ep_attribute=TuyaIlluminanceCluster.ep_attribute,
-        attribute_name=TuyaIlluminanceCluster.AttributeDefs.measured_value.name,
-        converter=lambda x: 10000 * math.log10(x) + 1 if x != 0 else 0,
-    )
+    .tuya_illuminance(104)
     .tuya_number(
         dp_id=105,
         attribute_name="entry_sensitivity",
@@ -937,12 +912,7 @@ base_tuya_motion = (
         attribute_name=OccupancySensing.AttributeDefs.occupancy.name,
         converter=lambda x: x == 1,
     )
-    .tuya_dp(
-        dp_id=101,
-        ep_attribute=TuyaIlluminanceCluster.ep_attribute,
-        attribute_name=TuyaIlluminanceCluster.AttributeDefs.measured_value.name,
-        converter=lambda x: 10000 * math.log10(x) + 1 if x != 0 else 0,
-    )
+    .tuya_illuminance(101)
     .adds(TuyaOccupancySensing)
     .tuya_switch(
         dp_id=102,
@@ -981,7 +951,7 @@ base_tuya_motion = (
         translation_key="presence_timeout",
         fallback_name="Fade time",
     )
-    .adds(TuyaIlluminanceCluster)
+    .adds(TuyaIlluminance)
     .skip_configuration()
     .add_to_registry()
 )
