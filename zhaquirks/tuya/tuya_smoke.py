df530de6301a5bceea599a56f70c295facdbb985
prairiesnpr <midnryanmiller@gmail.com>
2025-01-18 22:19:29 +0000

    Add tamper

--- zhaquirks/tuya/tuya_smoke.py
+++ zhaquirks/tuya/tuya_smoke.py
@@ -1,12 +1,14 @@
 """Smoke Sensor."""
 
+from zigpy.quirks.v2 import EntityType
+from zigpy.quirks.v2.homeassistant.binary_sensor import BinarySensorDeviceClass
+
 from zhaquirks.tuya.builder import TuyaPowerConfigurationCluster2AAA, TuyaQuirkBuilder
 
 (
     TuyaQuirkBuilder("_TZE200_aycxwiau", "TS0601")
     .applies_to("_TZE200_dq1mfjug", "TS0601")
     .applies_to("_TZE200_m9skfctm", "TS0601")
-    .applies_to("_TZE200_ntcy3xu1", "TS0601")
     .applies_to("_TZE200_rccxox8p", "TS0601")
     .applies_to("_TZE284_rccxox8p", "TS0601")
     .applies_to("_TZE200_vzekyi4c", "TS0601")
@@ -20,7 +22,16 @@ from zhaquirks.tuya.builder import TuyaPowerConfigurationCluster2AAA, TuyaQuirkB
 
 (
     TuyaQuirkBuilder("_TZE204_ntcy3xu1", "TS0601")
+    .applies_to("_TZE200_ntcy3xu1", "TS0601")
     .tuya_smoke(dp_id=1)
+    .tuya_binary_sensor(
+        dp_id=4,
+        attribute_name="tamper",
+        device_class=BinarySensorDeviceClass.TAMPER,
+        entity_type=EntityType.DIAGNOSTIC,
+        translation_key="tamper",
+        fallback_name="Tamper",
+    )
     .tuya_dp(
         dp_id=14,
         ep_attribute=TuyaPowerConfigurationCluster2AAA.ep_attribute,
