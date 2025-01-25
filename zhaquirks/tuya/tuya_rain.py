9a8d178d6b37b821f6b34857e471d518b6f96004
prairiesnpr <midnryanmiller@gmail.com>
2025-01-25 12:44:56 +0000

    Update methods add battery

--- zhaquirks/tuya/tuya_rain.py
+++ zhaquirks/tuya/tuya_rain.py
@@ -1,14 +1,11 @@
 """Quirk for TS0207 rain sensors."""
 
-import math
-
-from zigpy.quirks.v2.homeassistant import EntityType
+from zigpy.quirks.v2.homeassistant import LIGHT_LUX, EntityType
 from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass, SensorStateClass
 import zigpy.types as t
-from zigpy.zcl.clusters.measurement import IlluminanceMeasurement
 from zigpy.zcl.clusters.security import IasZone
 
-from zhaquirks.tuya import TuyaLocalCluster
+from zhaquirks.tuya import BatterySize, TuyaLocalCluster
 from zhaquirks.tuya.builder import TuyaQuirkBuilder
 
 
@@ -20,26 +17,19 @@ class TuyaIasZone(IasZone, TuyaLocalCluster):
     }
 
 
-class TuyaIlluminanceCluster(IlluminanceMeasurement, TuyaLocalCluster):
-    """Tuya Illuminance cluster."""
-
-
 (
     TuyaQuirkBuilder("_TZ3210_tgvtvdoc", "TS0207")
-    .tuya_dp(
-        dp_id=101,
-        ep_attribute=TuyaIlluminanceCluster.ep_attribute,
-        attribute_name=TuyaIlluminanceCluster.AttributeDefs.measured_value.name,
-        converter=lambda x: (10000.0 * math.log10(x) + 1.0 if x != 0 else 0),
+    .tuya_battery(
+        dp_id=4, battery_type=BatterySize.Other, battery_qty=1, battery_voltage=30
     )
-    .adds(TuyaIlluminanceCluster)
+    .tuya_illuminance(dp_id=101)
     .tuya_sensor(
         dp_id=102,
         attribute_name="average_light_intensity_20mins",
         type=t.uint32_t,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
-        # unit=LIGHT_LUX,
+        unit=LIGHT_LUX,
         entity_type=EntityType.DIAGNOSTIC,
         translation_key="average_light_intensity_20mins",
         fallback_name="Average light intensity last 20 min",
@@ -50,10 +40,10 @@ class TuyaIlluminanceCluster(IlluminanceMeasurement, TuyaLocalCluster):
         type=t.uint32_t,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
-        # unit=LIGHT_LUX,
+        unit=LIGHT_LUX,
         entity_type=EntityType.DIAGNOSTIC,
         translation_key="todays_max_light_intensity",
-        fallback_name="Todays max light intensity",
+        fallback_name="Today's max light intensity",
     )
     .tuya_binary_sensor(
         dp_id=104,
