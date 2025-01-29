cb19ec2bd01a57300da8935b73db7ef2c98f9e7c
Wojciech Gomoła <szogun1987@gmail.com>
2025-01-29 15:18:44 +0100

    Restore _TZ3000_bjawzodf visibility as humidity/temp sensor
    Previous commit made this sensor discoverable as no-feature device

--- zhaquirks/tuya/ty0201.py
+++ zhaquirks/tuya/ty0201.py
@@ -1,12 +1,14 @@
 """Tuya TY0201 temperature and humidity sensor."""
 
 from zhaquirks.tuya import TuyaPowerConfigurationCluster2AA
-from zhaquirks.tuya.builder import TuyaQuirkBuilder
+from zhaquirks.tuya.builder import TuyaQuirkBuilder, TuyaRelativeHumidity, TuyaTemperatureMeasurement
 
 (
     TuyaQuirkBuilder("_TZ3000_bjawzodf", "TY0201")
     .applies_to("_TZ3000_zl1kmjqx", "TY0201")
     .applies_to("_TZ3000_zl1kmjqx", "")
+    .replaces(TuyaRelativeHumidity)
+    .replaces(TuyaTemperatureMeasurement)
     .replaces(TuyaPowerConfigurationCluster2AA)
     .tuya_enchantment()
     .skip_configuration()
