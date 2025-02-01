453ea0f119a7a1633a7799599d65f9013e01d987
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-02-01 07:14:52 +0100

    Combine if statements

--- zhaquirks/smarthjemmet/quadzigsw.py
+++ zhaquirks/smarthjemmet/quadzigsw.py
@@ -46,10 +46,12 @@ class CustomMultistateInputCluster(CustomCluster, MultistateInput):
 
     def _update_attribute(self, attrid, value):
         super()._update_attribute(attrid, value)
-        if attrid == MultistateInput.AttributeDefs.present_value.id:
-            if (action := ACTION_TYPE.get(value)) is not None:
-                event_args = {VALUE: value}
-                self.listener_event(ZHA_SEND_EVENT, action, event_args)
+        if (
+            attrid == MultistateInput.AttributeDefs.present_value.id
+            and (action := ACTION_TYPE.get(value)) is not None
+        ):
+            event_args = {VALUE: value}
+            self.listener_event(ZHA_SEND_EVENT, action, event_args)
 
 
 (
