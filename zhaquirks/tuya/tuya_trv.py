23c57abd88b46cc64ec48af689debb27b45d7f31
PrairieSnpr <midnryanmiller@gmail.com>
2025-02-12 16:46:49 -0500

    invert running state

--- zhaquirks/tuya/tuya_trv.py
+++ zhaquirks/tuya/tuya_trv.py
@@ -223,7 +223,7 @@ class TuyaThermostatV2(Thermostat, TuyaAttributesCluster):
         dp_id=6,
         ep_attribute=TuyaThermostatV2.ep_attribute,
         attribute_name=TuyaThermostatV2.AttributeDefs.running_state.name,
-        converter=lambda x: 0x01 if not x else 0x00,  # Heat, Idle
+        converter=lambda x: 0x01 if x else 0x00,  # Heat, Idle
     )
     .tuya_binary_sensor(
         dp_id=7,
