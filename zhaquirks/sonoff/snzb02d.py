88045841d56e6b4f35cb2b72cc5bc747ab43f048
Federico G. Schwindt <fgsch@lodoss.net>
2025-03-02 11:37:57 +0000

    Rename attributes and labels
    Alternatively use "comfort_{humidity,temperature}_{min,max}".

--- zhaquirks/sonoff/snzb02d.py
+++ zhaquirks/sonoff/snzb02d.py
@@ -22,22 +22,22 @@ class CustomSonoffCluster(CustomCluster):
     class AttributeDefs(BaseAttributeDefs):
         """Attribute definitions."""
 
-        temperature_threshold_high = ZCLAttributeDef(
+        comfort_temperature_max = ZCLAttributeDef(
             id=0x0003,
             type=t.int16s,
         )
 
-        temperature_threshold_low = ZCLAttributeDef(
+        comfort_temperature_min = ZCLAttributeDef(
             id=0x0004,
             type=t.int16s,
         )
 
-        humidity_threshold_low = ZCLAttributeDef(
+        comfort_humidity_min = ZCLAttributeDef(
             id=0x0005,
             type=t.uint16_t,
         )
 
-        humidity_threshold_high = ZCLAttributeDef(
+        comfort_humidity_max = ZCLAttributeDef(
             id=0x0006,
             type=t.uint16_t,
         )
@@ -57,48 +57,48 @@ class CustomSonoffCluster(CustomCluster):
     QuirkBuilder("SONOFF", "SNZB-02D")
     .replaces(CustomSonoffCluster)
     .number(
-        CustomSonoffCluster.AttributeDefs.temperature_threshold_low.name,
+        CustomSonoffCluster.AttributeDefs.comfort_temperature_min.name,
         CustomSonoffCluster.cluster_id,
         min_value=-10,
         max_value=60,
         step=0.1,
         unit=UnitOfTemperature.CELSIUS,
         multiplier=0.01,
-        translation_key="temperature_threshold_low",
-        fallback_name="Temperature min threshold",
+        translation_key="comfort_temperature_min",
+        fallback_name="Comfort temperature min",
     )
     .number(
-        CustomSonoffCluster.AttributeDefs.temperature_threshold_high.name,
+        CustomSonoffCluster.AttributeDefs.comfort_temperature_max.name,
         CustomSonoffCluster.cluster_id,
         min_value=-10,
         max_value=60,
         step=0.1,
         unit=UnitOfTemperature.CELSIUS,
         multiplier=0.01,
-        translation_key="temperature_threshold_high",
-        fallback_name="Temperature max threshold",
+        translation_key="comfort_temperature_max",
+        fallback_name="Comfort temperature max",
     )
     .number(
-        CustomSonoffCluster.AttributeDefs.humidity_threshold_low.name,
+        CustomSonoffCluster.AttributeDefs.comfort_humidity_min.name,
         CustomSonoffCluster.cluster_id,
         min_value=5,
         max_value=95,
         step=0.1,
         unit=PERCENTAGE,
         multiplier=0.01,
-        translation_key="humidity_threshold_low",
-        fallback_name="Humidity min threshold",
+        translation_key="comfort_humidity_min",
+        fallback_name="Comfort humidity min",
     )
     .number(
-        CustomSonoffCluster.AttributeDefs.humidity_threshold_high.name,
+        CustomSonoffCluster.AttributeDefs.comfort_humidity_max.name,
         CustomSonoffCluster.cluster_id,
         min_value=5,
         max_value=95,
         step=0.1,
         unit=PERCENTAGE,
         multiplier=0.01,
-        translation_key="humidity_threshold_high",
-        fallback_name="Humidity max threshold",
+        translation_key="comfort_humidity_max",
+        fallback_name="Comfort humidity max",
     )
     .enum(
         CustomSonoffCluster.AttributeDefs.temperature_unit.name,
