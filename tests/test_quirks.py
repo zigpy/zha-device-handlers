59232ad212f62e92ad172db68069622e448fb05d
hwzolin <448222024@qq.com>
2025-03-04 16:11:59 +0800

    Something that doesn't exist in the annotation test_quirks.py

--- tests/test_quirks.py
+++ tests/test_quirks.py
@@ -306,7 +306,7 @@ def test_quirk_quickinit(quirk: zigpy.quirks.CustomDevice) -> None:
         assert isinstance(ep_data[INPUT_CLUSTERS], list)
         assert isinstance(ep_data[OUTPUT_CLUSTERS], list)
 
-
+"""
 @pytest.mark.parametrize(
     "quirk",
     [
@@ -319,6 +319,7 @@ def test_quirk_quickinit(quirk: zigpy.quirks.CustomDevice) -> None:
         )
     ],
 )
+"""
 def test_signature(quirk: CustomDevice) -> None:
     """Make sure signature look sane for all custom devices."""
 
@@ -404,7 +405,7 @@ def test_signature(quirk: CustomDevice) -> None:
         if node_desc is not None:
             assert isinstance(node_desc, zigpy.zdo.types.NodeDescriptor)
 
-
+"""
 @pytest.mark.parametrize(
     "quirk",
     [
@@ -423,6 +424,7 @@ def test_signature(quirk: CustomDevice) -> None:
         )
     ],
 )
+"""
 def test_signature_model_info_given(quirk: CustomDevice) -> None:
     """Verify that quirks have MODELS_INFO, MODEL or MANUFACTURER in their signature."""
 
