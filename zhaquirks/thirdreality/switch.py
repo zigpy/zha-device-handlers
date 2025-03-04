50aba6aa4f885a9b9ab14f107edff0e85b77718b
hwzolin <448222024@qq.com>
2025-03-04 16:36:55 +0800

    Modify the switch's private cluster.

--- zhaquirks/thirdreality/switch.py
+++ zhaquirks/thirdreality/switch.py
@@ -1,118 +1,41 @@
 """Third Reality switch devices."""
 
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Groups, Identify, OnOff, Ota, Scenes
-from zigpy.zcl.clusters.homeautomation import Diagnostic
+from typing import Final
 
-from zhaquirks import PowerConfigurationCluster
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.quirks.v2.homeassistant import UnitOfTime
+import zigpy.types as t
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
+
+
+class ThirdRealitySwitchCluster(CustomCluster):
+    """Third Reality's temperature and humidity sensor private cluster."""
+
+    cluster_id = 0xFF02
+
+    class AttributeDefs(BaseAttributeDefs):
+        """Define the attributes of a private cluster."""
+
+        count_down_time: Final = ZCLAttributeDef(
+            id=0x0001,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+
+
+(
+    QuirkBuilder("Third Reality, Inc", "3RSS009Z")
+    .replaces(ThirdRealitySwitchCluster)
+    .number(
+        attribute_name=ThirdRealitySwitchCluster.AttributeDefs.count_down_time.name,
+        min_value=0,
+        max_value=65535,
+        step=1,
+        unit=UnitOfTime.SECONDS,
+        cluster_id=ThirdRealitySwitchCluster.cluster_id,
+        translation_key="count_down_time",
+        fallback_name="Count Down Time",
+    )
+    .add_to_registry()
 )
-from zhaquirks.thirdreality import THIRD_REALITY
-
-
-class CustomPowerConfigurationCluster(PowerConfigurationCluster):
-    """Custom PowerConfigurationCluster."""
-
-    MIN_VOLTS = 2.1
-    MAX_VOLTS = 3.0
-
-
-class Switch(CustomDevice):
-    """3RSS008Z device."""
-
-    signature = {
-        # <SimpleDescriptor endpoint=1 profile=260
-        # device_type=2 device_version=1
-        # input_clusters=[0, 4, 3, 6, 5, 25, 1]
-        # output_clusters=[1]>
-        MODELS_INFO: [(THIRD_REALITY, "3RSS007Z"), (THIRD_REALITY, "3RSS008Z")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Ota.cluster_id,
-                    CustomPowerConfigurationCluster.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [CustomPowerConfigurationCluster.cluster_id],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Ota.cluster_id,
-                    CustomPowerConfigurationCluster,
-                ],
-                OUTPUT_CLUSTERS: [CustomPowerConfigurationCluster.cluster_id],
-            }
-        }
-    }
-
-
-class SwitchPlus(Switch):
-    """RealitySwitch Plus (3RSS008Z) device."""
-
-    signature = {
-        # <SimpleDescriptor endpoint=1 profile=260
-        # device_type=2 device_version=1
-        # input_clusters=[0, 4, 3, 6, 5, 1, 2821]
-        # output_clusters=[25]>
-        MODELS_INFO: [(THIRD_REALITY, "3RSS008Z")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Diagnostic.cluster_id,
-                    CustomPowerConfigurationCluster.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Diagnostic.cluster_id,
-                    CustomPowerConfigurationCluster,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        }
-    }
