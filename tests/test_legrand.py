beced0960f765f09117cd2d34bc359c9c7639532
André Auzi <aauzi@raspberrypi>
2025-03-09 01:22:30 +0100

    Add read_attributes coverage

--- tests/test_legrand.py
+++ tests/test_legrand.py
@@ -140,6 +140,19 @@ async def test_legrand_contactor_switch(zigpy_device_from_v2_quirk):
     auto_on_off_cluster.turn_off.assert_not_awaited()
     auto_on_off_cluster.toggle.assert_awaited_once()
 
+
+    # cover _read_attributes
+    auto_on_off_cluster._read_states = mock.AsyncMock()
+    switch_on_off_cluster.request = mock.AsyncMock()
+    await switch_on_off_cluster._read_attributes(["on_off"])
+    auto_on_off_cluster._read_states.assert_awaited_once()
+    switch_on_off_cluster.request.assert_awaited_once()
+
+    auto_on_off_cluster._read_states = mock.AsyncMock()
+    switch_on_off_cluster.request = mock.AsyncMock()
+    await switch_on_off_cluster._read_attributes([LegrandContactorSwitchOnOff.ON_OFF_ID])
+    auto_on_off_cluster._read_states.assert_awaited_once()
+    switch_on_off_cluster.request.assert_awaited_once()
     # cover _update_attribute
     switch_on_off_cluster._update_attribute(LegrandContactorSwitchOnOff.ON_OFF_ID, 0)
 
@@ -187,5 +200,17 @@ async def test_legrand_contactor_switch(zigpy_device_from_v2_quirk):
     auto_on_off_cluster.toggle.assert_not_awaited()
     switch_on_off_cluster.request.assert_awaited_once()
 
+    # cover _read_attributes
+    auto_on_off_cluster._read_states = mock.AsyncMock()
+    switch_on_off_cluster.request = mock.AsyncMock()
+    await switch_on_off_cluster._read_attributes(["on_off"])
+    auto_on_off_cluster._read_states.assert_not_awaited()
+    switch_on_off_cluster.request.assert_awaited_once()
+
+    auto_on_off_cluster._read_states = mock.AsyncMock()
+    switch_on_off_cluster.request = mock.AsyncMock()
+    await switch_on_off_cluster._read_attributes([LegrandContactorSwitchOnOff.ON_OFF_ID])
+    auto_on_off_cluster._read_states.assert_not_awaited()
+    switch_on_off_cluster.request.assert_awaited_once()
     # cover _update_attribute
     switch_on_off_cluster._update_attribute(LegrandContactorSwitchOnOff.ON_OFF_ID, 0)
