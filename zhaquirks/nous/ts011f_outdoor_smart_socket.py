0b911a4ed648de7da1a32e1b7cc9d0300b69940a
Julian Meisel <julian.meisel@gmail.com>
2025-03-10 21:14:20 +0100

    fix: back to v1 as data it is working here

--- zhaquirks/nous/ts011f_outdoor_smart_socket.py
+++ zhaquirks/nous/ts011f_outdoor_smart_socket.py
@@ -1,41 +1,128 @@
 """Nous A4Z Outdoor 2 plug with metering."""
 
-from zigpy.quirks import CustomCluster
-from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.profiles import zha
+from zigpy.quirks import CustomCluster, CustomDevice
+from zigpy.zcl.clusters.general import (
+    Basic,
+    GreenPowerProxy,
+    Groups,
+    Identify,
+    OnOff,
+    Ota,
+    Scenes,
+    Time,
+)
 from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
 from zigpy.zcl.clusters.smartenergy import Metering
 
-
-class CustomElectricalMeasurement(ElectricalMeasurement, CustomCluster):
-    """Custom electrical measurement cluster."""
-
-    _CONSTANT_ATTRIBUTES = {
-        ElectricalMeasurement.AttributeDefs.ac_current_multiplier.id: 1,
-        ElectricalMeasurement.AttributeDefs.ac_current_divisor.id: 1000,
-    }
-
-
-class CustomMetering(Metering, CustomCluster):
-    """Custom metering cluster."""
-
-    KILOWATT_HOURS = 0x0
-    ELECTRIC_METERING = 0x0
-
-    _CONSTANT_ATTRIBUTES = {
-        Metering.AttributeDefs.unit_of_measure.id: KILOWATT_HOURS,
-        Metering.AttributeDefs.metering_device_type.id: ELECTRIC_METERING,
-        Metering.AttributeDefs.multiplier.id: 1,
-        Metering.AttributeDefs.divisor.id: 100,
-    }
-
-
-(
-    QuirkBuilder("_TZ3000_uwkja6z1", "TS011F")
-    .friendly_name(manufacturer="Nous", model="A4Z ZigBee Outdoor Smart Socket")
-    .replaces(CustomMetering, endpoint_id=1)
-    .replaces(CustomElectricalMeasurement, endpoint_id=1)
-    # We remove endpoint 2 measurement and metering, because its duplicated
-    .removes(Metering.cluster_id, endpoint_id=2)
-    .removes(ElectricalMeasurement.cluster_id, endpoint_id=2)
-    .add_to_registry()
+from zhaquirks.const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
 )
+
+
+class MeteringCluster(CustomCluster, Metering):
+    """Correct wrong metering with Nous A4Z."""
+
+    MULTIPLIER = 0x0301
+    DIVISOR = 0x0302
+    _CONSTANT_ATTRIBUTES = {MULTIPLIER: 1, DIVISOR: 100}
+
+
+class TuyaZBElectricalMeasurement(CustomCluster, ElectricalMeasurement):
+    """Divides the Current for tuya."""
+
+    AC_CURRENT_MULTIPLIER = 0x0602
+    AC_CURRENT_DIVISOR = 0x0603
+    _CONSTANT_ATTRIBUTES = {AC_CURRENT_MULTIPLIER: 1, AC_CURRENT_DIVISOR: 1000}
+
+
+class NousOutdoor2Plug(CustomDevice):
+    """Nous A4Z Outdoor 2 plug with metering."""
+
+    signature = {
+        MODELS_INFO: [("_TZ3000_uwkja6z1", "TS011F")],
+        ENDPOINTS: {
+            1: {
+                "profile_id": 0x0104,
+                "device_type": zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
+                "input_clusters": [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    Metering.cluster_id,
+                    0x0B04,
+                    0xE000,
+                    0xE001,
+                ],
+                "output_clusters": [Time.cluster_id, Ota.cluster_id],
+            },
+            2: {
+                "profile_id": 0x0104,
+                "device_type": zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
+                "input_clusters": [
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    Metering.cluster_id,
+                    0x0B04,
+                    0xE000,
+                    0xE001,
+                ],
+                "output_clusters": [],
+            },
+            242: {
+                PROFILE_ID: 0xA1E0,
+                DEVICE_TYPE: 0x0061,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
+
+    replacement = {
+        "endpoints": {
+            1: {
+                "profile_id": 0x0104,
+                "device_type": zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
+                "input_clusters": [
+                    Basic.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    MeteringCluster,
+                    TuyaZBElectricalMeasurement,
+                    0xE000,
+                    0xE001,
+                ],
+                "output_clusters": [Time.cluster_id, Ota.cluster_id],
+            },
+            2: {
+                "profile_id": 0x0104,
+                "device_type": zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
+                "input_clusters": [
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    OnOff.cluster_id,
+                    0xE000,
+                    0xE001,
+                ],
+                "output_clusters": [],
+            },
+            242: {
+                PROFILE_ID: 0xA1E0,
+                DEVICE_TYPE: 0x0061,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        }
+    }
