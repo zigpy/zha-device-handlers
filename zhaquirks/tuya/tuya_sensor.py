2b875ca46c934bd371245a853598234e39b00915
Anton Berezhnyi <berejant@gmail.com>
2025-04-10 22:31:01 +0300

    Update tuya_sensor.py
    New devices has new model code

--- zhaquirks/tuya/tuya_sensor.py
+++ zhaquirks/tuya/tuya_sensor.py
@@ -77,6 +77,7 @@ class NoManufTimeTuyaMCUCluster(TuyaMCUCluster):
     .applies_to("_TZE284_qyflbnbj", "TS0601")
     .applies_to("_TZE200_44af8vyi", "TS0601")
     .applies_to("_TZE200_vvmbj46n", "TS0601")
+    .applies_to("_TZE284_vvmbj46n", "TS0601")
     # Not using tuya_temperature because device reports negative values incorrectly
     .tuya_dp(
         dp_id=1,
