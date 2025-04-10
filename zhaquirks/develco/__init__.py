026d23e9519abda5cf88512b24b3c4f588959efb
puddly <32534428+puddly@users.noreply.github.com>
2025-04-10 13:50:05 -0400

    Clean up duplicate clusters

--- zhaquirks/develco/__init__.py
+++ zhaquirks/develco/__init__.py
@@ -1,9 +1,10 @@
 """Quirks for Develco Products A/S."""
 
-from zigpy import types as t
-from zigpy.quirks import CustomCluster
+from typing import Final
+
+import zigpy.types as t
 from zigpy.zcl import foundation
-from zigpy.zcl.clusters.security import IasZone
+from zigpy.zcl.clusters.security import IasZone, ZoneStatus
 
 from zhaquirks import PowerConfigurationCluster
 
@@ -18,23 +19,20 @@ class DevelcoPowerConfiguration(PowerConfigurationCluster):
     MAX_VOLTS = 3.0  # old 3.2
 
 
-class DevelcoIasZone(CustomCluster, IasZone):
-    """Custom IasZone for Develco."""
+class DevelcoIasZone(IasZone):
+    """IAS Zone, patched to fix a bug with the status change notification command."""
 
-    client_commands = {
-        0x00: foundation.ZCLCommandDef(
-            "status_change_notification",
-            {
-                "zone_status": IasZone.ZoneStatus,
-                "extended_status?": t.bitmap8,
+    class ClientCommandDefs(IasZone.ClientCommandDefs):
+        """IAS Zone command definitions."""
+
+        status_change_notification: Final = foundation.ZCLCommandDef(
+            id=0x00,
+            schema={
+                "zone_status": ZoneStatus,
+                "extended_status": t.bitmap8,
+                # These two should not be optional
                 "zone_id?": t.uint8_t,
                 "delay?": t.uint16_t,
             },
-            False,
-        ),
-        0x01: foundation.ZCLCommandDef(
-            "enroll",
-            {"zone_type": IasZone.ZoneType, "manufacturer_code": t.uint16_t},
-            False,
-        ),
-    }
+            direction=foundation.Direction.Client_to_Server,
+        )
--- zhaquirks/develco/open_close.py
+++ zhaquirks/develco/open_close.py
@@ -1,14 +1,11 @@
 """Door/Windows sensors."""
 
-from typing import Final
-
 from zigpy.quirks.v2 import QuirkBuilder
-import zigpy.types as t
 from zigpy.zcl.clusters.general import BinaryInput
-from zigpy.zcl.clusters.security import IasZone
-from zigpy.zcl.foundation import Direction, ZCLCommandDef, ZoneStatus
 
 from zhaquirks import PowerConfigurationCluster
+
+from . import DevelcoIasZone
 
 
 class DevelcoPowerConfiguration(PowerConfigurationCluster):
@@ -18,32 +15,13 @@ class DevelcoPowerConfiguration(PowerConfigurationCluster):
     MAX_VOLTS = 3.0
 
 
-class DevelcoIASZone(IasZone):
-    """IAS Zone, patched to fix a bug with the status change notification command."""
-
-    class ClientCommandDefs(IasZone.ClientCommandDefs):
-        """IAS Zone command definitions."""
-
-        status_change_notification: Final = ZCLCommandDef(
-            id=0x00,
-            schema={
-                "zone_status": ZoneStatus,
-                "extended_status": t.bitmap8,
-                # These two should not be optional
-                "zone_id?": t.uint8_t,
-                "delay?": t.uint16_t,
-            },
-            direction=Direction.Client_to_Server,
-        )
-
-
 (
     QuirkBuilder("frient A/S", "WISZB-131")
     .applies_to("Develco Products A/S", "WISZB-120")
     .applies_to("frient A/S", "WISZB-120")
     .applies_to("Develco Products A/S", "WISZB-121")
     .applies_to("frient A/S", "WISZB-121")
-    .replaces(DevelcoIASZone, endpoint_id=35)
+    .replaces(DevelcoIasZone, endpoint_id=35)
     .replaces(DevelcoPowerConfiguration, endpoint_id=35)
     # The binary input cluster is a duplicate
     .prevent_default_entity_creation(endpoint_id=35, cluster_id=BinaryInput.cluster_id)
