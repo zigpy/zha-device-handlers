69262bfaeb4057d199f1c6b7b4db612c23654c6d
puddly <32534428+puddly@users.noreply.github.com>
2025-04-21 17:49:55 -0400

    Use automation triggers for button

--- zhaquirks/develco/smart_button.py
+++ zhaquirks/develco/smart_button.py
@@ -2,17 +2,30 @@
 
 from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl import ClusterType
-from zigpy.zcl.clusters.general import OnOff
+from zigpy.zcl.clusters.general import BinaryInput, OnOff
+
+from zhaquirks.const import BUTTON, CLUSTER_ID, COMMAND, COMMAND_CLICK, ENDPOINT_ID
 
 (
     QuirkBuilder("frient A/S", "SBTZB-110")
-    # The button emits `toggle()` commands in addition to attribute updates.
-    # The `toggle()` command is unreliable, since the entity state will never match the
-    # real state of the button if a command is lost.
     .prevent_default_entity_creation(
         endpoint_id=32,
         cluster_id=OnOff.cluster_id,
         cluster_type=ClusterType.Client,
+    )
+    # Don't create a binary entity for a button, instead create automation triggers
+    .prevent_default_entity_creation(
+        endpoint_id=32,
+        cluster_id=BinaryInput.cluster_id,
+    )
+    .device_automation_triggers(
+        {
+            (COMMAND_CLICK, BUTTON): {
+                ENDPOINT_ID: 32,
+                CLUSTER_ID: int(OnOff.cluster_id),
+                COMMAND: OnOff.ServerCommandDefs.toggle.name,
+            },
+        }
     )
     .add_to_registry()
 )
