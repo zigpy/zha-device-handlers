d10e193bfa41b9875d88f3896c064570bdf456ac
cohackw <49957005+codyhackw@users.noreply.github.com>
2025-04-30 18:47:29 -0400

    Update VZM35SN.py
    Update VZM35-SN to include the manufacturer cluster for EP2 and 3

--- zhaquirks/inovelli/VZM35SN.py
+++ zhaquirks/inovelli/VZM35SN.py
@@ -7,6 +7,8 @@ from zhaquirks.inovelli import INOVELLI_AUTOMATION_TRIGGERS, InovelliVZM35SNClus
 (
     QuirkBuilder("Inovelli", "VZM35-SN")
     .replace_cluster_occurrences(InovelliVZM35SNCluster)
+    .replaces(InovelliVZM35SNCluster, endpoint_id=2)
+    .replaces(InovelliVZM35SNCluster, endpoint_id=3)
     .device_automation_triggers(INOVELLI_AUTOMATION_TRIGGERS)
     .add_to_registry()
 )
