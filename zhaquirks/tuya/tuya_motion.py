460f0eae5b0f4068b1c88c0b445f7959b713c116
Lasse Brun <bruners@gmail.com>
2025-05-03 13:55:14 +0200

    Add Tuya mmWave 24Ghz radar , _TZE204_gkfbdvyx
    This commits adds support for Tuya compatible mmWave 24Ghz ceiling mounted human presence sensor.
    The unit seems to be in line with _TZE204_ya4ft0w so they could possible be merged if tested. This unit does not like sensitivity set to 0.

--- zhaquirks/tuya/tuya_motion.py
+++ zhaquirks/tuya/tuya_motion.py
@@ -1610,3 +1610,110 @@ base_tuya_motion = (
     .skip_configuration()
     .add_to_registry()
 )
+
+# Tuya mmWave radar 24GHz, _TZE204_gkfbdvyx
+# Loginovo Smart Human Presence Sensor M100, Model C3007
+# TZ-HS-24G / ZY-M100-24G / Ceiling Human Breathe Sensor 24G
+(
+    TuyaQuirkBuilder("_TZE204_gkfbdvyx", "TS0601")
+    # 0: presence="none", state=false
+    # 1: presence="presence" state=true triggers only with presence sensitivy on low
+    # 2: presence="move?" state=true
+    .tuya_enum(
+        dp_id=1,
+        attribute_name="presence",
+        enum_class=TuyaPresenceStateV02, # needs more work
+        entity_platform=EntityPlatform.SENSOR,
+        entity_type=EntityType.STANDARD,
+        translation_key="presence_state",
+        fallback_name="Presence state",
+    )
+    .tuya_dp(
+        dp_id=104,
+        ep_attribute=TuyaOccupancySensing.ep_attribute,
+        attribute_name=OccupancySensing.AttributeDefs.occupancy.name,
+        converter=lambda x: x == 1,
+    )
+    .adds(TuyaOccupancySensing)
+    .tuya_number(
+        dp_id=2,
+        attribute_name="radar_sensitivity",
+        type=t.uint16_t,
+        min_value=1,
+        max_value=10,
+        step=1,
+        translation_key="radar_sensitivity",
+        fallback_name="Radar sensitivity",
+    )
+    .tuya_number(
+        dp_id=3,
+        attribute_name="detection_distance_min",
+        type=t.uint16_t,
+        multiplier=0.01,
+        device_class=SensorDeviceClass.DISTANCE,
+        unit=UnitOfLength.METERS,
+        min_value=0,
+        max_value=6,
+        step=0.5,
+        translation_key="detection_distance_min",
+        fallback_name="Minimum range",
+    )
+    .tuya_number(
+        dp_id=4,
+        attribute_name="detection_distance_max",
+        type=t.uint16_t,
+        multiplier=0.01,
+        device_class=SensorDeviceClass.DISTANCE,
+        unit=UnitOfLength.METERS,
+        min_value=0.5,
+        max_value=9, # tested on 6m
+        step=0.5,
+        translation_key="detection_distance_max",
+        fallback_name="Maximum range",
+    )
+    .tuya_sensor(
+        dp_id=9,
+        attribute_name="distance",
+        type=t.uint16_t,
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.DISTANCE,
+        unit=UnitOfLength.METERS,
+        multiplier=0.1,
+        entity_type=EntityType.STANDARD,
+        translation_key="target_distance",
+        fallback_name="Target distance",
+    )
+    .tuya_switch(
+        # Turns distance reporting on and off
+        dp_id=101,
+        attribute_name="distance_tracking",
+        entity_type=EntityType.STANDARD,
+        translation_key="distance_tracking",
+        fallback_name="Distance tracking",
+    )
+    .tuya_number(
+        dp_id=102,
+        attribute_name="presence_sensitivity",
+        type=t.uint16_t,
+        min_value=1,
+        max_value=10,
+        step=1,
+        translation_key="presence_sensitivity",
+        fallback_name="Presence Sensitivity",
+    )
+    .tuya_illuminance(dp_id=103)
+    .tuya_number(
+        dp_id=105,
+        attribute_name="presence_timeout",
+        type=t.uint16_t,
+        device_class=SensorDeviceClass.DURATION,
+        unit=UnitOfTime.SECONDS,
+        min_value=1,
+        max_value=1500,
+        step=1,
+        translation_key="fading_time",
+        fallback_name="Fading time",
+    )
+    .skip_configuration()
+    .add_to_registry()
+)
