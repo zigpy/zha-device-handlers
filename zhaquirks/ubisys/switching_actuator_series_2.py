d131bcce86b2a1bbf43a990347721003d96748db
puddly <32534428+puddly@users.noreply.github.com>
2025-05-09 14:22:26 -0400

    Initial support for S1-R

---
+++ zhaquirks/ubisys/switching_actuator_series_2.py
@@ -0,0 +1,21 @@
+"""Ubisys Switching Actuator S1-R (Series 2) quirk."""
+
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
+
+
+class UbisysElectricalMeasurement(CustomCluster, ElectricalMeasurement):
+    """Sets divisor attributes missing on the device."""
+
+    _CONSTANT_ATTRIBUTES = {
+        ElectricalMeasurement.AttributeDefs.ac_voltage_divisor.id: 10,
+        ElectricalMeasurement.AttributeDefs.ac_frequency_divisor.id: 10,
+    }
+
+
+(
+    QuirkBuilder(manufacturer="ubisys", model="S1-R (5601)")
+    .replaces(UbisysElectricalMeasurement)
+    .add_to_registry()
+)
