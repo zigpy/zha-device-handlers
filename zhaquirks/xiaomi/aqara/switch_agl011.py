eb0f7d838e1f6973b52b5ef1bbd07f6c776c4090
lersveen <7195448+lersveen@users.noreply.github.com>
2025-05-29 22:44:48 +0200

    Update switch_agl011.py

--- zhaquirks/xiaomi/aqara/switch_agl011.py
+++ zhaquirks/xiaomi/aqara/switch_agl011.py
@@ -2,7 +2,6 @@
 
 from zigpy import types as t
 from zigpy.profiles import zha
-from zigpy.quirks import CustomCluster
 from zigpy.zcl.clusters.general import (
     AnalogInput,
     Basic,
@@ -46,6 +45,12 @@ class OppleCluster(XiaomiAqaraE1Cluster):
         Medium = 0x0168  # 360
         High = 0x00B4  # 180
 
+    class Phase(t.enum8):
+        """Startup mode."""
+
+        Forward = 0x0000
+        Reverse = 0x0001
+
     class MinBrightness(t.uint8_t):
         """Minimum brightness."""
 
@@ -64,23 +69,17 @@ class OppleCluster(XiaomiAqaraE1Cluster):
             if not (1 <= value <= 100):
                 raise ValueError("Maximum brightness must be between 1 and 100.")
 
-    class Phase(t.enum8):
-        """Startup mode."""
-
-        Forward = 0
-        Reverse = 1
-
     attributes = {
-        0x0234: ("sensitivity", Sensitivity),
-        0x0515: ("min_brightness", MinBrightness),
-        0x0516: ("max_brightness", MaxBrightness),
-        0x030a: ("phase", t.enum8),
+        0x0234: ("sensitivity", Sensitivity, True),
+        0x030A: ("phase", Phase, True),
+        0x0515: ("min_brightness", MinBrightness, True),
+        0x0516: ("max_brightness", MaxBrightness, True),
     }
 
     def _update_attribute(self, attrid, value):
         """Handle attribute updates."""
         super()._update_attribute(attrid, value)
-        if attrid in [0x0234, 0x0515, 0x0516, 0x030a]:
+        if attrid in self.attributes:
             self.listener_event(
                 ZHA_SEND_EVENT,
                 {
@@ -174,7 +173,7 @@ class AqaraDimmerSwitchH2EU(XiaomiCustomDevice):
         ENDPOINTS: {
             1: {
                 PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+                DEVICE_TYPE: zha.DeviceType.DIMMABLE_LIGHT,
                 INPUT_CLUSTERS: [
                     BasicCluster,
                     Identify.cluster_id,
@@ -185,24 +184,9 @@ class AqaraDimmerSwitchH2EU(XiaomiCustomDevice):
                     MultistateInput.cluster_id,
                     MeteringCluster,
                     ElectricalMeasurementCluster,
-                    AqaraRotationSensitivityCluster,
+                    OppleCluster,
                 ],
                 OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
             },
         },
     }
-
-
-# 2025-05-29 21:14:28.721 DEBUG (MainThread) [bellows.ezsp.protocol] Received command incomingMessageHandler: {'type': <EmberIncomingMessageType.INCOMING_UNICAST: 0>, 'apsFrame': EmberApsFrame(profileId=260, clusterId=64704, sourceEndpoint=1, destinationEndpoint=1, options=<EmberApsOption.APS_OPTION_ENABLE_ROUTE_DISCOVERY: 256>, groupId=0, sequence=135), 'lastHopLqi': 204, 'lastHopRssi': -60, 'sender': 0xF686, 'bindingIndex': 255, 'addressIndex': 6, 'messageContents': b'\x1c_\x11S\n\x15\x05 A\x16\x05 d'}
-# 2025-05-29 21:14:28.721 DEBUG (MainThread) [bellows.ezsp.protocol] Frame contains trailing data: b'\x04'
-# 2025-05-29 21:14:28.721 DEBUG (MainThread) [bellows.zigbee.application] Received incomingMessageHandler frame with [<EmberIncomingMessageType.INCOMING_UNICAST: 0>, EmberApsFrame(profileId=260, clusterId=64704, sourceEndpoint=1, destinationEndpoint=1, options=<EmberApsOption.APS_OPTION_ENABLE_ROUTE_DISCOVERY: 256>, groupId=0, sequence=135), 204, -60, 0xF686, 255, 6, b'\x1c_\x11S\n\x15\x05 A\x16\x05 d']
-# 2025-05-29 21:14:28.722 DEBUG (MainThread) [zigpy.application] Received a packet: ZigbeePacket(timestamp=datetime.datetime(2025, 5, 29, 19, 14, 28, 722327, tzinfo=datetime.timezone.utc), priority=0, src=AddrModeAddress(addr_mode=<AddrMode.NWK: 2>, address=0xF686), src_ep=1, dst=AddrModeAddress(addr_mode=<AddrMode.NWK: 2>, address=0x0000), dst_ep=1, source_route=None, extended_timeout=False, tsn=135, profile_id=260, cluster_id=64704, data=Serialized[b'\x1c_\x11S\n\x15\x05 A\x16\x05 d'], tx_options=<TransmitOptions.NONE: 0>, radius=0, non_member_radius=0, lqi=204, rssi=-60)
-# 2025-05-29 21:14:28.723 DEBUG (MainThread) [zigpy.zcl] [0xF686:1:0xfcc0] Received ZCL frame: b'\x1c_\x11S\n\x15\x05 A\x16\x05 d'
-# 2025-05-29 21:14:28.723 DEBUG (MainThread) [zigpy.zcl] [0xF686:1:0xfcc0] Decoded ZCL frame header: ZCLHeader(frame_control=FrameControl<0x1C>(frame_type=<FrameType.GLOBAL_COMMAND: 0>, is_manufacturer_specific=True, direction=<Direction.Server_to_Client: 1>, disable_default_response=1, reserved=0, *is_cluster=False, *is_general=True), manufacturer=4447, tsn=83, command_id=10, *direction=<Direction.Server_to_Client: 1>)
-# 2025-05-29 21:14:28.725 DEBUG (MainThread) [zigpy.zcl] [0xF686:1:0xfcc0] Decoded ZCL frame: ManufacturerSpecificCluster:Report_Attributes(attribute_reports=[Attribute(attrid=0x0515, value=TypeValue(type=uint8_t, value=65)), Attribute(attrid=0x0516, value=TypeValue(type=uint8_t, value=100))])
-# 2025-05-29 21:14:28.725 DEBUG (MainThread) [zigpy.zcl] [0xF686:1:0xfcc0] Received command 0x0A (TSN 83): Report_Attributes(attribute_reports=[Attribute(attrid=0x0515, value=TypeValue(type=uint8_t, value=65)), Attribute(attrid=0x0516, value=TypeValue(type=uint8_t, value=100))])
-# 2025-05-29 21:14:28.729 DEBUG (MainThread) [zigpy.zcl] [0xF686:1:0xfcc0] Attribute report received: 0x0515=65, 0x0516=100
-# 2025-05-29 21:14:28.729 DEBUG (MainThread) [zha.zigbee.cluster_handlers] [0xF686:1:0xfcc0]: cluster_handler[manufacturer_specific] attribute_updated - cluster[Manufacturer Specific] attr[1301] value[65]
-# 2025-05-29 21:14:28.729 DEBUG (MainThread) [zha] Emitting event cluster_handler_attribute_updated with data ClusterAttributeUpdatedEvent(attribute_id=1301, attribute_name=1301, attribute_value=65, cluster_handler_unique_id='54:ef:44:10:01:1c:6f:78:1:0xfcc0', cluster_id=64704, event_type='cluster_handler_event', event='cluster_handler_attribute_updated') (0 listeners)
-# 2025-05-29 21:14:28.730 DEBUG (MainThread) [zha.zigbee.cluster_handlers] [0xF686:1:0xfcc0]: cluster_handler[manufacturer_specific] attribute_updated - cluster[Manufacturer Specific] attr[1302] value[100]
-# 2025-05-29 21:14:28.730 DEBUG (MainThread) [zha] Emitting event cluster_handler_attribute_updated with data ClusterAttributeUpdatedEvent(attribute_id=1302, attribute_name=1302, attribute_value=100, cluster_handler_unique_id='54:ef:44:10:01:1c:6f:78:1:0xfcc0', cluster_id=64704, event_type='cluster_handler_event', event='cluster_handler_attribute_updated') (0 listeners)
