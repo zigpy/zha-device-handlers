e776debadfeaa86ee3b25c935fc2ade18aa81c30
jintj <jintj@3reality.com>
2025-06-06 14:57:26 +0800

    updatye

---
+++ zhaquirks/thirdreality/plug_dual.py
@@ -0,0 +1,104 @@
+"""Third Reality plug devices."""
+
+from typing import Final
+
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
+import zigpy.types as t
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
+
+
+class ThirdRealityPlugCluster(CustomCluster):
+    """Third Reality's plug private cluster."""
+
+    cluster_id = 0xFF03
+
+    class AttributeDefs(BaseAttributeDefs):
+        """Define the attributes of a private cluster."""
+
+        reset_summation_delivered: Final = ZCLAttributeDef(
+            id=0x0000,
+            type=t.uint8_t,
+            is_manufacturer_specific=True,
+        )
+        
+        on_to_off_delay: Final = ZCLAttributeDef(
+            id=0x0001,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+        
+        off_to_on_delay: Final = ZCLAttributeDef(
+            id=0x0002,
+            type=t.uint16_t,
+            is_manufacturer_specific=True,
+        )
+
+
+(
+    QuirkBuilder("Third Reality, Inc", "3RDP01072Z")
+    .also_applies_to("Third Reality, Inc", "3RSPE01044BZ")
+    .replaces(ThirdRealityPlugCluster, endpoint_id=1)
+	.replaces(ThirdRealityPlugCluster, endpoint_id=2)
+    .write_attr_button(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.reset_summation_delivered.name,
+        attribute_value=0x01,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        translation_key="reset_summation_delivered",
+        fallback_name="Reset summation delivered1",
+        endpoint_id=1,
+    )
+	.write_attr_button(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.reset_summation_delivered.name,
+        attribute_value=0x01,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        translation_key="reset_summation_delivered",
+        fallback_name="Reset summation delivered1",
+        endpoint_id=2,
+    )
+    .number(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.on_to_off_delay.name,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        endpoint_id=1,
+        min_value=0,
+        max_value=65535,
+        step=1,
+        mode="box",
+        translation_key="on_to_off_delay",
+        fallback_name="On to off delay",
+    )
+    .number(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.on_to_off_delay.name,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        endpoint_id=2,
+        min_value=0,
+        max_value=65535,
+        step=1,
+        mode="box",
+        translation_key="on_to_off_delay",
+        fallback_name="On to off delay",
+    )
+    .number(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.off_to_on_delay.name,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        endpoint_id=1,
+        min_value=0,
+        max_value=65535,
+        step=1,
+        mode="box",
+        translation_key="off_to_on_delay",
+        fallback_name="Off to on delay",
+    )
+    .number(
+        attribute_name=ThirdRealityPlugCluster.AttributeDefs.off_to_on_delay.name,
+        cluster_id=ThirdRealityPlugCluster.cluster_id,
+        endpoint_id=2,
+        min_value=0,
+        max_value=65535,
+        step=1,
+        mode="box",
+        translation_key="off_to_on_delay",
+        fallback_name="Off to on delay",
+    )
+    .add_to_registry()
+)
