270c11981f5b71a6b0df56df17cce0b62de3045f
martyn-vesternet <development@candeo.io>
2025-06-12 11:17:50 +0100

    Update candeo_sensors_V2.py
    Reinstate Identify Cluster

--- zhaquirks/candeo/candeo_sensors_V2.py
+++ zhaquirks/candeo/candeo_sensors_V2.py
@@ -11,28 +11,21 @@ from zhaquirks.candeo import (
     CandeoIlluminanceMeasurementCluster,
 )
 
-base_quirk = QuirkBuilder().removes(Identify.cluster_id)
-
 (
-    base_quirk.clone()
-    .applies_to(CANDEO, "C-ZB-SEDC")
+    QuirkBuilder(CANDEO, "C-ZB-SEDC")
     .replaces(CandeoIasZoneContactCluster)
     .add_to_registry()
 )
 
 (
-    base_quirk.clone()
-    .applies_to(CANDEO, "C-ZB-SEMO")
+    QuirkBuilder(CANDEO, "C-ZB-SEMO")
     .replaces(CandeoIasZoneMotionCluster)
     .replaces(CandeoIlluminanceMeasurementCluster)
     .add_to_registry()
 )
 
 (
-    base_quirk.clone()
-    .applies_to(CANDEO, "C-ZB-SEWA")
+    QuirkBuilder(CANDEO, "C-ZB-SEWA")
     .replaces(CandeoIasZoneWaterCluster)
     .add_to_registry()
 )
-
-(base_quirk.clone().applies_to(CANDEO, "C-ZB-SETE").add_to_registry())
