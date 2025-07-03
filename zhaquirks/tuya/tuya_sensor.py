368f7faef250c9e04e03ad15bb20a787c2310e09
Marc-André Kolly <marc-andre@kolly.de>
2025-07-03 02:05:19 +0200

    Add Tuya soil sensor `_TZE200_wqashyqo` variant

--- zhaquirks/tuya/tuya_sensor.py
+++ zhaquirks/tuya/tuya_sensor.py
@@ -4,10 +4,13 @@ import copy
 
 from zigpy.quirks.v2 import EntityPlatform, EntityType
 from zigpy.quirks.v2.homeassistant import PERCENTAGE, UnitOfTemperature, UnitOfTime
+from zigpy.quirks.v2.homeassistant.binary_sensor import BinarySensorDeviceClass
+from zigpy.quirks.v2.homeassistant.number import NumberDeviceClass
 from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass
 import zigpy.types as t
 from zigpy.zcl import foundation
 
+from zhaquirks.const import BatterySize
 from zhaquirks.tuya import (
     TUYA_SET_TIME,
     TuyaPowerConfigurationCluster2AAA,
@@ -295,6 +298,112 @@ class NoManufTimeTuyaMCUCluster(TuyaMCUCluster):
     .add_to_registry()
 )
 
+
+(
+    TuyaQuirkBuilder("_TZE200_wqashyqo", "TS0601")
+    .tuya_binary_sensor(
+        dp_id=1,
+        attribute_name="dry",
+        entity_type=EntityType.STANDARD,
+        device_class=BinarySensorDeviceClass.MOISTURE,
+        translation_key="dry",
+        fallback_name="Dry",
+    )
+    .tuya_number(
+        dp_id=102,
+        attribute_name="soil_moisture_calibration",
+        type=t.uint16_t,
+        unit=PERCENTAGE,
+        min_value=-30,
+        max_value=30,
+        step=1,
+        entity_type=EntityType.CONFIG,
+        device_class=NumberDeviceClass.HUMIDITY,
+        translation_key="soil_moisture_calibration",
+        fallback_name="Soil moisture Calibration",
+    )
+    .tuya_temperature(dp_id=103, scale=10)
+    .tuya_number(
+        dp_id=104,
+        attribute_name="temperature_calibration",
+        type=t.uint16_t,
+        unit=UnitOfTemperature.KELVIN,
+        min_value=-2,
+        max_value=2,
+        step=0.1,
+        multiplier=10,
+        entity_type=EntityType.CONFIG,
+        device_class=NumberDeviceClass.TEMPERATURE,
+        translation_key="temperature_calibration",
+        fallback_name="Temperature Calibration",
+    )
+    .tuya_number(
+        dp_id=105,
+        attribute_name="humidity_calibration",
+        type=t.uint16_t,
+        unit=PERCENTAGE,
+        min_value=-30,
+        max_value=30,
+        step=1,
+        entity_type=EntityType.CONFIG,
+        device_class=NumberDeviceClass.HUMIDITY,
+        translation_key="humidity_calibration",
+        fallback_name="Humidity Calibration",
+    )
+    .tuya_enum(
+        dp_id=106,
+        attribute_name="display_unit",
+        enum_class=TuyaTempUnitConvert,
+        entity_type=EntityType.CONFIG,
+        translation_key="display_unit",
+        fallback_name="Display unit",
+    )
+    .tuya_soil_moisture(dp_id=107)
+    .tuya_battery(dp_id=108, battery_type=BatterySize.AAA)
+    .tuya_humidity(dp_id=109)
+    .tuya_number(
+        dp_id=110,
+        attribute_name="alarm_soil_moisture_min",
+        type=t.uint16_t,
+        unit=PERCENTAGE,
+        min_value=0,
+        max_value=100,
+        step=1,
+        entity_type=EntityType.CONFIG,
+        device_class=NumberDeviceClass.MOISTURE,
+        translation_key="alarm_soil_moisture_min",
+        fallback_name="Alarm soil moisture min",
+    )
+    .tuya_number(
+        dp_id=111,
+        attribute_name="temperature_sampling",
+        type=t.uint16_t,
+        unit=UnitOfTime.SECONDS,
+        min_value=5,
+        max_value=3600,
+        step=1,
+        entity_type=EntityType.CONFIG,
+        device_class=NumberDeviceClass.DURATION,
+        translation_key="temperature_sampling",
+        fallback_name="Temperature Sampling",
+    )
+    .tuya_number(
+        dp_id=112,
+        attribute_name="soil_moisture_sampling",
+        type=t.uint16_t,
+        unit=UnitOfTime.SECONDS,
+        min_value=5,
+        max_value=3600,
+        step=1,
+        entity_type=EntityType.CONFIG,
+        device_class=NumberDeviceClass.DURATION,
+        translation_key="soil_moisture_sampling",
+        fallback_name="Soil Moisture Sampling",
+    )
+    .tuya_enchantment(data_query_spell=True)
+    .skip_configuration()
+    .add_to_registry()
+)
 
 (
     TuyaQuirkBuilder("_TZE200_upagmta9", "TS0601")
