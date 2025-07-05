3d852bb82d0936d97ea501abdc0a23b277d57aa3
romaniuc <dev@arr.aff.pet>
2025-07-04 23:00:49 -0300

    Add quirk for Tuya ZTH05 (TS0601 _TZE284_upagmta9) under TS0601 _TZE200_upagmta9

--- zhaquirks/tuya/tuya_sensor.py
+++ zhaquirks/tuya/tuya_sensor.py
@@ -311,6 +311,7 @@ class NoManufTimeTuyaMCUCluster(TuyaMCUCluster):
     .applies_to("_TZE204_utkemkbs", "TS0601")
     .applies_to("_TZE284_utkemkbs", "TS0601")
     .applies_to("_TZE204_ksz749x8", "TS0601")
+    .applies_to("_TZE284_upagmta9", "TS0601")
     .tuya_temperature(dp_id=1, scale=10)
     .tuya_humidity(dp_id=2)
     .tuya_dp(
