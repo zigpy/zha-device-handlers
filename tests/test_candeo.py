2065d430c3a3076b44933008b91700bce5c06227
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-07-10 03:14:31 +0200

    Use `parametrize` in test and optimize

--- tests/test_candeo.py
+++ tests/test_candeo.py
@@ -1,5 +1,6 @@
 """Tests for Candeo."""
 
+import pytest
 from zigpy.zcl.clusters.measurement import IlluminanceMeasurement
 
 from tests.common import ClusterListener
@@ -9,26 +10,25 @@ from zhaquirks.candeo import CANDEO
 zhaquirks.setup()
 
 
-async def test_candeo_motion_illuminance(zigpy_device_from_v2_quirk):
+@pytest.mark.parametrize(
+    "lux_in, lux_out",
+    (
+        (0, 1),
+        (24112, 1),  # 1 lux
+        (33598, 18074),  # 64 lux
+        (34299, 26989),  # 500 lux
+        (34977, 36895),  # 4891 lux
+    ),
+)
+async def test_candeo_motion_illuminance(zigpy_device_from_v2_quirk, lux_in, lux_out):
     """Test that illuminance value is converted correctly."""
     device = zigpy_device_from_v2_quirk(CANDEO, "C-ZB-SEMO")
 
-    test_values = {
-        "test1": {"in": 33598, "out": 18074},
-        "test2": {"in": 24112, "out": 1},
-        "test3": {"in": 34977, "out": 36895},
-        "test4": {"in": 34299, "out": 26989},
-    }
+    illuminance_cluster = device.endpoints[1].illuminance
+    illuminance_listener = ClusterListener(illuminance_cluster)
+    illuminance_attr_id = IlluminanceMeasurement.AttributeDefs.measured_value.id
 
-    for test_value in test_values.values():
-        dev_illuminance_cluster = device.endpoints[1].illuminance
-        dev_illuminance_listener = ClusterListener(dev_illuminance_cluster)
-        dev_illuminance_attr_id = IlluminanceMeasurement.AttributeDefs.measured_value.id
-        test_in = test_value.get("in")
-        dev_illuminance_cluster.update_attribute(dev_illuminance_attr_id, test_in)
-        assert len(dev_illuminance_listener.attribute_updates) == 1
-        assert (
-            dev_illuminance_listener.attribute_updates[0][0] == dev_illuminance_attr_id
-        )
-        test_out = test_value.get("out")
-        assert dev_illuminance_listener.attribute_updates[0][1] == test_out
+    illuminance_cluster.update_attribute(illuminance_attr_id, lux_in)
+    assert len(illuminance_listener.attribute_updates) == 1
+    assert illuminance_listener.attribute_updates[0][0] == illuminance_attr_id
+    assert illuminance_listener.attribute_updates[0][1] == lux_out
