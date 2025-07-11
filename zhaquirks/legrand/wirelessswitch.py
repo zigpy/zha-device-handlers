931e069de877930aebf5793675cbf9975b85ec82
Niccolo Zapponi <nzapponi@gmail.com>
2025-07-11 12:11:33 +0200

    Updated device automation triggers

--- zhaquirks/legrand/wirelessswitch.py
+++ zhaquirks/legrand/wirelessswitch.py
@@ -47,17 +47,45 @@ from zhaquirks.legrand import LEGRAND, LegrandPowerConfigurationCluster
     .prevent_default_entity_creation(endpoint_id=1, cluster_id=BinaryInput.cluster_id)
     .device_automation_triggers(
         {
-            (SHORT_PRESS, TURN_ON): {COMMAND: COMMAND_ON},
-            (LONG_PRESS, TURN_ON): {
+            (TURN_ON, BUTTON_1): {COMMAND: COMMAND_ON, ENDPOINT_ID: 1},
+            (DIM_UP, BUTTON_1): {
                 COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 1,
                 PARAMS: {"move_mode": 0, "rate": 255},
             },
-            (SHORT_PRESS, TURN_OFF): {COMMAND: COMMAND_OFF},
-            (LONG_PRESS, TURN_OFF): {
+            (TURN_OFF, BUTTON_1): {COMMAND: COMMAND_OFF},
+            (DIM_DOWN, BUTTON_1): {
                 COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 1,
                 PARAMS: {"move_mode": 1, "rate": 255},
             },
-            (LONG_RELEASE, BUTTON): {COMMAND: COMMAND_STOP},
+            (LONG_RELEASE, BUTTON_1): {COMMAND: COMMAND_STOP, ENDPOINT_ID: 1},
+            (TURN_ON, BUTTON_2): {COMMAND: COMMAND_ON, ENDPOINT_ID: 2},
+            (DIM_UP, BUTTON_2): {
+                COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 2,
+                PARAMS: {"move_mode": 0, "rate": 255},
+            },
+            (TURN_OFF, BUTTON_2): {COMMAND: COMMAND_OFF},
+            (DIM_DOWN, BUTTON_2): {
+                COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 2,
+                PARAMS: {"move_mode": 1, "rate": 255},
+            },
+            (LONG_RELEASE, BUTTON_2): {COMMAND: COMMAND_STOP, ENDPOINT_ID: 2},
+            (TURN_ON, BUTTON_3): {COMMAND: COMMAND_ON, ENDPOINT_ID: 3},
+            (DIM_UP, BUTTON_3): {
+                COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 3,
+                PARAMS: {"move_mode": 0, "rate": 255},
+            },
+            (TURN_OFF, BUTTON_3): {COMMAND: COMMAND_OFF},
+            (DIM_DOWN, BUTTON_3): {
+                COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 3,
+                PARAMS: {"move_mode": 1, "rate": 255},
+            },
+            (LONG_RELEASE, BUTTON_3): {COMMAND: COMMAND_STOP, ENDPOINT_ID: 3},
         }
     )
     .add_to_registry()
@@ -69,17 +97,32 @@ from zhaquirks.legrand import LEGRAND, LegrandPowerConfigurationCluster
     .prevent_default_entity_creation(endpoint_id=1, cluster_id=BinaryInput.cluster_id)
     .device_automation_triggers(
         {
-            (SHORT_PRESS, TURN_ON): {COMMAND: COMMAND_ON},
-            (LONG_PRESS, TURN_ON): {
+            (TURN_ON, BUTTON_1): {COMMAND: COMMAND_ON, ENDPOINT_ID: 1},
+            (DIM_UP, BUTTON_1): {
                 COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 1,
                 PARAMS: {"move_mode": 0, "rate": 255},
             },
-            (SHORT_PRESS, TURN_OFF): {COMMAND: COMMAND_OFF},
-            (LONG_PRESS, TURN_OFF): {
+            (TURN_OFF, BUTTON_1): {COMMAND: COMMAND_OFF},
+            (DIM_DOWN, BUTTON_1): {
                 COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 1,
                 PARAMS: {"move_mode": 1, "rate": 255},
             },
-            (LONG_RELEASE, BUTTON): {COMMAND: COMMAND_STOP},
+            (LONG_RELEASE, BUTTON_1): {COMMAND: COMMAND_STOP, ENDPOINT_ID: 1},
+            (TURN_ON, BUTTON_2): {COMMAND: COMMAND_ON, ENDPOINT_ID: 2},
+            (DIM_UP, BUTTON_2): {
+                COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 2,
+                PARAMS: {"move_mode": 0, "rate": 255},
+            },
+            (TURN_OFF, BUTTON_2): {COMMAND: COMMAND_OFF},
+            (DIM_DOWN, BUTTON_2): {
+                COMMAND: COMMAND_MOVE,
+                ENDPOINT_ID: 2,
+                PARAMS: {"move_mode": 1, "rate": 255},
+            },
+            (LONG_RELEASE, BUTTON_2): {COMMAND: COMMAND_STOP, ENDPOINT_ID: 2},
         }
     )
     .add_to_registry()
