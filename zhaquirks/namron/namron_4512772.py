1f17a985af12f931cd142f6f730b48c368b757ac
Quint1337 <36964445+MAFL74@users.noreply.github.com>
2025-07-14 14:08:04 +0200

    Update namron_4512772.py
    Updated code to address ruff/PEP8 feedback from CI.  
    - Added docstrings  
    - Fixed imports and punctuation  
    - Reformatted for compliance

--- zhaquirks/namron/namron_4512772.py
+++ zhaquirks/namron/namron_4512772.py
@@ -1,18 +1,23 @@
-"""MAFL74 - Quirk for Namron 8-button Zigbee Switch 4512772"""
+"""Quirk for Namron 8-button Zigbee Switch 4512772.
 
-from zigpy.quirks import CustomDevice
+Custom ZHA quirk for exposing all button events for the Namron 4512772
+8-button Zigbee switch as device triggers for use in Home Assistant.
+"""
 
 from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
     MODELS_INFO,
-    OUTPUT_CLUSTERS,
+    ENDPOINTS,
     PROFILE_ID,
+    DEVICE_TYPE,
+    INPUT_CLUSTERS,
+    OUTPUT_CLUSTERS,
 )
+from zigpy.quirks import CustomDevice
 
 
 class Namron4512772(CustomDevice):
+    """Custom device handler for Namron 4512772 8-button Zigbee switch."""
+
     signature = {
         MODELS_INFO: [("NAMRON AS", "4512772")],
         ENDPOINTS: {
@@ -41,51 +46,23 @@ class Namron4512772(CustomDevice):
                 PROFILE_ID: 0x0104,
                 DEVICE_TYPE: 0x0001,
                 INPUT_CLUSTERS: [0x0000, 0x0001, 0x0003, 0x0B05, 0x1000],
-                OUTPUT_CLUSTERS: [
-                    0x0003,
-                    0x0004,
-                    0x0005,
-                    0x0006,
-                    0x0008,
-                    0x0019,
-                    0x0300,
-                    0x1000,
-                ],
+                OUTPUT_CLUSTERS: [0x0003, 0x0004, 0x0005, 0x0006, 0x0008, 0x0019, 0x0300, 0x1000],
             },
             3: {
                 PROFILE_ID: 0x0104,
                 DEVICE_TYPE: 0x0001,
                 INPUT_CLUSTERS: [0x0000, 0x0001, 0x0003, 0x0B05, 0x1000],
-                OUTPUT_CLUSTERS: [
-                    0x0003,
-                    0x0004,
-                    0x0005,
-                    0x0006,
-                    0x0008,
-                    0x0019,
-                    0x0300,
-                    0x1000,
-                ],
+                OUTPUT_CLUSTERS: [0x0003, 0x0004, 0x0005, 0x0006, 0x0008, 0x0019, 0x0300, 0x1000],
             },
             4: {
                 PROFILE_ID: 0x0104,
                 DEVICE_TYPE: 0x0001,
                 INPUT_CLUSTERS: [0x0000, 0x0001, 0x0003, 0x0B05, 0x1000],
-                OUTPUT_CLUSTERS: [
-                    0x0003,
-                    0x0004,
-                    0x0005,
-                    0x0006,
-                    0x0008,
-                    0x0019,
-                    0x0300,
-                    0x1000,
-                ],
+                OUTPUT_CLUSTERS: [0x0003, 0x0004, 0x0005, 0x0006, 0x0008, 0x0019, 0x0300, 0x1000],
             },
         },
     }
 
-    # Only endpoint 1 exposes battery (PowerConfiguration) and firmware (OTA)
     replacement = {
         MODELS_INFO: [("NAMRON AS", "4512772")],
         ENDPOINTS: {
@@ -114,43 +91,19 @@ class Namron4512772(CustomDevice):
                 PROFILE_ID: 0x0104,
                 DEVICE_TYPE: 0x0001,
                 INPUT_CLUSTERS: [0x0000, 0x0003, 0x0B05, 0x1000],
-                OUTPUT_CLUSTERS: [
-                    0x0003,
-                    0x0004,
-                    0x0005,
-                    0x0006,
-                    0x0008,
-                    0x0300,
-                    0x1000,
-                ],
+                OUTPUT_CLUSTERS: [0x0003, 0x0004, 0x0005, 0x0006, 0x0008, 0x0300, 0x1000],
             },
             3: {
                 PROFILE_ID: 0x0104,
                 DEVICE_TYPE: 0x0001,
                 INPUT_CLUSTERS: [0x0000, 0x0003, 0x0B05, 0x1000],
-                OUTPUT_CLUSTERS: [
-                    0x0003,
-                    0x0004,
-                    0x0005,
-                    0x0006,
-                    0x0008,
-                    0x0300,
-                    0x1000,
-                ],
+                OUTPUT_CLUSTERS: [0x0003, 0x0004, 0x0005, 0x0006, 0x0008, 0x0300, 0x1000],
             },
             4: {
                 PROFILE_ID: 0x0104,
                 DEVICE_TYPE: 0x0001,
                 INPUT_CLUSTERS: [0x0000, 0x0003, 0x0B05, 0x1000],
-                OUTPUT_CLUSTERS: [
-                    0x0003,
-                    0x0004,
-                    0x0005,
-                    0x0006,
-                    0x0008,
-                    0x0300,
-                    0x1000,
-                ],
+                OUTPUT_CLUSTERS: [0x0003, 0x0004, 0x0005, 0x0006, 0x0008, 0x0300, 0x1000],
             },
         },
     }
@@ -158,86 +111,54 @@ class Namron4512772(CustomDevice):
     device_automation_triggers = {
         # Channel 1
         ("short_press", "channel_1_on"): {
-            "command": "on",
-            "endpoint_id": 1,
-            "cluster_id": 0x0006,
+            "command": "on", "endpoint_id": 1, "cluster_id": 0x0006
         },
         ("short_press", "channel_1_off"): {
-            "command": "off",
-            "endpoint_id": 1,
-            "cluster_id": 0x0006,
+            "command": "off", "endpoint_id": 1, "cluster_id": 0x0006
         },
         ("long_hold", "channel_1_on"): {
-            "command": "move_with_on_off",
-            "endpoint_id": 1,
-            "cluster_id": 0x0008,
+            "command": "move_with_on_off", "endpoint_id": 1, "cluster_id": 0x0008
         },
         ("long_release", "channel_1_on"): {
-            "command": "stop_with_on_off",
-            "endpoint_id": 1,
-            "cluster_id": 0x0008,
+            "command": "stop_with_on_off", "endpoint_id": 1, "cluster_id": 0x0008
         },
         # Channel 2
         ("short_press", "channel_2_on"): {
-            "command": "on",
-            "endpoint_id": 2,
-            "cluster_id": 0x0006,
+            "command": "on", "endpoint_id": 2, "cluster_id": 0x0006
         },
         ("short_press", "channel_2_off"): {
-            "command": "off",
-            "endpoint_id": 2,
-            "cluster_id": 0x0006,
+            "command": "off", "endpoint_id": 2, "cluster_id": 0x0006
         },
         ("long_hold", "channel_2_on"): {
-            "command": "move_with_on_off",
-            "endpoint_id": 2,
-            "cluster_id": 0x0008,
+            "command": "move_with_on_off", "endpoint_id": 2, "cluster_id": 0x0008
         },
         ("long_release", "channel_2_on"): {
-            "command": "stop_with_on_off",
-            "endpoint_id": 2,
-            "cluster_id": 0x0008,
+            "command": "stop_with_on_off", "endpoint_id": 2, "cluster_id": 0x0008
         },
         # Channel 3
         ("short_press", "channel_3_on"): {
-            "command": "on",
-            "endpoint_id": 3,
-            "cluster_id": 0x0006,
+            "command": "on", "endpoint_id": 3, "cluster_id": 0x0006
         },
         ("short_press", "channel_3_off"): {
-            "command": "off",
-            "endpoint_id": 3,
-            "cluster_id": 0x0006,
+            "command": "off", "endpoint_id": 3, "cluster_id": 0x0006
         },
         ("long_hold", "channel_3_on"): {
-            "command": "move_with_on_off",
-            "endpoint_id": 3,
-            "cluster_id": 0x0008,
+            "command": "move_with_on_off", "endpoint_id": 3, "cluster_id": 0x0008
         },
         ("long_release", "channel_3_on"): {
-            "command": "stop_with_on_off",
-            "endpoint_id": 3,
-            "cluster_id": 0x0008,
+            "command": "stop_with_on_off", "endpoint_id": 3, "cluster_id": 0x0008
         },
         # Channel 4
         ("short_press", "channel_4_on"): {
-            "command": "on",
-            "endpoint_id": 4,
-            "cluster_id": 0x0006,
+            "command": "on", "endpoint_id": 4, "cluster_id": 0x0006
         },
         ("short_press", "channel_4_off"): {
-            "command": "off",
-            "endpoint_id": 4,
-            "cluster_id": 0x0006,
+            "command": "off", "endpoint_id": 4, "cluster_id": 0x0006
         },
         ("long_hold", "channel_4_on"): {
-            "command": "move_with_on_off",
-            "endpoint_id": 4,
-            "cluster_id": 0x0008,
+            "command": "move_with_on_off", "endpoint_id": 4, "cluster_id": 0x0008
         },
         ("long_release", "channel_4_on"): {
-            "command": "stop_with_on_off",
-            "endpoint_id": 4,
-            "cluster_id": 0x0008,
+            "command": "stop_with_on_off", "endpoint_id": 4, "cluster_id": 0x0008
         },
     }
