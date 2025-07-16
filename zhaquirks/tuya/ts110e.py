172b919ba8f79174a56ec875772a6da38c1f962d
puddly <32534428+puddly@users.noreply.github.com>
2025-07-16 15:17:07 -0400

    WIP

--- zhaquirks/tuya/ts110e.py
+++ zhaquirks/tuya/ts110e.py
@@ -1,6 +1,6 @@
 """Tuya Dimmer TS110E."""
 
-from typing import Any, Optional, Union
+from typing import Any, Final, Optional, Union
 
 from zigpy.profiles import zgp, zha
 import zigpy.types as t
@@ -15,6 +15,7 @@ from zigpy.zcl.clusters.general import (
     Scenes,
     Time,
 )
+from zigpy.zcl.foundation import ZCLAttributeDef
 
 from zhaquirks.const import (
     DEVICE_TYPE,
@@ -62,19 +63,25 @@ class F000LevelControlCluster(NoManufacturerCluster, LevelControl):
         is_manufacturer_specific=False,
     )
 
-    attributes = LevelControl.attributes.copy()
-    attributes.update(
-        {
-            # 0xF000
-            TUYA_LEVEL_ATTRIBUTE: ("manufacturer_current_level", t.uint16_t),
-            # 0xFC02
-            TUYA_BULB_TYPE_ATTRIBUTE: ("bulb_type", TuyaBulbType),
-            # 0xFC03
-            TUYA_MIN_LEVEL_ATTRIBUTE: ("manufacturer_min_level", t.uint16_t),
-            # 0xFC04
-            TUYA_MAX_LEVEL_ATTRIBUTE: ("manufacturer_max_level", t.uint16_t),
-        }
-    )
+    class AttributeDefs(LevelControl.AttributeDefs):
+        """Attribute definitions."""
+
+        # 0xF000
+        manufacturer_current_level: Final = ZCLAttributeDef(
+            id=TUYA_LEVEL_ATTRIBUTE, type=t.uint16_t
+        )
+        # 0xFC02
+        bulb_type: Final = ZCLAttributeDef(
+            id=TUYA_BULB_TYPE_ATTRIBUTE, type=TuyaBulbType
+        )
+        # 0xFC03
+        manufacturer_min_level: Final = ZCLAttributeDef(
+            id=TUYA_MIN_LEVEL_ATTRIBUTE, type=t.uint16_t
+        )
+        # 0xFC04
+        manufacturer_max_level: Final = ZCLAttributeDef(
+            id=TUYA_MAX_LEVEL_ATTRIBUTE, type=t.uint16_t
+        )
 
     # 0xF000 reported values are 10-1000, convert to 0-254
     def _update_attribute(self, attrid, value):
