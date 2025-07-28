fd257056f640324632a7b9188d8283eab563c656
puddly <32534428+puddly@users.noreply.github.com>
2025-07-28 13:41:47 -0400

    Use new quirks APIs to rename entities

--- zhaquirks/develco/io_module.py
+++ zhaquirks/develco/io_module.py
@@ -1,16 +1,53 @@
 """Develco IO Module."""
 
 from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl.clusters.general import BinaryInput, OnOff
 
 (
     QuirkBuilder("frient A/S", "IOMZB-110")
     # Name the two outputs
-    # .add_metadata(unique_id="116-0x0006-on_off", name="COM 1")
-    # .add_metadata(unique_id="117-0x0006-on_off", name="COM 2")
+    .change_entity_metadata(
+        endpoint_id=116,
+        cluster_id=OnOff.cluster_id,
+        unique_id_suffix="116-6",
+        new_fallback_name="COM 1",
+        new_translation_key="frient_com1",
+    )
+    .change_entity_metadata(
+        endpoint_id=117,
+        cluster_id=OnOff.cluster_id,
+        unique_id_suffix="117-6",
+        new_fallback_name="COM 2",
+        new_translation_key="frient_com2",
+    )
     # And the two inputs
-    # .add_metadata(unique_id="112-0x000f-binary_input", name="IN1")
-    # .add_metadata(unique_id="112-0x000f-binary_input", name="IN2")
-    # .add_metadata(unique_id="112-0x000f-binary_input", name="IN3")
-    # .add_metadata(unique_id="112-0x000f-binary_input", name="IN4")
+    .change_entity_metadata(
+        endpoint_id=112,
+        cluster_id=BinaryInput.cluster_id,
+        unique_id_suffix="112-15",
+        new_fallback_name="IN1",
+        new_translation_key="frient_in1",
+    )
+    .change_entity_metadata(
+        endpoint_id=113,
+        cluster_id=BinaryInput.cluster_id,
+        unique_id_suffix="113-15",
+        new_fallback_name="IN2",
+        new_translation_key="frient_in2",
+    )
+    .change_entity_metadata(
+        endpoint_id=114,
+        cluster_id=BinaryInput.cluster_id,
+        unique_id_suffix="114-15",
+        new_fallback_name="IN3",
+        new_translation_key="frient_in3",
+    )
+    .change_entity_metadata(
+        endpoint_id=115,
+        cluster_id=BinaryInput.cluster_id,
+        unique_id_suffix="115-15",
+        new_fallback_name="IN4",
+        new_translation_key="frient_in4",
+    )
     .add_to_registry()
 )
