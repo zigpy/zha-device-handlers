e3b080976af075fd688b5731ee39d7e2f117d8b8
Fexiven <48439988+Fexiven@users.noreply.github.com>
2025-07-28 11:51:24 +0200

    Update switch_agl011.py - apply suggestions

--- zhaquirks/xiaomi/aqara/switch_agl011.py
+++ zhaquirks/xiaomi/aqara/switch_agl011.py
@@ -7,8 +7,13 @@ from zigpy.profiles import zha
 from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
 
-from zhaquirks.xiaomi import DeviceTemperatureCluster
+from zhaquirks.xiaomi import DeviceTemperatureCluster, XiaomiAqaraE1Cluster
 
+class ModeSwitch(types.enum16):
+    """Enum for dimmer mode switch."""
+
+    Quick Mode = 0x01
+    Anti Flicker Mode = 0x04
 
 class OperationMode(types.enum8):
     """Enum for dimmer operation mode."""
@@ -16,16 +21,14 @@ class OperationMode(types.enum8):
     Decoupled = 0x00
     Relay = 0x01
 
-
 class Phase(types.enum8):
     """Enum for dimmer phase."""
 
     Leading = 0x00
     Trailing = 0x01
 
-
-class PowerOnBehaviour(types.enum8):
-    """Enum for dimmer power-on behaviour."""
+class PowerOnState(types.enum8):
+    """Enum for dimmer power-on state."""
 
     On = 0x00
     Previous = 0x01
@@ -50,6 +53,9 @@ class OppleCluster(XiaomiAqaraE1Cluster):
         )
         min_brightness = ZCLAttributeDef(
             id=0x0515, type=types.uint8_t, access="rw", is_manufacturer_specific=True
+        )
+        mode_switch = ZCLAttributeDef(
+            id=0x0004, type=types.uint16_t, access="rw", is_manufacturer_specific=True
         )
         operation_mode = ZCLAttributeDef(
             id=0x0200, type=types.uint8_t, access="rw", is_manufacturer_specific=True
@@ -78,16 +84,15 @@ class OppleCluster(XiaomiAqaraE1Cluster):
     .adds(DeviceTemperatureCluster)
     .adds(OppleCluster)
     .switch(
-        OppleCluster.AttributeDefs.flip_indicator_light.name,
         OppleCluster.cluster_id,
         translation_key="flip_indicator_light",
-        fallback_name="Flip Indicator Light",
+        fallback_name="Flip indicator light",
     )
     .switch(
         OppleCluster.AttributeDefs.led_indicator.name,
         OppleCluster.cluster_id,
         translation_key="led_indicator",
-        fallback_name="LED Indicator",
+        fallback_name="LED indicator",
     )
     .number(
         OppleCluster.AttributeDefs.max_brightness.name,
@@ -96,7 +101,7 @@ class OppleCluster(XiaomiAqaraE1Cluster):
         max_value=100,
         step=1,
         translation_key="max_brightness",
-        fallback_name="Maximum Brightness",
+        fallback_name="Maximum brightness",
     )
     .number(
         OppleCluster.AttributeDefs.min_brightness.name,
@@ -105,14 +110,21 @@ class OppleCluster(XiaomiAqaraE1Cluster):
         max_value=99,
         step=1,
         translation_key="min_brightness",
-        fallback_name="Minimum Brightness",
+        fallback_name="Minimum brightness",
+    )
+    .number(
+        OppleCluster.AttributeDefs.mode_switch.name,
+        ModeSwitch,
+        OppleCluster.cluster_id,
+        translation_key="mode_switch",
+        fallback_name="Mode switch",
     )
     .enum(
         OppleCluster.AttributeDefs.operation_mode.name,
         OperationMode,
         OppleCluster.cluster_id,
         translation_key="operation_mode",
-        fallback_name="Operation Mode",
+        fallback_name="Operation mode",
     )
     .enum(
         OppleCluster.AttributeDefs.phase.name,
@@ -122,11 +134,11 @@ class OppleCluster(XiaomiAqaraE1Cluster):
         fallback_name="Phase",
     )
     .enum(
-        OppleCluster.AttributeDefs.power_on_behaviour.name,
-        PowerOnBehaviour,
+        OppleCluster.AttributeDefs.power_on_state.name,
+        PowerOnState,
         OppleCluster.cluster_id,
-        translation_key="power_on_behaviour",
-        fallback_name="Power On Behaviour",
+        translation_key="power_on_state",
+        fallback_name="Power on state",
     )
     .number(
         OppleCluster.AttributeDefs.reporting_interval.name,
@@ -135,7 +147,7 @@ class OppleCluster(XiaomiAqaraE1Cluster):
         max_value=3600,
         step=1,
         translation_key="reporting_interval",
-        fallback_name="Reporting Interval",
+        fallback_name="Reporting interval",
     )
     .number(
         OppleCluster.AttributeDefs.sensitivity.name,
