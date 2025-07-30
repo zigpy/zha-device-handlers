b10552a36362981bcff06f0c92c5cb4b77b495b3
puddly <32534428+puddly@users.noreply.github.com>
2025-07-30 14:13:02 -0400

    WIP

--- zhaquirks/smartthings/moisturev4.py
+++ zhaquirks/smartthings/moisturev4.py
@@ -1,21 +1,11 @@
 """Device handler for smartthings moistureV4 sensor."""
 
-from zigpy.profiles import zha
-from zigpy.quirks import CustomCluster, CustomDevice
-from zigpy.zcl.clusters.general import Basic, BinaryInput, Identify, Ota, PollControl
-from zigpy.zcl.clusters.measurement import TemperatureMeasurement
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.clusters.security import IasZone
 
 from zhaquirks import PowerConfigurationCluster
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-    ZONE_TYPE,
-)
+from zhaquirks.const import ZONE_TYPE
 from zhaquirks.smartthings import SMART_THINGS
 
 
@@ -25,46 +15,17 @@ class CustomIasZone(CustomCluster, IasZone):
     _CONSTANT_ATTRIBUTES = {ZONE_TYPE: IasZone.ZoneType.Water_Sensor}
 
 
-class SmartThingsMoistureV4(CustomDevice):
-    """SmartThingsMoistureV4."""
-
-    signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
-        #  device_version=0
-        #  input_clusters=[0, 1, 3, 15, 1026, 1280, 32]
-        #  output_clusters=[25]>
-        MODELS_INFO: [(SMART_THINGS, "moisturev4")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster.cluster_id,
-                    Identify.cluster_id,
-                    BinaryInput.cluster_id,
-                    PollControl.cluster_id,
-                    TemperatureMeasurement.cluster_id,
-                    IasZone.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster,
-                    Identify.cluster_id,
-                    BinaryInput.cluster_id,
-                    PollControl.cluster_id,
-                    TemperatureMeasurement.cluster_id,
-                    CustomIasZone,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        }
-    }
+(
+    QuirkBuilder(SMART_THINGS, "moisturev4")
+    .replaces(
+        replacement_cluster_class=PowerConfigurationCluster,
+        cluster_id=PowerConfigurationCluster.cluster_id,
+        endpoint_id=1,
+    )
+    .replaces(
+        replacement_cluster_class=CustomIasZone,
+        cluster_id=IasZone.cluster_id,
+        endpoint_id=1,
+    )
+    .add_to_registry()
+)
--- zhaquirks/thirdreality/switch.py
+++ zhaquirks/thirdreality/switch.py
@@ -1,19 +1,8 @@
 """Third Reality switch devices."""
 
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Groups, Identify, OnOff, Ota, Scenes
-from zigpy.zcl.clusters.homeautomation import Diagnostic
+from zigpy.quirks.v2 import QuirkBuilder
 
 from zhaquirks import PowerConfigurationCluster
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
 from zhaquirks.thirdreality import THIRD_REALITY
 
 
@@ -24,95 +13,13 @@ class CustomPowerConfigurationCluster(PowerConfigurationCluster):
     MAX_VOLTS = 3.0
 
 
-class Switch(CustomDevice):
-    """3RSS008Z device."""
-
-    signature = {
-        # <SimpleDescriptor endpoint=1 profile=260
-        # device_type=2 device_version=1
-        # input_clusters=[0, 4, 3, 6, 5, 25, 1]
-        # output_clusters=[1]>
-        MODELS_INFO: [(THIRD_REALITY, "3RSS007Z"), (THIRD_REALITY, "3RSS008Z")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Ota.cluster_id,
-                    CustomPowerConfigurationCluster.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [CustomPowerConfigurationCluster.cluster_id],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Ota.cluster_id,
-                    CustomPowerConfigurationCluster,
-                ],
-                OUTPUT_CLUSTERS: [CustomPowerConfigurationCluster.cluster_id],
-            }
-        }
-    }
+(
+    QuirkBuilder(THIRD_REALITY, "3RSS007Z")
+    .applies_to(THIRD_REALITY, "3RSS008Z")
+    .replaces(CustomPowerConfigurationCluster)
+    .add_to_registry()
+)
 
 
-class SwitchPlus(Switch):
-    """RealitySwitch Plus (3RSS008Z) device."""
-
-    signature = {
-        # <SimpleDescriptor endpoint=1 profile=260
-        # device_type=2 device_version=1
-        # input_clusters=[0, 4, 3, 6, 5, 1, 2821]
-        # output_clusters=[25]>
-        MODELS_INFO: [(THIRD_REALITY, "3RSS008Z")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Diagnostic.cluster_id,
-                    CustomPowerConfigurationCluster.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_OUTPUT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Diagnostic.cluster_id,
-                    CustomPowerConfigurationCluster,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        }
-    }
+# Note: SwitchPlus (3RSS008Z) with Diagnostic cluster is handled by the main quirk above
+# The v2 API only replaces the power configuration, other cluster differences are handled automatically
