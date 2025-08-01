dd4ec1207f6f69f7bc96fdadbc66fb7b11fa4f54
Ricky Tsai <ricky@rtnztech.com>
2025-08-02 00:02:38 +1200

    Add model

--- zhaquirks/tuya/ts0601_switch.py
+++ zhaquirks/tuya/ts0601_switch.py
@@ -133,6 +133,7 @@ class TuyaSingleSwitch_GP(TuyaSwitch):
             ("_TZE204_d0ypnbvn", "TS0601"),  # reported in #3220 & #3291
             ("_TZE204_ptaqh9tk", "TS0601"),  # reported in #2780
             ("_TZE204_v5xjyphj", "TS0601"),  # reported in #4078
+            ("_TZE204_gbagoilo", "TS0601"),  # reported in #4101
         ],
         ENDPOINTS: {
             # <SimpleDescriptor endpoint=1 profile=260 device_type=51 device_version=1
