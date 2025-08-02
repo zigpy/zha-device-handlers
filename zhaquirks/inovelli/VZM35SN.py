d88350fe47de9fb008f41b473d64dfc93be9af29
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-08-02 04:02:34 +0200

    Fix replacing Inovelli VZM35-SM clusters on non-existent endpoint

--- zhaquirks/inovelli/VZM35SN.py
+++ zhaquirks/inovelli/VZM35SN.py
@@ -9,8 +9,6 @@ from zhaquirks.inovelli import INOVELLI_AUTOMATION_TRIGGERS, InovelliVZM35SNClus
     QuirkBuilder("Inovelli", "VZM35-SN")
     .replaces_endpoint(1, device_type=zha.DeviceType.DIMMABLE_LIGHT)
     .replace_cluster_occurrences(InovelliVZM35SNCluster)
-    .replaces(InovelliVZM35SNCluster, endpoint_id=2)
-    .replaces(InovelliVZM35SNCluster, endpoint_id=3)
     .device_automation_triggers(INOVELLI_AUTOMATION_TRIGGERS)
     .add_to_registry()
 )
