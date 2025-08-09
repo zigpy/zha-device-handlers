de830b15afc2b704fa058e53da5d152a50516a6a
jezzaaa <jeremy@laidman.org>
2025-08-09 15:40:19 +1000

    fix indentation

--- zhaquirks/tuya/ts0601_vibration.py
+++ zhaquirks/tuya/ts0601_vibration.py
@@ -10,7 +10,7 @@ from zhaquirks.tuya.builder import TuyaQuirkBuilder
 
 
 def uint_to_sint(value: t.uint8_t) -> t.int8s:
-   if value & 0x80:
+    if value & 0x80:
         return t.int8s(value - 256)
     else:
         return t.int8s(value)
