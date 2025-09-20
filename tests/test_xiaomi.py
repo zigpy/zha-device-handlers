e4a773bf30b803ff5083559a850e68041400e828
lonevvolf <lonevvolf@gmail.com>
2025-09-20 15:29:37 +0200

    Simplified test

--- tests/test_xiaomi.py
+++ tests/test_xiaomi.py
@@ -2232,25 +2232,12 @@ def test_h1_wireless_remotes(zigpy_device_from_v2_quirk):
     assert MultistateInput.cluster_id in device.endpoints[3].in_clusters
 
 
-def test_h2_switch_attribute_update(zigpy_device_from_v2_quirk):
+def test_h2_switch(zigpy_device_from_v2_quirk):
     """Test Aqara H2 switch quirk adds power measurement cluster."""
     device = zigpy_device_from_v2_quirk(AQARA, "lumi.switch.agl010")
 
     # verify the quirk adds endpoint 21
     assert 21 in device.endpoints
-
-    # verify the quirk adds the correct clusters to the new endpoints
-    assert PowerMeasurementCluster.cluster_id in device.endpoints[21].in_clusters
-
-    # verify update attribute works
-    powermeasurement_cluster = device.endpoints[21].in_clusters[
-        PowerMeasurementCluster.cluster_id
-    ]
-    powermeasurement_listener = ClusterListener(powermeasurement_cluster)
-
-    powermeasurement_cluster.update_attribute(0x0055, 0)
-    assert len(powermeasurement_listener.attribute_updates) == 1
-
 
 @pytest.mark.parametrize("endpoint", [(1), (2)])
 def test_t1m_ceiling_light(zigpy_device_from_v2_quirk, endpoint):
