63a1b15579ac94cb795414512597d441347131bc
support <117250319+3reality-support@users.noreply.github.com>
2025-09-25 09:46:05 +0800

    Update test_third_button.py

--- tests/test_third_button.py
+++ tests/test_third_button.py
@@ -3,7 +3,7 @@
 import pytest
 
 import zhaquirks
-from zhaquirks.thirdreality.button_v2 import MultistateInputCluster
+from zhaquirks.thirdreality.button-v2 import MultistateInputCluster
 
 
 class MockListener:
