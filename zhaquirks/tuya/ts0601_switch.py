a5272502b9d4308b6c414c2d57754545f94c6c10
Daniel D. <danieldelnaaij@gmail.com>
2025-10-01 21:41:16 +0200

    feat(tuya): Add double switch model
    Add _TZE200_nkjintbl to TuyaDoubleSwitchTO

--- zhaquirks/tuya/ts0601_switch.py
+++ zhaquirks/tuya/ts0601_switch.py
@@ -202,6 +202,7 @@ class TuyaDoubleSwitchTO(TuyaSwitch):
         MODELS_INFO: [
             ("_TZE200_g1ib5ldv", "TS0601"),
             ("_TZE200_wunufsil", "TS0601"),
+            ("_TZE200_nkjintbl", "TS0601"),
             ("_TZE204_wvovwe9h", "TS0601"),
         ],
         ENDPOINTS: {
