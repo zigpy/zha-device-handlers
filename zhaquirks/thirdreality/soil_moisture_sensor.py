01294fb256d9d3a23bedb86abf3629d6e0f60e50
jintj <jintj@3reality.com>
2025-10-09 15:53:42 +0800

    update

---
+++ zhaquirks/thirdreality/soil_moisture_sensor.py
@@ -0,0 +1,78 @@
+"""Third Reality soil moisture sensor devices."""
+
+from typing import Final
+
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
+import zigpy.types as t
+from zigpy.zcl.clusters.measurement import SoilMoisture
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
+from zigpy.quirks.v2.homeassistant import UnitOfTemperature, PERCENTAGE
+
+
+class ThirdRealitySoilMoistureCluster(CustomCluster):
+    """Third Reality's soil moisture sensor private cluster."""
+
+    cluster_id = 0xFF01
+
+    class AttributeDefs(BaseAttributeDefs):
+        """Define the attributes of a private cluster."""
+
+        temperature_correction_fahrenheit: Final = ZCLAttributeDef(
+            id=0x0033,
+            type=t.int16s,
+            is_manufacturer_specific=True,
+        )
+
+        temperature_correction_celsius: Final = ZCLAttributeDef(
+            id=0x0031,
+            type=t.int16s,
+            is_manufacturer_specific=True,
+        )
+
+        humidity_correction: Final = ZCLAttributeDef(
+            id=0x0032,
+            type=t.int16s,
+            is_manufacturer_specific=True,
+        )
+
+
+(
+    QuirkBuilder("Third Reality, Inc", "3RSM0147Z")
+    .replaces(ThirdRealitySoilMoistureCluster)
+    .replaces(SoilMoisture)
+    .number(
+        attribute_name=ThirdRealitySoilMoistureCluster.AttributeDefs.temperature_correction_celsius.name,
+        min_value=-10000,
+        max_value=10000,
+        multiplier=0.01,
+        step=0.1,
+        unit=UnitOfTemperature.CELSIUS,
+        cluster_id=ThirdRealitySoilMoistureCluster.cluster_id,
+        translation_key="temperature_correction_celsius",
+        fallback_name="Celsius correction",
+    )
+    .number(
+        attribute_name=ThirdRealitySoilMoistureCluster.AttributeDefs.temperature_correction_fahrenheit.name,
+        min_value=-10000,
+        max_value=10000,
+        multiplier=0.01,
+        step=0.1,
+        unit=UnitOfTemperature.FAHRENHEIT,
+        cluster_id=ThirdRealitySoilMoistureCluster.cluster_id,
+        translation_key="temperature_correction_fahrenheit",
+        fallback_name="Fahrenheit correction",
+    )
+    .number(
+        attribute_name=ThirdRealitySoilMoistureCluster.AttributeDefs.humidity_correction.name,
+        min_value=-10000,
+        max_value=10000,
+        multiplier=0.01,
+        step=0.1,
+        unit=PERCENTAGE,
+        cluster_id=ThirdRealitySoilMoistureCluster.cluster_id,
+        translation_key="humidity_correction",
+        fallback_name="Humidity correction",
+    )
+    .add_to_registry()
+)
