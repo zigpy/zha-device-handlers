6fef043efcdd0f41dbd60993a72163b1121c53e4
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-10-21 21:57:15 +0200

    Completely redo by using standalone cluster

--- zhaquirks/develco/emi.py
+++ zhaquirks/develco/emi.py
@@ -1,105 +1,72 @@
 """Frient Electricity Meter Interface."""
 
-from typing import Any, Final
+from typing import Final
 
 from zigpy.quirks import CustomCluster
-from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.quirks.v2 import CustomDeviceV2, QuirkBuilder
 import zigpy.types as t
-from zigpy.zcl import foundation
 from zigpy.zcl.clusters.smartenergy import Metering
-from zigpy.zcl.foundation import TypeValue, ZCLAttributeDef
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
 
 
-class MeteringClusterEMI(CustomCluster, Metering):
-    """Metering cluster."""
+class ManufacturerDevice(CustomDeviceV2):
+    """Custom device class used to remap cluster IDs in requests."""
 
-    class AttributeDefs(Metering.AttributeDefs):
-        """Define custom attributes."""
+    async def request(
+        self,
+        *args,
+        **kwargs,
+    ):
+        """Remap cluster IDs for clusters that substitute for others."""
+        # TODO: verify these are always kwargs
+        endpoint_id = kwargs["src_ep"]
+        cluster_id = kwargs["cluster"]
+
+        cluster = self.endpoints[endpoint_id].in_clusters.get(cluster_id)
+        substitution_cluster = getattr(cluster, "SUBSTITUTION_FOR", None)
+
+        if substitution_cluster:
+            kwargs["cluster"] = t.ClusterId(substitution_cluster)
+
+        return await super().request(
+            *args,
+            **kwargs,
+        )
+
+
+class ManufacturerMetering(CustomCluster):
+    """Fake manufacturer specific cluster for Metering manufacturer attributes."""
+
+    SUBSTITUTION_FOR: int = Metering.cluster_id
+    cluster_id = 0xFD10  # TODO: use a different one? (0xEF01?)
+
+    class AttributeDefs(BaseAttributeDefs):
+        """Define manufacturer specific attributes."""
 
         pulse_configuration: Final = ZCLAttributeDef(
-            id=0xEF01,  # it's actually 0x0300 but this is with a manufacturer code
+            id=0x0300,
             type=t.uint16_t,
-            is_manufacturer_specific=True,  # should automatically use 0x1015
+            is_manufacturer_specific=True,
         )
-
-    # TODO: clean up and fix batch reads
-    async def read_attributes(
-        self,
-        attributes: list[int | str],
-        allow_cache: bool = False,
-        only_cache: bool = False,
-        manufacturer: int | t.uint16_t | None = None,
-        **kwargs,
-    ) -> Any:
-        """Redirect reads of custom attribute."""
-        # TODO: there's an issue with ZHA reading attributes in chunks during init,
-        #  where the manufacturer code is not handled correctly if some attributes
-        #  require it and some don't. We'll need to split up those reads later.
-
-        if (
-            len(attributes) == 1
-            and self.find_attribute(attributes[0])
-            == self.AttributeDefs.pulse_configuration
-        ):
-            # redirect to 0x0300 with manufacturer code
-            return await super().read_attributes(
-                [0x0300],
-                allow_cache=allow_cache,
-                only_cache=only_cache,
-                manufacturer=0x1015,
-                **kwargs,
-            )
-
-        return await super().read_attributes(
-            attributes, allow_cache, only_cache, manufacturer, **kwargs
-        )
-
-    # TODO: also clean up
-    async def write_attributes(
-        self,
-        attributes: dict[str | int, Any],
-        manufacturer: int | None = None,
-        **kwargs,
-    ) -> list:
-        """Redirect writes of custom attribute."""
-        # we can just get the first key (name or id) here if we only write one attribute
-        if (
-            len(attributes) == 1
-            and self.find_attribute(key := next(iter(attributes)))
-            == self.AttributeDefs.pulse_configuration
-        ):
-            # redirect to 0x0300 with manufacturer code and fixed type
-            value = t.uint16_t(attributes[key])
-            return await super()._write_attributes(
-                [
-                    foundation.Attribute(
-                        attrid=0x0300, value=TypeValue(type=t.uint16_t, value=value)
-                    )
-                ],
-                manufacturer=0x1015,
-                **kwargs,
-            )
-
-        return await super().write_attributes(attributes, manufacturer, **kwargs)
 
 
 base_quirk = (
-    QuirkBuilder().replaces(
-        MeteringClusterEMI, endpoint_id=2
-    )  # TODO: check if this ep is correct
-    # todo: adding the entity below might cause unexpected reading of the attribute
-    # .number(
-    #     attribute_name=MeteringClusterEMI.AttributeDefs.pulse_configuration.name,
-    #     cluster_id=MeteringClusterEMI.cluster_id,
-    #     endpoint_id=2,
-    #     min_value=0,
-    #     max_value=65535,
-    #     step=1,
-    #     unit="pulses/kWh",
-    #     mode="box",
-    #     translation_key="pulse_configuration",
-    #     fallback_name="Pulse configuration",
-    # )
+    QuirkBuilder()
+    .adds(Metering, endpoint_id=2)  # TODO: remove, only for tests
+    .replaces(ManufacturerMetering, endpoint_id=2)
+    .device_class(ManufacturerDevice)
+    .number(
+        attribute_name=ManufacturerMetering.AttributeDefs.pulse_configuration.name,
+        cluster_id=ManufacturerMetering.cluster_id,
+        endpoint_id=2,
+        min_value=0,
+        max_value=65535,
+        step=1,
+        unit="pulses/kWh",
+        mode="box",
+        translation_key="pulse_configuration",
+        fallback_name="Pulse configuration",
+    )
 )
 
 (
