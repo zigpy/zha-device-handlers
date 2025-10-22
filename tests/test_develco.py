504291a8f9b662363ba24571393af27ad5f3fc5c
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-10-22 03:02:23 +0200

    WIP: Adapt test to new method

--- tests/test_develco.py
+++ tests/test_develco.py
@@ -4,7 +4,7 @@ from unittest import mock
 
 import zigpy.types as t
 from zigpy.zcl import foundation
-from zigpy.zcl.foundation import TypeValue
+from zigpy.zcl.clusters.smartenergy import Metering
 
 import zhaquirks
 
@@ -13,51 +13,68 @@ zhaquirks.setup()
 
 async def test_frient_emi(zigpy_device_from_v2_quirk):
     """Test that the EMI correctly forwards custom attributes."""
+    # TODO: fix/improve
+
     device = zigpy_device_from_v2_quirk("frient A/S", "EMIZB-141", endpoint_ids=[1, 2])
 
     metering_cluster = device.endpoints[2].smartenergy_metering
     # metering_listener = ClusterListener(metering_cluster)
-    pulse_config_attr_id = metering_cluster.AttributeDefs.pulse_configuration.id
 
-    # todo: likely replace with read/write attribute mocks for proper result
-    request_patch = mock.patch("zigpy.zcl.Cluster.request", mock.AsyncMock())
+    manufacturer_cluster = device.endpoints[2].in_clusters[0xFD10]
+    # manufacturer_listener = ClusterListener(manufacturer_cluster)
+
+    pulse_config_attr_id = manufacturer_cluster.AttributeDefs.pulse_configuration.id
+
+    async def send_packet(packet: t.ZigbeePacket):
+        # TODO: maybe use
+        pass
+
+    device.application.send_packet = mock.AsyncMock(side_effect=send_packet)
+
+    request_patch = mock.patch("zigpy.device.Device.request", mock.AsyncMock())
     with request_patch as request_mock:
         request_mock.return_value = (foundation.Status.SUCCESS, "done")
 
         # read custom attribute
-        result = await metering_cluster.read_attributes([pulse_config_attr_id])
+        result = await manufacturer_cluster.read_attributes([pulse_config_attr_id])
         assert request_mock.call_count == 1
         assert request_mock.call_args == mock.call(
-            True,
-            foundation.GeneralCommand.Read_Attributes,
-            mock.ANY,  # todo: fix?
-            [0x0300],
-            manufacturer=0x1015,
+            profile=None,
+            cluster=Metering.cluster_id,
+            src_ep=2,
+            dst_ep=2,
+            sequence=mock.ANY,
+            data=b"\x04\xd2\x04\x01\x00\x00\x03",  # TODO: make data interpretable
+            timeout=5,
             expect_reply=True,
-            tsn=None,
+            use_ieee=False,
+            ask_for_ack=None,
+            priority=None,
         )
 
         # assert result == ({pulse_config_attr_id: "done"}, {})
-        assert result == ({}, {0x0300: foundation.Status.SUCCESS})  # todo: fix?
+        assert result == ({}, {0x0300: foundation.Status.SUCCESS})
         request_mock.reset_mock()
 
         # write custom attribute
-        result = await metering_cluster.write_attributes({pulse_config_attr_id: "42"})
+        result = await manufacturer_cluster.write_attributes(
+            {pulse_config_attr_id: "42"}
+        )
         assert request_mock.call_count == 1
         assert request_mock.call_args == mock.call(
-            True,
-            foundation.GeneralCommand.Write_Attributes,
-            mock.ANY,  # todo: fix?
-            [
-                foundation.Attribute(
-                    attrid=0x0300, value=TypeValue(type=t.uint16_t, value=42)
-                )
-            ],
-            manufacturer=0x1015,
+            profile=None,
+            cluster=Metering.cluster_id,
+            src_ep=2,
+            dst_ep=2,
+            sequence=mock.ANY,
+            data=b"\x04\xd2\x04\x02\x02\x00\x03!*\x00",
+            timeout=5,
             expect_reply=True,
-            tsn=None,
+            use_ieee=False,
+            ask_for_ack=None,
+            priority=None,
         )
-        assert result == (foundation.Status.SUCCESS, "done")  # todo: fix?
+        assert result == (foundation.Status.SUCCESS, "done")
         request_mock.reset_mock()
 
         # read non-custom attribute
@@ -66,13 +83,17 @@ async def test_frient_emi(zigpy_device_from_v2_quirk):
         )
         assert request_mock.call_count == 1
         assert request_mock.call_args == mock.call(
-            True,
-            foundation.GeneralCommand.Read_Attributes,
-            mock.ANY,  # todo: fix?
-            [metering_cluster.AttributeDefs.current_summ_delivered.id],
-            manufacturer=None,
+            profile=None,
+            cluster=Metering.cluster_id,
+            src_ep=2,
+            dst_ep=2,
+            sequence=mock.ANY,
+            data=b"\x00\x03\x00\x00\x00",
+            timeout=5,
             expect_reply=True,
-            tsn=None,
+            use_ieee=False,
+            ask_for_ack=None,
+            priority=None,
         )
         assert result == (
             {},
@@ -88,17 +109,17 @@ async def test_frient_emi(zigpy_device_from_v2_quirk):
         )
         assert request_mock.call_count == 1
         assert request_mock.call_args == mock.call(
-            True,
-            foundation.GeneralCommand.Write_Attributes,
-            mock.ANY,  # todo: fix?
-            [
-                foundation.Attribute(
-                    attrid=metering_cluster.AttributeDefs.current_summ_delivered.id,
-                    value=TypeValue(type=mock.ANY, value=100),
-                )
-            ],  # todo: fix type
-            manufacturer=None,
+            profile=None,
+            cluster=Metering.cluster_id,
+            src_ep=2,
+            dst_ep=2,
+            sequence=mock.ANY,
+            data=b"\x00\x04\x02\x00\x00%d\x00\x00\x00\x00\x00",
+            timeout=5,
             expect_reply=True,
-            tsn=None,
+            use_ieee=False,
+            ask_for_ack=None,
+            priority=None,
         )
+
         assert result == (foundation.Status.SUCCESS, "done")
