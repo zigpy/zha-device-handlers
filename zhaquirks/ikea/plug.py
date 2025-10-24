e8fba043ffe0b9ba37278dc378b7e3e739f6764f
Remmy Cat Stock <3317423+remmycat@users.noreply.github.com>
2025-10-24 15:00:06 +0200

    Add Ikea Inspelning entities for Child Lock and LED Disable functionality

--- zhaquirks/ikea/plug.py
+++ zhaquirks/ikea/plug.py
@@ -1,15 +1,15 @@
-"""IKEA plugs quirk."""
+"""IKEA TRADFRI plug quirk."""
 
 from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.clusters.general import LevelControl
 
 from zhaquirks.ikea import IKEA
 
-# remove LevelControl for plugs to not show config options in ZHA
+# remove LevelControl for plug to not show config options in ZHA
+# TRETAKT likely also has Child Lock and LED control, see INSPELNING
 (
     QuirkBuilder(IKEA, "TRADFRI control outlet")
     .also_applies_to(IKEA, "TRETAKT Smart plug")
-    .also_applies_to(IKEA, "INSPELNING Smart plug")
     .removes(LevelControl.cluster_id)
     .add_to_registry()
 )
