c2845afb9c0fec485f9610c1e40b89f36029f379
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-10-28 21:53:07 +0100

    Prepare for splitting `replaces` calls into multiple lines

--- zhaquirks/candeo/rotary_dimmer_switches.py
+++ zhaquirks/candeo/rotary_dimmer_switches.py
@@ -35,9 +35,9 @@ from zhaquirks.const import (
 
 remote_quirk = (
     QuirkBuilder()
-    .replaces(CandeoOnOffRemoteCluster, endpoint_id=2, cluster_type=ClusterType.Client)
+    .replaces(CandeoOnOffRemoteCluster, endpoint_id=2, cluster_type=ClusterType.Client,)
     .replaces(
-        CandeoLevelControlRemoteCluster, endpoint_id=2, cluster_type=ClusterType.Client
+        CandeoLevelControlRemoteCluster, endpoint_id=2, cluster_type=ClusterType.Client,
     )
     .device_automation_triggers(
         {
