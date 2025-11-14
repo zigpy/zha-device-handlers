a100c5f55a5690cd6cc8685eec907dea9ad8f054
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-11-14 09:24:00 +0100

    Remove tamper sensor for Frient Entry Sensor (basic) and Entry Sensor 2 Pro

--- zhaquirks/develco/open_close.py
+++ zhaquirks/develco/open_close.py
@@ -16,16 +16,28 @@ class DevelcoPowerConfiguration(PowerConfigurationCluster):
     MAX_VOLTS = 3.0
 
 
-(
-    QuirkBuilder("frient A/S", "WISZB-131")
-    .applies_to("Develco Products A/S", "WISZB-120")
-    .applies_to("frient A/S", "WISZB-120")
-    .applies_to("Develco Products A/S", "WISZB-121")
-    .applies_to("frient A/S", "WISZB-121")
+base_quirk = (
+    QuirkBuilder()
     .replaces(DevelcoIasZone, endpoint_id=35)
     .replaces(DevelcoPowerConfiguration, endpoint_id=35)
     # The binary input cluster is a duplicate
     .prevent_default_entity_creation(endpoint_id=35, cluster_id=BinaryInput.cluster_id)
+)
+
+# Entry Sensor 2 Pro + Entry Sensor (basic), no tamper
+(
+    base_quirk.clone()
+    .applies_to("frient A/S", "WISZB-131")
+    .applies_to("Develco Products A/S", "WISZB-121")
+    .applies_to("frient A/S", "WISZB-121")
+    .add_to_registry()
+)
+
+# Entry Sensor Pro, with tamper
+(
+    base_quirk.clone()
+    .applies_to("Develco Products A/S", "WISZB-120")
+    .applies_to("frient A/S", "WISZB-120")
     .binary_sensor(
         endpoint_id=35,
         cluster_id=IasZone.cluster_id,
