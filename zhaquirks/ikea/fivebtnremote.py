d7743eefb32a93c98170a293b315f72a055a694c
puddly <32534428+puddly@users.noreply.github.com>
2025-11-15 15:16:55 -0500

    Migrate quirks, pass 6 (IKEA)

--- zhaquirks/ikea/fivebtnremote.py
+++ zhaquirks/ikea/fivebtnremote.py
@@ -2,6 +2,8 @@
 
 from zigpy.profiles import zha, zll
 from zigpy.quirks import CustomDevice
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl import ClusterType
 from zigpy.zcl.clusters.general import (
     Alarms,
     Basic,
@@ -324,125 +326,97 @@ class IkeaTradfriRemote3(IkeaTradfriRemote1):
     }
 
 
-class IkeaTradfriRemote4(IkeaTradfriRemote1):
-    """Custom device representing variation of IKEA five button remote."""
-
-    signature = {
-        # <SimpleDescriptor endpoint=1 profile=260 device_type=2080
-        # device_version=1
-        # input_clusters=[0, 1, 3, 32, 4096, 64636]
-        # output_clusters=[3, 4, 5, 6, 8, 25, 4096]>
-        MODELS_INFO: [(IKEA, "TRADFRI remote control")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.NON_COLOR_CONTROLLER,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfiguration.cluster_id,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    LightLink.cluster_id,
-                    IKEA_CLUSTER_ID,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    ScenesCluster.cluster_id,
-                    OnOff.cluster_id,
-                    LevelControl.cluster_id,
-                    Ota.cluster_id,
-                    LightLink.cluster_id,
-                ],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.NON_COLOR_CONTROLLER,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    DoublingPowerConfig1CRCluster,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    LightLink.cluster_id,
-                    IKEA_CLUSTER_ID,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    ScenesCluster,
-                    OnOff.cluster_id,
-                    LevelControl.cluster_id,
-                    Ota.cluster_id,
-                    LightLink.cluster_id,
-                ],
-            }
+(
+    QuirkBuilder(IKEA, "TRADFRI remote control")
+    .filter(lambda device: WWAH_CLUSTER_ID in device.endpoints[1].in_clusters)
+    .replaces(PowerConfig1CRCluster, endpoint_id=1)
+    .replaces(ScenesCluster, cluster_type=ClusterType.Client, endpoint_id=1)
+    .device_automation_triggers(
+        {
+            (SHORT_PRESS, TURN_ON): {
+                COMMAND: COMMAND_TOGGLE,
+                CLUSTER_ID: 6,
+                ENDPOINT_ID: 1,
+            },
+            (LONG_PRESS, TURN_ON): {
+                COMMAND: COMMAND_RELEASE,
+                CLUSTER_ID: 5,
+                ENDPOINT_ID: 1,
+                PARAMS: {"param1": 0},
+            },
+            (SHORT_PRESS, DIM_UP): {
+                COMMAND: COMMAND_STEP_ON_OFF,
+                CLUSTER_ID: 8,
+                ENDPOINT_ID: 1,
+                PARAMS: {"step_mode": 0},
+            },
+            (LONG_PRESS, DIM_UP): {
+                COMMAND: COMMAND_MOVE_ON_OFF,
+                CLUSTER_ID: 8,
+                ENDPOINT_ID: 1,
+                PARAMS: {"move_mode": 0},
+            },
+            (LONG_RELEASE, DIM_UP): {
+                COMMAND: COMMAND_STOP_ON_OFF,
+                CLUSTER_ID: 8,
+                ENDPOINT_ID: 1,
+            },
+            (SHORT_PRESS, DIM_DOWN): {
+                COMMAND: COMMAND_STEP,
+                CLUSTER_ID: 8,
+                ENDPOINT_ID: 1,
+                PARAMS: {"step_mode": 1},
+            },
+            (LONG_PRESS, DIM_DOWN): {
+                COMMAND: COMMAND_MOVE,
+                CLUSTER_ID: 8,
+                ENDPOINT_ID: 1,
+                PARAMS: {"move_mode": 1},
+            },
+            (LONG_RELEASE, DIM_DOWN): {
+                COMMAND: COMMAND_STOP,
+                CLUSTER_ID: 8,
+                ENDPOINT_ID: 1,
+            },
+            (SHORT_PRESS, LEFT): {
+                COMMAND: COMMAND_PRESS,
+                CLUSTER_ID: 5,
+                ENDPOINT_ID: 1,
+                PARAMS: {
+                    "param1": 257,
+                    "param2": 13,
+                    "param3": 0,
+                },
+            },
+            (LONG_PRESS, LEFT): {
+                COMMAND: COMMAND_HOLD,
+                CLUSTER_ID: 5,
+                ENDPOINT_ID: 1,
+                PARAMS: {
+                    "param1": 3329,
+                    "param2": 0,
+                },
+            },
+            (SHORT_PRESS, RIGHT): {
+                COMMAND: COMMAND_PRESS,
+                CLUSTER_ID: 5,
+                ENDPOINT_ID: 1,
+                PARAMS: {
+                    "param1": 256,
+                    "param2": 13,
+                    "param3": 0,
+                },
+            },
+            (LONG_PRESS, RIGHT): {
+                COMMAND: COMMAND_HOLD,
+                CLUSTER_ID: 5,
+                ENDPOINT_ID: 1,
+                PARAMS: {
+                    "param1": 3328,
+                    "param2": 0,
+                },
+            },
         }
-    }
-
-
-class IkeaTradfriRemote5(IkeaTradfriRemote1):
-    """Custom device representing variation of IKEA five button remote."""
-
-    signature = {
-        # <SimpleDescriptor endpoint=1 profile=260 device_type=2080
-        # device_version=1
-        # input_clusters=[0, 1, 3, 32, 4096, 64599, 64636]
-        # output_clusters=[3, 4, 5, 6, 8, 25, 4096]>
-        MODELS_INFO: [(IKEA, "TRADFRI remote control")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.NON_COLOR_CONTROLLER,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfiguration.cluster_id,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    LightLink.cluster_id,
-                    WWAH_CLUSTER_ID,
-                    IKEA_CLUSTER_ID,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    ScenesCluster.cluster_id,
-                    OnOff.cluster_id,
-                    LevelControl.cluster_id,
-                    Ota.cluster_id,
-                    LightLink.cluster_id,
-                ],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.NON_COLOR_CONTROLLER,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfig1CRCluster,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    LightLink.cluster_id,
-                    WWAH_CLUSTER_ID,
-                    IKEA_CLUSTER_ID,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    ScenesCluster,
-                    OnOff.cluster_id,
-                    LevelControl.cluster_id,
-                    Ota.cluster_id,
-                    LightLink.cluster_id,
-                ],
-            }
-        }
-    }
+    )
+    .add_to_registry()
+)
--- zhaquirks/ikea/motion.py
+++ zhaquirks/ikea/motion.py
@@ -1,82 +1,14 @@
 """Device handler for IKEA of Sweden TRADFRI remote control."""
 
-from zigpy.profiles import zll
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import (
-    Alarms,
-    Basic,
-    Groups,
-    Identify,
-    OnOff,
-    Ota,
-    PowerConfiguration,
-)
-from zigpy.zcl.clusters.homeautomation import Diagnostic
-from zigpy.zcl.clusters.lightlink import LightLink
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl.clusters.general import PowerConfiguration
 
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
 from zhaquirks.ikea import IKEA, DoublingPowerConfig2CRCluster
 
-
-class IkeaTradfriMotion(CustomDevice):
-    """Custom device representing IKEA of Sweden TRADFRI remote control."""
-
-    signature = {
-        # <SimpleDescriptor endpoint=1 profile=49246 device_type=2128
-        # device_version=2
-        # input_clusters=[0, 1, 3, 9, 2821, 4096]
-        # output_clusters=[3, 4, 6, 25, 4096]>
-        MODELS_INFO: [(IKEA, "TRADFRI motion sensor")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zll.PROFILE_ID,
-                DEVICE_TYPE: zll.DeviceType.ON_OFF_SENSOR,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfiguration.cluster_id,
-                    Identify.cluster_id,
-                    Alarms.cluster_id,
-                    Diagnostic.cluster_id,
-                    LightLink.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    OnOff.cluster_id,
-                    Ota.cluster_id,
-                    LightLink.cluster_id,
-                ],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zll.PROFILE_ID,
-                DEVICE_TYPE: zll.DeviceType.ON_OFF_SENSOR,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    DoublingPowerConfig2CRCluster,
-                    Identify.cluster_id,
-                    Alarms.cluster_id,
-                    Diagnostic.cluster_id,
-                    LightLink.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    OnOff.cluster_id,
-                    Ota.cluster_id,
-                    LightLink.cluster_id,
-                ],
-            }
-        }
-    }
+(
+    QuirkBuilder(IKEA, "TRADFRI motion sensor")
+    .replaces(
+        DoublingPowerConfig2CRCluster, PowerConfiguration.cluster_id, endpoint_id=1
+    )
+    .add_to_registry()
+)
