aba1ed6874ef6eb02e7251c9fd7b6788ad19de9a
puddly <32534428+puddly@users.noreply.github.com>
2025-11-15 15:39:31 -0500

    Migrate quirks, pass 8

--- zhaquirks/gledopto/glc009p.py
+++ zhaquirks/gledopto/glc009p.py
@@ -3,4 +3,9 @@
 from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.clusters.lighting import Color
 
-(QuirkBuilder("GLEDOPTO", "GL-C-009P").removes(Color, endpoint_id=11).add_to_registry())
+(
+    # TODO: this quirk is likely unneeded
+    QuirkBuilder("GLEDOPTO", "GL-C-009P")
+    .removes(Color, endpoint_id=11)
+    .add_to_registry()
+)
--- zhaquirks/innr/innr_sp234_plug.py
+++ zhaquirks/innr/innr_sp234_plug.py
@@ -1,98 +1,12 @@
 """Innr SP 234 plug."""
 
-from zigpy.profiles import zgp, zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import (
-    Basic,
-    GreenPowerProxy,
-    Groups,
-    Identify,
-    OnOff,
-    Ota,
-    Scenes,
-    Time,
-)
-from zigpy.zcl.clusters.homeautomation import Diagnostic, ElectricalMeasurement
-from zigpy.zcl.clusters.lightlink import LightLink
-from zigpy.zcl.clusters.smartenergy import Metering
+from zigpy.quirks.v2 import QuirkBuilder
 
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
 from zhaquirks.innr import INNR, ElectricalMeasurementClusterInnr, MeteringClusterInnr
 
-
-class SP234(CustomDevice):
-    """Innr SP 234 smart plug."""
-
-    signature = {
-        MODELS_INFO: [(INNR, "SP 234")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    Metering.cluster_id,
-                    ElectricalMeasurement.cluster_id,
-                    Diagnostic.cluster_id,
-                    LightLink.cluster_id,
-                    0xFC57,
-                    0xFC82,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Time.cluster_id,
-                    Ota.cluster_id,
-                ],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
-            },
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    MeteringClusterInnr,
-                    ElectricalMeasurementClusterInnr,
-                    Diagnostic.cluster_id,
-                    LightLink.cluster_id,
-                    0xFC57,
-                    0xFC82,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Time.cluster_id,
-                    Ota.cluster_id,
-                ],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
-            },
-        },
-    }
+(
+    QuirkBuilder(INNR, "SP 234")
+    .replaces(MeteringClusterInnr, endpoint_id=1)
+    .replaces(ElectricalMeasurementClusterInnr, endpoint_id=1)
+    .add_to_registry()
+)
