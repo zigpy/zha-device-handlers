72c772bab1bf4a2e1248a2418c9036dcf3d1572a
puddly <32534428+puddly@users.noreply.github.com>
2025-11-15 17:25:18 -0500

    Migrate quirks, pass 16

--- zhaquirks/smartthings/pgc313.py
+++ zhaquirks/smartthings/pgc313.py
@@ -1,22 +1,10 @@
 """SmartThings SmartSense Multi Sensor quirk."""
 
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Ota
+from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.clusters.security import IasZone
 
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-    ZONE_TYPE,
-)
+from zhaquirks.const import ZONE_TYPE
 from zhaquirks.smartthings import SMART_THINGS, SmartThingsIasZone
-
-SMARTSENSE_MULTI_DEVICE_TYPE = 0x0139  # decimal = 313
 
 
 class IasZoneContactSwitchCluster(SmartThingsIasZone):
@@ -25,36 +13,8 @@ class IasZoneContactSwitchCluster(SmartThingsIasZone):
     _CONSTANT_ATTRIBUTES = {ZONE_TYPE: IasZone.ZoneType.Contact_Switch}
 
 
-class SmartthingsSmartSenseMultiSensor(CustomDevice):
-    """Multipurpose sensor."""
-
-    signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=313
-        #  device_version=0 input_clusters=[0] output_clusters=[25]>
-        MODELS_INFO: [(SMART_THINGS, "PGC313")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: SMARTSENSE_MULTI_DEVICE_TYPE,
-                INPUT_CLUSTERS: [Basic.cluster_id],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
-            #  <SimpleDescriptor endpoint=2 profile=64513 device_type=313
-            #  device_version=0 input_clusters=[] output_clusters=[]>
-            2: {
-                PROFILE_ID: 0xFC01,  # decimal = 64513
-                DEVICE_TYPE: SMARTSENSE_MULTI_DEVICE_TYPE,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [],
-            },
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                INPUT_CLUSTERS: [Basic.cluster_id, IasZoneContactSwitchCluster],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        }
-    }
+(
+    QuirkBuilder(SMART_THINGS, "PGC313")
+    .adds(IasZoneContactSwitchCluster)
+    .add_to_registry()
+)
--- zhaquirks/smartthings/pgc314.py
+++ zhaquirks/smartthings/pgc314.py
@@ -1,20 +1,8 @@
 """SmartThings SmartSense Motion quirk."""
 
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Ota
+from zigpy.quirks.v2 import QuirkBuilder
 
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
 from zhaquirks.smartthings import SMART_THINGS, SmartThingsIasZone
-
-SMARTSENSE_MOTION_DEVICE_TYPE = 0x013A  # decimal = 314
 
 
 class IasZoneMotionCluster(SmartThingsIasZone):
@@ -25,36 +13,4 @@ class IasZoneMotionCluster(SmartThingsIasZone):
     _CONSTANT_ATTRIBUTES = {ZONE_TYPE: MOTION_TYPE}
 
 
-class SmartthingsSmartSenseMotionSensor(CustomDevice):
-    """SmartSense Motion Sensor."""
-
-    signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=314
-        #  device_version=0 input_clusters=[0] output_clusters=[25]>
-        MODELS_INFO: [(SMART_THINGS, "PGC314")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: SMARTSENSE_MOTION_DEVICE_TYPE,
-                INPUT_CLUSTERS: [Basic.cluster_id],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
-            #  <SimpleDescriptor endpoint=2 profile=64513 device_type=314
-            #  device_version=0 input_clusters=[] output_clusters=[]>
-            2: {
-                PROFILE_ID: 0xFC01,  # decimal = 64513
-                DEVICE_TYPE: SMARTSENSE_MOTION_DEVICE_TYPE,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [],
-            },
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                INPUT_CLUSTERS: [Basic.cluster_id, IasZoneMotionCluster],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            }
-        }
-    }
+(QuirkBuilder(SMART_THINGS, "PGC314").adds(IasZoneMotionCluster).add_to_registry())
