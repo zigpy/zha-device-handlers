ec59a69f0edece34116ad31956f94af6edbe34b9
puddly <32534428+puddly@users.noreply.github.com>
2025-11-15 14:10:02 -0500

    Migrate quirks, pass 2

--- zhaquirks/centralite/cl_3305S.py
+++ zhaquirks/centralite/cl_3305S.py
@@ -1,109 +1,35 @@
 """Device handler for centralite 3305."""
 
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Identify, Ota, PollControl
-from zigpy.zcl.clusters.homeautomation import Diagnostic
-from zigpy.zcl.clusters.measurement import OccupancySensing, TemperatureMeasurement
-from zigpy.zcl.clusters.security import IasZone
+from zigpy.quirks.v2 import QuirkBuilder
 
 from zhaquirks import PowerConfigurationCluster
 from zhaquirks.centralite import CENTRALITE
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
+
+#  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
+#  device_version=0
+#  input_clusters=[0, 1, 3, 1026, 1280, 32, 2821]
+#  output_clusters=[25]>
+#  <SimpleDescriptor endpoint=2 profile=260 device_type=263
+#  device_version=0
+#  input_clusters=[0, 1, 3, 1030, 2821]
+#  output_clusters=[3]>
+(
+    QuirkBuilder(CENTRALITE, "3305-S")
+    .applies_to(CENTRALITE, "3305")
+    .applies_to(CENTRALITE, "3325-S")
+    .applies_to(CENTRALITE, "3325")
+    .applies_to(CENTRALITE, "3326-L")
+    .applies_to(CENTRALITE, "3326")
+    .applies_to(CENTRALITE, "3328-G")
+    .applies_to(CENTRALITE, "Motion Sensor-A")
+    .replaces(PowerConfigurationCluster, endpoint_id=1)
+    .removes(PowerConfigurationCluster, endpoint_id=2)
+    .add_to_registry()
 )
 
-
-class CentraLite3305S(CustomDevice):
-    """Custom device representing centralite 3305."""
-
-    signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
-        #  device_version=0
-        #  input_clusters=[0, 1, 3, 1026, 1280, 32, 2821]
-        #  output_clusters=[25]>
-        MODELS_INFO: [
-            (CENTRALITE, "3305-S"),
-            (CENTRALITE, "3305"),
-            (CENTRALITE, "3325-S"),
-            (CENTRALITE, "3325"),
-            (CENTRALITE, "3326-L"),
-            (CENTRALITE, "3326"),
-            (CENTRALITE, "3328-G"),
-            (CENTRALITE, "Motion Sensor-A"),
-        ],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster.cluster_id,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    TemperatureMeasurement.cluster_id,
-                    IasZone.cluster_id,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
-            #  <SimpleDescriptor endpoint=2 profile=260 device_type=263
-            #  device_version=0
-            #  input_clusters=[0, 1, 3, 1030, 2821]
-            #  output_clusters=[3]>
-            2: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.OCCUPANCY_SENSOR,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster.cluster_id,
-                    Identify.cluster_id,
-                    OccupancySensing.cluster_id,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id],
-            },
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    TemperatureMeasurement.cluster_id,
-                    IasZone.cluster_id,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
-            2: {
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    OccupancySensing.cluster_id,
-                    Diagnostic.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id],
-            },
-        }
-    }
-
-
-class CentraLite3305S2(CentraLite3305S):
-    """Custom device representing centralite 3305 with one endpoint."""
-
-    signature = {
-        MODELS_INFO: [(CENTRALITE, "3305")],
-        ENDPOINTS: {1: {**CentraLite3305S.signature["endpoints"][1]}},
-    }
-
-    replacement = {ENDPOINTS: {1: {**CentraLite3305S.replacement["endpoints"][1]}}}
+(
+    QuirkBuilder(CENTRALITE, "3305")
+    .replaces(PowerConfigurationCluster, endpoint_id=1)
+    .skip_configuration()
+    .add_to_registry()
+)
--- zhaquirks/centralite/cl_3321S.py
+++ zhaquirks/centralite/cl_3321S.py
@@ -1,101 +1,20 @@
 """Centralite 3321S quirk."""
 
 # pylint disable=C0103
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Identify, Ota, PollControl
-from zigpy.zcl.clusters.homeautomation import Diagnostic
-from zigpy.zcl.clusters.measurement import TemperatureMeasurement
-from zigpy.zcl.clusters.security import IasZone
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl.clusters.general import PowerConfiguration
 
 from zhaquirks import PowerConfigurationCluster
 from zhaquirks.centralite import CENTRALITE, CentraLiteAccelCluster
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
 from zhaquirks.samjin import SAMJIN
 
-MANUFACTURER_SPECIFIC_CLUSTER_ID = 0xFC0F  # decimal = 64527
-MANUFACTURER_SPECIFIC_PROFILE_ID = 0xC2DF  # decimal = 49887
-
-
-class CentraLite3321S(CustomDevice):
-    """CentraLite3321S custom device implementation."""
-
-    signature = {
-        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
-        #  device_version=0
-        #  input_clusters=[0, 1, 3, 1026, 1280, 32, 2821, 64514]
-        #  output_clusters=[25]>
-        MODELS_INFO: [
-            (CENTRALITE, "3320"),
-            (CENTRALITE, "3321-S"),
-            (CENTRALITE, "3321"),
-            (SAMJIN, "multi"),
-        ],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster.cluster_id,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    TemperatureMeasurement.cluster_id,
-                    IasZone.cluster_id,
-                    Diagnostic.cluster_id,
-                    CentraLiteAccelCluster.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
-            #  <SimpleDescriptor endpoint=2 profile=49887 device_type=12
-            #  device_version=0
-            #  input_clusters=[0, 1, 3, 2821, 64527]
-            #  output_clusters=[3]>
-            2: {
-                PROFILE_ID: MANUFACTURER_SPECIFIC_PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.SIMPLE_SENSOR,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster.cluster_id,
-                    Identify.cluster_id,
-                    Diagnostic.cluster_id,
-                    MANUFACTURER_SPECIFIC_CLUSTER_ID,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id],
-            },
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    PowerConfigurationCluster,
-                    Identify.cluster_id,
-                    PollControl.cluster_id,
-                    TemperatureMeasurement.cluster_id,
-                    IasZone.cluster_id,
-                    Diagnostic.cluster_id,
-                    CentraLiteAccelCluster,
-                ],
-                OUTPUT_CLUSTERS: [Ota.cluster_id],
-            },
-            2: {
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Diagnostic.cluster_id,
-                    MANUFACTURER_SPECIFIC_CLUSTER_ID,
-                ],
-                OUTPUT_CLUSTERS: [Identify.cluster_id],
-            },
-        }
-    }
+(
+    QuirkBuilder(CENTRALITE, "3320")
+    .applies_to(CENTRALITE, "3321-S")
+    .applies_to(CENTRALITE, "3321")
+    .applies_to(SAMJIN, "multi")
+    .replaces(PowerConfigurationCluster, endpoint_id=1)
+    .replaces(CentraLiteAccelCluster, endpoint_id=1)
+    .removes(PowerConfiguration.cluster_id, endpoint_id=2)
+    .add_to_registry()
+)
