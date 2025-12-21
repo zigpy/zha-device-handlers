652b0741fe1b73000d2479b4f5744e18c6216742
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-12-21 22:13:48 +0100

    Remove custom divisor for Innr SP 240 on `0x191E3685`
    Co-authored-by: puddly <32534428+puddly@users.noreply.github.com>

--- zhaquirks/innr/innr_sp240_plug.py
+++ zhaquirks/innr/innr_sp240_plug.py
@@ -1,31 +1,9 @@
 """Innr SP 240 plug."""
 
-from zigpy.profiles import zgp, zha
-from zigpy.quirks import CustomCluster, CustomDevice
-from zigpy.zcl.clusters.general import (
-    Basic,
-    GreenPowerProxy,
-    Groups,
-    Identify,
-    LevelControl,
-    OnOff,
-    Ota,
-    Scenes,
-    Time,
-)
-from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
-from zigpy.zcl.clusters.lightlink import LightLink
-from zigpy.zcl.clusters.smartenergy import Metering
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
 
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
-)
-from zhaquirks.innr import INNR, MeteringClusterInnr
+from zhaquirks.innr import MeteringClusterInnr
 
 
 class InnrCluster(CustomCluster):
@@ -34,72 +12,11 @@ class InnrCluster(CustomCluster):
     cluster_id = 0xE001
 
 
-class SP240(CustomDevice):
-    """Innr SP 240  smart plug."""
-
-    signature = {
-        MODELS_INFO: [(INNR, "SP 240")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    LevelControl.cluster_id,
-                    Metering.cluster_id,
-                    ElectricalMeasurement.cluster_id,
-                    LightLink.cluster_id,
-                    InnrCluster.cluster_id,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Time.cluster_id,
-                    Ota.cluster_id,
-                ],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [
-                    GreenPowerProxy.cluster_id,
-                ],
-            },
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.ON_OFF_PLUG_IN_UNIT,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    LevelControl.cluster_id,
-                    MeteringClusterInnr,
-                    ElectricalMeasurement.cluster_id,
-                    LightLink.cluster_id,
-                    InnrCluster,
-                ],
-                OUTPUT_CLUSTERS: [
-                    Time.cluster_id,
-                    Ota.cluster_id,
-                ],
-            },
-            242: {
-                PROFILE_ID: zgp.PROFILE_ID,
-                DEVICE_TYPE: zgp.DeviceType.PROXY_BASIC,
-                INPUT_CLUSTERS: [],
-                OUTPUT_CLUSTERS: [
-                    GreenPowerProxy.cluster_id,
-                ],
-            },
-        },
-    }
+(
+    QuirkBuilder(manufacturer="innr", model="SP 240")
+    # Firmware version `421410437` fixed the divisor and multiplier bug
+    .firmware_version_filter(max_version=0x191E3685)
+    .replaces(MeteringClusterInnr, endpoint_id=1)
+    .replaces(InnrCluster, endpoint_id=1)
+    .add_to_registry()
+)
