9e8533b6f5087ca41170d8be2ff0f30b820a20f2
puddly <32534428+puddly@users.noreply.github.com>
2025-12-23 00:31:24 -0500

    WIP

--- tests/test_tuya_mcu.py
+++ tests/test_tuya_mcu.py
@@ -361,8 +361,8 @@ async def test_tuya_mcu_classes():
     assert not mcu_version.version
 
     # test TuyaClusterData.manufacturer values
-    t_c_d = TuyaClusterData(manufacturer=foundation.ZCLHeader.NO_MANUFACTURER_ID)
-    assert t_c_d.manufacturer == -1
+    t_c_d = TuyaClusterData(manufacturer=None)
+    assert t_c_d.manufacturer is None
     t_c_d = TuyaClusterData(manufacturer=4619)
     assert t_c_d.manufacturer == 4619
     t_c_d = TuyaClusterData(manufacturer="4098")
