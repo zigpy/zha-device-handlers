9aec96952d266f6e2818520febd8fbff59609626
1775939187 <1775939187@qq.com>
2025-12-26 09:43:10 +0800

    Add Sonoff ZBM5 3-channel switch

--- zhaquirks/sonoff/zbm5.py
+++ zhaquirks/sonoff/zbm5.py
@@ -70,7 +70,6 @@ class SonoffCluster(CustomCluster):
 
         if attrid == self.AttributeDefs.detach_relay_mask.id:
             # Convert bitmap to individual relay states
-
             mask = value
 
             self._update_attribute(
@@ -105,15 +104,15 @@ class SonoffCluster(CustomCluster):
             for attr_def, bit_mask in relay_attr_defs:
                 if attrid in (attr_def.id, attr_def.name):
                     new_attributes.pop(attrid)
-
+                    
                     if value:
                         mask |= bit_mask
-
+                    
                     else:
                         mask &= ~bit_mask
-
+                    
                     new_attributes[mask_attr] = mask
-
+                    
                     break
 
         return await super().write_attributes(new_attributes, manufacturer, **kwargs)
