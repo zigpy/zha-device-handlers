aa09f0519f9b982c6720a4822977818a545f1552
Simon Leiner <simon@leiner.me>
2025-12-28 20:09:27 +0100

    Add _TZB210_rkgngb5o manufacturer for TS0501B (single-color)

--- zhaquirks/tuya/ts0501bs.py
+++ zhaquirks/tuya/ts0501bs.py
@@ -41,6 +41,7 @@ class DimmableLedController(CustomDevice):
             ("_TZ3210_agjx0pxt", "TS0501B"),
             ("_TZ3210_d062rv7j", "TS0501B"),
             ("_TZ3210_syh4kuef", "TS0501B"),
+            ("_TZB210_rkgngb5o", "TS0501B"),
         ],
         ENDPOINTS: {
             # <SimpleDescriptor endpoint=1 profile=260 device_type=257
