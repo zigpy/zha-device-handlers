cea1671fc6f556e442fc23981de8eeb9b55277e7
Josh K <28068117+iamjoshk@users.noreply.github.com>
2025-12-31 16:43:06 -0500

    clean up tracking move commands

--- zhaquirks/ikea/__init__.py
+++ zhaquirks/ikea/__init__.py
@@ -72,16 +72,14 @@ class IkeaBilresaLevelControl(CustomCluster, LevelControl):
     ) -> None:
         """Handle cluster specific commands.
 
-        Track move commands to remember direction for stop commands
-        """
+        Track move commands to remember direction for stop commands"""
         if hdr.command_id in (0x01, 0x05):
             move_mode = args[0]
             self._last_move_direction = move_mode
-        elif hdr.command_id in (0x03, 0x07):
-            if self._last_move_direction == 0:
-                self.listener_event(ZHA_SEND_EVENT, "move_up_release", [])
-            elif self._last_move_direction == 1:
-                self.listener_event(ZHA_SEND_EVENT, "move_down_release", [])
+        elif hdr.command_id in (0x03, 0x07) and self._last_move_direction is not None:
+            event = "move_up_release" if self._last_move_direction == 0 else "move_down_release"
+            self.listener_event(ZHA_SEND_EVENT, event, [])
+            self.listener_cluster_command(hdr.tsn, event, [])
 
 
 class ShortcutV1Cluster(EventableCluster):
