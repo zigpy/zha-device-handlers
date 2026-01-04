81b0ca75e272e2fff1ac5bbc57129a0cd0b4a639
彭锡楷 <xikai.peng@itead.cc>
2026-01-04 10:30:56 +0800

    update ZBM5 quick

--- zhaquirks/sonoff/zbm5.py
+++ zhaquirks/sonoff/zbm5.py
@@ -66,7 +66,7 @@ class SonoffCluster(CustomCluster):
     def _update_attribute(self, attrid, value):
         """Update attribute and handle relay mask conversion."""
 
-        super()._update_attribute(attrid, value)
+        # super()._update_attribute(attrid, value)
 
         if attrid == self.AttributeDefs.detach_relay_mask.id:
             # Convert bitmap to individual relay states
@@ -104,15 +104,11 @@ class SonoffCluster(CustomCluster):
             for attr_def, bit_mask in relay_attr_defs:
                 if attrid in (attr_def.id, attr_def.name):
                     new_attributes.pop(attrid)
-
                     if value:
                         mask |= bit_mask
-
                     else:
                         mask &= ~bit_mask
-
                     new_attributes[mask_attr] = mask
-
                     break
 
         return await super().write_attributes(new_attributes, manufacturer, **kwargs)
