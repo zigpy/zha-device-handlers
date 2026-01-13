9add2c335115a759c6e9e81d0fce3d5cb45f5bf5
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-01-14 00:34:52 +0100

    Add TODOs about polling EM attributes

--- zhaquirks/ubisys/switching_actuator_series_2.py
+++ zhaquirks/ubisys/switching_actuator_series_2.py
@@ -38,5 +38,9 @@ class UbisysElectricalMeasurement(CustomCluster, ElectricalMeasurement):
     # SmartEnergy summation attributes do not support attribute reporting, need polling
     # TODO: Add support for this in ZHA
     .exposes_feature(SE_POLL_SUMMATION)
+    # TODO: Fix/rework/rethink EM polling
+    # ElectricalMeasurement cluster does not support attribute reporting at all,
+    # so poll current explicitly (active power does, but if disabled, nothing will poll)
+    # .exposes_feature(EM_POLL_CURRENT)
     .add_to_registry()
 )
