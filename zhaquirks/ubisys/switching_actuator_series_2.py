45c9e6431ed92285b64d36c110f28d6416c35ae9
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-01-13 23:23:24 +0100

    Add `ac_current_divisor`

--- zhaquirks/ubisys/switching_actuator_series_2.py
+++ zhaquirks/ubisys/switching_actuator_series_2.py
@@ -9,6 +9,7 @@ class UbisysElectricalMeasurement(CustomCluster, ElectricalMeasurement):
     """Sets divisor attributes missing on the device."""
 
     _CONSTANT_ATTRIBUTES = {
+        ElectricalMeasurement.AttributeDefs.ac_current_divisor.id: 10,
         ElectricalMeasurement.AttributeDefs.ac_frequency_divisor.id: 10,
     }
 
