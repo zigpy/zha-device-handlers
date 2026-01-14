c0d7065bffd9f8e6b989c200c884be13cf04b99f
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-01-14 04:42:07 +0100

    Add device triggers to switch S1-R

--- zhaquirks/ubisys/switch_s1r.py
+++ zhaquirks/ubisys/switch_s1r.py
@@ -2,8 +2,17 @@
 
 from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl.clusters.general import OnOff
 from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
 
+from zhaquirks.const import (
+    BUTTON_1,
+    BUTTON_2,
+    CLUSTER_ID,
+    COMMAND,
+    COMMAND_CLICK,
+    ENDPOINT_ID,
+)
 from zhaquirks.quirk_ids import SE_POLL_SUMMATION
 
 
@@ -44,5 +53,20 @@ class UbisysElectricalMeasurement(CustomCluster, ElectricalMeasurement):
     # ElectricalMeasurement cluster does not support attribute reporting at all,
     # so poll current explicitly (active power does, but if disabled, nothing will poll)
     # .exposes_feature(EM_POLL_CURRENT)
+    .device_automation_triggers(
+        {
+            # this also toggles light by default
+            (COMMAND_CLICK, BUTTON_1): {
+                ENDPOINT_ID: 2,
+                CLUSTER_ID: OnOff.cluster_id,
+                COMMAND: OnOff.ServerCommandDefs.toggle.name,
+            },
+            (COMMAND_CLICK, BUTTON_2): {
+                ENDPOINT_ID: 3,
+                CLUSTER_ID: OnOff.cluster_id,
+                COMMAND: OnOff.ServerCommandDefs.toggle.name,
+            },
+        }
+    )
     .add_to_registry()
 )
