1977e44a502245e03bb182bd9ae2345a8ea6de12
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-01-14 03:39:22 +0100

    Add Ubisys S2 (5502) support

---
+++ zhaquirks/ubisys/switching_actuator_s2.py
@@ -0,0 +1,56 @@
+"""Ubisys Switching Actuator S2 quirk."""
+
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
+from zigpy.zcl.clusters.smartenergy import Metering
+
+from zhaquirks.quirk_ids import SE_POLL_SUMMATION
+
+# TODO: Combine with switching_actuator_s1.py?
+
+
+class UbisysElectricalMeasurement(CustomCluster, ElectricalMeasurement):
+    """Sets divisor attributes missing on the device."""
+
+    _CONSTANT_ATTRIBUTES = {
+        ElectricalMeasurement.AttributeDefs.ac_current_divisor.id: 1000,
+        ElectricalMeasurement.AttributeDefs.ac_frequency_divisor.id: 1000,
+    }
+
+
+(
+    QuirkBuilder(manufacturer="ubisys", model="S2 (5502)")
+    .replaces(UbisysElectricalMeasurement, endpoint_id=5)
+    # The device exposes total active power on multiple attributes,
+    # but only supports attribute reporting on the SE "instantaneous demand" attribute,
+    # so we disable the other entities by default
+    # TODO: Disabling this entity also disables polling for the entire EM cluster in ZHA
+    .change_entity_metadata(
+        endpoint_id=5,
+        cluster_id=ElectricalMeasurement.cluster_id,
+        unique_id_suffix="5-2820",  # no translation key and no actual suffix for this
+        new_entity_registry_enabled_default=False,
+    )
+    .change_entity_metadata(
+        endpoint_id=5,
+        cluster_id=ElectricalMeasurement.cluster_id,
+        unique_id_suffix="total_active_power",
+        new_entity_registry_enabled_default=False,
+    )
+    # Summation received stuck at 102648430084968, hide for now
+    .change_entity_metadata(
+        endpoint_id=5,
+        cluster_id=Metering.cluster_id,
+        unique_id_suffix="summation_received",
+        new_entity_registry_enabled_default=False,
+    )
+    # SmartEnergy summation attributes do not support attribute reporting, need polling
+    # TODO: Add support for this in ZHA
+    .exposes_feature(SE_POLL_SUMMATION)
+    # TODO: Fix/rework/rethink EM polling
+    # ElectricalMeasurement cluster does not support attribute reporting at all,
+    # so poll current explicitly (active power does, but if disabled, nothing will poll)
+    # .exposes_feature(EM_POLL_CURRENT)
+    .add_to_registry()
+)
