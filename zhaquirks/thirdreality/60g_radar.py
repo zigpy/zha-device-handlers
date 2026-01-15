1c9da835dd0d713e32a6fc4797e3f1e1b921aed2
jintj <jintj@3reality.com>
2026-01-15 15:10:04 +0800

    update

---
+++ zhaquirks/thirdreality/60g_radar.py
@@ -0,0 +1,40 @@
+"""Third Reality formaldehyde sensor devices."""
+
+from typing import Final
+
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder, SensorDeviceClass, SensorStateClass
+from zigpy.quirks.v2.homeassistant import CONCENTRATION_PARTS_PER_BILLION
+import zigpy.types as t
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
+
+
+class ThirdRealityRadarCluster(CustomCluster):
+    """Third Reality's plug private cluster."""
+
+    cluster_id = 0x042e
+
+    class AttributeDefs(BaseAttributeDefs):
+        """Define the attributes of a private cluster."""
+
+        # measure the value of voc
+        volatile_organic_compounds: Final = ZCLAttributeDef(
+            id=0x0000,
+            type=t.Single,
+            is_manufacturer_specific=True,
+        )
+
+(
+    QuirkBuilder("Third Reality, Inc", "3RPL01084Z")
+    .replaces(ThirdRealityRadarCluster)
+    .sensor(
+        endpoint_id=1,
+        attribute_name=ThirdRealityRadarCluster.AttributeDefs.volatile_organic_compounds.name,
+        cluster_id=ThirdRealityRadarCluster.cluster_id,
+        device_class=SensorDeviceClass.VOLATILE_ORGANIC_COMPOUNDS_PARTS,
+        state_class=SensorStateClass.MEASUREMENT,
+        unit=CONCENTRATION_PARTS_PER_BILLION,
+        fallback_name="Volatile organic compounds",
+    )
+    .add_to_registry()
+)
