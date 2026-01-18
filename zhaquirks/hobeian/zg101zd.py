38476ddf6fae19e479784bcd0b4bb9815d7b3bcc
Danilo Kühn <danilo@kuehns.club>
2026-01-18 22:08:44 +0100

    FIX python3.13 problems

--- zhaquirks/hobeian/zg101zd.py
+++ zhaquirks/hobeian/zg101zd.py
@@ -2,13 +2,15 @@
 
 from typing import Any, Final
 
-from zigpy.quirks.v2 import QuirkBuilder
 import zigpy.types as t
+from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.clusters.general import OnOff
 from zigpy.zcl.foundation import ZCLHeader
 
+from zhaquirks import CustomCluster
 
-class HobeianOnOffCluster(OnOff):
+
+class HobeianOnOffCluster(CustomCluster, OnOff):
     """Custom OnOff cluster for HOBEIAN rotation commands.
 
     This device uses proprietary commands on OnOff cluster for rotation:
