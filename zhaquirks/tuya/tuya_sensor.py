da71a8e60d085a80e32026f1c06fa7fb9df18dfe
Felix Eckhofer <felix@eckhofer.com>
2026-01-22 21:23:16 +0100

    Add Tuya ZTH13 temperature sensor `_TZE204_kwi6bbk4` variant

--- zhaquirks/tuya/tuya_sensor.py
+++ zhaquirks/tuya/tuya_sensor.py
@@ -312,6 +312,7 @@ class NoManufTimeTuyaMCUCluster(TuyaMCUCluster):
     .applies_to("_TZE284_upagmta9", "TS0601")
     .applies_to("_TZE204_1wnh8bqp", "TS0601")
     .applies_to("_TZE284_1wnh8bqp", "TS0601")
+    .applies_to("_TZE204_kwi6bbk4", "TS0601")
     .tuya_temperature(dp_id=1, scale=10)
     .tuya_humidity(dp_id=2)
     .tuya_dp(
