497f5b80ebbe256e363f989b3fea14c0242237a8
puddly <32534428+puddly@users.noreply.github.com>
2026-01-27 18:58:31 -0500

    Fix formatting and remove unnecessary `cluster_id=`

--- zhaquirks/imagic/gs1117s.py
+++ zhaquirks/imagic/gs1117s.py
@@ -1,17 +1,12 @@
 """Device handler for iMagic by Greatstar."""
 
 from zigpy.quirks.v2 import QuirkBuilder
-from zigpy.zcl.clusters.general import PowerConfiguration
 
 from zhaquirks import PowerConfigurationCluster
 from zhaquirks.imagic import IMAGIC
 
 (
     QuirkBuilder(IMAGIC, "1117-S")
-    .replaces(
-        PowerConfigurationCluster,
-        cluster_id=PowerConfiguration.cluster_id,
-        endpoint_id=1,
-    )
+    .replaces(PowerConfigurationCluster, endpoint_id=1)
     .add_to_registry()
-)
+)  # fmt: skip
