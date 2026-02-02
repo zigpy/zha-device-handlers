32e104c96777d4b195ccb9364b3a3a6c0cc3a673
Zevroc <90315997+Zevroc@users.noreply.github.com>
2026-02-02 15:40:48 +0100

    Add Tuya TS0601 pilot wire heating control quirk

---
+++ zhaquirks/tuya/ts0601_pilot_wire.py
@@ -0,0 +1,58 @@
+"""Tuya TS0601 pilot wire heating control quirk for ZHA - Complete."""
+from zigpy.quirks.v2 import EntityPlatform, EntityType
+import zigpy.types as t
+from zhaquirks.tuya.builder import TuyaQuirkBuilder
+class PilotWireMode(t.enum8):
+    """Pilot wire mode enum."""
+    Comfort = 0x00
+    Comfort_Minus_1 = 0x01
+    Comfort_Minus_2 = 0x02
+    Eco = 0x03
+    Anti_Frost = 0x04
+    Off = 0x05
+class OperatingMode(t.enum8):
+    """Operating mode enum."""
+    Auto = 0x00
+    Manual = 0x01
+(
+    TuyaQuirkBuilder("_TZE204_3q3maeoo", "TS0601")
+    .tuya_enum(
+        dp_id=2,
+        attribute_name="operating_mode",
+        enum_class=OperatingMode,
+        translation_key="operating_mode",
+        fallback_name="Operating Mode",
+        entity_type=EntityType.STANDARD,
+        entity_platform=EntityPlatform.SELECT,
+    )
+    .tuya_enum(
+        dp_id=127,
+        attribute_name="pilot_wire_mode",
+        enum_class=PilotWireMode,
+        translation_key="pilot_wire_mode",
+        fallback_name="Pilot Wire Mode",
+        entity_type=EntityType.STANDARD,
+        entity_platform=EntityPlatform.SELECT,
+    )
+    .tuya_temperature(dp_id=16, scale=10)
+"""
+    .tuya_sensor(
+        dp_id=8,
+        attribute_name="unknown_dp8",
+        type=t.uint8_t,
+        entity_type=EntityType.DIAGNOSTIC,
+        translation_key="unknown_dp8",
+        fallback_name="Unknown DP8 (41-42)",
+    )
+    .tuya_sensor(
+        dp_id=126,
+        attribute_name="unknown_dp126",
+        type=t.uint8_t,
+        entity_type=EntityType.DIAGNOSTIC,
+        translation_key="unknown_dp126",
+        fallback_name="Unknown DP126",
+    )
+"""
+    .skip_configuration()
+    .add_to_registry()
+)
