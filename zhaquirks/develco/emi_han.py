03fcf8eae093bccd003e283b9d65f99a31ee64b9
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-02-12 17:32:56 +0100

    Remove custom `FrientMetering` cluster

--- zhaquirks/develco/emi_han.py
+++ zhaquirks/develco/emi_han.py
@@ -2,17 +2,9 @@
 
 from __future__ import annotations
 
-from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import QuirkBuilder
-from zigpy.zcl.clusters.smartenergy import Metering
-
-
-class FrientMetering(CustomCluster, Metering):
-    """Frient EMI Norwegian HAN Metering cluster definition."""
-
 
 (
     QuirkBuilder("frient A/S", "EMIZB-132")
-    .replaces(FrientMetering, endpoint_id=2)
     .add_to_registry()
-)
+)  # fmt: skip
