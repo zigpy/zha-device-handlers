e8afd9b1fe7f81cd021174601630d737828b3445
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-02-12 17:37:04 +0100

    Add comment explaining empty quirk

--- zhaquirks/develco/emi_han.py
+++ zhaquirks/develco/emi_han.py
@@ -4,6 +4,14 @@ from __future__ import annotations
 
 from zigpy.quirks.v2 import QuirkBuilder
 
+# This quirk had a workaround for blocking attribute reports with the ZCL divisor
+# attribute ID and manufacturer-specific bit set. The underlying issue of incorrectly
+# parsing manufacturer-specific attribute reports for ZCL attributes was resolved
+# with zigpy 0.91.0.
+# There's still a test for this device which tests that the divisor is not incorrectly
+# updated. The quirk will also be expanded to use support manufacturer-specific
+# attributes for this device, so it's kept, even though there's no functionality now.
+
 (
     QuirkBuilder("frient A/S", "EMIZB-132")
     .add_to_registry()
