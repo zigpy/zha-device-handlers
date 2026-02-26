36408e746ee099909719f3961762736988820079
puddly <32534428+puddly@users.noreply.github.com>
2026-02-26 11:51:55 -0500

    Add clarification for skipping `nodata`

--- zhaquirks/xiaomi/__init__.py
+++ zhaquirks/xiaomi/__init__.py
@@ -292,6 +292,9 @@ class XiaomiCluster(CustomCluster):
         attr_id, data = t.uint16_t.deserialize(data)
         attr_type, data = t.uint8_t.deserialize(data)
 
+        # Bad parsing states sometimes eat into the attribute ID's zero octet, which can
+        # be interpreted as the `nodata` type (a single null byte). This isn't really
+        # used by real devices and can be skipped.
         if attr_type == DataTypeId.nodata:
             return
 
