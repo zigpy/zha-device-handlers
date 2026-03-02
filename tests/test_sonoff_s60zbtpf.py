75660311e5e17284a12ef07b6532940258b7633b
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-03-02 21:57:08 +0100

    Small tweaks

--- tests/test_sonoff_s60zbtpf.py
+++ tests/test_sonoff_s60zbtpf.py
@@ -1,7 +1,6 @@
 """Tests for the SONOFF S60ZBTPF device."""
 
-import pytest
-from zigpy.zcl import ClusterType
+from zigpy.zcl import foundation
 from zigpy.zcl.clusters.general import OnOff
 from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
 
@@ -15,15 +14,10 @@ VOLTAGE_ID = ElectricalMeasurement.AttributeDefs.rms_voltage.id
 ON_OFF_ID = OnOff.AttributeDefs.on_off.id
 
 
-@pytest.fixture
-def device(zigpy_device_from_v2_quirk):
-    """Create a SONOFF S60ZBTPF zigpy device mock for testing."""
-    cluster_ids = {1: {ElectricalMeasurement.cluster_id: ClusterType.Server}}
-    return zigpy_device_from_v2_quirk("SONOFF", "S60ZBTPF", cluster_ids=cluster_ids)
+async def test_sonoff_plug_power_fix(zigpy_device_from_v2_quirk):
+    """Test Sonoff plug power measurement overrides."""
+    device = zigpy_device_from_v2_quirk("SONOFF", "S60ZBTPF")
 
-
-async def test_power_fix(device):
-    """Test power measurement overrides."""
     electrical_cluster = device.endpoints[1].electrical_measurement
     on_off_cluster = device.endpoints[1].on_off
 
@@ -37,19 +31,19 @@ async def test_power_fix(device):
     on_off_cluster.update_attribute(ON_OFF_ID, False)
     assert electrical_cluster.get(POWER_ID) == 0
     assert electrical_cluster.get(CURRENT_ID) == 0
-    assert electrical_cluster.get(VOLTAGE_ID) is None
+    assert electrical_cluster.get(VOLTAGE_ID) == foundation.DataType.uint16.non_value
 
     electrical_cluster.update_attribute(POWER_ID, 300)
     electrical_cluster.update_attribute(CURRENT_ID, 13)
     electrical_cluster.update_attribute(VOLTAGE_ID, 263)
     assert electrical_cluster.get(POWER_ID) == 0
     assert electrical_cluster.get(CURRENT_ID) == 0
-    assert electrical_cluster.get(VOLTAGE_ID) is None
+    assert electrical_cluster.get(VOLTAGE_ID) == foundation.DataType.uint16.non_value
 
     on_off_cluster.update_attribute(ON_OFF_ID, True)
     assert electrical_cluster.get(POWER_ID) == 0
     assert electrical_cluster.get(CURRENT_ID) == 0
-    assert electrical_cluster.get(VOLTAGE_ID) is None
+    assert electrical_cluster.get(VOLTAGE_ID) == foundation.DataType.uint16.non_value
 
     electrical_cluster.update_attribute(POWER_ID, 300)
     electrical_cluster.update_attribute(CURRENT_ID, 13)
--- zhaquirks/sonoff/s60zbtpf.py
+++ zhaquirks/sonoff/s60zbtpf.py
@@ -5,31 +5,37 @@ even when the socket is turned off. This quirk fixes that by setting the
 `active_power` and `rms_current` to 0 when the `on_off` state is False.
 """
 
+from typing import Any
+
 from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import QuirkBuilder
 import zigpy.types as t
+from zigpy.zcl import foundation
 from zigpy.zcl.clusters.general import OnOff
 from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
-
-POWER_ID = ElectricalMeasurement.AttributeDefs.active_power.id
-CURRENT_ID = ElectricalMeasurement.AttributeDefs.rms_current.id
-VOLTAGE_ID = ElectricalMeasurement.AttributeDefs.rms_voltage.id
-ON_OFF_ID = OnOff.AttributeDefs.on_off.id
 
 
 class SonoffS60OnOff(CustomCluster, OnOff):
     """Custom OnOff cluster that resets power readings when the socket is turned off."""
 
-    def _update_attribute(self, attrid, value):
+    def _update_attribute(
+        self, attrid: int | t.uint16_t | foundation.ZCLAttributeDef, value: Any
+    ) -> None:
         """Reset attributes to zero when the socket is turned off."""
-
-        if attrid == ON_OFF_ID and value == t.Bool.false:
-            self.debug(
-                "Socket turned off, resetting power and current measurements to zero"
+        if (
+            self.find_attribute(attrid) == OnOff.AttributeDefs.on_off
+            and value == t.Bool.false
+        ):
+            self.endpoint.electrical_measurement.update_attribute(
+                ElectricalMeasurement.AttributeDefs.active_power, 0
             )
-            self.endpoint.electrical_measurement.update_attribute(POWER_ID, 0)
-            self.endpoint.electrical_measurement.update_attribute(CURRENT_ID, 0)
-            self.endpoint.electrical_measurement.update_attribute(VOLTAGE_ID, None)
+            self.endpoint.electrical_measurement.update_attribute(
+                ElectricalMeasurement.AttributeDefs.rms_current, 0
+            )
+            self.endpoint.electrical_measurement.update_attribute(
+                ElectricalMeasurement.AttributeDefs.rms_voltage,
+                foundation.DataType.uint16.non_value,
+            )
 
         super()._update_attribute(attrid, value)
 
@@ -37,28 +43,28 @@ class SonoffS60OnOff(CustomCluster, OnOff):
 class SonoffS60ElectricalMeasurement(CustomCluster, ElectricalMeasurement):
     """Custom ElectricalMeasurement cluster that prevents power updates when the socket is turned off."""
 
-    def _update_attribute(self, attrid, value):
+    def _update_attribute(
+        self, attrid: int | t.uint16_t | foundation.ZCLAttributeDef, value: Any
+    ) -> None:
         """Prevent updates when the socket is turned off."""
-
-        if self.endpoint.on_off.get(ON_OFF_ID) == t.Bool.false:
-            if attrid == POWER_ID:
-                self.debug("Socket turned off, preventing power measurement update")
-                return
-
-            if attrid == CURRENT_ID:
-                self.debug("Socket turned off, preventing current measurement update")
-                return
-
-            if attrid == VOLTAGE_ID:
-                self.debug("Socket turned off, preventing voltage measurement update")
-                return
+        if (
+            self.endpoint.on_off.get(OnOff.AttributeDefs.on_off.id) == t.Bool.false
+            # we should always get the ID here, but just in case, check for def too
+            and self.find_attribute(attrid)
+            in (
+                ElectricalMeasurement.AttributeDefs.active_power,
+                ElectricalMeasurement.AttributeDefs.rms_current,
+                ElectricalMeasurement.AttributeDefs.rms_voltage,
+            )
+        ):
+            return
 
         super()._update_attribute(attrid, value)
 
 
 (
     QuirkBuilder("SONOFF", "S60ZBTPF")
-    .also_applies_to("SONOFF", "S60ZBTPG")
+    .applies_to("SONOFF", "S60ZBTPG")
     .replaces(SonoffS60OnOff)
     .replaces(SonoffS60ElectricalMeasurement)
     .add_to_registry()
