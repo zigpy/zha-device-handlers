b26696b7c07b0990465eac4214d96de0ec979cab
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-03-13 17:42:08 +0100

    Rename attribute to `disable_double_click`
    Co-authored-by: TheJulianJES <TheJulianJES@users.noreply.github.com>

--- zhaquirks/thirdreality/button.py
+++ zhaquirks/thirdreality/button.py
@@ -47,7 +47,7 @@ class ThirdRealityButtonCluster(CustomCluster):
     class AttributeDefs(BaseAttributeDefs):
         """Define the attributes of a private cluster."""
 
-        cancel_double_click: Final = ZCLAttributeDef(
+        disable_double_click: Final = ZCLAttributeDef(
             id=0x0000,
             type=t.uint8_t,
             is_manufacturer_specific=True,
@@ -59,7 +59,7 @@ class ThirdRealityButtonCluster(CustomCluster):
     .replaces(ThirdRealityButtonCluster)
     .replaces(MultistateInputCluster)
     .switch(
-        attribute_name=ThirdRealityButtonCluster.AttributeDefs.cancel_double_click.name,
+        attribute_name=ThirdRealityButtonCluster.AttributeDefs.disable_double_click.name,
         cluster_id=ThirdRealityButtonCluster.cluster_id,
         translation_key="cancel_double_click",
         fallback_name="Cancel double click",
