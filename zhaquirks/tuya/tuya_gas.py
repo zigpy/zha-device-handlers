3956ad8d2a2798aa1b0b7f0cf65707f3c8f31bd0
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-03-25 02:14:29 +0100

    Demote "fault alarm" to diagnostics type

--- zhaquirks/tuya/tuya_gas.py
+++ zhaquirks/tuya/tuya_gas.py
@@ -224,7 +224,6 @@ tuya_gas_alarm_base = (
     .tuya_binary_sensor(
         dp_id=11,
         attribute_name="fault_alarm",
-        entity_type=EntityType.STANDARD,
         device_class=BinarySensorDeviceClass.PROBLEM,
         translation_key="fault_alarm",
         fallback_name="Fault alarm",
