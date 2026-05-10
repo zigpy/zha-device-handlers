8fb4e1096dc3b9f55f13791c8539e97a51fffad4
Hafnium <haf@hafnium.me>
2026-05-10 19:55:30 +0200

    Add Tuya soil sensor _TZE284_0ints6w variant
    Signed-off-by: Hafnium <haf@hafnium.me>

--- zhaquirks/tuya/tuya_sensor.py
+++ zhaquirks/tuya/tuya_sensor.py
@@ -270,6 +270,7 @@ class NoManufTimeTuyaMCUCluster(TuyaMCUCluster):
     .applies_to("_TZE284_awepdiwi", "TS0601")  # Solar powered
     .applies_to("_TZE284_33bwcga2", "TS0601")  # iHseno
     .applies_to("_TZE284_tgrzpqf4", "TS0601")
+    .applies_to("_TZE284_0ints6wl", "TS0601")
     .tuya_temperature(dp_id=5, scale=10)
     .tuya_battery(dp_id=15)
     .tuya_soil_moisture(dp_id=3)
