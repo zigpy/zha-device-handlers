7ff7a9ed4e355db697d9bf8174b1c2ebfb8c3c22
leo <2442926161@qq.com>
2026-05-22 10:04:36 +0800

    fix: changed manufacture's implementation to Zigpy compliance

--- zhaquirks/heiman/hs1rm_e.py
+++ zhaquirks/heiman/hs1rm_e.py
@@ -3,42 +3,7 @@
 from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.quirks.v2.homeassistant import EntityType
-import zigpy.types as t
-from zigpy.zcl.clusters.general import DeviceTemperature
-from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
-
-
-class SwitchTypeEnum(t.enum8):
-    """switch type type."""
-
-    Toggle = 0
-    Momentary = 1
-
-
-class SwitchActionEnum(t.enum8):
-    """switch action type."""
-
-    State1_to_state2 = 0
-    State2_to_state1 = 1
-    Toggle = 2
-
-
-class OnoffSwitchConfigurationCluster(CustomCluster):
-    """On/Off Switch Configuration cluster."""
-
-    cluster_id = 0x0007
-
-    class AttributeDefs(BaseAttributeDefs):
-        """Attribute definitions."""
-
-        switch_type = ZCLAttributeDef(
-            id=0x0000,
-            type=t.enum8,
-        )
-        switch_actions = ZCLAttributeDef(
-            id=0x0010,
-            type=t.enum8,
-        )
+from zigpy.zcl.clusters.general import DeviceTemperature, OnOffConfiguration
 
 
 class HeimanDeviceTemperature(CustomCluster, DeviceTemperature):
@@ -46,7 +11,7 @@ class HeimanDeviceTemperature(CustomCluster, DeviceTemperature):
 
     def _update_attribute(self, attrid, value):
         # Attribute 0x0000 is current_temperature
-        if attrid == 0x0000 and value is not None:
+        if attrid == self.AttributeDefs.current_temperature.id and value is not None:
             value = value * 100
         super()._update_attribute(attrid, value)
 
@@ -55,40 +20,39 @@ class HeimanDeviceTemperature(CustomCluster, DeviceTemperature):
     QuirkBuilder()
     .applies_to("HEIMAN", "RelayModule-EF-3.0")
     .friendly_name(manufacturer="HEIMAN", model="HS1RM-E")
-    .replaces(OnoffSwitchConfigurationCluster)
     .replaces(HeimanDeviceTemperature)
     # heiman functions
     .enum(
-        OnoffSwitchConfigurationCluster.AttributeDefs.switch_type.name,
-        SwitchTypeEnum,
-        OnoffSwitchConfigurationCluster.cluster_id,
+        OnOffConfiguration.AttributeDefs.switch_type.name,
+        OnOffConfiguration.SwitchTypeEnum,
+        OnOffConfiguration.cluster_id,
         endpoint_id=1,
         entity_type=EntityType.CONFIG,
         translation_key="switch_type_l1",
         fallback_name="Switch type l1",
     )
     .enum(
-        OnoffSwitchConfigurationCluster.AttributeDefs.switch_actions.name,
-        SwitchActionEnum,
-        OnoffSwitchConfigurationCluster.cluster_id,
+        OnOffConfiguration.AttributeDefs.switch_actions.name,
+        OnOffConfiguration.SwitchActionEnum,
+        OnOffConfiguration.cluster_id,
         endpoint_id=1,
         entity_type=EntityType.CONFIG,
         translation_key="switch_action_l1",
         fallback_name="Switch action l1",
     )
     .enum(
-        OnoffSwitchConfigurationCluster.AttributeDefs.switch_type.name,
-        SwitchTypeEnum,
-        OnoffSwitchConfigurationCluster.cluster_id,
+        OnOffConfiguration.AttributeDefs.switch_type.name,
+        OnOffConfiguration.SwitchTypeEnum,
+        OnOffConfiguration.cluster_id,
         endpoint_id=2,
         entity_type=EntityType.CONFIG,
         translation_key="switch_type_l2",
         fallback_name="Switch type l2",
     )
     .enum(
-        OnoffSwitchConfigurationCluster.AttributeDefs.switch_actions.name,
-        SwitchActionEnum,
-        OnoffSwitchConfigurationCluster.cluster_id,
+        OnOffConfiguration.AttributeDefs.switch_actions.name,
+        OnOffConfiguration.SwitchActionEnum,
+        OnOffConfiguration.cluster_id,
         endpoint_id=2,
         entity_type=EntityType.CONFIG,
         translation_key="switch_action_l2",
