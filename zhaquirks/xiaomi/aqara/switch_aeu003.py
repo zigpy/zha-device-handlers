e93df955aa2d8978a605409390595645843d291a
Panagiotis Kolokotronis <panos122008@gmail.com>
2026-06-05 18:58:40 +0300

    Correct shutter position inversion

--- zhaquirks/xiaomi/aqara/switch_aeu003.py
+++ zhaquirks/xiaomi/aqara/switch_aeu003.py
@@ -1,4 +1,5 @@
 from zigpy.profiles import zha
+from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.clusters.closures import WindowCovering
 from zigpy.zcl.clusters.general import Basic, Identify
@@ -10,7 +11,7 @@ from zhaquirks.const import (
 )
 from zhaquirks.xiaomi import (
     AQARA,
-    AnalogInputCluster, 
+    AnalogInputCluster,
     ElectricalMeasurementCluster,
     MeteringCluster
 )
@@ -19,6 +20,23 @@ from zhaquirks.xiaomi.aqara.opple_remote import (
     COMMAND_4_SINGLE,
 )
 from zhaquirks.xiaomi.aqara.opple_switch import MultistateInputCluster
+
+
+class InvertedWindowCoveringCluster(CustomCluster, WindowCovering):
+    """WindowCovering cluster that corrects the Aqara H2 position inversion.
+
+    The Aqara H2 shutter switch reports lift percentage in Home Assistant
+    convention (0=closed, 100=open), but ZHA expects Zigbee spec convention
+    (0=open, 100=closed) and inverts the value itself. This cluster
+    pre-inverts the value to cancel out the double-flip.
+    """
+
+    CURRENT_POSITION_LIFT_PERCENTAGE = 0x0008
+
+    def _update_attribute(self, attrid, value):
+        if attrid == self.CURRENT_POSITION_LIFT_PERCENTAGE and value is not None:
+            value = 100 - value
+        super()._update_attribute(attrid, value)
 
 
 (
@@ -30,7 +48,7 @@ from zhaquirks.xiaomi.aqara.opple_switch import MultistateInputCluster
     .adds_endpoint(21, zha.DeviceType.ON_OFF_SWITCH)
     .adds(Basic, endpoint_id=1)
     .adds(Identify, endpoint_id=1)
-    .adds(WindowCovering, endpoint_id=1)
+    .replaces(InvertedWindowCoveringCluster, endpoint_id=1)
     .replaces(MultistateInputCluster, endpoint_id=3)
     .replaces(MultistateInputCluster, endpoint_id=4)
     .replaces(ElectricalMeasurementCluster, endpoint_id=1)
