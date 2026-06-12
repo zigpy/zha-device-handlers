6777fe1b79e9d7739cc1ffad9d5ae29e5d22e59d
FerrumLogic <ferrumlogic@gmail.com>
2026-06-12 15:05:30 +0300

    Add Tuya TS004F _TZ3000_egvb1p2g and TS0202 _TZ3000_kmh5qpmb/_TZ3000_lf56vpxj model variants

---
+++ zhaquirks/tuya/ts0202.py
@@ -0,0 +1,14 @@
+"""Quirk for Tuya TS0202 IAS Zone motion sensors."""
+
+from zigpy.quirks.v2 import QuirkBuilder
+from zhaquirks import MotionWithReset
+from zhaquirks.tuya import TuyaPowerConfigurationCluster2AAA
+
+(
+    QuirkBuilder("_TZ3000_kmh5qpmb", "TS0202")
+    .applies_to("_TZ3000_lf56vpxj", "TS0202")
+    .replaces(MotionWithReset)
+    .replaces(TuyaPowerConfigurationCluster2AAA)
+    .skip_configuration()
+    .add_to_registry()
+)
