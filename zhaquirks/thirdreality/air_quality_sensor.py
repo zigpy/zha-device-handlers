2749076bba96be41b93c9e4c997d6eafbc1b9675
support <117250319+3reality-support@users.noreply.github.com>
2026-06-23 16:33:30 +0800

    Add files via upload

--- zhaquirks/thirdreality/air_quality_sensor.py
+++ zhaquirks/thirdreality/air_quality_sensor.py
@@ -3,38 +3,36 @@
 from typing import Final
 
 from zigpy.quirks import CustomCluster
-from zigpy.quirks.v2 import QuirkBuilder, SensorDeviceClass, SensorStateClass
+from zigpy.quirks.v2 import QuirkBuilder, ReportingConfig, SensorDeviceClass, SensorStateClass
 import zigpy.types as t
 from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
 
 
-class ThirdRealityRadarCluster(CustomCluster):
+class ThirdRealityAirQualityCluster(CustomCluster):
     """Third Reality's air quality sensor private cluster."""
 
-    cluster_id = 0x042E
+    cluster_id = 0x042e
 
     class AttributeDefs(BaseAttributeDefs):
         """Define the attributes of a private cluster."""
 
-        voc_index: Final = ZCLAttributeDef(
+        voc_Index: Final = ZCLAttributeDef(
             id=0x0100,
             type=t.Single,
             manufacturer_code=0x1407,
         )
 
-
 (
     QuirkBuilder("Third Reality, Inc", "3RAQ1096Z")
-    .replaces(ThirdRealityRadarCluster)
+    .replaces(ThirdRealityAirQualityCluster)
     .sensor(
         endpoint_id=1,
-        attribute_name=ThirdRealityRadarCluster.AttributeDefs.voc_index.name,
-        cluster_id=ThirdRealityRadarCluster.cluster_id,
-        unit=" ",
+        attribute_name=ThirdRealityAirQualityCluster.AttributeDefs.voc_Index.name,
+        cluster_id=ThirdRealityAirQualityCluster.cluster_id,
         device_class=SensorDeviceClass.AQI,
         state_class=SensorStateClass.MEASUREMENT,
-        translation_key="VOC_index",
-        fallback_name="Voc index",
+        translation_key="voc_index",
+        fallback_name="VOC index",
     )
     .add_to_registry()
 )
