f45b666be48f769beedd37fbd45072fce426e5af
support <117250319+3reality-support@users.noreply.github.com>
2026-06-30 15:43:04 +0800

    Remove unused import of CustomCluster

--- zhaquirks/thirdreality/vibrate.py
+++ zhaquirks/thirdreality/vibrate.py
@@ -2,7 +2,6 @@
 
 from typing import Final
 
-from zigpy.quirks import CustomCluster
 from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.quirks.v2.homeassistant import UnitOfTime
 from zigpy.quirks.v2.homeassistant.number import NumberDeviceClass
