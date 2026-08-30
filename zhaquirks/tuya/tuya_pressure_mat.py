3678bfde97009c4670ebe9bcad0026eac4f4069a
skaggerik <155855409+skaggerik@users.noreply.github.com>
2026-08-30 20:45:53 +0200

    Update tuya_pressure_mat.py

--- zhaquirks/tuya/tuya_pressure_mat.py
+++ zhaquirks/tuya/tuya_pressure_mat.py
@@ -1,16 +1,11 @@
 """Custom ZHA Quirk for Tuya TS0601 _TZE200_seq9cm6u Pressure/Occupancy Mat."""
 
 from zigpy.zcl.clusters.measurement import OccupancySensing
-
 from zhaquirks.tuya.builder import TuyaQuirkBuilder
 from zhaquirks.tuya.mcu import TuyaPowerConfigurationCluster
 
 (
     TuyaQuirkBuilder("_TZE200_seq9cm6u", "TS0601")
-    .applies_to(
-        b_input_clusters=[0x0000, 0xEF00],
-        b_output_clusters=[0x000A, 0x0019],
-    )
     .tuya_dp(
         dp_id=1,
         ep_attribute=OccupancySensing.ep_attribute,
