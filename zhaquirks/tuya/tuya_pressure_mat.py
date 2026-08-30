a48feec7cc7a667a758fd3635c37be305ffe0220
skaggerik <155855409+skaggerik@users.noreply.github.com>
2026-08-30 20:40:18 +0200

    Update tuya_pressure_mat.py

--- zhaquirks/tuya/tuya_pressure_mat.py
+++ zhaquirks/tuya/tuya_pressure_mat.py
@@ -1,78 +1,27 @@
 """Custom ZHA Quirk for Tuya TS0601 _TZE200_seq9cm6u Pressure/Occupancy Mat."""
 
-from zigpy.profiles import zha
-from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, Ota, Time
 from zigpy.zcl.clusters.measurement import OccupancySensing
+from zhaquirks.tuya.builder import TuyaQuirkBuilder
+from zhaquirks.tuya.mcu import TuyaPowerConfigurationCluster
 
-from zhaquirks.const import (
-    DEVICE_TYPE,
-    ENDPOINTS,
-    INPUT_CLUSTERS,
-    MODELS_INFO,
-    OUTPUT_CLUSTERS,
-    PROFILE_ID,
+(
+    TuyaQuirkBuilder("_TZE200_seq9cm6u", "TS0601")
+    .applies_to(
+        b_input_clusters=[0x0000, 0xEF00],
+        b_output_clusters=[0x000A, 0x0019],
+    )
+    .tuya_dp(
+        dp_id=1,
+        ep_attribute=OccupancySensing.ep_attribute,
+        attribute_name="occupancy",
+        converter=lambda x: not x,
+    )
+    .tuya_dp(
+        dp_id=4,
+        ep_attribute=TuyaPowerConfigurationCluster.ep_attribute,
+        attribute_name="battery_percentage_remaining",
+    )
+    .adds(OccupancySensing)
+    .adds(TuyaPowerConfigurationCluster)
+    .add_to_registry()
 )
-from zhaquirks.tuya import TuyaLocalCluster
-from zhaquirks.tuya.mcu import (
-    DPToAttributeMapping,
-    TuyaMCUCluster,
-    TuyaPowerConfigurationCluster,
-)
-
-
-class TuyaOccupancySensing(OccupancySensing, TuyaLocalCluster):
-    """Tuya local OccupancySensing cluster."""
-
-
-class TuyaBedOccupancyMCUCluster(TuyaMCUCluster):
-    """Tuya MCU Cluster for Bed Occupancy."""
-
-    dp_to_attribute: dict[int, DPToAttributeMapping] = {
-        1: DPToAttributeMapping(
-            TuyaOccupancySensing.ep_attribute,
-            "occupancy",
-            converter=lambda x: not x,
-        ),
-        4: DPToAttributeMapping(
-            TuyaPowerConfigurationCluster.ep_attribute,
-            "battery_percentage_remaining",
-        ),
-    }
-
-    data_point_handlers = {
-        1: "_dp_2_attr_update",
-        4: "_dp_2_attr_update",
-    }
-
-
-class TuyaPressureMat(CustomDevice):
-    """Tuya pressure mat replacement."""
-
-    signature = {
-        MODELS_INFO: [("_TZE200_seq9cm6u", "TS0601")],
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: 0x0104,
-                DEVICE_TYPE: 0x000C,
-                INPUT_CLUSTERS: [0x0000, 0xEF00],
-                OUTPUT_CLUSTERS: [0x000A, 0x0019],
-            }
-        },
-    }
-
-    replacement = {
-        ENDPOINTS: {
-            1: {
-                PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.OCCUPANCY_SENSOR,
-                INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    TuyaBedOccupancyMCUCluster,
-                    TuyaPowerConfigurationCluster,
-                    TuyaOccupancySensing,
-                ],
-                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
-            }
-        }
-    }
