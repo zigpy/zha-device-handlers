5a9e294cd273811e9ee89231e472de9060471a0d
Qiu-A-Qiu <qjygongzuo@163.com>
2026-09-14 17:06:27 +0800

    Update snzb02b.py

--- zhaquirks/sonoff/snzb02b.py
+++ zhaquirks/sonoff/snzb02b.py
@@ -18,6 +18,7 @@ from zhaquirks.builder import (
 )
 from zhaquirks.clusters import CustomCluster
 
+
 MEASURED_VALUE_ATTR = 0x0000
 
 
@@ -48,8 +49,9 @@ class SonoffTemperatureCluster(CustomCluster, TemperatureMeasurement):
     def _update_attribute(self, attrid, value):
         """Update temperature and refresh derived values."""
         super()._update_attribute(attrid, value)
-        if attrid == self.AttributeDefs.measured_value.id and hasattr(
-            self.endpoint, SonoffCalculatedClimateCluster.ep_attribute
+        if (
+            attrid == self.AttributeDefs.measured_value.id
+            and hasattr(self.endpoint, SonoffCalculatedClimateCluster.ep_attribute)
         ):
             self.endpoint.sonoff_calculated_climate.update_calculated_values()
 
@@ -60,8 +62,9 @@ class SonoffRelativeHumidityCluster(CustomCluster, RelativeHumidity):
     def _update_attribute(self, attrid, value):
         """Update relative humidity and refresh derived values."""
         super()._update_attribute(attrid, value)
-        if attrid == self.AttributeDefs.measured_value.id and hasattr(
-            self.endpoint, SonoffCalculatedClimateCluster.ep_attribute
+        if (
+            attrid == self.AttributeDefs.measured_value.id
+            and hasattr(self.endpoint, SonoffCalculatedClimateCluster.ep_attribute)
         ):
             self.endpoint.sonoff_calculated_climate.update_calculated_values()
 
@@ -113,7 +116,7 @@ class SonoffCalculatedClimateCluster(LocalDataCluster):
         """Calculate Vapor Pressure Deficit (VPD)."""
         if temperature is None or humidity is None:
             return None
-        if humidity < 0 or humidity > 100:
+        if humidity <= 0 or humidity > 100:
             return None
 
         e_sat = cls.calculate_saturation_vapor_pressure(temperature)
